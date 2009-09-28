3497ebd96697d13dbb9b713aa28e20237152a74b
urban-warrior <urban-warrior@users.noreply.github.com>
2009-09-28 19:01:46 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@246 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- tests/validate-convert.sh
+++ tests/validate-convert.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
-# Copyright (C) 1999-2009 ImageMagick Studio LLC
 #
-# This program is covered by multiple licenses, which are described in
-# LICENSE. You should have received a copy of LICENSE with this
-# package; otherwise see http://www.imagemagick.org/script/license.php.
+#  Copyright 1999-2009 ImageMagick Studio LLC, a non-profit organization
+#  dedicated to making software imaging solutions freely available.
+#
+#  You may not use this file except in compliance with the License.  You may
+#  obtain a copy of the License at
+#
+#    http://www.imagemagick.org/script/license.php
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 #
 #  Test for 'validate' utility.
 #
