72a50c3bdeea00c20fa9b58e0c493732c2ff6bac
urban-warrior <urban-warrior@users.noreply.github.com>
2010-01-14 14:21:27 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@984 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/xml-tree.c
+++ magick/xml-tree.c
@@ -1614,7 +1614,7 @@ static MagickBooleanType ParseInternalDoctype(XMLTreeRoot *root,char *xml,
             while ((root->attributes[i] != (char **) NULL) &&
                    (strcmp(n,root->attributes[i][0]) != 0))
               i++;
-            while (*(n=(++xml)+strspn(xml,XMLWhitespace)) && (*n != '>'))
+            while ((*(n=xml+strspn(xml+1,XMLWhitespace)+1) != 0) && (*n != '>'))
             {
               xml=n+strcspn(n,XMLWhitespace);
               if (*xml != '\0')
