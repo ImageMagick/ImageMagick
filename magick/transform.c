9184bccaea378e300696a4be1722334de6c10fe3
urban-warrior <urban-warrior@users.noreply.github.com>
2010-04-01 00:25:40 +0000

    Fixed bug in Equal-Size Tile Cropping, when image has a page offset.
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@1576 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/transform.c
+++ magick/transform.c
@@ -1737,6 +1737,7 @@ MagickExport MagickBooleanType TransformImage(Image **image,
                   (offset.y+(geometry.y < 0 ? geometry.y : 0)));
               }
             crop.height-=crop.y;
+            crop.y += transform_image->page.y;
             for (offset.x=0; offset.x < (double) width; )
             {
               if ((flags & AspectValue) == 0)
@@ -1756,6 +1757,7 @@ MagickExport MagickBooleanType TransformImage(Image **image,
                     (offset.x+(geometry.x < 0 ? geometry.x : 0)));
                 }
               crop.width-=crop.x;
+              crop.x += transform_image->page.x;
               next=CropImage(transform_image,&crop,&(*image)->exception);
               if (next == (Image *) NULL)
                 break;
