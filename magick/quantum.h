e8a28eb7599891c98209ddb90441d0df1fd34af9
urban-warrior <urban-warrior@users.noreply.github.com>
2010-05-11 13:33:11 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@1867 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/quantum.h
+++ magick/quantum.h
@@ -138,13 +138,13 @@ static inline unsigned char ScaleQuantumToChar(const Quantum quantum)
 static inline unsigned char ScaleQuantumToChar(const Quantum quantum)
 {
 #if !defined(MAGICKCORE_HDRI_SUPPORT)
-  return((unsigned char) (quantum/72340172838076673.0+0.5));
+  return((unsigned char) (quantum/72340172838076675.0+0.5));
 #else
   if (quantum <= 0.0)
     return(0);
-  if ((quantum/72340172838076673.0) >= 255.0)
+  if ((quantum/72340172838076675.0) >= 255.0)
     return(255);
-  return((unsigned char) (quantum/72340172838076673.0+0.5));
+  return((unsigned char) (quantum/72340172838076675.0+0.5));
 #endif
 }
 #endif
