90835747ae14e0c46d8021db6b177006ee26609a
urban-warrior <urban-warrior@users.noreply.github.com>
2010-05-12 17:58:41 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@1885 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/quantum-private.h
+++ magick/quantum-private.h
@@ -564,7 +564,7 @@ static inline unsigned short ScaleQuantumToShort(const Quantum quantum)
 {
   if (quantum <= 0.0)
     return(0);
-  if ((quantum/65537.0) >= 65535.0)
+  if ((quantum/281479271743489.0) >= 65535.0)
     return(65535);
   return((unsigned short) (quantum/281479271743489.0+0.5));
 }
