c8504a9876a28df07fdce0d29a8a29e6c83a0afa
urban-warrior <urban-warrior@users.noreply.github.com>
2010-05-14 23:22:12 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@1912 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/quantum.c
+++ magick/quantum.c
@@ -565,8 +565,8 @@ MagickExport MagickBooleanType SetQuantumDepth(const Image *image,
     }
   if (quantum_info->pixels != (unsigned char **) NULL)
     DestroyQuantumPixels(quantum_info);
-  status=AcquireQuantumPixels(quantum_info,(quantum_info->pad+5)*image->columns*
-    ((quantum_info->depth+7)/8));
+  status=AcquireQuantumPixels(quantum_info,(6+quantum_info->pad)*image->columns*
+    ((quantum_info->depth+7)/8));  /* estimate for CMYKA + RLE byte */
   return(status);
 }
 
