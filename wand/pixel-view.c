52c3c8f1c69f823f644b39dae5b027a0b5915e31
urban-warrior <urban-warrior@users.noreply.github.com>
2010-06-07 13:19:01 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@2166 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- wand/pixel-view.c
+++ wand/pixel-view.c
@@ -549,7 +549,7 @@ WandExport MagickBooleanType GetPixelViewIterator(PixelView *source,
   status=MagickTrue;
   progress=0;
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
-  #pragma omp parallel for schedule(dynamic,4) shared(progress,status)
+  #pragma omp parallel for schedule(static,1) shared(progress,status)
 #endif
   for (y=source->region.y; y < (ssize_t) source->region.height; y++)
   {
@@ -997,7 +997,7 @@ WandExport MagickBooleanType SetPixelViewIterator(PixelView *destination,
   progress=0;
   exception=destination->exception;
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
-  #pragma omp parallel for schedule(dynamic,4) shared(progress,status)
+  #pragma omp parallel for schedule(static,1) shared(progress,status)
 #endif
   for (y=destination->region.y; y < (ssize_t) destination->region.height; y++)
   {
@@ -1133,7 +1133,7 @@ WandExport MagickBooleanType TransferPixelViewIterator(PixelView *source,
   progress=0;
   exception=destination->exception;
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
-  #pragma omp parallel for schedule(dynamic,4) shared(progress,status)
+  #pragma omp parallel for schedule(static,1) shared(progress,status)
 #endif
   for (y=source->region.y; y < (ssize_t) source->region.height; y++)
   {
@@ -1293,7 +1293,7 @@ WandExport MagickBooleanType UpdatePixelViewIterator(PixelView *source,
   progress=0;
   exception=source->exception;
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
-  #pragma omp parallel for schedule(dynamic,4) shared(progress,status)
+  #pragma omp parallel for schedule(static,1) shared(progress,status)
 #endif
   for (y=source->region.y; y < (ssize_t) source->region.height; y++)
   {
