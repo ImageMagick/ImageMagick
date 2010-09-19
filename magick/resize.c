efa3c9224f86d7f5933174ed4630e535d8ff1d3a
urban-warrior <urban-warrior@users.noreply.github.com>
2010-09-19 18:56:19 +0000

    nicolas resize.c cosmetic
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@2672 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/resize.c
+++ magick/resize.c
@@ -497,8 +497,8 @@ static MagickRealType Triangle(const MagickRealType x,
   const ResizeFilter *magick_unused(resize_filter))
 {
   /*
-    1st order (linear) B-Spline,  bilinear interpolation, Tent 1D filter, or a
-    Bartlett 2D Cone filter.
+    1st order (linear) B-Spline, bilinear interpolation, Tent 1D
+    filter, or a Bartlett 2D Cone filter.
   */
   if (x < 1.0)
     return(1.0-x);
