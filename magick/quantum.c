2d7c210ede25defb99a5c94ad8938f919b6ffd79
urban-warrior <urban-warrior@users.noreply.github.com>
2010-10-06 18:52:08 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@2834 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/quantum.c
+++ magick/quantum.c
@@ -250,17 +250,20 @@ static void DestroyQuantumPixels(QuantumInfo *quantum_info)
   register ssize_t
     i;
 
+  ssize_t
+    extent;
+
   assert(quantum_info != (QuantumInfo *) NULL);
   assert(quantum_info->signature == MagickSignature);
   assert(quantum_info->pixels != (unsigned char **) NULL);
+  extent=quantum_info->extent;
   for (i=0; i < (ssize_t) quantum_info->number_threads; i++)
     if (quantum_info->pixels[i] != (unsigned char *) NULL)
       {
         /*
           Did we overrun our quantum buffer?
         */
-        assert(quantum_info->pixels[i][quantum_info->extent] ==
-          QuantumSignature);
+        assert(quantum_info->pixels[i][extent] == QuantumSignature);
         quantum_info->pixels[i]=(unsigned char *) RelinquishMagickMemory(
           quantum_info->pixels[i]);
       }
