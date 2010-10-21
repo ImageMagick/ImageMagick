ab8ce07094ffe4444abc8075615730322a9ce212
urban-warrior <urban-warrior@users.noreply.github.com>
2010-10-21 16:24:59 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@2927 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/shear.c
+++ magick/shear.c
@@ -1084,7 +1084,7 @@ static Image *IntegralRotateImage(const Image *image,size_t rotations,
       */
       GetPixelCacheTileSize(image,&tile_width,&tile_height);
 #if defined(MAGICKCORE_OPENMP_SUPPORT) 
-  #pragma omp parallel for schedule(static,1) shared(progress, status) om_throttle(1)
+  #pragma omp parallel for schedule(static,1) shared(progress, status) omp_throttle(1)
 #endif
       for (tile_y=0; tile_y < (ssize_t) image->rows; tile_y+=(ssize_t) tile_height)
       {
