1246874f14cf78a525c5787414ea4e257bac76b7
urban-warrior <urban-warrior@users.noreply.github.com>
2010-11-06 00:59:38 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@3056 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/psd.c
+++ coders/psd.c
@@ -818,7 +818,8 @@ static Image *ReadPSDImage(const ImageInfo *image_info,ExceptionInfo *exception)
   image->depth=psd_info.depth;
   image->columns=psd_info.columns;
   image->rows=psd_info.rows;
-  if (SetImageBackgroundColor(image) == MagickFalse)
+  if ((image_info->ping != MagickFalse) &&
+      (SetImageBackgroundColor(image) == MagickFalse))
     {
       InheritException(exception,&image->exception);
       image=DestroyImageList(image);
