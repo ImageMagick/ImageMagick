9aa2b7546aabaf2e0d9fe1f688708fbde86ac3bf
urban-warrior <urban-warrior@users.noreply.github.com>
2010-11-15 12:35:31 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@3159 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/version.h
+++ magick/version.h
@@ -33,7 +33,7 @@ extern "C" {
 #define MagickLibAddendum  "-9"
 #define MagickLibInterface  4
 #define MagickLibMinInterface  4
-#define MagickReleaseDate  "2010-11-14"
+#define MagickReleaseDate  "2010-11-15"
 #define MagickChangeDate   "20101114"
 #define MagickAuthoritativeURL  "http://www.imagemagick.org"
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
