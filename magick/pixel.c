5572da1d6d9c848b5a854dd3958ca25bd0c3c444
urban-warrior <urban-warrior@users.noreply.github.com>
2011-03-28 16:25:23 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@4067 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/pixel.c
+++ magick/pixel.c
@@ -3425,6 +3425,7 @@ static inline void AlphaBlendMagickPixelPacket(const Image *image,
       pixel->green=(MagickRealType) color->green;
       pixel->blue=(MagickRealType) color->blue;
       pixel->opacity=(MagickRealType) color->opacity;
+      pixel->index=0.0;
       if (((image->colorspace == CMYKColorspace) ||
            (image->storage_class == PseudoClass)) &&
           (index != (const IndexPacket *) NULL))
@@ -3436,6 +3437,7 @@ static inline void AlphaBlendMagickPixelPacket(const Image *image,
   pixel->green=(MagickRealType) (*alpha*color->green);
   pixel->blue=(MagickRealType) (*alpha*color->blue);
   pixel->opacity=(MagickRealType) (*alpha*color->opacity);
+  pixel->index=0.0;
   if (((image->colorspace == CMYKColorspace) ||
        (image->storage_class == PseudoClass)) &&
       (index != (const IndexPacket *) NULL))
