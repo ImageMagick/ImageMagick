980252b8f92d139d0281b12fefb5b5fa174f4297
urban-warrior <urban-warrior@users.noreply.github.com>
2011-04-26 14:44:36 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@4339 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/draw.c
+++ magick/draw.c
@@ -4474,8 +4474,11 @@ MagickExport MagickBooleanType DrawPrimitive(Image *image,
       affine.tx=(double) geometry.x;
       affine.ty=(double) geometry.y;
       composite_image->interpolate=image->interpolate;
-      (void) CompositeImage(image,draw_info->compose,composite_image,geometry.x,
-        geometry.y);
+      if (draw_info->compose == OverCompositeOp)
+        (void) DrawAffineImage(image,composite_image,&affine);
+      else
+        (void) CompositeImage(image,draw_info->compose,composite_image,
+          geometry.x,geometry.y);
       composite_image=DestroyImage(composite_image);
       break;
     }
