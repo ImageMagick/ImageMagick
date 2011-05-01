1f56410886f857326966e52cc944b9ed974f90c2
urban-warrior <urban-warrior@users.noreply.github.com>
2011-05-01 02:34:39 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@4385 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/magick-config.h
+++ magick/magick-config.h
@@ -502,9 +502,7 @@
 #endif
 
 /* Define to 1 if you have a working `mmap' system call. */
-#ifndef MAGICKCORE_HAVE_MMAP_FILEIO
-#define MAGICKCORE_HAVE_MMAP_FILEIO 1
-#endif
+/* #undef HAVE_MMAP_FILEIO */
 
 /* Define to 1 if you have the `munmap' function. */
 #ifndef MAGICKCORE_HAVE_MUNMAP
@@ -1029,9 +1027,7 @@
 #endif
 
 /* Define to 1 if the system has the type `_Bool'. */
-#ifndef MAGICKCORE_HAVE__BOOL
-#define MAGICKCORE_HAVE__BOOL 1
-#endif
+/* #undef HAVE__BOOL */
 
 /* Define to 1 if you have the `_exit' function. */
 #ifndef MAGICKCORE_HAVE__EXIT
@@ -1059,7 +1055,9 @@
 #endif
 
 /* accurately represent the wide range of intensity levels in real scenes */
-/* #undef HDRI_SUPPORT */
+#ifndef MAGICKCORE_HDRI_SUPPORT
+#define MAGICKCORE_HDRI_SUPPORT 1
+#endif
 
 /* Define if you have umem memory allocation library */
 /* #undef HasUMEM */
@@ -1102,9 +1100,7 @@
 /* #undef LQR_DELEGATE */
 
 /* Define if using libltdl to support dynamically loadable modules */
-#ifndef MAGICKCORE_LTDL_DELEGATE
-#define MAGICKCORE_LTDL_DELEGATE 1
-#endif
+/* #undef LTDL_DELEGATE */
 
 /* Define if the OS needs help to load dependent libraries for dlopen(). */
 /* #undef LTDL_DLOPEN_DEPLIBS */
