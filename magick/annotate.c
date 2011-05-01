f62cd261c409a24b515dfadd1ba305ff8eee9531
urban-warrior <urban-warrior@users.noreply.github.com>
2011-05-01 18:58:10 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@4391 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/annotate.c
+++ magick/annotate.c
@@ -535,9 +535,6 @@ MagickExport MagickBooleanType AnnotateImage(Image *image,
 MagickExport ssize_t FormatMagickCaption(Image *image,DrawInfo *draw_info,
   const MagickBooleanType split,TypeMetric *metrics,char **caption)
 {
-  char
-    *snippet;
-
   MagickBooleanType
     status;
 
@@ -554,16 +551,8 @@ MagickExport ssize_t FormatMagickCaption(Image *image,DrawInfo *draw_info,
 
   q=draw_info->text;
   s=(char *) NULL;
-  snippet=(char *) NULL;
   for (p=(*caption); GetUTFCode(p) != 0; p+=GetUTFOctets(p))
   {
-    if (snippet != (char *) NULL)
-      {
-        if ((p != (char *) NULL) && (memcmp(p,snippet,strlen(p)) == 0))
-          break;  /* caption won't fit in allotted space */
-        snippet=DestroyString(snippet);
-      }
-    snippet=AcquireString(p);
     if (IsUTFSpace(GetUTFCode(p)) != MagickFalse)
       s=p;
     for (i=0; i < (ssize_t) GetUTFOctets(p); i++)
@@ -612,8 +601,6 @@ MagickExport ssize_t FormatMagickCaption(Image *image,DrawInfo *draw_info,
     s=(char *) NULL;
     q=draw_info->text;
   }
-  if (snippet != (char *) NULL)
-    snippet=DestroyString(snippet);
   i=0;
   for (p=(*caption); GetUTFCode(p) != 0; p+=GetUTFOctets(p))
     if (GetUTFCode(p) == '\n')
