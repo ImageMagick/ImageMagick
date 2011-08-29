5d6fe4cc36f754040b5063d4cb7117a05db1abea
urban-warrior <urban-warrior@users.noreply.github.com>
2011-08-29 16:22:43 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6.7.2@5086 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/effect.c
+++ magick/effect.c
@@ -5346,8 +5346,7 @@ MagickExport Image *StatisticImageChannel(const Image *image,
         SetPixelGreen(q,ClampToQuantum(pixel.green));
       if ((channel & BlueChannel) != 0)
         SetPixelBlue(q,ClampToQuantum(pixel.blue));
-      if (((channel & OpacityChannel) != 0) &&
-          (image->matte != MagickFalse))
+      if (((channel & OpacityChannel) != 0) && (image->matte != MagickFalse))
         SetPixelOpacity(q,ClampToQuantum(pixel.opacity));
       if (((channel & IndexChannel) != 0) &&
           (image->colorspace == CMYKColorspace))
