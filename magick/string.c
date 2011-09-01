295f55cbccc93933f0b42644035667f743f9a157
urban-warrior <urban-warrior@users.noreply.github.com>
2011-09-01 13:51:17 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6.7.2@5133 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/string.c
+++ magick/string.c
@@ -207,13 +207,15 @@ MagickExport StringInfo *BlobToStringInfo(const void *blob,const size_t length)
     *string_info;
 
   string_info=AcquireStringInfo(0);
-  string_info->datum=(unsigned char *) AcquireQuantumMemory(length,
-    sizeof(*string_info->datum));
+  if (~string_info->length >= (MaxTextExtent-1))
+    string_info->datum=(unsigned char *) AcquireQuantumMemory(length+
+      MaxTextExtent,sizeof(*string_info->datum));
   if (string_info->datum == (unsigned char *) NULL)
     {
       string_info=DestroyStringInfo(string_info);
       return((StringInfo *) NULL);
     }
+  string_info->length=length;
   if (blob != (const void *) NULL)
     (void) memcpy(string_info->datum,blob,length);
   return(string_info);
