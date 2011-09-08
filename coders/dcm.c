24b7f82f80f4b878d46a1c6f856e04365859493d
urban-warrior <urban-warrior@users.noreply.github.com>
2011-09-08 12:32:45 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@5226 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/dcm.c
+++ coders/dcm.c
@@ -3061,9 +3061,9 @@ static Image *ReadDCMImage(const ImageInfo *image_info,ExceptionInfo *exception)
               count=ReadBlob(image,(size_t) quantum*length,data);
               if (count != (ssize_t) (quantum*length))
                 {
-                  (void) FormatLocaleFile(stdout,"count=%d quantum=%d length=%d "
-                    "group=%d\n",(int) count,(int) quantum,(int) length,(int)
-                    group);
+                  (void) FormatLocaleFile(stdout,"count=%d quantum=%d "
+                    "length=%d group=%d\n",(int) count,(int) quantum,(int)
+                    length,(int) group);
                    ThrowReaderException(CorruptImageError,
                      "InsufficientImageDataInFile");
                 }
@@ -3646,7 +3646,7 @@ static Image *ReadDCMImage(const ImageInfo *image_info,ExceptionInfo *exception)
         one=1;
         if (colors == 0)
           colors=one << depth;
-        if (AcquireImageColormap(image,colors,exception) == MagickFalse)
+        if (AcquireImageColormap(image,one << depth,exception) == MagickFalse)
           ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
         if (redmap != (int *) NULL)
           for (i=0; i < (ssize_t) colors; i++)
@@ -3820,8 +3820,6 @@ static Image *ReadDCMImage(const ImageInfo *image_info,ExceptionInfo *exception)
                           ReadDCMLSBShort(stream_info,image));
                       if (signed_data == 1)
                         pixel_value=((signed short) pixel_value);
-                      if (significant_bits == 12)
-                        pixel_value>>=4;
                     }
                   else
                     {
