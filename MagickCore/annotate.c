84ea0bc2d4a2190b00f229c66251b894ceeb7f11
urban-warrior <urban-warrior@users.noreply.github.com>
2011-10-03 18:47:13 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@5492 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/annotate.c
+++ MagickCore/annotate.c
@@ -80,10 +80,12 @@
 #if defined(__MINGW32__)
 #  undef interface
 #endif
+#if defined(MAGICKCORE_HAVE_FT2BUILD_H)
+#  include <ft2build.h>
+#endif
 #if defined(FT_FREETYPE_H)
 #  include FT_FREETYPE_H
 #else
-#  include <ft2build.h>
 #  include <freetype/freetype.h>
 #endif
 #if defined(FT_GLYPH_H)
