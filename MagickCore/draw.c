73745464c097b095b1c46118a5f558f99ece37bf
urban-warrior <urban-warrior@users.noreply.github.com>
2011-10-04 23:48:29 +0000

    Remove un-needed clone of image_info in GetDrawInfo()
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@5503 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -240,9 +240,11 @@ MagickExport DrawInfo *CloneDrawInfo(const ImageInfo *image_info,
   clone_info=(DrawInfo *) AcquireMagickMemory(sizeof(*clone_info));
   if (clone_info == (DrawInfo *) NULL)
     ThrowFatalException(ResourceLimitFatalError,"MemoryAllocationFailed");
+
   GetDrawInfo(image_info,clone_info);
   if (draw_info == (DrawInfo *) NULL)
     return(clone_info);
+
   if (clone_info->primitive != (char *) NULL)
     (void) CloneString(&clone_info->primitive,draw_info->primitive);
   if (draw_info->geometry != (char *) NULL)
@@ -4765,23 +4767,21 @@ MagickExport void GetDrawInfo(const ImageInfo *image_info,DrawInfo *draw_info)
   ExceptionInfo
     *exception;
 
-  ImageInfo
-    *clone_info;
-
   /*
     Initialize draw attributes.
   */
   (void) LogMagickEvent(TraceEvent,GetMagickModule(),"...");
   assert(draw_info != (DrawInfo *) NULL);
   (void) ResetMagickMemory(draw_info,0,sizeof(*draw_info));
-  clone_info=CloneImageInfo(image_info);
+  image_info=CloneImageInfo(image_info);
   GetAffineMatrix(&draw_info->affine);
   exception=AcquireExceptionInfo();
+
   (void) QueryColorCompliance("#000F",AllCompliance,&draw_info->fill,
     exception);
   (void) QueryColorCompliance("#FFF0",AllCompliance,&draw_info->stroke,
     exception);
-  draw_info->stroke_antialias=clone_info->antialias;
+  draw_info->stroke_antialias=image_info->antialias;
   draw_info->stroke_width=1.0;
   draw_info->alpha=OpaqueAlpha;
   draw_info->fill_rule=EvenOddRule;
@@ -4789,60 +4789,59 @@ MagickExport void GetDrawInfo(const ImageInfo *image_info,DrawInfo *draw_info)
   draw_info->linejoin=MiterJoin;
   draw_info->miterlimit=10;
   draw_info->decorate=NoDecoration;
-  if (clone_info->font != (char *) NULL)
-    draw_info->font=AcquireString(clone_info->font);
-  if (clone_info->density != (char *) NULL)
-    draw_info->density=AcquireString(clone_info->density);
-  draw_info->text_antialias=clone_info->antialias;
+  if (image_info->font != (char *) NULL)
+    draw_info->font=AcquireString(image_info->font);
+  if (image_info->density != (char *) NULL)
+    draw_info->density=AcquireString(image_info->density);
+  draw_info->text_antialias=image_info->antialias;
   draw_info->pointsize=12.0;
-  if (clone_info->pointsize != 0.0)
-    draw_info->pointsize=clone_info->pointsize;
+  if (image_info->pointsize != 0.0)
+    draw_info->pointsize=image_info->pointsize;
   draw_info->undercolor.alpha=(Quantum) TransparentAlpha;
-  draw_info->border_color=clone_info->border_color;
+  draw_info->border_color=image_info->border_color;
   draw_info->compose=OverCompositeOp;
-  if (clone_info->server_name != (char *) NULL)
-    draw_info->server_name=AcquireString(clone_info->server_name);
+  if (image_info->server_name != (char *) NULL)
+    draw_info->server_name=AcquireString(image_info->server_name);
   draw_info->render=MagickTrue;
   draw_info->debug=IsEventLogging();
-  option=GetImageOption(clone_info,"encoding");
+  option=GetImageOption(image_info,"encoding");
   if (option != (const char *) NULL)
     (void) CloneString(&draw_info->encoding,option);
-  option=GetImageOption(clone_info,"kerning");
+  option=GetImageOption(image_info,"kerning");
   if (option != (const char *) NULL)
     draw_info->kerning=InterpretLocaleValue(option,(char **) NULL);
-  option=GetImageOption(clone_info,"interline-spacing");
+  option=GetImageOption(image_info,"interline-spacing");
   if (option != (const char *) NULL)
     draw_info->interline_spacing=InterpretLocaleValue(option,(char **) NULL);
   draw_info->direction=UndefinedDirection;
-  option=GetImageOption(clone_info,"interword-spacing");
+  option=GetImageOption(image_info,"interword-spacing");
   if (option != (const char *) NULL)
     draw_info->interword_spacing=InterpretLocaleValue(option,(char **) NULL);
-  option=GetImageOption(clone_info,"direction");
+  option=GetImageOption(image_info,"direction");
   if (option != (const char *) NULL)
     draw_info->direction=(DirectionType) ParseCommandOption(
       MagickDirectionOptions,MagickFalse,option);
-  option=GetImageOption(clone_info,"fill");
+  option=GetImageOption(image_info,"fill");
   if (option != (const char *) NULL)
     (void) QueryColorCompliance(option,AllCompliance,&draw_info->fill,
       exception);
-  option=GetImageOption(clone_info,"stroke");
+  option=GetImageOption(image_info,"stroke");
   if (option != (const char *) NULL)
     (void) QueryColorCompliance(option,AllCompliance,&draw_info->stroke,
       exception);
-  option=GetImageOption(clone_info,"strokewidth");
+  option=GetImageOption(image_info,"strokewidth");
   if (option != (const char *) NULL)
     draw_info->stroke_width=InterpretLocaleValue(option,(char **) NULL);
-  option=GetImageOption(clone_info,"undercolor");
+  option=GetImageOption(image_info,"undercolor");
   if (option != (const char *) NULL)
     (void) QueryColorCompliance(option,AllCompliance,&draw_info->undercolor,
       exception);
-  option=GetImageOption(clone_info,"gravity");
+  option=GetImageOption(image_info,"gravity");
   if (option != (const char *) NULL)
     draw_info->gravity=(GravityType) ParseCommandOption(MagickGravityOptions,
       MagickFalse,option);
   exception=DestroyExceptionInfo(exception);
   draw_info->signature=MagickSignature;
-  clone_info=DestroyImageInfo(clone_info);
 }
 
 /*
