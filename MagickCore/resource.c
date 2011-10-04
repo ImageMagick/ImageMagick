94aa727e8f57a8b3d6365c91b91f790eef971846
urban-warrior <urban-warrior@users.noreply.github.com>
2011-10-04 18:12:42 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@5501 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/resource.c
+++ MagickCore/resource.c
@@ -104,7 +104,7 @@ static ResourceInfo
     MagickULLConstant(0),
     MagickULLConstant(0),
     MagickULLConstant(0),
-    MagickULLConstant(3072)*1024*1024/sizeof(PixelPacket),
+    MagickULLConstant(3072)*1024*1024,
     MagickULLConstant(1536)*1024*1024,
     MagickULLConstant(3072)*1024*1024,
     MagickResourceInfinity,
@@ -933,7 +933,7 @@ MagickPrivate MagickBooleanType ResourceComponentGenesis(void)
 #if defined(PixelCacheThreshold)
   memory=PixelCacheThreshold;
 #endif
-  (void) SetMagickResourceLimit(AreaResource,2*memory/sizeof(PixelPacket));
+  (void) SetMagickResourceLimit(AreaResource,2*memory);
   (void) SetMagickResourceLimit(MemoryResource,memory);
   (void) SetMagickResourceLimit(MapResource,2*memory);
   limit=GetEnvironmentValue("MAGICK_AREA_LIMIT");
