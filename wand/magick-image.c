13bce54e1df8caf1c214eec143ef221be62562b7
urban-warrior <urban-warrior@users.noreply.github.com>
2011-12-13 01:54:34 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6.7.4@6188 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- wand/magick-image.c
+++ wand/magick-image.c
@@ -4834,7 +4834,7 @@ WandExport size_t MagickGetImageDepth(MagickWand *wand)
     (void) LogMagickEvent(WandEvent,GetMagickModule(),"%s",wand->name);
   if (wand->images == (Image *) NULL)
     ThrowWandException(WandError,"ContainsNoImages",wand->name);
-  return(GetImageDepth(wand->images,wand->exception));
+  return(wand->images->depth);
 }
 
 /*
