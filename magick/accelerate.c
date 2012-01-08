931bbc5b676da117c7673aef2704022b029e5375
urban-warrior <urban-warrior@users.noreply.github.com>
2012-01-08 13:44:17 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6.7.4@6378 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/accelerate.c
+++ magick/accelerate.c
@@ -184,7 +184,7 @@ static char
     "static inline CLQuantum ClampToQuantum(const float value)\n"
     "{\n"
     "#if defined(MAGICKCORE_HDRI_SUPPORT)\n"
-    "  return((CLQuantum) value)\n"
+    "  return((CLQuantum) value);\n"
     "#else\n"
     "  if (value < 0.0)\n"
     "    return((CLQuantum) 0);\n"
