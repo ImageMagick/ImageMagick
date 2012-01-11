d7465ff3f77e336e87918583a53cdc06909917de
urban-warrior <urban-warrior@users.noreply.github.com>
2012-01-11 01:25:33 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6.7.4@6407 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/raw.c
+++ coders/raw.c
@@ -93,8 +93,7 @@ static MagickBooleanType
 %    o exception: return any errors or warnings in this structure.
 %
 */
-static Image *ReadRAWImage(const ImageInfo *image_info,
-  ExceptionInfo *exception)
+static Image *ReadRAWImage(const ImageInfo *image_info,ExceptionInfo *exception)
 {
   Image
     *canvas_image,
@@ -199,8 +198,8 @@ static Image *ReadRAWImage(const ImageInfo *image_info,
 
       if (count != (ssize_t) length)
         {
-          ThrowFileException(exception,CorruptImageError,
-            "UnexpectedEndOfFile",image->filename);
+          ThrowFileException(exception,CorruptImageError,"UnexpectedEndOfFile",
+            image->filename);
           break;
         }
       q=GetAuthenticPixels(canvas_image,0,0,canvas_image->columns,1,exception);
