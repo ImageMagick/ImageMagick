a37d2373c93e555a45f87ca10634f5a031a82c2e
urban-warrior <urban-warrior@users.noreply.github.com>
2012-01-12 13:45:52 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@6440 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/shear.c
+++ MagickCore/shear.c
@@ -1230,7 +1230,6 @@ MagickExport Image *IntegralRotateImage(const Image *image,size_t rotations,
         Rotate 270 degrees.
       */
       GetPixelCacheTileSize(image,&tile_width,&tile_height);
-      tile_width=image->columns;
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
       #pragma omp parallel for schedule(static,1) shared(progress,status)
 #endif
