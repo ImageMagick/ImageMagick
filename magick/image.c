b7ebdedd7e2214e9d6145288a5a85e5790505a00
urban-warrior <urban-warrior@users.noreply.github.com>
2012-01-23 12:47:54 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6.7.4@6573 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/image.c
+++ magick/image.c
@@ -4260,7 +4260,7 @@ MagickExport MagickBooleanType SyncImage(Image *image)
   exception=(&image->exception);
   image_view=AcquireCacheView(image);
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
-  #pragma omp parallel for schedule(static,4) shared(status)
+  #pragma omp parallel for schedule(static,4) shared(range_exception,status)
 #endif
   for (y=0; y < (ssize_t) image->rows; y++)
   {
