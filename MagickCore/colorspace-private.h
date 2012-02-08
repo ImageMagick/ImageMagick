99861788f8ffa51abe3bd3df8577fccbfac0493c
urban-warrior <urban-warrior@users.noreply.github.com>
2012-02-08 19:07:45 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@6778 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/colorspace-private.h
+++ MagickCore/colorspace-private.h
@@ -71,7 +71,8 @@ static inline MagickBooleanType IsGrayColorspace(
 static inline MagickBooleanType IsRGBColorspace(const ColorspaceType colorspace)
 {
   if ((IsGrayColorspace(colorspace) != MagickFalse) ||
-      (colorspace == sRGBColorspace) || (colorspace == TransparentColorspace))
+      (colorspace == RGBColorspace) || (colorspace == sRGBColorspace) ||
+      (colorspace == TransparentColorspace))
     return(MagickTrue);
   return(MagickFalse);
 }
