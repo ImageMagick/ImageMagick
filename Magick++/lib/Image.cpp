b4fd4f288d073d72bb12af9addf4ef52d973d610
urban-warrior <urban-warrior@users.noreply.github.com>
2012-02-10 17:52:19 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@6800 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- Magick++/lib/Image.cpp
+++ Magick++/lib/Image.cpp
@@ -643,10 +643,10 @@ void Magick::Image::colorMatrix (const size_t order_,
   kernel_info=AcquireKernelInfo((const char *) NULL);
   kernel_info->width=order_;
   kernel_info->height=order_;
-  kernel_info->values=(MagickRealType *) color_matrix_;
+  kernel_info->values=(double *) color_matrix_;
   MagickCore::Image* newImage =
     ColorMatrixImage( image(), kernel_info, &exceptionInfo );
-  kernel_info->values=(MagickRealType *) NULL;
+  kernel_info->values=(double *) NULL;
   kernel_info=DestroyKernelInfo(kernel_info);
   replaceImage( newImage );
   throwException( exceptionInfo );
