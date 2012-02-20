8c973114a1e35a716b5889f67a5d1ea34a716527
urban-warrior <urban-warrior@users.noreply.github.com>
2012-02-20 16:32:47 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@6920 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/jbig.c
+++ coders/jbig.c
@@ -58,7 +58,7 @@
 #include "MagickCore/memory_.h"
 #include "MagickCore/monitor.h"
 #include "MagickCore/monitor-private.h"
-#include "MagickCore/nt-base-private.h"
+#include "MagickCore/nt-feature.h"
 #include "MagickCore/pixel-accessor.h"
 #include "MagickCore/quantum-private.h"
 #include "MagickCore/static.h"
