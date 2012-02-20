7703dd0781d9bbc955bf0866a7e38eb1173e53f2
urban-warrior <urban-warrior@users.noreply.github.com>
2012-02-20 15:05:32 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@6917 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/clipboard.c
+++ coders/clipboard.c
@@ -344,7 +344,7 @@ static MagickBooleanType WriteCLIPBOARDImage(const ImageInfo *image_info,
 
     OpenClipboard(NULL);
     EmptyClipboard();
-    bitmapH=(HBITMAP) ImageToHBITMAP(image);
+    bitmapH=(HBITMAP) ImageToHBITMAP(image,exception);
     SetClipboardData(CF_BITMAP,bitmapH);
     CloseClipboard();
   }
