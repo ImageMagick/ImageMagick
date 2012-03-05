b7386591d75e1f3003f0ea5679d19b84d6b41510
urban-warrior <urban-warrior@users.noreply.github.com>
2012-03-05 18:55:14 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6.7.6@7059 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- PerlMagick/Magick.pm
+++ PerlMagick/Magick.pm
@@ -43,7 +43,7 @@ require AutoLoader;
       ConfigureError FatalErrorException
     );
 
-$VERSION = '6.75';
+$VERSION = '6.76';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
