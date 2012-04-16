140bc631a5804d0ae0758d1f28f6eea8e89c6d2e
anthony <anthony@git.imagemagick.org>
2012-04-16 03:11:53 +0000

    Added colorspace validation tests

--- tests/common.sh
+++ tests/common.sh
@@ -2,5 +2,7 @@ SRCDIR=`dirname $0`
 SRCDIR=`cd $SRCDIR && pwd`
 TOPSRCDIR=`cd $srcdir && pwd`
 REFERENCE_IMAGE="${TOPSRCDIR}/images/rose.pnm"
+[ "X$CONVERT" = "X" ] && CONVERT=convert
+[ "X$IDENTIFY" = "X" ] && IDENTIFY=identify
 export SRCDIR TOPSRCDIR
 cd tests || exit 1
