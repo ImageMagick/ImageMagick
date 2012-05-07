90c81d536851049a15d0079fa25759ea9a054411
urban-warrior <urban-warrior@users.noreply.github.com>
2012-05-07 23:41:51 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@7828 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/transform.c
+++ magick/transform.c
@@ -161,7 +161,7 @@ MagickExport Image *ChopImage(const Image *image,const RectangleInfo *chop_info,
   chop_view=AcquireAuthenticCacheView(chop_image,exception);
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
   #pragma omp parallel for schedule(static) shared(progress,status) \
-    dynamic_number_threads(extent.y,extent.y,1)
+    dynamic_number_threads(image->columns,image->rows,1)
 #endif
   for (y=0; y < (ssize_t) extent.y; y++)
   {
