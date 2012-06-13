52a6a7d66c60dc51594d39d89bf4b479ec8a02d6
urban-warrior <urban-warrior@users.noreply.github.com>
2012-06-13 22:29:25 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@8254 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/image.c
+++ magick/image.c
@@ -2399,7 +2399,7 @@ MagickExport MagickBooleanType SeparateImageChannel(Image *image,
     (void) LogMagickEvent(TraceEvent,GetMagickModule(),"%s",image->filename);
   if (SetImageStorageClass(image,DirectClass) == MagickFalse)
     return(MagickFalse);
-  SetImageColorspace(image,GRAYColorspace);
+  (void) SetImageColorspace(image,GRAYColorspace);
   /*
     Separate image channels.
   */
