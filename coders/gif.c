06fb8efd1d2af1b5ee2fc3aeeb7e0d3ca3791d76
urban-warrior <urban-warrior@users.noreply.github.com>
2012-06-20 13:57:38 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@8366 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/gif.c
+++ coders/gif.c
@@ -1558,7 +1558,8 @@ static MagickBooleanType WriteGIFImage(const ImageInfo *image_info,Image *image)
   one=1;
   do
   {
-    if (IssRGBColorspace(image->colorspace) == MagickFalse)
+    if ((IssRGBColorspace(image->colorspace) == MagickFalse) &&
+        (IsGrayImage(image,&image->exception) == MagickFalse))
       (void) TransformImageColorspace(image,sRGBColorspace);
     opacity=(-1);
     if (IsOpaqueImage(image,&image->exception) != MagickFalse)
