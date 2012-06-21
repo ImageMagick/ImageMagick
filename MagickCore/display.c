d224563f7a962c76c5fd151e5c8b8f35bbde511c
urban-warrior <urban-warrior@users.noreply.github.com>
2012-06-21 01:30:15 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@8387 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/display.c
+++ MagickCore/display.c
@@ -41,6 +41,7 @@
 */
 #include "MagickCore/studio.h"
 #include "MagickCore/artifact.h"
+#include "MagickCore/attribute.h"
 #include "MagickCore/blob.h"
 #include "MagickCore/cache.h"
 #include "MagickCore/cache-private.h"
--- coders/palm.c
+++ coders/palm.c
@@ -42,6 +42,7 @@
   Include declarations.
 */
 #include "MagickCore/studio.h"
+#include "MagickCore/attribute.h"
 #include "MagickCore/blob.h"
 #include "MagickCore/blob-private.h"
 #include "MagickCore/cache.h"
