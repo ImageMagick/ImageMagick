1270b001716df69c176fc1b314fa444b3b2811f0
glennrp <glennrp@git.imagemagick.org>
2012-06-27 13:40:25 +0000

    Increase PLTE chunk length to accommodate background color index

--- coders/png.c
+++ coders/png.c
@@ -8172,6 +8172,7 @@ static MagickBooleanType WriteOnePNGImage(MngInfo *mng_info,
             {
                opaque[i] = image->background_color;
                ping_background.index = i;
+               number_opaque++;
                if (logging != MagickFalse)
                  {
                    (void) LogMagickEvent(CoderEvent,GetMagickModule(),
