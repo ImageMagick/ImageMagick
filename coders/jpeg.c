77cf51865df9be0bcf7c92141f195940a5135125
urban-warrior <urban-warrior@users.noreply.github.com>
2012-06-28 16:14:12 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@8540 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/jpeg.c
+++ coders/jpeg.c
@@ -356,10 +356,8 @@ static MagickBooleanType JPEGWarningHandler(j_common_ptr jpeg_info,int level)
       (jpeg_info->err->format_message)(jpeg_info,message);
       if (jpeg_info->err->num_warnings++ > JPEGExcessiveWarnings)
         JPEGErrorHandler(jpeg_info);
-      if ((jpeg_info->err->num_warnings == 0) ||
-          (jpeg_info->err->trace_level >= 3))
-        ThrowBinaryException(CorruptImageWarning,(char *) message,
-          image->filename);
+      ThrowBinaryException(CorruptImageWarning,(char *) message,
+        image->filename);
     }
   else
     if ((image->debug != MagickFalse) &&
