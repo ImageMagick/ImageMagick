62cccde424060533e038633fc88f812640f7b02c
urban-warrior <urban-warrior@users.noreply.github.com>
2012-07-03 00:33:28 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@8578 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/geometry.c
+++ MagickCore/geometry.c
@@ -206,33 +206,39 @@ MagickExport MagickStatusType GetGeometry(const char *geometry,ssize_t *x,
   (void) value;
   if (LocaleNCompare(p,"0x",2) == 0)
     value=(double) strtol(p,&q,10);
-  c=(int) ((unsigned char) *q);
-  if ((c == 215) || (*q == 'x') || (*q == 'X') || (*q == '\0'))
+  if ((*p != '+') && (*p != '-'))
     {
-      /*
-        Parse width.
-      */
-      q=p;
-      if (LocaleNCompare(p,"0x",2) == 0)
-        *width=(size_t) strtol(p,&p,10);
-      else
-        *width=(size_t) floor(StringToDouble(p,&p)+0.5);
-      if (p != q)
-        flags|=WidthValue;
-    }
-  c=(int) ((unsigned char) *p);
-  if ((c == 215) || (*p == 'x') || (*p == 'X'))
-    {
-      p++;
-      if ((*p != '+') && (*p != '-'))
+      c=(int) ((unsigned char) *q);
+      if ((c == 215) || (*q == 'x') || (*q == 'X') || (*q == '\0'))
         {
           /*
-            Parse height.
+            Parse width.
           */
           q=p;
-          *height=(size_t) floor(StringToDouble(p,&p)+0.5);
+          if (LocaleNCompare(p,"0x",2) == 0)
+            *width=(size_t) strtol(p,&p,10);
+          else
+            *width=(size_t) floor(StringToDouble(p,&p)+0.5);
           if (p != q)
-            flags|=HeightValue;
+            flags|=WidthValue;
+        }
+    }
+  if ((*p != '+') && (*p != '-'))
+    {
+      c=(int) ((unsigned char) *p);
+      if ((c == 215) || (*p == 'x') || (*p == 'X'))
+        {
+          p++;
+          if ((*p != '+') && (*p != '-'))
+            {
+              /*
+                Parse height.
+              */
+              q=p;
+              *height=(size_t) floor(StringToDouble(p,&p)+0.5);
+              if (p != q)
+                flags|=HeightValue;
+            }
         }
     }
   if ((*p == '+') || (*p == '-'))
@@ -241,18 +247,18 @@ MagickExport MagickStatusType GetGeometry(const char *geometry,ssize_t *x,
         Parse x value.
       */
       while ((*p == '+') || (*p == '-'))
-        {
-          if (*p == '-')
-            flags^=XNegative;  /* negate sign */
-          p++;
-        }
+      {
+        if (*p == '-')
+          flags^=XNegative;  /* negate sign */
+        p++;
+      }
       q=p;
       *x=(ssize_t) ceil(StringToDouble(p,&p)-0.5);
       if (p != q)
         {
           flags|=XValue;
-          if ( (flags&XNegative) != 0 )
-            *x = -*x;
+          if ((flags & XNegative) != 0)
+            *x=(-*x);
         }
     }
   if ((*p == '+') || (*p == '-'))
@@ -261,30 +267,30 @@ MagickExport MagickStatusType GetGeometry(const char *geometry,ssize_t *x,
         Parse y value.
       */
       while ((*p == '+') || (*p == '-'))
-        {
-          if (*p == '-')
-            flags^=YNegative;  /* negate sign */
-          p++;
-        }
+      {
+        if (*p == '-')
+          flags^=YNegative;  /* negate sign */
+        p++;
+      }
       q=p;
       *y=(ssize_t) ceil(StringToDouble(p,&p)-0.5);
       if (p != q)
         {
           flags|=YValue;
-          if ( (flags&YNegative) != 0 )
-            *y = -*y;
+          if ((flags & YNegative) != 0)
+            *y=(-*y);
         }
     }
 #if 0
   /* Debugging Geometry */
-  fprintf(stderr,"GetGeometry...\n");
-  fprintf(stderr,"Input: %s\n",geometry);
-  fprintf(stderr,"Flags: %c %c %s %s\n",
-       (flags&WidthValue)?'W':' ',(flags&HeightValue)?'H':' ',
-       (flags&XValue)?((flags&XNegative)?"-X":"+X"):"  ",
-       (flags&YValue)?((flags&YNegative)?"-Y":"+Y"):"  ");
-  fprintf(stderr,"Geometry: %ldx%ld%+ld%+ld\n",
-       (long)*width,(long)*height,(long)*x,(long)*y);
+  (void) fprintf(stderr,"GetGeometry...\n");
+  (void) fprintf(stderr,"Input: %s\n",geometry);
+  (void) fprintf(stderr,"Flags: %c %c %s %s\n",
+    (flags & WidthValue) ? 'W' : ' ',(flags & HeightValue) ? 'H' : ' ',
+    (flags & XValue) ? ((flags & XNegative) ? "-X" : "+X") : "  ",
+    (flags & YValue) ? ((flags & YNegative) ? "-Y" : "+Y") : "  ");
+  (void) fprintf(stderr,"Geometry: %ldx%ld%+ld%+ld\n",(long) *width,(long)
+    *height,(long) *x,(long) *y);
 #endif
   return(flags);
 }
@@ -735,8 +741,8 @@ MagickExport MagickStatusType ParseAffineGeometry(const char *geometry,
       }
     }
   }
-  determinant=(affine_matrix->sx*affine_matrix->sy
-                 - affine_matrix->rx*affine_matrix->ry);
+  determinant=(affine_matrix->sx*affine_matrix->sy-affine_matrix->rx*
+    affine_matrix->ry);
   if (fabs(determinant) < MagickEpsilon)
     (void) ThrowMagickException(exception,GetMagickModule(),OptionError,
       "InvalidArgument","'%s' : 'Indeterminate Matrix'",geometry);
@@ -955,18 +961,18 @@ MagickExport MagickStatusType ParseGeometry(const char *geometry,
       if ((*p == ',') || (*p == '/') || (*p == ':') )
         p++;
       while ((*p == '+') || (*p == '-'))
-        {
-          if (*p == '-')
-            flags^=XiNegative;  /* negate sign */
-          p++;
-        }
+      {
+        if (*p == '-')
+          flags^=XiNegative;  /* negate sign */
+        p++;
+      }
       q=p;
       value=StringToDouble(p,&p);
       if (p != q)
         {
           flags|=XiValue;
-          if ( (flags&XiNegative) != 0 )
-            value = -value;
+          if ((flags & XiNegative) != 0)
+            value=(-value);
           geometry_info->xi=value;
         }
       while (isspace((int) ((unsigned char) *p)) != 0)
@@ -980,20 +986,21 @@ MagickExport MagickStatusType ParseGeometry(const char *geometry,
           if ((*p == ',') || (*p == '/') || (*p == ':'))
             p++;
           while ((*p == '+') || (*p == '-'))
-            {
-              if (*p == '-')
-                flags^=PsiNegative;  /* negate sign */
-              p++;
-            }
+          {
+            if (*p == '-')
+              flags^=PsiNegative;  /* negate sign */
+            p++;
+          }
           q=p;
           value=StringToDouble(p,&p);
-          if (p != q) {
-            flags|=PsiValue;
-            if ( (flags&PsiNegative) != 0 )
-              value = -value;
-            geometry_info->psi=value;
-          }
-        }
+          if (p != q)
+            {
+              flags|=PsiValue;
+              if ((flags & PsiNegative) != 0)
+                value=(-value);
+              geometry_info->psi=value;
+            }
+      }
       while (isspace((int) ((unsigned char) *p)) != 0)
         p++;
       if ((*p == '+') || (*p == '-') || (*p == ',') || (*p == '/') ||
@@ -1005,19 +1012,20 @@ MagickExport MagickStatusType ParseGeometry(const char *geometry,
           if ((*p == ',') || (*p == '/') || (*p == ':'))
             p++;
           while ((*p == '+') || (*p == '-'))
-            {
-              if (*p == '-')
-                flags^=ChiNegative;  /* negate sign */
-              p++;
-            }
+          {
+            if (*p == '-')
+              flags^=ChiNegative;  /* negate sign */
+            p++;
+          }
           q=p;
           value=StringToDouble(p,&p);
-          if (p != q) {
-            flags|=ChiValue;
-            if ( (flags&ChiNegative) != 0 )
-              value = -value;
-            geometry_info->chi=value;
-          }
+          if (p != q)
+            {
+              flags|=ChiValue;
+              if ((flags & ChiNegative) != 0)
+                value=(-value);
+              geometry_info->chi=value;
+            }
         }
     }
   if (strchr(pedantic_geometry,':') != (char *) NULL)
@@ -1043,15 +1051,15 @@ MagickExport MagickStatusType ParseGeometry(const char *geometry,
     }
 #if 0
   /* Debugging Geometry */
-  fprintf(stderr,"ParseGeometry...\n");
-  fprintf(stderr,"Flags: %c %c %s %s %s\n",
-       (flags&RhoValue)?'W':' ',(flags&SigmaValue)?'H':' ',
-       (flags&XiValue)?((flags&XiNegative)?"-X":"+X"):"  ",
-       (flags&PsiValue)?((flags&PsiNegative)?"-Y":"+Y"):"  ",
-       (flags&ChiValue)?((flags&ChiNegative)?"-Z":"+Z"):"  ");
-  fprintf(stderr,"Geometry: %lg,%lg,%lg,%lg,%lg\n",
-       geometry_info->rho,geometry_info->sigma,
-       geometry_info->xi,geometry_info->psi,geometry_info->chi);
+  (void) fprintf(stderr,"ParseGeometry...\n");
+  (void) fprintf(stderr,"Flags: %c %c %s %s %s\n",
+    (flags & RhoValue) ? 'W' : ' ',(flags & SigmaValue) ? 'H' : ' ',
+    (flags & XiValue) ? ((flags & XiNegative) ? "-X" : "+X") : "  ",
+    (flags & PsiValue) ? ((flags & PsiNegative) ? "-Y" : "+Y") : "  ",
+    (flags & ChiValue) ? ((flags & ChiNegative) ? "-Z" : "+Z") : "  ");
+  (void) fprintf(stderr,"Geometry: %lg,%lg,%lg,%lg,%lg\n",geometry_info->rho,
+    geometry_info->sigma,geometry_info->xi,geometry_info->psi,
+    geometry_info->chi);
 #endif
   return(flags);
 }
