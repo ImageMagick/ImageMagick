d325c6f3f8bb7d099e356b795701e3259cf65a8b
urban-warrior <urban-warrior@users.noreply.github.com>
2012-07-13 12:13:49 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@8670 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/gradient.c
+++ coders/gradient.c
@@ -154,6 +154,7 @@ static Image *ReadGRADIENTImage(const ImageInfo *image_info,
       return((Image *) NULL);
     }
   (void) QueryMagickColor(colorname,&stop_pixel,exception);
+  (void) SetImageColorspace(image,start_pixel.colorspace);
   status=GradientImage(image,LocaleCompare(image_info->magick,"GRADIENT") == 0 ?
     LinearGradient : RadialGradient,PadSpread,&start_color,&stop_color);
   if (status == MagickFalse)
