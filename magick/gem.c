f87856a546907a9ecce0aa389b5f0bd6f06676c3
urban-warrior <urban-warrior@users.noreply.github.com>
2012-08-15 13:11:25 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@8967 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/gem.c
+++ magick/gem.c
@@ -109,38 +109,38 @@ MagickExport void ConvertHCLToRGB(const double hue,const double chroma,
   b=0.0;
   if ((0.0 <= h) && (h < 1.0))
     {
-      r=c;      
-      g=x;      
+      r=c;
+      g=x;
     }
   else
     if ((1.0 <= h) && (h < 2.0))
       {
-        r=x;      
-        g=c;      
+        r=x;
+        g=c;
       }
     else
       if ((2.0 <= h) && (h < 3.0))
         {
-          g=c;      
-          b=x;      
+          g=c;
+          b=x;
         }
       else
         if ((3.0 <= h) && (h < 4.0))
           {
-            g=x;      
-            b=c;      
+            g=x;
+            b=c;
           }
         else
           if ((4.0 <= h) && (h < 5.0))
             {
-              r=x;      
-              b=c;      
+              r=x;
+              b=c;
             }
           else
             if ((5.0 <= h) && (h < 6.0))
               {
-                r=c;      
-                b=x;      
+                r=c;
+                b=x;
               }
   m=luma-(0.298839*r+0.586811*g+0.114350*b);
   /*
