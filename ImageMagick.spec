395dbb2ca01d40ab91bed8ee69017cc4dd587f8d
urban-warrior <urban-warrior@users.noreply.github.com>
2012-08-15 17:50:47 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@8973 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- ImageMagick.spec
+++ ImageMagick.spec
@@ -1,5 +1,5 @@
 %global VERSION  6.7.9
-%global Patchlevel  0
+%global Patchlevel  1
 
 Name:           ImageMagick
 Version:        %{VERSION}
--- magick/version.h
+++ magick/version.h
@@ -27,15 +27,15 @@ extern "C" {
 */
 #define MagickPackageName "ImageMagick"
 #define MagickCopyright  "Copyright (C) 1999-2012 ImageMagick Studio LLC"
-#define MagickSVNRevision  "8921:8954M"
+#define MagickSVNRevision  "8970:8972M"
 #define MagickLibVersion  0x679
 #define MagickLibVersionText  "6.7.9"
 #define MagickLibVersionNumber  5,0,0
-#define MagickLibAddendum  "-0"
+#define MagickLibAddendum  "-1"
 #define MagickLibInterface  5
 #define MagickLibMinInterface  5
-#define MagickReleaseDate  "2012-08-14"
-#define MagickChangeDate   "20120814"
+#define MagickReleaseDate  "2012-08-15"
+#define MagickChangeDate   "20120815"
 #define MagickAuthoritativeURL  "http://www.imagemagick.org"
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
 #define MagickOpenMPFeature  "OpenMP "
