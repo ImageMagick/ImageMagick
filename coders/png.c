949f888016a1ae166a81dcf7f8b77011c2434b62
glennrp <glennrp@users.noreply.github.com>
2012-08-25 17:36:19 +0000

    Fix Q32 read of 16-bit grayscale PNGs
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@9117 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/png.c
+++ coders/png.c
@@ -2810,7 +2810,8 @@ static Image *ReadOnePNGImage(MngInfo *mng_info,
       "  image->colorspace=%d",(int) image->colorspace);
 
   if (((int) ping_color_type == PNG_COLOR_TYPE_PALETTE) ||
-      ((int) ping_color_type == PNG_COLOR_TYPE_GRAY))
+      ((int) ping_bit_depth < 16 &&
+      (int) ping_color_type == PNG_COLOR_TYPE_GRAY))
     {
       size_t
         one;
