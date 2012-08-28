f9d932651e38114c66dcd67d608ae55d1212f305
urban-warrior <urban-warrior@users.noreply.github.com>
2012-08-28 11:21:44 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@9147 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/url.c
+++ coders/url.c
@@ -220,7 +220,23 @@ static Image *ReadURLImage(const ImageInfo *image_info,ExceptionInfo *exception)
     }
 #endif
   (void) fclose(file);
-  *read_info->magick='\0';
+  {
+    ExceptionInfo
+      *sans;
+
+    ImageInfo
+      *clone_info;
+
+    /*
+      Guess image format from URL.
+    */
+    clone_info=CloneImageInfo(image_info);
+    sans=AcquireExceptionInfo();
+    (void) SetImageInfo(clone_info,0,sans);
+    (void) CopyMagickString(read_info->magick,clone_info->magick,MaxTextExtent);
+    clone_info=DestroyImageInfo(clone_info);
+    sans=DestroyExceptionInfo(sans);
+  }
   image=ReadImage(read_info,exception);
   if (unique_file != -1)
     (void) RelinquishUniqueFileResource(read_info->filename);
