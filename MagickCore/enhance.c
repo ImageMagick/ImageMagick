c1481424d3b2cc60ce070b91dbef1bb8ea09daf2
urban-warrior <urban-warrior@users.noreply.github.com>
2012-09-15 20:00:31 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@9370 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/enhance.c
+++ MagickCore/enhance.c
@@ -3350,7 +3350,8 @@ static inline double InverseScaledSigmoidal(const double a,const double b,
   const double x)
 {
   const double sig0=Sigmoidal(a,b,0.0);
-  const double argument=(Sigmoidal(a,b,1.0)-sig0)*x+sig0;
+  const double sig1=Sigmoidal(a,b,1.0);
+  const double argument=(sig1-sig0)*x+sig0;
   const double clamped=
     (
 #if defined(MAGICKCORE_HAVE_ATANH)
