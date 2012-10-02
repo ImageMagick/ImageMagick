f112b4c56d4cb28a0cc24f29b5bc914ab260e81b
urban-warrior <urban-warrior@users.noreply.github.com>
2012-10-02 17:23:53 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@9480 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/miff.c
+++ coders/miff.c
@@ -523,6 +523,7 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
   *id='\0';
   (void) ResetMagickMemory(keyword,0,sizeof(keyword));
   version=0.0;
+  (void) version;
   do
   {
     /*
