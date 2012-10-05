67465251dcf63d3eaf4de7c5a801138a8a579eac
urban-warrior <urban-warrior@users.noreply.github.com>
2012-10-05 14:00:54 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@9499 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/effect.c
+++ magick/effect.c
@@ -1323,25 +1323,19 @@ MagickExport Image *ConvolveImageChannel(const Image *image,
 {
 #define ConvolveImageTag  "Convolve/Image"
 
-#if (MAGICKCORE_QUANTUM_DEPTH > 16)
-  typedef double MagickKernelType;
-#else
-  typedef float MagickKernelType;
-#endif
-
   CacheView
     *convolve_view,
     *image_view;
+
+  double
+    gamma,
+    *normal_kernel;
 
   Image
     *convolve_image;
 
   MagickBooleanType
     status;
-
-  MagickKernelType
-    gamma,
-    *normal_kernel;
 
   MagickOffsetType
     progress;
@@ -1414,9 +1408,9 @@ MagickExport Image *ConvolveImageChannel(const Image *image,
   /*
     Normalize kernel.
   */
-  normal_kernel=(MagickKernelType *) MagickAssumeAligned(AcquireAlignedMemory(
-    width*width,sizeof(*normal_kernel)));
-  if (normal_kernel == (MagickKernelType *) NULL)
+  normal_kernel=(double *) MagickAssumeAligned(AcquireAlignedMemory(width*width,
+    sizeof(*normal_kernel)));
+  if (normal_kernel == (double *) NULL)
     {
       convolve_image=DestroyImage(convolve_image);
       ThrowImageException(ResourceLimitError,"MemoryAllocationFailed");
@@ -1478,7 +1472,7 @@ MagickExport Image *ConvolveImageChannel(const Image *image,
       MagickPixelPacket
         pixel;
 
-      register const MagickKernelType
+      register const double
         *restrict k;
 
       register const PixelPacket
@@ -1639,7 +1633,7 @@ MagickExport Image *ConvolveImageChannel(const Image *image,
   convolve_image->type=image->type;
   convolve_view=DestroyCacheView(convolve_view);
   image_view=DestroyCacheView(image_view);
-  normal_kernel=(MagickKernelType *) RelinquishAlignedMemory(normal_kernel);
+  normal_kernel=(double *) RelinquishAlignedMemory(normal_kernel);
   if (status == MagickFalse)
     convolve_image=DestroyImage(convolve_image);
   return(convolve_image);
