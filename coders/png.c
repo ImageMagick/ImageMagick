3fcfa950eeb4614ffea1dba94a68569bb4491ded
glennrp <glennrp@git.imagemagick.org>
2012-10-11 13:50:26 +0000

    The PNG encoder sometimes would write indexed PNG when grayscale was reqested.

--- coders/png.c
+++ coders/png.c
@@ -8367,16 +8367,11 @@ static MagickBooleanType WriteOnePNGImage(MngInfo *mng_info,
          ping_have_color=MagickFalse;
          ping_have_non_bw=MagickFalse;
 
-         if (IssRGBCompatibleColorspace(image->colorspace) == MagickFalse)
+         if ((IssRGBCompatibleColorspace(image->colorspace) == MagickFalse) &&
+             (IssRGBColorspace(image->colorspace) == MagickFalse))
          {
            ping_have_color=MagickTrue;
            ping_have_non_bw=MagickFalse;
-         }
-
-         if (IssRGBColorspace(image->colorspace) != MagickFalse)
-         {
-           ping_have_color=MagickTrue;
-           ping_have_non_bw=MagickTrue;
          }
 
          if(image_colors > 256)
