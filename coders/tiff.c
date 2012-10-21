1eee47f5243968a45192a614c53d0c960724ed0f
urban-warrior <urban-warrior@users.noreply.github.com>
2012-10-21 20:40:49 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@9737 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/tiff.c
+++ coders/tiff.c
@@ -3187,7 +3187,7 @@ static MagickBooleanType WriteTIFFImage(const ImageInfo *image_info,
         (void) TIFFSetField(tiff,TIFFTAG_RESOLUTIONUNIT,(uint16) units);
         (void) TIFFSetField(tiff,TIFFTAG_XRESOLUTION,image->x_resolution);
         (void) TIFFSetField(tiff,TIFFTAG_YRESOLUTION,image->y_resolution);
-        if ((image->page.x*image->page.y) > 0.0)
+        if ((image->page.x > 0) && (image->page.y > 0))
           {
             /*
               Set image position.
