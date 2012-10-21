4c62613130f0c310889145c54dddacef5358753a
urban-warrior <urban-warrior@users.noreply.github.com>
2012-10-21 12:55:54 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@9723 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/type.c
+++ magick/type.c
@@ -630,6 +630,7 @@ MagickExport MagickBooleanType LoadFontConfigFonts(SplayTreeInfo *type_list,
   FcChar8
     *family,
     *file,
+    *fullname,
     *style;
 
   FcConfig
@@ -706,6 +707,9 @@ MagickExport MagickBooleanType LoadFontConfigFonts(SplayTreeInfo *type_list,
     status=FcPatternGetString(font_set->fonts[i],FC_STYLE,0,&style);
     if (status == FcResultMatch)
       (void) ConcatenateMagickString(name,(const char *) style,MaxTextExtent);
+    status=FcPatternGetString(font_set->fonts[i],FC_FULLNAME,0,&fullname);
+    if (status == FcResultMatch)
+      (void) CopyMagickString(name,(const char *) fullname,MaxTextExtent);
     type_info->name=ConstantString(name);
     (void) SubstituteString(&type_info->name," ","-");
     (void) SubstituteString(&type_info->name,"-L-","-");
