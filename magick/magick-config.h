8c7bd5c28e7894337ba921c849cebc51b0746ca9
urban-warrior <urban-warrior@users.noreply.github.com>
2012-10-21 14:34:39 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@9732 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/magick-config.h
+++ magick/magick-config.h
@@ -1181,7 +1181,7 @@
 
 /* Define to the full name and version of this package. */
 #ifndef MAGICKCORE_PACKAGE_STRING
-#define MAGICKCORE_PACKAGE_STRING "ImageMagick 6.8.0-2"
+#define MAGICKCORE_PACKAGE_STRING "ImageMagick 6.8.0-3"
 #endif
 
 /* Define to the one symbol short name of this package. */
@@ -1196,7 +1196,7 @@
 
 /* Define to the version of this package. */
 #ifndef MAGICKCORE_PACKAGE_VERSION
-#define MAGICKCORE_PACKAGE_VERSION "6.8.0-2"
+#define MAGICKCORE_PACKAGE_VERSION "6.8.0-3"
 #endif
 
 /* Define if you have PANGOCAIRO library */
@@ -1386,7 +1386,7 @@
 
 /* Version number of package */
 #ifndef MAGICKCORE_VERSION
-#define MAGICKCORE_VERSION "6.8.0-2"
+#define MAGICKCORE_VERSION "6.8.0-3"
 #endif
 
 /* Define if you have WEBP library */
