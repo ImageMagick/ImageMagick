44d4efc0bf4e4c704b66d68a924d08919dfabe41
glennrp <glennrp@git.imagemagick.org>
2012-11-22 03:50:51 +0000

    Fixed an old typo in the chromaticity normalization code in coders/bmp.c

--- coders/bmp.c
+++ coders/bmp.c
@@ -730,19 +730,19 @@ static Image *ReadBMPImage(const ImageInfo *image_info,ExceptionInfo *exception)
             bmp_info.blue_primary.x=(double) ReadBlobLSBLong(image)/0x40000000;
             bmp_info.blue_primary.y=(double) ReadBlobLSBLong(image)/0x40000000;
             bmp_info.blue_primary.z=(double) ReadBlobLSBLong(image)/0x40000000;
-            sum=bmp_info.red_primary.x+bmp_info.red_primary.x+
+            sum=bmp_info.red_primary.x+bmp_info.red_primary.y+
               bmp_info.red_primary.z;
             bmp_info.red_primary.x/=sum;
             bmp_info.red_primary.y/=sum;
             image->chromaticity.red_primary.x=bmp_info.red_primary.x;
             image->chromaticity.red_primary.y=bmp_info.red_primary.y;
-            sum=bmp_info.green_primary.x+bmp_info.green_primary.x+
+            sum=bmp_info.green_primary.x+bmp_info.green_primary.y+
               bmp_info.green_primary.z;
             bmp_info.green_primary.x/=sum;
             bmp_info.green_primary.y/=sum;
             image->chromaticity.green_primary.x=bmp_info.green_primary.x;
             image->chromaticity.green_primary.y=bmp_info.green_primary.y;
-            sum=bmp_info.blue_primary.x+bmp_info.blue_primary.x+
+            sum=bmp_info.blue_primary.x+bmp_info.blue_primary.y+
               bmp_info.blue_primary.z;
             bmp_info.blue_primary.x/=sum;
             bmp_info.blue_primary.y/=sum;
