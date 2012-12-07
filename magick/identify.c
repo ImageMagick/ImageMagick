f0147b6dca593c4bbc81d007c582b65dd8cdd86f
urban-warrior <urban-warrior@users.noreply.github.com>
2012-12-07 18:58:00 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@10123 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/identify.c
+++ magick/identify.c
@@ -650,7 +650,7 @@ MagickExport MagickBooleanType IdentifyImage(Image *image,FILE *file,
     }
   if (image->storage_class == PseudoClass)
     {
-      (void) FormatLocaleFile(file,"  Colormap: %.20g\n",(double) image->colors);
+      (void) FormatLocaleFile(file,"  Colormap:\n");
       if (image->colors <= 1024)
         {
           char
