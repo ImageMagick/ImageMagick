fe07f931154f7fbfaa3b71aac1edc07dd4327cf2
urban-warrior <urban-warrior@users.noreply.github.com>
2012-12-16 23:34:05 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@10273 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/shear.c
+++ MagickCore/shear.c
@@ -1151,6 +1151,10 @@ MagickExport Image *IntegralRotateImage(const Image *image,size_t rotations,
       /*
         Rotate 180 degrees.
       */
+#if defined(MAGICKCORE_OPENMP_SUPPORT)
+      #pragma omp parallel for schedule(static,2048) shared(progress,status) \
+        dynamic_number_threads(image,image,image->rows,1)
+#endif
       for (y=0; y < (ssize_t) image->rows; y++)
       {
         MagickBooleanType
@@ -1214,6 +1218,9 @@ MagickExport Image *IntegralRotateImage(const Image *image,size_t rotations,
             MagickBooleanType
               proceed;
 
+#if defined(MAGICKCORE_OPENMP_SUPPORT)
+            #pragma omp critical (MagickCore_IntegralRotateImage)
+#endif
             proceed=SetImageProgress(image,RotateImageTag,progress++,
               image->rows);
             if (proceed == MagickFalse)
@@ -1243,7 +1250,7 @@ MagickExport Image *IntegralRotateImage(const Image *image,size_t rotations,
       GetPixelCacheTileSize(image,&tile_width,&tile_height);
       tile_width=image->columns;
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
-      #pragma omp parallel for schedule(static,4) shared(progress,status) \
+      #pragma omp parallel for schedule(static,2048) shared(progress,status) \
         dynamic_number_threads(image,image,image->rows/tile_height,1)
 #endif
       for (tile_y=0; tile_y < (ssize_t) image->rows; tile_y+=(ssize_t) tile_height)
