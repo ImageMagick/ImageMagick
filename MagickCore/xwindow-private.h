cf4785828adc64113e03d1560aac8191a46d6d9a
urban-warrior <urban-warrior@users.noreply.github.com>
2012-12-20 12:19:41 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@10335 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/xwindow-private.h
+++ MagickCore/xwindow-private.h
@@ -603,9 +603,9 @@ static inline double XPixelIntensity(const XColor *pixel)
     green,
     red;
 
-  red=sRGBExpandGamma((MagickRealType) pixel->red);
-  green=sRGBExpandGamma((MagickRealType) pixel->green);
-  blue=sRGBExpandGamma((MagickRealType) pixel->blue);
+  red=ExpandPixelGamma((MagickRealType) pixel->red);
+  green=ExpandPixelGamma((MagickRealType) pixel->green);
+  blue=ExpandPixelGamma((MagickRealType) pixel->blue);
   return(0.298839f*red+0.586811f*green+0.114350f*blue);
 }
 
