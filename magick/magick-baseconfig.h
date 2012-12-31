2fda2698c1543e900921968224d776bb82b677c3
urban-warrior <urban-warrior@users.noreply.github.com>
2012-12-31 15:12:24 +0000

    magick-baseconfig.h is an architecture dependant header. Add it to arch include part.
    Now headers are quantum depth independant. QUANTUMDEPTH should be supplied at
    compile time.
    
    
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@10463 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/magick-baseconfig.h
+++ magick/magick-baseconfig.h
@@ -1060,11 +1060,18 @@
 #define MAGICKCORE_HAVE___ATTRIBUTE__ 1
 #endif
 
-/* accurately represent the wide range of intensity levels in real scenes */
-/* #undef HDRI_SUPPORT */
+/* Whether hdri is enable or not */
+#ifndef MAGICKCORE_HDRI_ENABLE_OBSOLETE_IN_H
+#define MAGICKCORE_HDRI_ENABLE_OBSOLETE_IN_H 0
+#endif
 
 /* Define if you have umem memory allocation library */
 /* #undef HasUMEM */
+
+/* Directory where ImageMagick architecture headers live. */
+#ifndef MAGICKCORE_INCLUDEARCH_PATH
+#define MAGICKCORE_INCLUDEARCH_PATH "/usr/local/include/ImageMagick/"
+#endif
 
 /* Directory where ImageMagick headers live. */
 #ifndef MAGICKCORE_INCLUDE_PATH
@@ -1210,8 +1217,8 @@
 /* #undef PixelCacheThreshold */
 
 /* Number of bits in a pixel Quantum (8/16/32/64) */
-#ifndef MAGICKCORE_QUANTUM_DEPTH
-#define MAGICKCORE_QUANTUM_DEPTH 16
+#ifndef MAGICKCORE_QUANTUM_DEPTH_OBSOLETE_IN_H
+#define MAGICKCORE_QUANTUM_DEPTH_OBSOLETE_IN_H 16
 #endif
 
 /* Define as the return type of signal handlers (`int' or `void'). */
