62ac5207154a0d93be7a7d236af9f40395788980
urban-warrior <urban-warrior@users.noreply.github.com>
2013-01-06 23:55:31 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@10546 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- wand/compare.c
+++ wand/compare.c
@@ -89,7 +89,7 @@ static MagickBooleanType CompareUsage(void)
     *miscellaneous[]=
     {
       "-debug events        display copious debugging information",
-      "-distributed-cache port",
+      "-distribute-cache port",
       "                     distributed pixel cache spanning one or more servers",
       "-help                print program options",
       "-list type           print a list of supported option arguments",
