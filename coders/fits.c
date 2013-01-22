d1315ba8ae1788200d8aad5018c12ff75f2a5cdb
urban-warrior <urban-warrior@users.noreply.github.com>
2013-01-22 20:44:47 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@10726 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/fits.c
+++ coders/fits.c
@@ -248,6 +248,7 @@ static Image *ReadFITSImage(const ImageInfo *image_info,
     value[73];
 
   double
+    half_interval,
     pixel,
     scale;
 
@@ -408,6 +409,7 @@ static Image *ReadFITSImage(const ImageInfo *image_info,
   if ((fits_info.simple == MagickFalse) || (fits_info.number_axes < 1) ||
       (fits_info.number_axes > 4) || (number_pixels == 0))
     ThrowReaderException(CorruptImageError,"ImageTypeNotSupported");
+  half_interval=pow(2.0,(double) image->depth-1);
   for (scene=0; scene < (ssize_t) fits_info.number_planes; scene++)
   {
     image->columns=(size_t) fits_info.columns;
@@ -447,7 +449,7 @@ static Image *ReadFITSImage(const ImageInfo *image_info,
         pixel=GetFITSPixel(image,fits_info.bits_per_pixel);
         if ((image->depth == 16) || (image->depth == 32) ||
             (image->depth == 64))
-          pixel-=pow(2.0,(double) image->depth-1);
+          pixel-=half_interval;
         SetPixelRed(q,ClampToQuantum(scale*(fits_info.scale*(pixel-
           fits_info.min_data)+fits_info.zero)));
         SetPixelGreen(q,GetPixelRed(q));
