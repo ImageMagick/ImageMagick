a1f1bb29018b09005b0b8fb761080ca9b32ed5cc
urban-warrior <urban-warrior@users.noreply.github.com>
2013-01-23 14:38:51 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@10741 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickWand/display.c
+++ MagickWand/display.c
@@ -109,8 +109,6 @@ static MagickBooleanType DisplayUsage(void)
     *miscellaneous[]=
     {
       "-debug events        display copious debugging information",
-      "-distribute-cache port",
-      "                     distributed pixel cache spanning one or more servers",
       "-help                print program options",
       "-list type           print a list of supported option arguments",
       "-log format          format of debugging information",
