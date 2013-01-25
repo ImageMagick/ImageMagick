e065178632b34e92642a6f1502b4736eb39c4694
urban-warrior <urban-warrior@users.noreply.github.com>
2013-01-25 19:04:41 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@10777 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/magick.c
+++ MagickCore/magick.c
@@ -1312,9 +1312,6 @@ MagickExport void MagickCoreGenesis(const char *path,
       if (signal_handlers[SIGXFSZ] == (SignalHandler *) NULL)
         signal_handlers[SIGXFSZ]=RegisterMagickSignalHandler(SIGXFSZ);
 #endif
-#if defined(SIGPIPE)
-      (void) signal(SIGPIPE,SIG_IGN);
-#endif
     }
   /*
     Instantiate magick resources.
