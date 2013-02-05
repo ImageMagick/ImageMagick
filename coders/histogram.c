0f2e2cf1872f8232d94b4a81345552f379c17bad
urban-warrior <urban-warrior@users.noreply.github.com>
2013-02-05 14:07:59 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@10915 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/histogram.c
+++ coders/histogram.c
@@ -339,7 +339,7 @@ static MagickBooleanType WriteHISTOGRAMImage(const ImageInfo *image_info,
   }
   histogram=(PixelInfo *) RelinquishMagickMemory(histogram);
   option=GetImageOption(image_info,"histogram:unique-colors");
-  if ((option == (const char *) NULL) || (IsMagickTrue(option) != MagickFalse))
+  if ((option == (const char *) NULL) || (IsStringTrue(option) != MagickFalse))
     {
       FILE
         *file;
