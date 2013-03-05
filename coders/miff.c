22e02f3056683482f18ca49eb29bce63309d7950
urban-warrior <urban-warrior@users.noreply.github.com>
2013-03-05 01:06:14 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@11255 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/miff.c
+++ coders/miff.c
@@ -699,8 +699,7 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
                       MagickFalse,options);
                     if (colorspace < 0)
                       break;
-                    (void) SetImageColorspace(image,(ColorspaceType)
-                      colorspace);
+                    image->colorspace=(ColorspaceType) colorspace;
                     break;
                   }
                 if (LocaleCompare(keyword,"compression") == 0)
