981d8dc32866321b92232e20446fa2c27363286f
urban-warrior <urban-warrior@users.noreply.github.com>
2013-03-28 13:05:04 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@11567 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/option.c
+++ MagickCore/option.c
@@ -2791,21 +2791,25 @@ MagickExport MagickBooleanType SetImageOption(ImageInfo *image_info,
   if (image_info->debug != MagickFalse)
     (void) LogMagickEvent(TraceEvent,GetMagickModule(),"%s",
       image_info->filename);
-
-  /* Specific global option settings */
+  /*
+    Specific global option settings.
+  */
   if (LocaleCompare(option,"size") == 0)
     (void) CloneString(&image_info->size,value);
-
-  /* create tree if needed - specify how key,values are to be freed */
+  /*
+    Create tree if needed - specify how key,values are to be freed.
+  */
   if (image_info->options == (void *) NULL)
     image_info->options=NewSplayTree(CompareSplayTreeString,
       RelinquishMagickMemory,RelinquishMagickMemory);
-
-  /* Delete Option if NULL --  empty string values are valid! */
+  /*
+    Delete Option if NULL --  empty string values are valid!
+  */
   if (value == (const char *) NULL)
     return(DeleteImageOption(image_info,option));
-
-  /* add option to splay-tree */
+  /*
+    Add option to splay-tree.
+  */
   return(AddValueToSplayTree((SplayTreeInfo *) image_info->options,
     ConstantString(option),ConstantString(value)));
 }
