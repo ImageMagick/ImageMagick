9e1667a0d4a0aefa08163629ed4cc9f1a333b924
urban-warrior <urban-warrior@users.noreply.github.com>
2013-04-01 00:09:53 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@11627 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/enhance.c
+++ MagickCore/enhance.c
@@ -2494,8 +2494,6 @@ MagickExport MagickBooleanType LevelizeImage(Image *image,
   assert(image->signature == MagickSignature);
   if (image->debug != MagickFalse)
     (void) LogMagickEvent(TraceEvent,GetMagickModule(),"%s",image->filename);
-  if (IsGrayColorspace(image->colorspace) != MagickFalse)
-    (void) SetImageColorspace(image,sRGBColorspace,exception);
   if (image->storage_class == PseudoClass)
     for (i=0; i < (ssize_t) image->colors; i++)
     {
