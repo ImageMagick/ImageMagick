3de7e94f2b05591b4b936210c0f15d8e232b2f2a
urban-warrior <urban-warrior@users.noreply.github.com>
2013-04-02 18:47:09 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@11644 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/composite.c
+++ magick/composite.c
@@ -1670,8 +1670,6 @@ MagickExport MagickBooleanType CompositeImageChannel(Image *image,
   composite_image=CloneImage(composite,0,0,MagickTrue,exception);
   if (composite_image == (const Image *) NULL)
     return(MagickFalse);
-  if (IsGrayColorspace(image->colorspace) != MagickFalse)
-    (void) SetImageColorspace(image,sRGBColorspace);
   (void) SetImageColorspace(composite_image,image->colorspace);
   GetMagickPixelPacket(image,&zero);
   destination_image=(Image *) NULL;
