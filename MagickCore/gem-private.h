414f7e7cb6329d3826c02c4d7086940fddb250db
urban-warrior <urban-warrior@users.noreply.github.com>
2013-04-03 19:10:51 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@11660 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/gem-private.h
+++ MagickCore/gem-private.h
@@ -22,6 +22,12 @@
 extern "C" {
 #endif
 
+#define D65X  (0.950456f)
+#define D65Y  (1.0f)
+#define D65Z  (1.088754f)
+#define CIEEpsilon  (216.0f/24389.0f)
+#define CIEK  (24389.0f/27.0f)
+
 extern MagickPrivate double
   GenerateDifferentialNoise(RandomInfo *,const Quantum,const NoiseType,
     const double);
@@ -44,6 +50,106 @@ extern MagickPrivate void
     double *),
   ConvertRGBToHWB(const double,const double,const double,double *,double *,
     double *);
+
+static inline void ConvertLabToXYZ(const double L,const double a,const double b,
+  double *X,double *Y,double *Z)
+{
+  double
+    x,
+    y,
+    z;
+
+  assert(X != (double *) NULL);
+  assert(Y != (double *) NULL);
+  assert(Z != (double *) NULL);
+  y=(100.0f*L+16.0f)/116.0f;
+  x=y+255.0f*(a-0.5f)/500.0f;
+  z=y-255.0f*(b-0.5f)/200.0f;
+  if ((x*x*x) > CIEEpsilon)
+    x=(x*x*x);
+  else
+    x=(116.0f*x-16.0f)/CIEK;
+  if ((y*y*y) > CIEEpsilon)
+    y=(y*y*y);
+  else
+    y=(100.0f*L)/CIEK;
+  if ((z*z*z) > CIEEpsilon)
+    z=(z*z*z);
+  else
+    z=(116.0f*z-16.0f)/CIEK;
+  *X=D65X*x;
+  *Y=D65Y*y;
+  *Z=D65Z*z;
+}
+
+static inline void ConvertRGBToXYZ(const double red,const double green,
+  const double blue,double *X,double *Y,double *Z)
+{
+  double
+    b,
+    g,
+    r;
+
+  assert(X != (double *) NULL);
+  assert(Y != (double *) NULL);
+  assert(Z != (double *) NULL);
+  r=QuantumScale*red;
+  g=QuantumScale*green;
+  b=QuantumScale*blue;
+  *X=0.41239558896741421610*r+0.35758343076371481710*g+0.18049264738170157350*b;
+  *Y=0.21258623078559555160*r+0.71517030370341084990*g+0.07220049864333622685*b;
+  *Z=0.01929721549174694484*r+0.11918386458084853180*g+0.95049712513157976600*b;
+}
+
+static inline void ConvertXYZToLab(const double X,const double Y,const double Z,
+  double *L,double *a,double *b)
+{
+  double
+    x,
+    y,
+    z;
+  assert(L != (double *) NULL);
+  assert(a != (double *) NULL);
+  assert(b != (double *) NULL);
+  if ((X/D65X) > CIEEpsilon)
+    x=pow(X/D65X,1.0/3.0);
+  else
+    x=(CIEK*X/D65X+16.0f)/116.0f;
+  if ((Y/D65Y) > CIEEpsilon)
+    y=pow(Y/D65Y,1.0/3.0);
+  else
+    y=(CIEK*Y/D65Y+16.0f)/116.0f;
+  if ((Z/D65Z) > CIEEpsilon)
+    z=pow(Z/D65Z,1.0/3.0);
+  else
+    z=(CIEK*Z/D65Z+16.0f)/116.0f;
+  *L=((116.0f*y)-16.0f)/100.0f;
+  *a=(500.0f*(x-y))/255.0f+0.5f;
+  *b=(200.0f*(y-z))/255.0f+0.5f;
+}
+
+static inline void ConvertXYZToRGB(const double x,const double y,const double z,
+  double *red,double *green,double *blue)
+{
+  double
+    b,
+    g,
+    r;
+
+  /*
+    Convert XYZ to sRGB colorspace.
+  */
+  assert(red != (double *) NULL);
+  assert(green != (double *) NULL);
+  assert(blue != (double *) NULL);
+  r=3.2406f*x-1.5372f*y-0.4986f*z;
+  g=(-0.9689f*x+1.8758f*y+0.0415f*z);
+  b=0.0557f*x-0.2040f*y+1.0570f*z;
+  *red=QuantumRange*r;
+  *green=QuantumRange*g;
+  *blue=QuantumRange*b;
+}
+
 
 #if defined(__cplusplus) || defined(c_plusplus)
 }
