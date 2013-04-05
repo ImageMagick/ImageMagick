6f879ed082ced69eb366256e92b65a0c3f9d2c98
urban-warrior <urban-warrior@users.noreply.github.com>
2013-04-05 00:50:58 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@11690 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/enhance.c
+++ MagickCore/enhance.c
@@ -2924,7 +2924,7 @@ static inline void ModulateHSL(const double percent_hue,
   hue+=0.5*(0.01*percent_hue-1.0);
   while (hue < 0.0)
     hue+=1.0;
-  while (hue > 1.0)
+  while (hue >= 1.0)
     hue-=1.0;
   saturation*=0.01*percent_saturation;
   lightness*=0.01*percent_lightness;
@@ -2947,7 +2947,7 @@ static inline void ModulateHWB(const double percent_hue,
   hue+=0.5*(0.01*percent_hue-1.0);
   while (hue < 0.0)
     hue+=1.0;
-  while (hue > 1.0)
+  while (hue >= 1.0)
     hue-=1.0;
   blackness*=0.01*percent_blackness;
   whiteness*=0.01*percent_whiteness;
@@ -2972,9 +2972,9 @@ static inline void ModulateLCH(const double percent_luma,
   hue+=0.5*(0.01*percent_hue-1.0);
   while (hue < 0.0)
     hue+=1.0;
-  while (hue > 1.0)
+  while (hue >= 1.0)
     hue-=1.0;
-  ConvertHCLToRGB(luma,chroma,hue,red,green,blue);
+  ConvertLCHToRGB(luma,chroma,hue,red,green,blue);
 }
 
 MagickExport MagickBooleanType ModulateImage(Image *image,const char *modulate,
