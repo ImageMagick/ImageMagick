873b8b9de7e71d97ae617832c005ff90de1ecce8
urban-warrior <urban-warrior@users.noreply.github.com>
2013-04-07 01:17:52 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@11733 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/miff.c
+++ coders/miff.c
@@ -1960,8 +1960,8 @@ static MagickBooleanType WriteMIFFImage(const ImageInfo *image_info,
     if ((image->storage_class == PseudoClass) &&
         (image->colors > (size_t) (GetQuantumRange(image->depth)+1)))
       (void) SetImageStorageClass(image,DirectClass);
-    if (IsImageGray(image,exception) != MagickFalse)
-      (void) SetImageColorspace(image,GRAYColorspace,exception);
+    if (IsGrayImage(image,&image->exception) != MagickFalse)
+      (void) SetImageColorspace(image,GRAYColorspace);
     image->depth=image->depth <= 8 ? 8UL : image->depth <= 16 ? 16UL :
       image->depth <= 32 ? 32UL : 64UL;
     quantum_info=AcquireQuantumInfo(image_info,image);
