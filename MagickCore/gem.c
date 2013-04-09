302d46c34ae4e12a9c5f31cf86c8f45795b8a62f
urban-warrior <urban-warrior@users.noreply.github.com>
2013-04-09 00:57:23 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@11767 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/gem.c
+++ MagickCore/gem.c
@@ -107,8 +107,8 @@ MagickPrivate void ConvertHCLToRGB(const double hue,const double chroma,
   L=luma;
   C=chroma;
   H=hue;
-  u=C*cos(360.0*H*MagickPI/180.0)+134.0/354.0;
-  v=C*sin(360.0*H*MagickPI/180.0)+140.0/262.0;
+  u=C*cos(360.0*H*MagickPI/180.0);
+  v=C*sin(360.0*H*MagickPI/180.0);
   ConvertLuvToXYZ(L,u,v,&X,&Y,&Z);
   ConvertXYZToRGB(X,Y,Z,red,green,blue);
 }
@@ -420,8 +420,8 @@ MagickPrivate void ConvertLCHabToRGB(const double luma,const double chroma,
   L=luma;
   C=chroma;
   H=hue;
-  a=C*cos(360.0*H*MagickPI/180.0)+0.5;
-  b=C*sin(360.0*H*MagickPI/180.0)+0.5;
+  a=C*cos(360.0*H*MagickPI/180.0);
+  b=C*sin(360.0*H*MagickPI/180.0);
   ConvertLabToXYZ(L,a,b,&X,&Y,&Z);
   ConvertXYZToRGB(X,Y,Z,red,green,blue);
 }
@@ -475,8 +475,8 @@ MagickPrivate void ConvertLCHuvToRGB(const double luma,const double chroma,
   L=luma;
   C=chroma;
   H=hue;
-  u=C*cos(360.0*H*MagickPI/180.0)+134.0/354.0;
-  v=C*sin(360.0*H*MagickPI/180.0)+140.0/262.0;
+  u=C*cos(360.0*H*MagickPI/180.0);
+  v=C*sin(360.0*H*MagickPI/180.0);
   ConvertLuvToXYZ(L,u,v,&X,&Y,&Z);
   ConvertXYZToRGB(X,Y,Z,red,green,blue);
 }
@@ -530,8 +530,8 @@ MagickPrivate void ConvertRGBToHCL(const double red,const double green,
   assert(hue != (double *) NULL);
   ConvertRGBToXYZ(red,green,blue,&X,&Y,&Z);
   ConvertXYZToLuv(X,Y,Z,&L,&u,&v);
-  C=hypot(u-134.0/354.0,v-140.0/262.0);
-  H=180.0*atan2(v-140.0/262.0,u-134.0/354.0)/MagickPI/360.0;
+  C=hypot(u,v);
+  H=180.0*atan2(v,u)/MagickPI/360.0;
   if (H < 0.0)
     H+=1.0;
   if (H >= 1.0)
@@ -822,8 +822,8 @@ MagickPrivate void ConvertRGBToLCHab(const double red,const double green,
   assert(hue != (double *) NULL);
   ConvertRGBToXYZ(red,green,blue,&X,&Y,&Z);
   ConvertXYZToLab(X,Y,Z,&L,&a,&b);
-  C=hypot(a-0.5,b-0.5);
-  H=180.0*atan2(b-0.5,a-0.5)/MagickPI/360.0;
+  C=hypot(a,b);
+  H=180.0*atan2(b,a)/MagickPI/360.0;
   if (H < 0.0)
     H+=1.0;
   if (H >= 1.0)
@@ -882,8 +882,8 @@ MagickPrivate void ConvertRGBToLCHuv(const double red,const double green,
   assert(hue != (double *) NULL);
   ConvertRGBToXYZ(red,green,blue,&X,&Y,&Z);
   ConvertXYZToLuv(X,Y,Z,&L,&u,&v);
-  C=hypot(u-134.0/354.0,v-140.0/262.0);
-  H=180.0*atan2(v-140.0/262.0,u-134.0/354.0)/MagickPI/360.0;
+  C=hypot(uu,v);
+  H=180.0*atan2(v,u)/MagickPI/360.0;
   if (H < 0.0)
     H+=1.0;
   if (H >= 1.0)
