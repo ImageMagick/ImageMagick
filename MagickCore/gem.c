12987cacf73fea3173a19a184d92c0b66a215e1c
urban-warrior <urban-warrior@users.noreply.github.com>
2013-04-19 20:24:33 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@11937 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/gem.c
+++ MagickCore/gem.c
@@ -457,9 +457,10 @@ MagickPrivate void ConvertLCHabToRGB(const double luma,const double chroma,
   L=luma;
   C=chroma;
   H=hue;
-  a=C*cos(360.0*H*MagickPI/180.0)+0.5;
-  b=C*sin(360.0*H*MagickPI/180.0)+0.5;
-  ConvertLabToXYZ(L,a,b,&X,&Y,&Z);
+  a=C*cos(360.0*H*MagickPI/180.0);
+  b=C*sin(360.0*H*MagickPI/180.0);
+  ConvertLabToXYZ(((116.0*L)-16.0)/100.0,(500.0*a)/255.0+0.5,(200.0*b)/255.0+
+    0.5,&X,&Y,&Z);
   ConvertXYZToRGB(X,Y,Z,red,green,blue);
 }
 
@@ -512,9 +513,9 @@ MagickPrivate void ConvertLCHuvToRGB(const double luma,const double chroma,
   L=luma;
   C=chroma;
   H=hue;
-  u=C*cos(360.0*H*MagickPI/180.0)+(134.0/354.0);
-  v=C*sin(360.0*H*MagickPI/180.0)+(140.0/262.0);
-  ConvertLuvToXYZ(L,u,v,&X,&Y,&Z);
+  u=C*cos(360.0*H*MagickPI/180.0);
+  v=C*sin(360.0*H*MagickPI/180.0);
+  ConvertLuvToXYZ(L,(u+1.34)/3.54,v+1.4/2.62,&X,&Y,&Z);
   ConvertXYZToRGB(X,Y,Z,red,green,blue);
 }
 
