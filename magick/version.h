e934e057c60dfbea69f662840bcce8178bca9711
urban-warrior <urban-warrior@users.noreply.github.com>
2013-04-26 19:04:45 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@12006 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/version.h
+++ magick/version.h
@@ -31,11 +31,11 @@ extern "C" {
 #define MagickLibVersion  0x685
 #define MagickLibVersionText  "6.8.5"
 #define MagickLibVersionNumber  1,0,0
-#define MagickLibAddendum  "-2"
+#define MagickLibAddendum  "-3"
 #define MagickLibInterface  1
 #define MagickLibMinInterface  1
-#define MagickReleaseDate  "2013-04-25"
-#define MagickChangeDate   "20130427"
+#define MagickReleaseDate  "2013-04-26"
+#define MagickChangeDate   "20130428"
 #define MagickFeatures "DPC OpenMP"
 #define MagickDelegates "bzlib fontconfig freetype jng jp2 jpeg lcms lzma pango png ps tiff x xml zlib"
 #define MagickHomeURL  "file:///usr/share/doc/ImageMagick-6/index.html"
