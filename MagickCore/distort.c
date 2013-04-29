b029a21beb1738e1a6a5162e186d4e6e9dadb859
urban-warrior <urban-warrior@users.noreply.github.com>
2013-04-29 08:29:35 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@12049 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/distort.c
+++ MagickCore/distort.c
@@ -373,7 +373,7 @@ static inline double MagickRound(double x)
   /*
     Round the fraction to nearest integer.
   */
-  if ((x-floor(x)) < (ceil(x)-x)
+  if ((x-floor(x)) < (ceil(x)-x))
     return(floor(x));
   return(ceil(x));
 }
