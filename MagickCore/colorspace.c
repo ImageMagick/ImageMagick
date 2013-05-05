38e97388bb9ab2d86c7b6b99057c0b0a374dd028
cristy <urban-warrior@git.imagemagick.org>
2013-05-05 19:04:17 +0000

    M    MagickCore/enhance.c M    MagickCore/colorspace.c

--- MagickCore/colorspace.c
+++ MagickCore/colorspace.c
@@ -190,14 +190,14 @@ static void ConvertRGBToYPbPr(const double red,const double green,
   assert(Pr != (double *) NULL);
   *Y=QuantumScale*(0.298839*red+0.586811*green+0.114350*blue);
   *Pb=QuantumScale*((-0.1687367)*red-0.331264*green+0.5*blue)+0.5;
-  *Pr=QuantumScale*(0.5*QuantumScale*red-0.418688*green-0.081312*blue)+0.5;
+  *Pr=QuantumScale*(0.5*red-0.418688*green-0.081312*blue)+0.5;
 }
 
 static void ConvertRGBToYCbCr(const double red,const double green,
   const double blue,double *Y,double *Cb,double *Cr)
 {
   /*
-    Convert RGB to -YCbCr colorspace.
+    Convert RGB to YCbCr colorspace.
   */
   assert(Y != (double *) NULL);
   assert(Cb != (double *) NULL);
@@ -1328,7 +1328,7 @@ static void ConvertYCbCrToRGB(const double Y,const double Cb,
   const double Cr,double *red,double *green,double *blue)
 {
   /*
-    Convert -YCbCr to RGB colorspace.
+    Convert YCbCr to RGB colorspace.
   */
   assert(red != (double *) NULL);
   assert(green != (double *) NULL);
