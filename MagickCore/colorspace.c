38cad20959780748e8bf34fcbe81120a35f1e03c
urban-warrior <urban-warrior@users.noreply.github.com>
2013-05-05 18:33:58 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@12151 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/colorspace.c
+++ MagickCore/colorspace.c
@@ -215,8 +215,8 @@ static void ConvertRGBToYUV(const double red,const double green,
   assert(U != (double *) NULL);
   assert(V != (double *) NULL);
   *Y=QuantumScale*(0.298839*red+0.586811*green+0.114350*blue);
-  *U=QuantumScale*((-0.147)*red-0.289*green+0.436*blue+0.5);
-  *V=QuantumScale*(0.615*red-0.515*green-0.100*blue+0.5);
+  *U=QuantumScale*((-0.147)*red-0.289*green+0.436*blue)+0.5;
+  *V=QuantumScale*(0.615*red-0.515*green-0.100*blue)+0.5;
 }
 
 static MagickBooleanType sRGBTransformImage(Image *image,
