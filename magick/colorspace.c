5c8db78df6e023020ce97e85f4d6d94b4bf5b89a
urban-warrior <urban-warrior@users.noreply.github.com>
2013-05-07 00:39:32 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@12196 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/colorspace.c
+++ magick/colorspace.c
@@ -183,8 +183,8 @@ static void ConvertRGBToYDbDr(const Quantum red,const Quantum green,
   const Quantum blue,double *Y,double *Db,double *Dr)
 {
   *Y=QuantumScale*(0.298839*red+0.586811*green+0.114350*blue);
-  *Db=QuantumScale*((-0.450)*red-0.883*green+1.333*blue)+0.5;
-  *Dr=QuantumScale*(1.333*red-0.116*green-0.217*blue)+0.5;
+  *Db=QuantumScale*(-0.450*red-0.883*green+1.333*blue)+0.5;
+  *Dr=QuantumScale*(-1.333*red+0.116*green-0.217*blue)+0.5;
 }
 
 static void ConvertRGBToYIQ(const Quantum red,const Quantum green,
@@ -1319,11 +1319,11 @@ static void ConvertYCbCrToRGB(const double Y,const double Cb,
 static void ConvertYDbDrToRGB(const double Y,const double Db,const double Dr,
   Quantum *red,Quantum *green,Quantum *blue)
 {
-  *red=ClampToQuantum(QuantumRange*(Y-9.2303716147657e-05*(Db-0.5)+
+  *red=ClampToQuantum(QuantumRange*(Y+9.2303716147657e-05*(Db-0.5)-
     0.52591263066186533*(Dr-0.5)));
-  *green=ClampToQuantum(QuantumRange*(Y-12913289889050927*(Db-0.5)-
+  *green=ClampToQuantum(QuantumRange*(Y-0.12913289889050927*(Db-0.5)+
     0.26789932820759876*(Dr-0.5)));
-  *blue=ClampToQuantum(QuantumRange*(Y+0.66467905997895482*(Db-0.5)+
+  *blue=ClampToQuantum(QuantumRange*(Y+0.66467905997895482*(Db-0.5)-
     7.9202543533108e-05*(Dr-0.5)));
 }
 
