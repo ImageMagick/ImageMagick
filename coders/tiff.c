7e2e7cc2549498296311704eb06541e08507f908
urban-warrior <urban-warrior@users.noreply.github.com>
2013-05-07 13:34:36 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@12218 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/tiff.c
+++ coders/tiff.c
@@ -3172,9 +3172,7 @@ static MagickBooleanType WriteTIFFImage(const ImageInfo *image_info,
       default:
         break;
     }
-    option=GetImageOption(image_info,"tiff:rows-per-strip");
-    if (option != (const char *) NULL)
-      (void) TIFFSetField(tiff,TIFFTAG_ROWSPERSTRIP,rows_per_strip);
+    (void) TIFFSetField(tiff,TIFFTAG_ROWSPERSTRIP,rows_per_strip);
     if ((image->resolution.x != 0.0) && (image->resolution.y != 0.0))
       {
         unsigned short
