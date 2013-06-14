e43df80648a6d02a13d8f940d0d61f43dc1389be
urban-warrior <urban-warrior@users.noreply.github.com>
2013-06-14 17:54:58 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@12451 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/morphology.c
+++ magick/morphology.c
@@ -2790,7 +2790,7 @@ static ssize_t MorphologyPrimitive(const Image *image, Image *result_image,
             SetPixelBlue(q,ClampToQuantum(gamma*result.blue));
             SetPixelOpacity(q,ClampToQuantum(result.opacity));
             if (image->colorspace == CMYKColorspace)
-              SetPixelIndex(q_indexes+x,ClampToQuantum(gamma*result.index));
+              SetPixelIndex(q_indexes+y,ClampToQuantum(gamma*result.index));
           }
 
         /* Count up changed pixels */
