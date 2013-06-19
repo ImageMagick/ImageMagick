f3f0e345915395f21af909ce0334473505f99193
urban-warrior <urban-warrior@users.noreply.github.com>
2013-06-19 23:40:01 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@12504 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/gem.c
+++ MagickCore/gem.c
@@ -815,7 +815,7 @@ MagickPrivate void ConvertLCHuvToRGB(const double luma,const double chroma,
   assert(red != (double *) NULL);
   assert(green != (double *) NULL);
   assert(blue != (double *) NULL);
-  ConvertLCHuvToXYZ(100.0*luma,255.0*(chroma-0.5),360.0*hue,&X,&Y,&Z)
+  ConvertLCHuvToXYZ(100.0*luma,255.0*(chroma-0.5),360.0*hue,&X,&Y,&Z);
   ConvertXYZToRGB(X,Y,Z,red,green,blue);
 }
 
