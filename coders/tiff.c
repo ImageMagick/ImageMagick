901cdc71cbd64ee8778da431049d31ad089ec7c5
urban-warrior <urban-warrior@users.noreply.github.com>
2013-07-24 17:25:21 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@12750 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/tiff.c
+++ coders/tiff.c
@@ -3178,15 +3178,18 @@ static MagickBooleanType WriteTIFFImage(const ImageInfo *image_info,
         (void) TIFFSetField(tiff,TIFFTAG_RESOLUTIONUNIT,(uint16) units);
         (void) TIFFSetField(tiff,TIFFTAG_XRESOLUTION,image->x_resolution);
         (void) TIFFSetField(tiff,TIFFTAG_YRESOLUTION,image->y_resolution);
-        if ((image->page.x >= 0) && (image->page.y >= 0))
+        if (((image->page.x > 0) && (image->page.y >= 0)) ||
+            ((image->page.y > 0) && (image->page.x >= 0)))
           {
             /*
               Set image position.
             */
-            (void) TIFFSetField(tiff,TIFFTAG_XPOSITION,(float) image->page.x/
-              image->x_resolution);
-            (void) TIFFSetField(tiff,TIFFTAG_YPOSITION,(float) image->page.y/
-              image->y_resolution);
+            if (image->x_resolution != 0.0)
+              (void) TIFFSetField(tiff,TIFFTAG_XPOSITION,(float) image->page.x/
+                image->x_resolution);
+            if (image->y_resolution != 0.0)
+              (void) TIFFSetField(tiff,TIFFTAG_YPOSITION,(float) image->page.y/
+                image->y_resolution);
           }
       }
     if (image->chromaticity.white_point.x != 0.0)
