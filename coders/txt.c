66cb1379f8e4110a0940bf693b65eefcc8b2edf0
urban-warrior <urban-warrior@users.noreply.github.com>
2013-09-02 01:57:38 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@13140 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/txt.c
+++ coders/txt.c
@@ -674,8 +674,7 @@ static MagickBooleanType WriteTXTImage(const ImageInfo *image_info,Image *image)
       {
         (void) FormatLocaleString(buffer,MaxTextExtent,
           "# ImageMagick pixel enumeration: %.20g,%.20g,%.20g,%s\n",(double)
-          image->columns,(double) image->rows,(double)
-          ((MagickOffsetType) GetQuantumRange(image->depth)),colorspace);
+          image->columns,(double) image->rows,(double) QuantumRange,colorspace);
         (void) WriteBlobString(image,buffer);
       }
     GetMagickPixelPacket(image,&pixel);
