d0d892e5a549862b4b49d5bbad4e305a562c7a90
urban-warrior <urban-warrior@users.noreply.github.com>
2013-09-05 16:15:56 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@13164 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/tiff.c
+++ coders/tiff.c
@@ -3084,10 +3084,7 @@ static MagickBooleanType WriteTIFFImage(const ImageInfo *image_info,
       if ((image_info->interlace == PlaneInterlace) ||
           (image_info->interlace == PartitionInterlace))
         (void) TIFFSetField(tiff,TIFFTAG_PLANARCONFIG,PLANARCONFIG_SEPARATE);
-    rows_per_strip=1;
-    if (TIFFScanlineSize(tiff) != 0)
-      rows_per_strip=(uint32) MagickMax((size_t) TIFFDefaultStripSize(tiff,0),
-        1);
+     rows_per_strip=TIFFDefaultStripSize(tiff,0);
     option=GetImageOption(image_info,"tiff:rows-per-strip");
     if (option != (const char *) NULL)
       rows_per_strip=(size_t) strtol(option,(char **) NULL,10);
@@ -3197,6 +3194,8 @@ static MagickBooleanType WriteTIFFImage(const ImageInfo *image_info,
       default:
         break;
     }
+    if (rows_per_strip < 1)
+      rows_per_strip=1;
     if ((image->rows/rows_per_strip) >= (1UL << 15))
       rows_per_strip=(image->rows >> 15);
     (void) TIFFSetField(tiff,TIFFTAG_ROWSPERSTRIP,rows_per_strip);
