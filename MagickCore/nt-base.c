155976ad8e6395cfb8603702f882e01cd49ed764
dlemstra <dlemstra@users.noreply.github.com>
2013-09-20 20:21:54 +0000

    Minor bug fix.
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@13255 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/nt-base.c
+++ MagickCore/nt-base.c
@@ -1574,9 +1574,7 @@ static UINT ChangeErrorMode(void)
   if (handle == (HMODULE) NULL)
     return SetErrorMode(mode);
 
-  if (getErrorMode == (GETERRORMODE) NULL)
-    getErrorMode=(GETERRORMODE) NTGetLibrarySymbol(handle,"GetErrorMode");
-
+  getErrorMode=(GETERRORMODE) NTGetLibrarySymbol(handle,"GetErrorMode");
   if (getErrorMode != (GETERRORMODE) NULL)
     mode=getErrorMode() | SEM_FAILCRITICALERRORS | SEM_NOOPENFILEERRORBOX;
 
