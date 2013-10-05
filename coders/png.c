660e8b496fa2d9b0d5533a19373d4d5bca8f833d
glennrp <glennrp@users.noreply.github.com>
2013-10-05 14:55:05 +0000

    Set intensity method when reading a gray PNG image.
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@13366 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/png.c
+++ coders/png.c
@@ -3653,6 +3653,7 @@ static Image *ReadOnePNGImage(MngInfo *mng_info,
           /* Set image->gamma to 1.0, image->rendering_intent to Undefined,
            * image->colorspace to GRAY, and reset image->chromaticity.
            */
+          image->intensity = Rec709LuminancePixelIntensityMethod;
           SetImageColorspace(image,GRAYColorspace);
         }
     }
