9b42d600fcaed72369e29dc5c482425dafd04394
urban-warrior <urban-warrior@users.noreply.github.com>
2013-10-12 01:03:37 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@13442 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/xwindow.c
+++ magick/xwindow.c
@@ -5469,12 +5469,20 @@ MagickExport MagickBooleanType XMakeImage(Display *display,
             Resize image.
           */
           resize_image=NewImageList();
-          if (window->pixel_info->colors != 0)
-            resize_image=SampleImage(window->image,width,height,
-              &image->exception);
+          if ((window->pixel_info->colors == 0) &&
+              (window->image->rows > (unsigned long) XDisplayHeight(display,window->screen)) &&
+              (window->image->columns > (unsigned long) XDisplayWidth(display,window->screen)))
+              resize_image=ResizeImage(window->image,width,height,
+                image->filter,image->blur,&image->exception);
           else
-            resize_image=ThumbnailImage(window->image,width,height,
-              &image->exception);
+            {
+              if (window->image->storage_class == PseudoClass)
+                resize_image=SampleImage(window->image,width,height,
+                  &image->exception);
+              else
+                resize_image=ThumbnailImage(window->image,width,height,
+                  &image->exception);
+            }
           if (resize_image != (Image *) NULL)
             {
               if (window->image != image)
