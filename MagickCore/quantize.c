0fdb1260d14f36d58eeb50f0e59e14bd237a7042
urban-warrior <urban-warrior@users.noreply.github.com>
2013-10-25 21:23:03 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@13563 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/quantize.c
+++ MagickCore/quantize.c
@@ -339,9 +339,7 @@ static MagickBooleanType
   SetGrayscaleImage(Image *,ExceptionInfo *);
 
 static size_t
-  DefineImageColormap(Image *,CubeInfo *,NodeInfo *),
-  QuantizeErrorFlatten(const Image *,const CubeInfo *,const NodeInfo *,
-    const ssize_t,const size_t,MagickRealType *);
+  DefineImageColormap(Image *,CubeInfo *,NodeInfo *);
 
 static void
   ClosestColor(const Image *,CubeInfo *,const NodeInfo *),
@@ -2951,7 +2949,7 @@ MagickExport MagickBooleanType QuantizeImages(const QuantizeInfo *quantize_info,
 %  The format of the QuantizeImages method is:
 %
 %      size_t QuantizeErrorFlatten(const Image *image,const CubeInfo *cube_info,
-%        const NodeInfo *node_info,const ssize_t offset,const size_t extent,
+%        const NodeInfo *node_info,const ssize_t offset,
 %        MagickRealType *quantize_error)
 %
 %  A description of each parameter follows.
@@ -2964,14 +2962,11 @@ MagickExport MagickBooleanType QuantizeImages(const QuantizeInfo *quantize_info,
 %
 %    o offset: quantize error offset.
 %
-%    o extent: quantize error offset maximum limit.
-%
 %    o quantize_error: the quantization error vector.
 %
 */
 static size_t QuantizeErrorFlatten(const Image *image,const CubeInfo *cube_info,
-  const NodeInfo *node_info,const ssize_t offset,const size_t extent,
-  MagickRealType *quantize_error)
+  const NodeInfo *node_info,const ssize_t offset,MagickRealType *quantize_error)
 {
   register ssize_t
     i;
@@ -2980,7 +2975,7 @@ static size_t QuantizeErrorFlatten(const Image *image,const CubeInfo *cube_info,
     n,
     number_children;
 
-  if (offset >= (ssize_t) extent)
+  if (offset >= (ssize_t) cube_info->nodes)
     return(0);
   quantize_error[offset]=node_info->quantize_error;
   n=1;
@@ -2988,7 +2983,7 @@ static size_t QuantizeErrorFlatten(const Image *image,const CubeInfo *cube_info,
   for (i=0; i < (ssize_t) number_children ; i++)
     if (node_info->child[i] != (NodeInfo *) NULL)
       n+=QuantizeErrorFlatten(image,cube_info,node_info->child[i],offset+n,
-        extent,quantize_error);
+        quantize_error);
   return(n);
 }
 
@@ -3146,7 +3141,7 @@ static void ReduceImageColors(const Image *image,CubeInfo *cube_info)
       if (quantize_error != (MagickRealType *) NULL)
         {
           (void) QuantizeErrorFlatten(image,cube_info,cube_info->root,0,
-            cube_info->nodes,quantize_error);
+            quantize_error);
           qsort(quantize_error,cube_info->nodes,sizeof(MagickRealType),
             MagickRealTypeCompare);
           cube_info->next_threshold=quantize_error[cube_info->nodes-
