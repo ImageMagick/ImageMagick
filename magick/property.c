027e08fdab224a8f4074a86d89300992b6bf9b21
dirk <dirk@git.imagemagick.org>
2013-11-07 21:20:05 +0000

    Added standard_deviation to GetMagickProperty.

--- magick/property.c
+++ magick/property.c
@@ -2822,7 +2822,8 @@ MagickExport const char *GetMagickProperty(const ImageInfo *image_info,
             GetMagickPrecision(),skewness);
           break;
         }
-      if (LocaleCompare("standard-deviation",property) == 0)
+      if ((LocaleCompare("standard-deviation",property) == 0) ||
+          (LocaleCompare("standard_deviation",property) == 0))
         {
           double
             mean,
