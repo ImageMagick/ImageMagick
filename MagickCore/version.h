74399fd86f7439d4a48c771804d793b9233aa743
urban-warrior <urban-warrior@users.noreply.github.com>
2013-11-18 13:03:38 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@13794 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/version.h
+++ MagickCore/version.h
@@ -1,5 +1,5 @@
 /*
-  Copyright 1999-2013 ImageMagick Studio LLC, a non-profit organization
+  Copyright 1999-2014 ImageMagick Studio LLC, a non-profit organization
   dedicated to making software imaging solutions freely available.
   
   You may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ extern "C" {
   Define declarations.
 */
 #define MagickPackageName "ImageMagick"
-#define MagickCopyright  "Copyright (C) 1999-2013 ImageMagick Studio LLC"
+#define MagickCopyright  "Copyright (C) 1999-2014 ImageMagick Studio LLC"
 #define MagickSVNRevision  "13660:13664M"
 #define MagickLibVersion  0x700
 #define MagickLibVersionText  "7.0.0"
