24a603c4becaf1d4016a1fd95dc56bac66d65f70
urban-warrior <urban-warrior@users.noreply.github.com>
2013-12-01 14:58:08 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@13885 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- PerlMagick/quantum/quantum.pm
+++ PerlMagick/quantum/quantum.pm
@@ -101,7 +101,7 @@ __END__
 
 =head1 NAME
 
-Image::Magick::Q16 - objected-oriented Perl interface to ImageMagick (Q16). Use it to read, manipulate, or write an image or image sequence from within a Perl script.
+Image::Magick::Q16 - objected-oriented Perl interface to ImageMagick (Q16). Use it to create, edit, compose, or convert bitmap images from within a Perl script.
 
 =head1 SYNOPSIS
 
