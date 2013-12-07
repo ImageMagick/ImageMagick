c780271eaf1553c50ceeef3aa5db9ef543a9a5fb
dlemstra <dlemstra@users.noreply.github.com>
2013-12-07 19:11:02 +0000

    Code cleanup and refactoring.
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@13929 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- Magick++/lib/Magick++/Color.h
+++ Magick++/lib/Magick++/Color.h
@@ -12,21 +12,20 @@
 
 namespace Magick
 {
-
   class MagickPPExport Color;
 
   // Compare two Color objects regardless of LHS/RHS
-  int MagickPPExport operator ==
+  MagickPPExport int operator ==
     (const Magick::Color &left_,const Magick::Color &right_);
-  int MagickPPExport operator !=
+  MagickPPExport int operator !=
     (const Magick::Color &left_,const Magick::Color &right_);
-  int MagickPPExport operator >
+  MagickPPExport int operator >
     (const Magick::Color &left_,const Magick::Color &right_);
-  int MagickPPExport operator <
+  MagickPPExport int operator <
     (const Magick::Color &left_,const Magick::Color &right_);
-  int MagickPPExport operator >=
+  MagickPPExport int operator >=
     (const Magick::Color &left_,const Magick::Color &right_);
-  int MagickPPExport operator <=
+  MagickPPExport int operator <=
     (const Magick::Color &left_,const Magick::Color &right_);
 
   // Base color class stores RGB components scaled to fit Quantum
