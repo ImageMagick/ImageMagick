60ebd28312db7768b922179b46102df2b57ff11a
urban-warrior <urban-warrior@users.noreply.github.com>
2013-12-30 13:08:54 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@14088 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/nt-base.c
+++ magick/nt-base.c
@@ -2249,8 +2249,8 @@ MagickExport int NTTruncateFile(int file,off_t length)
     high,
     low;
 
-  file_handle=(HANDLE)_get_osfhandle(file);
-  if (file_handle == (HANDLE)-1L)
+  file_handle=(HANDLE) _get_osfhandle(file);
+  if (file_handle == (HANDLE) -1L)
     return(-1);
   low=(long) (length & 0xffffffffUL);
   high=(long) ((((MagickOffsetType) length) >> 32) & 0xffffffffUL);
