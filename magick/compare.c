4c03268cdb4018028dfafbc7d8b6c1c5a09a698d
urban-warrior <urban-warrior@users.noreply.github.com>
2014-01-09 14:33:30 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@14276 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/compare.c
+++ magick/compare.c
@@ -1314,38 +1314,38 @@ static MagickBooleanType GetPerceptualHashDistortion(const Image *image,
     */
     if ((channel & RedChannel) != 0)
       {
-        difference=reconstruct_moments[RedChannel].I[i]-
-          image_moments[RedChannel].I[i];
+        difference=log10(fabs(reconstruct_moments[RedChannel].I[i]))-
+          log10(fabs(image_moments[RedChannel].I[i]));
         distortion[RedChannel]+=difference*difference;
         distortion[CompositeChannels]+=difference*difference;
       }
     if ((channel & GreenChannel) != 0)
       {
-        difference=reconstruct_moments[GreenChannel].I[i]-
-          image_moments[GreenChannel].I[i];
+        difference=log10(fabs(reconstruct_moments[GreenChannel].I[i]))-
+          log10(fabs(image_moments[GreenChannel].I[i]));
         distortion[GreenChannel]+=difference*difference;
         distortion[CompositeChannels]+=difference*difference;
       }
     if ((channel & BlueChannel) != 0)
       {
-        difference=reconstruct_moments[BlueChannel].I[i]-
-          image_moments[BlueChannel].I[i];
+        difference=log10(fabs(reconstruct_moments[BlueChannel].I[i]))-
+          log10(fabs(image_moments[BlueChannel].I[i]));
         distortion[BlueChannel]+=difference*difference;
         distortion[CompositeChannels]+=difference*difference;
       }
     if (((channel & OpacityChannel) != 0) &&
         (image->matte != MagickFalse))
       {
-        difference=reconstruct_moments[OpacityChannel].I[i]-
-          image_moments[OpacityChannel].I[i];
+        difference=log10(fabs(reconstruct_moments[OpacityChannel].I[i]))-
+          log10(fabs(image_moments[OpacityChannel].I[i]));
         distortion[OpacityChannel]+=difference*difference;
         distortion[CompositeChannels]+=difference*difference;
       }
     if (((channel & IndexChannel) != 0) &&
         (image->colorspace == CMYKColorspace))
       {
-        difference=reconstruct_moments[IndexChannel].I[i]-
-          image_moments[IndexChannel].I[i];
+        difference=log10(fabs(reconstruct_moments[IndexChannel].I[i]))-
+          log10(fabs(image_moments[IndexChannel].I[i]));
         distortion[IndexChannel]+=difference*difference;
         distortion[CompositeChannels]+=difference*difference;
       }
