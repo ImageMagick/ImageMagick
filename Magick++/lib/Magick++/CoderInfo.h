1d7b45843f3173f36162ae7cc11c3b2a3834ef75
dirk <dirk@git.imagemagick.org>
2014-01-10 20:56:39 +0000

    Added unregister method to CoderInfo class.

--- Magick++/lib/Magick++/CoderInfo.h
+++ Magick++/lib/Magick++/CoderInfo.h
@@ -58,6 +58,9 @@ namespace Magick
     // Format name
     std::string name(void) const;
 
+    // Unregisters this coder
+    bool unregister(void) const;
+
   private:
     std::string _name;
     std::string _description;
