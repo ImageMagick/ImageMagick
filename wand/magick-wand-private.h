aa6f12ae3287a806d24a22860f9eceb64461b661
urban-warrior <urban-warrior@users.noreply.github.com>
2014-01-14 21:18:18 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@14392 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- wand/magick-wand-private.h
+++ wand/magick-wand-private.h
@@ -22,13 +22,11 @@
 extern "C" {
 #endif
 
-#define DegreesToRadians(x)  (MagickPI*(x)/180.0)
 #define MagickPI  3.14159265358979323846264338327950288419716939937510
 #define MagickWandId  "MagickWand"
 #define QuantumTick(i,span) ((MagickBooleanType) ((((i) & ((i)-1)) == 0) || \
    (((i) & 0xfff) == 0) || \
    ((MagickOffsetType) (i) == ((MagickOffsetType) (span)-1))))
-#define RadiansToDegrees(x) (180.0*(x)/MagickPI)
 #define ThrowWandFatalException(severity,tag,context) \
 { \
   ExceptionInfo \
