224a0227410f745725e9c72a7961537f346f1246
urban-warrior <urban-warrior@users.noreply.github.com>
2014-01-15 01:29:57 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@14426 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/vid.c
+++ coders/vid.c
@@ -328,7 +328,7 @@ static MagickBooleanType WriteVIDImage(const ImageInfo *image_info,Image *image)
     &image->exception);
   montage_info=DestroyMontageInfo(montage_info);
   if (montage_image == (Image *) NULL)
-    ThrowWriterException(CorruptImageError,image->exception.reason);
+    ThrowWriterException(CorruptImageError,strerror(error));
   (void) CopyMagickString(montage_image->filename,image_info->filename,
     MaxTextExtent);
   write_info=CloneImageInfo(image_info);
