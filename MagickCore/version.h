0340ce44cc278bb565b43c75a3c9a1ab68741201
urban-warrior <urban-warrior@users.noreply.github.com>
2014-01-18 01:16:17 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@14473 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/version.h
+++ MagickCore/version.h
@@ -27,7 +27,7 @@ extern "C" {
 */
 #define MagickPackageName "ImageMagick"
 #define MagickCopyright  "Copyright (C) 1999-2014 ImageMagick Studio LLC"
-#define MagickSVNRevision  "14365:14455M"
+#define MagickSVNRevision  "14467:14472M"
 #define MagickLibVersion  0x700
 #define MagickLibVersionText  "7.0.0"
 #define MagickLibVersionNumber  1,0,0
@@ -48,7 +48,7 @@ extern "C" {
 #define MagickppLibAddendum  "-0"
 #define MagickppLibInterface  1
 #define MagickppLibMinInterface  1
-#define MagickReleaseDate  "2014-01-16"
+#define MagickReleaseDate  "2014-01-17"
 #define MagickChangeDate   "20120427"
 #define MagickAuthoritativeURL  "http://www.imagemagick.org"
 #define MagickFeatures "DPC HDRI OpenMP"
