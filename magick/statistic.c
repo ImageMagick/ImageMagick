a1b7a84865993c3eb8c4f2a881c5d049a3b132d0
urban-warrior <urban-warrior@users.noreply.github.com>
2014-01-30 22:12:12 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@14721 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/statistic.c
+++ magick/statistic.c
@@ -1586,6 +1586,8 @@ MagickExport ChannelMoments *GetImageChannelMoments(const Image *image,
     /*
       Compute center of mass (centroid).
     */
+    centroid[channel].x=image->columns/2.0;
+    centroid[channel].y=image->rows/2.0;
     if (fabs(M00[channel]) < MagickEpsilon)
       continue;
     centroid[channel].x=M10[channel]/M00[channel];
