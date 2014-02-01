c20fa63e4b57d62d33db77a5adc746ede902e5ed
dirk <dirk@git.imagemagick.org>
2014-02-01 10:31:32 +0000

    Added subImageSearch.

--- Magick++/lib/Image.cpp
+++ Magick++/lib/Image.cpp
@@ -4194,6 +4194,28 @@ void Magick::Image::strip(void)
   ThrowPPException;
 }
 
+Magick::Image Magick::Image::subImageSearch(const Image &reference_,
+  const MetricType metric_,Geometry *offset_,double *similarityMetric_,
+  const double similarityThreshold)
+{
+  MagickCore::Image
+    *newImage;
+
+  RectangleInfo
+    offset;
+
+  GetPPException;
+  newImage=SimilarityImage(image(),reference_.constImage(),metric_,
+    similarityThreshold,&offset,similarityMetric_,&exceptionInfo);
+  ThrowPPException;
+  if (offset_ != (Geometry *) NULL)
+    *offset_=offset;
+  if (newImage == (MagickCore::Image *) NULL)
+    return(Magick::Image());
+  else
+    return(Magick::Image(newImage));
+}
+
 void Magick::Image::swirl(const double degrees_)
 {
   MagickCore::Image
