7e618b0ed0953f93c7a7b607dd7b49ea5bd78c21
urban-warrior <urban-warrior@users.noreply.github.com>
2014-02-05 18:01:41 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@14818 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/feature.c
+++ MagickCore/feature.c
@@ -138,7 +138,7 @@ static inline ssize_t MagickAbsoluteValue(const ssize_t x)
 
 static inline double MagickLog10(const double x)
 {
-#define Log10Epsilon  (1.0e-25)
+#define Log10Epsilon  (1.0e-11)
 
  if (fabs(x) < Log10Epsilon)
    return(log10(Log10Epsilon));
