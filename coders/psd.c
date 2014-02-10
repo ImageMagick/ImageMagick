1d34c2705b02ea610e01aaa104cab3af150260f2
urban-warrior <urban-warrior@users.noreply.github.com>
2014-02-10 15:31:12 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@14851 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/psd.c
+++ coders/psd.c
@@ -847,8 +847,7 @@ static MagickStatusType ReadPSDChannelRLE(Image *image,PSDInfo *psd_info,
         image->filename);
     }
 
-  compact_pixels=(unsigned char *) AcquireQuantumMemory(length,
-    sizeof(*pixels));
+  compact_pixels=(unsigned char *) AcquireQuantumMemory(length,sizeof(*pixels));
   if (compact_pixels == (unsigned char *) NULL)
     {
       pixels=(unsigned char *) RelinquishMagickMemory(pixels);
