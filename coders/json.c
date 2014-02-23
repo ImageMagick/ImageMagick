c12f65a49ac5372653c9dedb310b128377888081
urban-warrior <urban-warrior@users.noreply.github.com>
2014-02-23 12:28:57 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@14957 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/json.c
+++ coders/json.c
@@ -566,11 +566,11 @@ static ssize_t PrintChannelPerceptualHash(FILE *file,const ChannelType channel,
   n=FormatLocaleFile(file,"      \"%s\": {\n",name);
   for (i=0; i < 7; i++)
     n+=FormatLocaleFile(file,
-      "        \"phash%.20g\": [ \"%.*g\", \"%.*g\" ],\n",(double) i,
+      "        \"phash%.20g\": [ \"%.*g\", \"%.*g\" ],\n",i+1.0,
       GetMagickPrecision(),channel_phash[channel].P[i],
       GetMagickPrecision(),channel_phash[channel].Q[i]);
   n+=FormatLocaleFile(file,
-    "        \"phash%.20g\": [ \"%.*g\", \"%.*g\" ]\n",(double) i,
+    "        \"phash%.20g\": [ \"%.*g\", \"%.*g\" ]\n",i+1.0,
     GetMagickPrecision(),channel_phash[channel].P[i],
     GetMagickPrecision(),channel_phash[channel].Q[i]);
   (void) FormatLocaleFile(file,"      }");
