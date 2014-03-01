f087a6a9af5786006418b6a8347294d0147463d3
urban-warrior <urban-warrior@users.noreply.github.com>
2014-03-01 13:25:38 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@14994 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/blob.c
+++ magick/blob.c
@@ -1838,7 +1838,10 @@ MagickExport unsigned char *ImagesToBlob(const ImageInfo *image_info,
             {
               (void) FormatLocaleString(filename,MaxTextExtent,"%s:%s",
                 images->magick,unique);
+              images->blob->exempt=MagickTrue;
+              *images->filename='\0';
               status=WriteImages(blob_info,images,filename,exception);
+              (void) fclose(blob_info->file);
               if (status == MagickFalse)
                 InheritException(exception,&images->exception);
               else
