6dcca3afd65eb61b704a2456a9413cbd9ee6f554
dirk <dirk@git.imagemagick.org>
2014-04-04 17:08:37 +0000

    Fixed destroying SVGInfo.

--- coders/svg.c
+++ coders/svg.c
@@ -279,7 +279,7 @@ static SVGInfo *DestroySVGInfo(SVGInfo *svg_info)
   if (svg_info->text != (char *) NULL)
     svg_info->text=DestroyString(svg_info->text);
   if (svg_info->scale != (double *) NULL)
-    svg_info->scale=(double *) (svg_info->scale);
+    svg_info->scale=(double *) RelinquishMagickMemory(svg_info->scale);
   if (svg_info->title != (char *) NULL)
     svg_info->title=DestroyString(svg_info->title);
   if (svg_info->comment != (char *) NULL)
