3ccc2cf5c93ba4801a18337f78b35873984b1974
dirk <dirk@git.imagemagick.org>
2014-04-07 18:21:54 +0000

    Fixed detection of Ghostscript location.

--- MagickCore/nt-base.c
+++ MagickCore/nt-base.c
@@ -1137,7 +1137,7 @@ static int NTGhostscriptGetString(const char *name,BOOL *is_64_bit,
       else
         is_64_bit_version=TRUE;
 #else
-      flags=KEY_WOW64_64KEY;
+        flags=KEY_WOW64_64KEY;
 #endif
 #endif
     }
@@ -1145,6 +1145,9 @@ static int NTGhostscriptGetString(const char *name,BOOL *is_64_bit,
     {
       (void) NTLocateGhostscript(flags,&root_index,&product_family,
       &major_version,&minor_version);
+#if !defined(_WIN64)
+      is_64_bit_version=TRUE;
+#endif
     }
   if (product_family == NULL)
     return(FALSE);
