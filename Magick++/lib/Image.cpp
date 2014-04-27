c8c49d48975358aa8c6e14e113ef4c135e90b212
dirk <dirk@git.imagemagick.org>
2014-04-27 17:33:52 +0000

    Added hough line to Magick++.

--- Magick++/lib/Image.cpp
+++ Magick++/lib/Image.cpp
@@ -3173,6 +3173,19 @@ void  Magick::Image::haldClut(const Image &clutImage_)
   ThrowPPException;
 }
 
+void Magick::Image::houghLine(const size_t width_,const size_t height_,
+  const size_t threshold_)
+{
+  MagickCore::Image
+    *newImage;
+
+  GetPPException;
+  newImage=HoughLineImage(constImage(),width_,height_,threshold_,
+    &exceptionInfo);
+  replaceImage(newImage);
+  ThrowPPException;
+}
+
 void Magick::Image::implode(const double factor_)
 {
   MagickCore::Image
