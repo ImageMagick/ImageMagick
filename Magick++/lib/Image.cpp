806f6d43e9ce7b9ace3c1fa796e6877c0296957d
dlemstra <dlemstra@users.noreply.github.com>
2014-04-27 17:33:16 +0000

    Added hough line to Magick++.
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@15581 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- Magick++/lib/Image.cpp
+++ Magick++/lib/Image.cpp
@@ -3066,6 +3066,19 @@ void Magick::Image::haldClut(const Image &clutImage_)
   throwImageException();
 }
 
+void Magick::Image::houghLine(const size_t width_,const size_t height_,
+  const size_t threshold_)
+{
+  MagickCore::Image
+    *newImage;
+
+  GetPPException;
+  newImage=HoughLineImage(constImage(),width_,height_,threshold_,
+    &exceptionInfo);
+  replaceImage(newImage);
+  ThrowPPException;
+}
+
 void Magick::Image::implode(const double factor_)
 {
   MagickCore::Image
