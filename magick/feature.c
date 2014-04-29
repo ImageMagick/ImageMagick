467034d1f0afb679e55fcec971bfc4e880338f75
urban-warrior <urban-warrior@users.noreply.github.com>
2014-04-29 19:44:05 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@15628 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/feature.c
+++ magick/feature.c
@@ -2092,7 +2092,7 @@ MagickExport Image *MeanShiftImage(const Image *image,const size_t width,
   pixel_view=AcquireVirtualCacheView(image,exception);
   mean_view=AcquireAuthenticCacheView(mean_image,exception);
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
-  #pragma omp parallel for schedule(static,4) shared(status) \
+  #pragma omp parallel for schedule(static,4) shared(status,progress) \
     magick_threads(mean_image,mean_image,mean_image->rows,1)
 #endif
   for (y=0; y < (ssize_t) mean_image->rows; y++)
