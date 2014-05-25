db5bb65a5819e137d7bdb7a39bb55e17a07d7861
urban-warrior <urban-warrior@users.noreply.github.com>
2014-05-25 18:19:09 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@15854 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/threshold.c
+++ MagickCore/threshold.c
@@ -1226,7 +1226,7 @@ MagickExport MagickBooleanType ListThresholdMaps(FILE *file,
   MagickStatusType
     status;
 
-  status=MagickTrue;
+  status=0;
   if (file == (FILE *) NULL)
     file=stdout;
   options=GetConfigureOptions(ThresholdsFilename,exception);
