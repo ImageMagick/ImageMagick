b67a21bbee579bd64b5e992ed3443f1d4f71724e
urban-warrior <urban-warrior@users.noreply.github.com>
2014-05-26 01:29:41 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@15870 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/image.c
+++ MagickCore/image.c
@@ -883,10 +883,14 @@ MagickExport Image *CloneImage(const Image *image,const size_t columns,
       clone_image->cache=ReferencePixelCache(image->cache);
       return(clone_image);
     }
-  scale=(double) columns/(double) image->columns;
+  scale=1.0;
+  if (image->columns != 0)
+    scale=(double) columns/(double) image->columns;
   clone_image->page.width=(size_t) floor(scale*image->page.width+0.5);
   clone_image->page.x=(ssize_t) ceil(scale*image->page.x-0.5);
   clone_image->tile_offset.x=(ssize_t) ceil(scale*image->tile_offset.x-0.5);
+  scale=1.0;
+  if (image->rows != 0)
   scale=(double) rows/(double) image->rows;
   clone_image->page.height=(size_t) floor(scale*image->page.height+0.5);
   clone_image->page.y=(ssize_t) ceil(scale*image->page.y-0.5);
