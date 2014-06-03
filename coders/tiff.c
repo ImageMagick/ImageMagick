64a45eef99409c95ad49cf72fc90dd41b3cfb761
urban-warrior <urban-warrior@users.noreply.github.com>
2014-06-03 11:13:24 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@15950 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/tiff.c
+++ coders/tiff.c
@@ -709,10 +709,15 @@ static void TIFFGetEXIFProperties(TIFF *tiff,Image *image)
       {
         if (exif_info[i].variable_length != 0)
           {
+            int
+              tiff_status;
+
             uint16
               *shorty;
 
-            if (TIFFGetField(tiff,exif_info[i].tag,&sans,&shorty,&sans,&sans) == 1)
+            tiff_status=TIFFGetField(tiff,exif_info[i].tag,&sans,&shorty,&sans,
+              &sans);
+            if (tiff_status == 1)
               (void) FormatLocaleString(value,MaxTextExtent,"%d",shorty[0]);
           }
         else
