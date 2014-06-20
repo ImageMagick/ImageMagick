c216f6ed8d4f7d9ef489e2ccf87b31a504a77f8c
urban-warrior <urban-warrior@users.noreply.github.com>
2014-06-20 20:56:46 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@16048 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/tiff.c
+++ coders/tiff.c
@@ -576,15 +576,18 @@ static void TIFFGetProfiles(TIFF *tiff,Image *image,ExceptionInfo *exception)
 
   length=0;
 #if defined(TIFFTAG_ICCPROFILE)
-  if (TIFFGetField(tiff,TIFFTAG_ICCPROFILE,&length,&profile) == 1)
+  if ((TIFFGetField(tiff,TIFFTAG_ICCPROFILE,&length,&profile) == 1) &&
+      (profile != (unsigned char *) NULL))
     (void) ReadProfile(image,"icc",profile,(ssize_t) length,exception);
 #endif
 #if defined(TIFFTAG_PHOTOSHOP)
-  if (TIFFGetField(tiff,TIFFTAG_PHOTOSHOP,&length,&profile) == 1)
+  if ((TIFFGetField(tiff,TIFFTAG_PHOTOSHOP,&length,&profile) == 1) &&
+      (profile != (unsigned char *) NULL))
     (void) ReadProfile(image,"8bim",profile,(ssize_t) length,exception);
 #endif
 #if defined(TIFFTAG_RICHTIFFIPTC)
-  if (TIFFGetField(tiff,TIFFTAG_RICHTIFFIPTC,&length,&profile) == 1)
+  if ((TIFFGetField(tiff,TIFFTAG_RICHTIFFIPTC,&length,&profile) == 1) &&
+      (profile != (unsigned char *) NULL))
     {
       if (TIFFIsByteSwapped(tiff) != 0)
         TIFFSwabArrayOfLong((uint32 *) profile,(size_t) length);
@@ -592,12 +595,15 @@ static void TIFFGetProfiles(TIFF *tiff,Image *image,ExceptionInfo *exception)
     }
 #endif
 #if defined(TIFFTAG_XMLPACKET)
-  if (TIFFGetField(tiff,TIFFTAG_XMLPACKET,&length,&profile) == 1)
+  if ((TIFFGetField(tiff,TIFFTAG_XMLPACKET,&length,&profile) == 1) &&
+      (profile != (unsigned char *) NULL))
     (void) ReadProfile(image,"xmp",profile,(ssize_t) length,exception);
 #endif
-  if (TIFFGetField(tiff,37724,&length,&profile) == 1)
+  if ((TIFFGetField(tiff,37724,&length,&profile) == 1) &&
+      (profile != (unsigned char *) NULL))
     (void) ReadProfile(image,"tiff:37724",profile,(ssize_t) length,exception);
-  if (TIFFGetField(tiff,34118,&length,&profile) == 1)
+  if ((TIFFGetField(tiff,34118,&length,&profile) == 1) &&
+      (profile != (unsigned char *) NULL))
     (void) ReadProfile(image,"tiff:34118",profile,(ssize_t) length,exception);
 }
 
