2a3f528084db83d64ad4f03fe89954a1236637d2
urban-warrior <urban-warrior@users.noreply.github.com>
2014-06-25 13:13:33 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@16070 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/jpeg.c
+++ coders/jpeg.c
@@ -70,6 +70,7 @@
 #include "MagickCore/monitor.h"
 #include "MagickCore/monitor-private.h"
 #include "MagickCore/option.h"
+#include "MagickCore/option-private.h"
 #include "MagickCore/pixel-accessor.h"
 #include "MagickCore/profile.h"
 #include "MagickCore/property.h"
@@ -1094,16 +1095,13 @@ static Image *ReadJPEGImage(const ImageInfo *image_info,
   JPEGSourceManager(&jpeg_info,image);
   jpeg_set_marker_processor(&jpeg_info,JPEG_COM,ReadComment);
   option=GetImageOption(image_info,"jpeg:skip-profile");
-  if ((option == (const char *) NULL) ||
-      (GlobExpression("ICC",option,MagickTrue) == MagickFalse))
+  if (IsOptionMember("ICC",option) == MagickFalse)
     jpeg_set_marker_processor(&jpeg_info,ICC_MARKER,ReadICCProfile);
-  if ((option == (const char *) NULL) ||
-      (GlobExpression("IPTC",option,MagickTrue) == MagickFalse))
+  if (IsOptionMember("IPTC",option) == MagickFalse)
     jpeg_set_marker_processor(&jpeg_info,IPTC_MARKER,ReadIPTCProfile);
   for (i=1; i < 16; i++)
     if ((i != 2) && (i != 13) && (i != 14))
-      if ((option == (const char *) NULL) ||
-          (GlobExpression("APP",option,MagickTrue) == MagickFalse))
+      if (IsOptionMember("APP",option) == MagickFalse)
         jpeg_set_marker_processor(&jpeg_info,(int) (JPEG_APP0+i),ReadProfile);
   i=(ssize_t) jpeg_read_header(&jpeg_info,TRUE);
   if ((image_info->colorspace == YCbCrColorspace) ||
