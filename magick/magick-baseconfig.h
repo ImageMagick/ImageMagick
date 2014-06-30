4409f78638ed1e11351d13416683c6225f62e57f
urban-warrior <urban-warrior@users.noreply.github.com>
2014-06-30 15:47:49 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@16123 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/magick-baseconfig.h
+++ magick/magick-baseconfig.h
@@ -1203,7 +1203,7 @@
 
 /* Define to the full name and version of this package. */
 #ifndef MAGICKCORE_PACKAGE_STRING
-#define MAGICKCORE_PACKAGE_STRING "ImageMagick 6.8.9-5"
+#define MAGICKCORE_PACKAGE_STRING "ImageMagick 6.8.9-6"
 #endif
 
 /* Define to the one symbol short name of this package. */
@@ -1218,7 +1218,7 @@
 
 /* Define to the version of this package. */
 #ifndef MAGICKCORE_PACKAGE_VERSION
-#define MAGICKCORE_PACKAGE_VERSION "6.8.9-5"
+#define MAGICKCORE_PACKAGE_VERSION "6.8.9-6"
 #endif
 
 /* Define if you have PANGOCAIRO library */
@@ -1430,7 +1430,7 @@
 
 /* Version number of package */
 #ifndef MAGICKCORE_VERSION
-#define MAGICKCORE_VERSION "6.8.9-5"
+#define MAGICKCORE_VERSION "6.8.9-6"
 #endif
 
 /* Define if you have WEBP library */
--- magick/version.h
+++ magick/version.h
@@ -27,11 +27,11 @@ extern "C" {
 */
 #define MagickPackageName "ImageMagick"
 #define MagickCopyright  "Copyright (C) 1999-2014 ImageMagick Studio LLC"
-#define MagickSVNRevision  "16055:16067M"
+#define MagickSVNRevision  "16122:16123M"
 #define MagickLibVersion  0x689
 #define MagickLibVersionText  "6.8.9"
 #define MagickLibVersionNumber  2,0,0
-#define MagickLibAddendum  "-5"
+#define MagickLibAddendum  "-6"
 #define MagickLibInterface  2
 #define MagickLibMinInterface  2
 #if defined(_WINDOWS)
@@ -45,11 +45,11 @@ extern "C" {
 #endif
 #define MagickppLibVersionText  "6.8.9"
 #define MagickppLibVersionNumber  5:0:0
-#define MagickppLibAddendum  "-5"
+#define MagickppLibAddendum  "-6"
 #define MagickppLibInterface  5
 #define MagickppLibMinInterface  5
-#define MagickReleaseDate  "2014-06-26"
-#define MagickChangeDate   "20140625"
+#define MagickReleaseDate  "2014-06-30"
+#define MagickChangeDate   "20140701"
 #define MagickFeatures "DPC OpenMP"
 #define MagickDelegates "bzlib djvu mpeg fftw fpx fontconfig freetype jbig jng jpeg lcms lzma openexr pango png ps tiff webp x xml zlib"
 #define MagickHomeURL  "file:///usr/local/share/doc/ImageMagick-6/index.html"
