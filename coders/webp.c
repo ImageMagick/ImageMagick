cd9c89b811977cb80957a6c0300cca99f1e7e941
urban-warrior <urban-warrior@users.noreply.github.com>
2014-08-10 23:23:22 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@16297 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/webp.c
+++ coders/webp.c
@@ -402,10 +402,10 @@ ModuleExport size_t RegisterWEBPImage(void)
 #if defined(MAGICKCORE_WEBP_DELEGATE)
   entry->decoder=(DecodeImageHandler *) ReadWEBPImage;
   entry->encoder=(EncodeImageHandler *) WriteWEBPImage;
-  (void) FormatLocaleString(version,MaxTextExtent,"libwebp %d.%d.%d (%04X)",
+  (void) FormatLocaleString(version,MaxTextExtent,"libwebp %d.%d.%d [%04X]",
     (WebPGetDecoderVersion() >> 16) & 0xff,
     (WebPGetDecoderVersion() >> 8) & 0xff,
-    (WebPGetDecoderVersion() >> 0) & 0xff,WEBP_ENCODER_ABI_VERSION);
+    (WebPGetDecoderVersion() >> 0) & 0xff,WEBP_DECODER_ABI_VERSION);
 #endif
   entry->description=ConstantString("WebP Image Format");
   entry->mime_type=ConstantString("image/x-webp");
