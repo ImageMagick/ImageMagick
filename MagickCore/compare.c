d2a3dc785fdc744f46b288673ec526c1da0c245f
urban-warrior <urban-warrior@users.noreply.github.com>
2014-08-13 10:51:08 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@16303 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/compare.c
+++ MagickCore/compare.c
@@ -111,8 +111,7 @@ static inline MagickBooleanType ValidateImageMorphology(
   /*
     Does the image match the reconstructed image morphology?
   */
-  if ((image->storage_class != reconstruct_image->storage_class) ||
-      (image->alpha_trait != reconstruct_image->alpha_trait) ||
+  if ((image->alpha_trait != reconstruct_image->alpha_trait) ||
       (image->number_channels != reconstruct_image->number_channels))
     return(MagickFalse);
   return(MagickTrue);
