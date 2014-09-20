ad6fc6c7a54431406e802de3feeba5a9f6ab952a
urban-warrior <urban-warrior@users.noreply.github.com>
2014-09-20 13:25:25 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@16498 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/constitute.c
+++ MagickCore/constitute.c
@@ -1036,6 +1036,7 @@ MagickExport MagickBooleanType WriteImage(const ImageInfo *image_info,
          }
     }
   (void) SyncImageProfiles(image);
+  DisassociateImageStream(image);
   option=GetImageOption(image_info,"delegate:bimodal");
   if ((IfMagickTrue(IsStringTrue(option))) &&
       (write_info->page == (char *) NULL) &&
@@ -1062,7 +1063,6 @@ MagickExport MagickBooleanType WriteImage(const ImageInfo *image_info,
     }
   status=MagickFalse;
   temporary=MagickFalse;
-  DisassociateImageStream(image);
   if ((magick_info != (const MagickInfo *) NULL) &&
       (GetMagickSeekableStream(magick_info) != MagickFalse))
     {
