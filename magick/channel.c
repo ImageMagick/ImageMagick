b5dfe84ea972915c35dbf8c671811ec5c0344fb4
urban-warrior <urban-warrior@users.noreply.github.com>
2014-10-03 20:27:33 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@16601 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/channel.c
+++ magick/channel.c
@@ -826,6 +826,7 @@ MagickExport MagickBooleanType SetImageAlphaChannel(Image *image,
       if (status == MagickFalse)
         break;
       image->matte=MagickTrue;
+      image_view=AcquireAuthenticCacheView(image,exception);
       #if defined(MAGICKCORE_OPENMP_SUPPORT)
         #pragma omp parallel for schedule(static,4) shared(status) \
           magick_threads(image,image,image->rows,1)
