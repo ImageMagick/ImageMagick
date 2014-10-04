8bf9fb87b3016fea98f230577532da395c6ab116
urban-warrior <urban-warrior@users.noreply.github.com>
2014-10-04 01:12:09 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@16613 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickWand/operation.c
+++ MagickWand/operation.c
@@ -2561,7 +2561,7 @@ static MagickBooleanType CLISimpleOperatorImage(MagickCLI *cli_wand,
           if ((flags & (RhoValue|SigmaValue)) == 0)
             CLIWandExceptArgBreak(OptionError,"InvalidArgument",option,arg1);
           if ((flags & SigmaValue) == 0)
-            geometry_info.sigma=1.0;
+            geometry_info.sigma=geometry_info.rho-0.5;
           new_image=KuwaharaImage(_image,geometry_info.rho,geometry_info.sigma,
            _exception);
           break;
