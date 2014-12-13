a9c9f21e042d67528dd0a2a7b7ba961efae32276
urban-warrior <urban-warrior@users.noreply.github.com>
2014-12-13 20:46:11 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@17197 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/draw.c
+++ magick/draw.c
@@ -5587,8 +5587,8 @@ static size_t TracePath(PrimitiveInfo *primitive_info,const char *path)
           }
           if (strchr("QqTt",last_attribute) == (char *) NULL)
             {
-              points[0]=points[2];
-              points[1]=points[3];
+              points[0]=point;
+              points[1]=point;
             }
           for (i=0; i < 3; i++)
             (q+i)->point=points[i];
