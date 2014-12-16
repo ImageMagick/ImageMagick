3612e3ba472907a22d57e09bef896c0a28ef04eb
urban-warrior <urban-warrior@users.noreply.github.com>
2014-12-16 18:59:44 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@17291 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/palm.c
+++ coders/palm.c
@@ -519,8 +519,10 @@ static Image *ReadPALMImage(const ImageInfo *image_info,
       }
     if (flags & PALM_HAS_TRANSPARENCY_FLAG)
       {
+        ssize_t index=ConstrainColormapIndex(image,(mask-transparentIndex),
+          exception);
         if (bits_per_pixel != 16)
-          transpix=image->colormap[mask-transparentIndex];
+          transpix=image->colormap[index];
         (void) TransparentPaintImage(image,&transpix,(Quantum) TransparentAlpha,
           MagickFalse,exception);
       }
