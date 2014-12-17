c3723b409bfe442938f3a58a16d4723216d3e1d6
dirk <dirk@git.imagemagick.org>
2014-12-17 07:35:06 +0000

    Resample in Magick++ now uses a Point instead of a Geometry.

--- Magick++/lib/Image.cpp
+++ Magick++/lib/Image.cpp
@@ -3948,27 +3948,14 @@ void Magick::Image::reduceNoise(const double order_)
   ThrowPPException;
 }
 
-void Magick::Image::resample(const Geometry &geometry_)
+void Magick::Image::resample(const Point &density_)
 {
   MagickCore::Image
     *newImage;
 
-  size_t
-    height=rows(),
-    width=columns();
-
-  ssize_t
-    x=0,
-    y=0;
-
-  // Calculate new size.  This code should be supported using binary arguments
-  // in the ImageMagick library.
-  ParseMetaGeometry(static_cast<std::string>(geometry_).c_str(),&x,&y,&width,
-    &height);
-
   GetPPException;
-  newImage=ResampleImage(constImage(),width,height,image()->filter,
-    exceptionInfo);
+  newImage=ResampleImage(constImage(),density_.x(),density_.y(),
+    image()->filter,exceptionInfo);
   replaceImage(newImage);
   ThrowPPException;
 }
