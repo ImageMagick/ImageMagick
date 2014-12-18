ceade3d20b55440572a7d315f89cc9f96b2604ca
dirk <dirk@git.imagemagick.org>
2014-12-18 09:20:38 +0000

    Added new constructor with size and imageSpec to Image class of Magick++. Added read method with size and color to Image class of Magick++.

--- Magick++/lib/Image.cpp
+++ Magick++/lib/Image.cpp
@@ -184,19 +184,30 @@ Magick::Image::Image(const Blob &blob_,const Geometry &size_,
 Magick::Image::Image(const Geometry &size_,const Color &color_)
   : _imgRef(new ImageRef)
 {
-  // xc: prefix specifies an X11 color string
-  std::string imageSpec("xc:");
-  imageSpec+=color_;
-
   try
   {
-    // Set image size
-    size(size_);
-
-    // Initialize, Allocate and Read images
-    read(imageSpec);
+    read(size_,color_);
   }
   catch(const Warning & /*warning_*/)
+  {
+    // FIXME: need a way to report warnings in constructor
+  }
+  catch(const Error & /*error_*/)
+  {
+    // Release resources
+    delete _imgRef;
+    throw;
+  }
+}
+
+Magick::Image::Image(const Geometry &size_,const std::string &imageSpec_)
+  : _imgRef(new ImageRef)
+{
+  try
+  {
+    read(size_,imageSpec_);
+  }
+  catch(const Warning &/*warning_*/)
   {
     // FIXME: need a way to report warnings in constructor
   }
@@ -3884,6 +3895,14 @@ void Magick::Image::read(const Blob &blob_)
   newImage=BlobToImage(imageInfo(),static_cast<const void *>(blob_.data()),
     blob_.length(),exceptionInfo);
   read(newImage,exceptionInfo);
+}
+
+void Magick::Image::read(const Geometry &size_,const Color &color_)
+{
+  // xc: prefix specifies an X11 color string
+  std::string imageSpec("xc:");
+  imageSpec+=color_;
+  read(size_,imageSpec);
 }
 
 void Magick::Image::read(const Blob &blob_,const Geometry &size_)
