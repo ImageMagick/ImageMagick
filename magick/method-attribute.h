7c02933999465babb60a5cbf061682ad803b981d
urban-warrior <urban-warrior@users.noreply.github.com>
2014-12-20 20:36:56 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@17351 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/method-attribute.h
+++ magick/method-attribute.h
@@ -123,14 +123,18 @@ extern "C" {
 #  define magick_unreferenced(x)  /* nothing */
 #endif
 
-#if (defined(__clang__) || (((__GNUC__) > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3)))) && !defined(__apple_build_version__)
+#if !defined(__clang__) && (((__GNUC__) > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3)))
 #  define magick_alloc_size(x)  __attribute__((__alloc_size__(x)))
 #  define magick_alloc_sizes(x,y)  __attribute__((__alloc_size__(x,y)))
-#  define magick_cold_spot  __attribute__((__cold__))
-#  define magick_hot_spot  __attribute__((__hot__))
 #else
 #  define magick_alloc_size(x)  /* nothing */
 #  define magick_alloc_sizes(x,y)  /* nothing */
+#endif
+
+#if defined(__clang__) || (((__GNUC__) > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3)))
+#  define magick_cold_spot  __attribute__((__cold__))
+#  define magick_hot_spot  __attribute__((__hot__))
+#else
 #  define magick_cold_spot
 #  define magick_hot_spot
 #endif
--- wand/method-attribute.h
+++ wand/method-attribute.h
@@ -116,14 +116,18 @@ extern "C" {
 #  define wand_unreferenced(x)  /* nothing */
 #endif
 
-#if (defined(__clang__) || (((__GNUC__) > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3)))) && !defined(__apple_build_version__)
+#if !defined(__clang__) && (((__GNUC__) > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3)))
 #  define wand_alloc_size(x)  __attribute__((__alloc_size__(x)))
 #  define wand_alloc_sizes(x,y)  __attribute__((__alloc_size__(x,y)))
-#  define wand_cold_spot  __attribute__((__cold__))
-#  define wand_hot_spot  __attribute__((__hot__))
 #else
 #  define wand_alloc_size(x)  /* nothing */
 #  define wand_alloc_sizes(x,y)  /* nothing */
+#endif
+
+#if defined(__clang__) || (((__GNUC__) > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3)))
+#  define wand_cold_spot  __attribute__((__cold__))
+#  define wand_hot_spot  __attribute__((__hot__))
+#else
 #  define wand_cold_spot
 #  define wand_hot_spot
 #endif
