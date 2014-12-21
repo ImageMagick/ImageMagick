6fccb65125a5544486fbf04dbdee0349d0e5ff71
glennrp <glennrp@users.noreply.github.com>
2014-12-21 23:56:34 +0000

    Use WidthResource and HeightResource to set png_user_limits().
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@17379 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/png.c
+++ coders/png.c
@@ -2183,8 +2183,10 @@ static Image *ReadOnePNGImage(MngInfo *mng_info,
 #endif
 
 #ifdef PNG_SET_USER_LIMITS_SUPPORTED
-  /* Reject images with more than 10 million rows or columns */
-  png_set_user_limits(ping, 10000000L, 10000000L);
+  /* Reject images with too many rows or columns */
+  png_set_user_limits(ping,
+    (png_uint_32) MagickMin(0x7fffffff,GetMagickResourceLimit(WidthResource)),
+    (png_uint_32) MagickMin(0x7fffffff,GetMagickResourceLimit(HeightResource)));
 #endif /* PNG_SET_USER_LIMITS_SUPPORTED */
 
   /*
