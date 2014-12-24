a28dd2482cfb868e4689faf10c9dcafa2b5f3d88
urban-warrior <urban-warrior@users.noreply.github.com>
2014-12-24 20:44:20 +0000

    Check SeekBlob() offset before we extend the blob data length.
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@17421 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/blob.c
+++ magick/blob.c
@@ -3719,20 +3719,22 @@ MagickExport MagickOffsetType SeekBlob(Image *image,
         if (image->blob->mapped != MagickFalse)
           return(-1);
         else
-          {
-            image->blob->extent=(size_t) (image->blob->offset+
-              image->blob->quantum);
-            image->blob->quantum<<=1;
-            image->blob->data=(unsigned char *) ResizeQuantumMemory(
-              image->blob->data,image->blob->extent+1,
-              sizeof(*image->blob->data));
-            (void) SyncBlob(image);
-            if (image->blob->data == (unsigned char *) NULL)
-              {
-                (void) DetachBlob(image->blob);
-                return(-1);
-              }
-          }
+          if (image->blob->offset >= (MagickOffsetType)
+              ((off_t) image->blob->extent))
+            {
+              image->blob->extent=(size_t) (image->blob->offset+
+                image->blob->quantum);
+              image->blob->quantum<<=1;
+              image->blob->data=(unsigned char *) ResizeQuantumMemory(
+                image->blob->data,image->blob->extent+1,
+                sizeof(*image->blob->data));
+              (void) SyncBlob(image);
+              if (image->blob->data == (unsigned char *) NULL)
+                {
+                  (void) DetachBlob(image->blob);
+                  return(-1);
+                }
+            }
       break;
     }
   }
