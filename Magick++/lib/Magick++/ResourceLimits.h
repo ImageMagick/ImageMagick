71e700b2d0bbb6467883ceb82a9da20684f5e98b
dlemstra <dlemstra@users.noreply.github.com>
2014-12-24 00:51:44 +0000

    Added methods to ResourceLimits class for the new ResourceType's.
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@17417 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- Magick++/lib/Magick++/ResourceLimits.h
+++ Magick++/lib/Magick++/ResourceLimits.h
@@ -26,6 +26,18 @@ namespace Magick
     static void disk(const MagickSizeType limit_);
     static MagickSizeType disk(void);
 
+    // The maximum number of open pixel cache files. When this limit is
+    // exceeded, any subsequent pixels cached to disk are closed and reopened
+    // on demand. This behavior permits a large number of images to be accessed
+    // simultaneously on disk, but with a speed penalty due to repeated
+    // open/close calls.
+    static void file(const MagickSizeType limit_);
+    static MagickSizeType file(void);
+
+    // The maximum height of an image.
+    static void height(const MagickSizeType limit_);
+    static MagickSizeType height(void);
+
     // Pixel cache limit in bytes.  Once this memory limit is exceeded,
     // all subsequent pixels cache operations are to/from disk.
     static void map(const MagickSizeType limit_);
@@ -40,6 +52,15 @@ namespace Magick
     // Limits the number of threads used in multithreaded operations.
     static void thread(const MagickSizeType limit_);
     static MagickSizeType thread(void);
+
+    // Periodically yield the CPU for at least the time specified in
+    // milliseconds.
+    static void throttle(const MagickSizeType limit_);
+    static MagickSizeType throttle(void);
+
+    // The maximum width of an image.
+    static void width(const MagickSizeType limit_);
+    static MagickSizeType width(void);
 
   private:
     ResourceLimits(void);
