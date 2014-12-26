09ee8ad0257465701592c76666194c81b9251c30
dirk <dirk@git.imagemagick.org>
2014-12-26 22:17:54 +0000

    Code cleanup.

--- Magick++/lib/Thread.cpp
+++ Magick++/lib/Thread.cpp
@@ -19,34 +19,39 @@ Magick::MutexLock::MutexLock(void)
   // POSIX threads
   : _mutex()
 {
-  ::pthread_mutexattr_t attr;
-  int sysError;
-  if ( (sysError = ::pthread_mutexattr_init( &attr )) == 0 )
-    if ( (sysError = ::pthread_mutex_init( &_mutex, &attr )) == 0 )
+  ::pthread_mutexattr_t
+    attr;
+
+  int
+    sysError;
+
+  if ((sysError=::pthread_mutexattr_init(&attr)) == 0)
+    if ((sysError=::pthread_mutex_init(&_mutex,&attr)) == 0)
       {
-        ::pthread_mutexattr_destroy( &attr );
+        ::pthread_mutexattr_destroy(&attr);
         return;
       }
-  throwExceptionExplicit( OptionError, "mutex initialization failed",
-                          strerror(sysError) );
+  throwExceptionExplicit(OptionError,"mutex initialization failed",
+    strerror(sysError));
 }
 #else
 #if defined(_VISUALC_) && defined(_MT)
 // Win32 threads
   : _mutex()
 {
-  SECURITY_ATTRIBUTES security;
+  SECURITY_ATTRIBUTES
+    security;
 
   /* Allow the semaphore to be inherited */
-  security.nLength = sizeof(security);
-  security.lpSecurityDescriptor = NULL;
-  security.bInheritHandle = TRUE;
+  security.nLength=sizeof(security);
+  security.lpSecurityDescriptor=(LPVOID) NULL;
+  security.bInheritHandle=TRUE;
 
   /* Create the semaphore, with initial value signaled */
-  _mutex.id = ::CreateSemaphore(&security, 1, MAXSEMLEN, NULL);
-  if ( _mutex.id != NULL )
+  _mutex.id=::CreateSemaphore(&security,1,MAXSEMLEN,(LPCSTR) NULL);
+  if (_mutex.id != (HANDLE) NULL)
     return;
-  throwExceptionExplicit( OptionError, "mutex initialization failed" );
+  throwExceptionExplicit(OptionError,"mutex initialization failed");
 }
 #else
 // Threads not supported
@@ -59,16 +64,18 @@ Magick::MutexLock::MutexLock(void)
 Magick::MutexLock::~MutexLock(void)
 {
 #if defined(MAGICKCORE_HAVE_PTHREAD)
-  int sysError;
-  if ( (sysError = ::pthread_mutex_destroy( &_mutex )) == 0 )
+  int
+    sysError;
+
+  if ((sysError=::pthread_mutex_destroy(&_mutex)) == 0)
     return;
-  throwExceptionExplicit( OptionError, "mutex destruction failed",
-                          strerror(sysError) );
+  throwExceptionExplicit(OptionError,"mutex destruction failed",
+    strerror(sysError));
 #endif
 #if defined(_MT) && defined(_VISUALC_)
-  if ( ::CloseHandle(_mutex.id) != 0 )
+  if (::CloseHandle(_mutex.id) != 0)
     return;
-  throwExceptionExplicit( OptionError, "mutex destruction failed" );
+  throwExceptionExplicit(OptionError,"mutex destruction failed");
 #endif
 }
 
@@ -76,16 +83,18 @@ Magick::MutexLock::~MutexLock(void)
 void Magick::MutexLock::lock(void)
 {
 #if defined(MAGICKCORE_HAVE_PTHREAD)
-  int sysError;
-  if ( (sysError = ::pthread_mutex_lock( &_mutex )) == 0)
+  int
+    sysError;
+
+  if ((sysError=::pthread_mutex_lock(&_mutex)) == 0)
     return;
-  throwExceptionExplicit( OptionError, "mutex lock failed",
-                          strerror(sysError));
+  throwExceptionExplicit(OptionError,"mutex lock failed",
+    strerror(sysError));
 #endif
 #if defined(_MT) && defined(_VISUALC_)
   if (WaitForSingleObject(_mutex.id,INFINITE) != WAIT_FAILED)
     return;
-  throwExceptionExplicit( OptionError, "mutex lock failed" );
+  throwExceptionExplicit(OptionError,"mutex lock failed");
 #endif
 }
 
@@ -93,15 +102,17 @@ void Magick::MutexLock::lock(void)
 void Magick::MutexLock::unlock(void)
 {
 #if defined(MAGICKCORE_HAVE_PTHREAD)
-  int sysError;
-  if ( (sysError = ::pthread_mutex_unlock( &_mutex )) == 0)
+  int
+    sysError;
+
+  if ((sysError=::pthread_mutex_unlock(&_mutex)) == 0)
     return;
-  throwExceptionExplicit( OptionError, "mutex unlock failed",
-                          strerror(sysError) );
+  throwExceptionExplicit(OptionError,"mutex unlock failed",
+    strerror(sysError));
 #endif
 #if defined(_MT) && defined(_VISUALC_)
-  if ( ReleaseSemaphore(_mutex.id, 1, NULL) == TRUE )
+  if (ReleaseSemaphore(_mutex.id,1,(LPLONG) NULL) == TRUE)
     return;
-  throwExceptionExplicit( OptionError, "mutex unlock failed" );
+  throwExceptionExplicit(OptionError,"mutex unlock failed");
 #endif
 }
