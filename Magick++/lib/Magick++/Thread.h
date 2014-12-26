7a3d5ccb352d4e95b9538f1d2c9933077a00d969
dlemstra <dlemstra@users.noreply.github.com>
2014-12-26 22:17:54 +0000

    Code cleanup.
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@17452 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- Magick++/lib/Magick++/Thread.h
+++ Magick++/lib/Magick++/Thread.h
@@ -15,8 +15,9 @@
 #if defined(_VISUALC_)
 #include <windows.h>
 #if defined(_MT)
-struct win32_mutex {
-	HANDLE id;
+struct win32_mutex
+{
+  HANDLE id;
 };
 
 // This is a binary semphore -- increase for a counting semaphore
@@ -34,6 +35,7 @@ namespace Magick
   class MagickPPExport MutexLock
   {
   public:
+
     // Default constructor
     MutexLock(void);
 
@@ -49,16 +51,16 @@ namespace Magick
   private:
 
     // Don't support copy constructor
-    MutexLock ( const MutexLock& original_ );
+    MutexLock(const MutexLock& original_);
     
     // Don't support assignment
-    MutexLock& operator = ( const MutexLock& original_ );
+    MutexLock& operator=(const MutexLock& original );
 
 #if defined(MAGICKCORE_HAVE_PTHREAD)
-    pthread_mutex_t  _mutex;
+    pthread_mutex_t \ _mutex;
 #endif
 #if defined(_MT) && defined(_VISUALC_)
-    win32_mutex  _mutex;
+    win32_mutex _mutex;
 #endif
   };
 
@@ -66,35 +68,37 @@ namespace Magick
   class MagickPPExport Lock
   {
   public:
+
     // Construct with mutex lock (locks mutex)
-    Lock( MutexLock *mutexLock_ );
+    Lock(MutexLock *mutexLock_);
 
     // Destrutor (unlocks mutex)
-    ~Lock( void );
+    ~Lock(void);
+
   private:
 
     // Don't support copy constructor
-    Lock ( const Lock& original_ );
-    
+    Lock(const Lock& original_);
+
     // Don't support assignment
-    Lock& operator = ( const Lock& original_ );
+    Lock& operator=(const Lock& original_);
 
     MutexLock* _mutexLock;
   };
 }
 
 // Construct with mutex lock (locks mutex)
-inline Magick::Lock::Lock( MutexLock *mutexLock_ )
+inline Magick::Lock::Lock(MutexLock *mutexLock_)
   : _mutexLock(mutexLock_)
 {
   _mutexLock->lock();
 }
 
 // Destrutor (unlocks mutex)
-inline Magick::Lock::~Lock( void )
+inline Magick::Lock::~Lock(void)
 {
   _mutexLock->unlock();
-  _mutexLock=0;
+  _mutexLock=(MutexLock*) NULL;
 }
 
 #endif // Magick_Thread_header
