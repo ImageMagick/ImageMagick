29d8138a0af4a65104a98226ba47ebad589b8a5c
dirk <dirk@git.imagemagick.org>
2014-12-29 11:53:12 +0000

    Set wsaData to NULL in NTWindowsGenesis.

--- MagickCore/nt-base.c
+++ MagickCore/nt-base.c
@@ -2667,7 +2667,7 @@ MagickPrivate void NTWindowsTerminus(void)
   if (wsaData != (WSADATA *) NULL)
     {
       WSACleanup();
-      (void *) RelinquishMagickMemory((void *) wsaData);
+      wsaData=(WSADATA *) RelinquishMagickMemory((void *) wsaData);
     }
   UnlockSemaphoreInfo(winsock_semaphore);
   RelinquishSemaphoreInfo(&winsock_semaphore);
