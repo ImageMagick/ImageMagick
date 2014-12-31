15a061373c2c7651c9ce2bae4952520eab08c163
urban-warrior <urban-warrior@users.noreply.github.com>
2014-12-31 14:21:00 +0000

    http://www.imagemagick.org/discourse-server/viewtopic.php?f=3&t=26814
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@17526 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/distribute-cache.c
+++ MagickCore/distribute-cache.c
@@ -93,6 +93,10 @@
 #define SOCKET_TYPE SOCKET
 #define MAGICKCORE_HAVE_DISTRIBUTE_CACHE
 #else
+#define CLOSE_SOCKET(socket) 
+#define HANDLER_RETURN_TYPE  void *
+#define HANDLER_RETURN_VALUE  (void *) NULL
+#define SOCKET_TYPE  int
 #undef send
 #undef recv
 #define send(file,buffer,length,flags)  0
