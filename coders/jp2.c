fa6f2a6e3780e6704a8ad0e5361ab7535dd0b824
urban-warrior <urban-warrior@users.noreply.github.com>
2015-01-09 19:23:07 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@17612 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/jp2.c
+++ coders/jp2.c
@@ -204,7 +204,7 @@ static OPJ_SIZE_T JP2ReadHandler(void *buffer,OPJ_SIZE_T length,void *context)
 
   image=(Image *) context;
   count=ReadBlob(image,(ssize_t) length,(unsigned char *) buffer);
-  if (count != (ssize_t) length)
+  if (count == 0)
     return((OPJ_SIZE_T) -1);
   return((OPJ_SIZE_T) count);
 }
