4ef307b82f5ebd3eca6d1b6a7126dfedde3e8731
urban-warrior <urban-warrior@users.noreply.github.com>
2015-01-11 15:07:17 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@17642 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/quantize.c
+++ magick/quantize.c
@@ -3073,8 +3073,7 @@ static void ReduceImageColors(const Image *image,CubeInfo *cube_info)
     span;
 
   cube_info->next_threshold=0.0;
-  if ((cube_info->colors > cube_info->maximum_colors) &&
-      (cube_info->nodes > 256))
+  if (cube_info->colors > cube_info->maximum_colors)
     {
       MagickRealType
         *quantize_error;
@@ -3090,8 +3089,9 @@ static void ReduceImageColors(const Image *image,CubeInfo *cube_info)
             quantize_error);
           qsort(quantize_error,cube_info->nodes,sizeof(MagickRealType),
             MagickRealTypeCompare);
-          cube_info->next_threshold=quantize_error[MagickMax((ssize_t)
-            cube_info->nodes-110*(cube_info->maximum_colors+1)/100,0)];
+          if (cube_info->nodes > (110*(cube_info->maximum_colors+1)/100))
+            cube_info->next_threshold=quantize_error[MagickMax((ssize_t)
+              cube_info->nodes-110*(cube_info->maximum_colors+1)/100,0)];
           quantize_error=(MagickRealType *) RelinquishMagickMemory(
             quantize_error);
         }
