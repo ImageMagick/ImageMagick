dca7c174b94ee7f8aac303d0de087ec32403074f
urban-warrior <urban-warrior@users.noreply.github.com>
2015-01-14 11:33:16 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@17680 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/wmf.c
+++ coders/wmf.c
@@ -808,8 +808,16 @@ static void ipa_device_close(wmfAPI * API)
   wmf_magick_t
     *ddata = WMF_MAGICK_GetData(API);
 
-  DestroyDrawingWand(ddata->draw_wand);
-  DestroyDrawInfo(ddata->draw_info);
+  if (ddata->draw_wand != (DrawingWand *) NULL)
+    {
+      DestroyDrawingWand(ddata->draw_wand);
+      ddata->draw_wand=(DrawingWand *) NULL;
+    }
+  if (ddata->draw_info != (DrawInfo *) NULL)
+    {
+      DestroyDrawInfo(ddata->draw_info);
+      ddata->draw_info=(DrawInfo *)NULL;
+    }
   RelinquishMagickMemory(WMF_MAGICK_GetFontData(API)->ps_name);
 }
 
