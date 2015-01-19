4cac3616c9179dadd586750a584cd2fd111d9a5e
urban-warrior <urban-warrior@users.noreply.github.com>
2015-01-19 01:43:11 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@17734 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/distribute-cache.c
+++ magick/distribute-cache.c
@@ -249,6 +249,7 @@ static int ConnectPixelCacheServer(const char *hostname,const int port,
       CLOSE_SOCKET(client_socket);
       client_socket=(SOCKET_TYPE) (-1);
     }
+  freeaddrinfo(result);
   return(client_socket);
 #else
   magick_unreferenced(hostname);
