316a107ecbef86893c298db346ba6a73d3d68431
urban-warrior <urban-warrior@users.noreply.github.com>
2015-01-24 23:05:59 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@17820 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- coders/ycbcr.c
+++ coders/ycbcr.c
@@ -183,6 +183,7 @@ static Image *ReadYCBCRImage(const ImageInfo *image_info,
       quantum_type=RGBAQuantum;
       image->matte=MagickTrue;
     }
+  pixels=NULL;
   if (image_info->number_scenes != 0)
     while (image->scene < image_info->scene)
     {
