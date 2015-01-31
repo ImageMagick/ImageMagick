531c8155f4d14b197f2b01aef6c84e24fde5e2a5
dirk <dirk@git.imagemagick.org>
2015-01-31 17:13:12 +0000

    Added checks for empty string when setting 'magick'.

--- Magick++/lib/Image.cpp
+++ Magick++/lib/Image.cpp
@@ -1034,10 +1034,18 @@ void Magick::Image::lowlightColor(const Color color_)
 
 void Magick::Image::magick(const std::string &magick_)
 {
+  size_t
+    length;
+
   modifyImage();
 
-  magick_.copy(image()->magick,sizeof(image()->magick)-1);
-  image()->magick[magick_.length()]=0;
+  length=sizeof(image()->magick)-1;
+  if (magick_.length() < length)
+    length=magick_.length();
+
+  if (!magick_.empty())
+    magick_.copy(image()->magick,length);
+  image()->magick[length]=0;
 
   options()->magick(magick_);
 }
