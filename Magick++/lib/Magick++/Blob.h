bed10b164d3d5f161ace78109adb5219446e9218
dirk <dirk@git.imagemagick.org>
2015-02-01 11:46:30 +0000

    Changed copyright

--- Magick++/lib/Magick++/Blob.h
+++ Magick++/lib/Magick++/Blob.h
@@ -1,6 +1,7 @@
 // This may look like C code, but it is really -*- C++ -*-
 //
 // Copyright Bob Friesenhahn, 1999, 2000, 2001, 2002
+// Copyright Dirk Lemstra 2015
 //
 // Reference counted container class for Binary Large Objects (BLOBs)
 //
