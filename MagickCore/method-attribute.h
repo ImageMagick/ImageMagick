f4a09cb6e30730bec34397761354b89542becc34
urban-warrior <urban-warrior@users.noreply.github.com>
2015-03-04 01:03:50 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@18190 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/method-attribute.h
+++ MagickCore/method-attribute.h
@@ -1,12 +1,12 @@
 /*
   Copyright 1999-2015 ImageMagick Studio LLC, a non-profit organization
   dedicated to making software imaging solutions freely available.
-  
+
   You may not use this file except in compliance with the License.
   obtain a copy of the License at
-  
+
     http://www.imagemagick.org/script/license.php
-  
+
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -103,7 +103,7 @@ extern "C" {
 
 #define MagickSignature  0xabacadabUL
 #if !defined(MaxTextExtent)
-# define MaxTextExtent  32768  /* always >= 4096 */
+# define MaxTextExtent  131072  /* always >= 4096 */
 #endif
 
 #if defined(MAGICKCORE_HAVE___ATTRIBUTE__)
