eb49a89fc648cdb51ddf56a877e3916a503408b8
urban-warrior <urban-warrior@users.noreply.github.com>
2015-04-04 12:51:29 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@18304 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/enhance.c
+++ MagickCore/enhance.c
@@ -2869,8 +2869,8 @@ MagickExport MagickBooleanType LinearStretchImage(Image *image,
       break;
   }
   histogram=(double *) RelinquishMagickMemory(histogram);
-  status=LevelImage(image,(double) ScaleMapToQuantum(black),(double)
-    ScaleMapToQuantum(white),1.0,exception);
+  status=LevelImage(image,(double) ScaleMapToQuantum((MagickRealType) black),
+    (double) ScaleMapToQuantum((MagickRealType) white),1.0,exception);
   return(status);
 }
 
