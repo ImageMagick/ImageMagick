10000110383d69781c0e0861c7293345ba3d879f
urban-warrior <urban-warrior@users.noreply.github.com>
2015-04-19 17:26:46 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@18429 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/version.h
+++ magick/version.h
@@ -27,11 +27,11 @@ extern "C" {
 */
 #define MagickPackageName "ImageMagick"
 #define MagickCopyright  "Copyright (C) 1999-2015 ImageMagick Studio LLC"
-#define MagickSVNRevision  "18398"
+#define MagickSVNRevision  "18428:18429M"
 #define MagickLibVersion  0x691
 #define MagickLibVersionText  "6.9.1"
 #define MagickLibVersionNumber  2,0,0
-#define MagickLibAddendum  "-2"
+#define MagickLibAddendum  "-3"
 #define MagickLibInterface  2
 #define MagickLibMinInterface  2
 #if defined(_WINDOWS)
@@ -45,11 +45,11 @@ extern "C" {
 #endif
 #define MagickppLibVersionText  "6.9.1"
 #define MagickppLibVersionNumber  6:0:0
-#define MagickppLibAddendum  "-2"
+#define MagickppLibAddendum  "-3"
 #define MagickppLibInterface  6
 #define MagickppLibMinInterface  6
-#define MagickReleaseDate  "2015-04-14"
-#define MagickChangeDate   "20140409"
+#define MagickReleaseDate  "2015-04-19"
+#define MagickChangeDate   "20150419"
 #define MagickFeatures "DPC OpenMP"
 #define MagickDelegates "bzlib djvu mpeg fftw fpx fontconfig freetype jbig jng jpeg lcms lzma openexr pango png ps tiff webp x xml zlib"
 #define MagickHomeURL  "file:///usr/local/share/doc/ImageMagick-6/index.html"
