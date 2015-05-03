e6e6d2511c8a1f40b718a236bbf72ebaae5ad6c7
urban-warrior <urban-warrior@users.noreply.github.com>
2015-05-03 22:16:54 +0000

    ://bugs.debian.org/cgi-bin/bugreport.cgi?bug=783933
    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/branches/ImageMagick-6@18479 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- magick/version.h
+++ magick/version.h
@@ -27,7 +27,7 @@ extern "C" {
 */
 #define MagickPackageName "ImageMagick"
 #define MagickCopyright  "Copyright (C) 1999-2015 ImageMagick Studio LLC"
-#define MagickSVNRevision  "18476M"
+#define MagickSVNRevision  "18478M"
 #define MagickLibVersion  0x691
 #define MagickLibVersionText  "6.9.1"
 #define MagickLibVersionNumber  2,0,0
