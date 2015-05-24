77551bfd907c23ffc19943b2c2b8b221ee2d18c8
urban-warrior <urban-warrior@users.noreply.github.com>
2015-05-24 22:30:01 +0000

    git-svn-id: https://subversion.imagemagick.org/subversion/ImageMagick/trunk@18632 aa41f4f7-0bf4-0310-aa73-e5a19afd5a74

--- MagickCore/pixel-accessor.h
+++ MagickCore/pixel-accessor.h
@@ -31,20 +31,6 @@ extern "C" {
 
 #undef index
 
-static inline double PerceptibleReciprocal(const double x)
-{
-  double
-    sign;
-
-  /*
-    Return 1/x where x is perceptible (not unlimited or infinitesimal).
-  */
-  sign=x < 0.0 ? -1.0 : 1.0;
-  if ((sign*x) >= MagickEpsilon)
-    return(1.0/x);
-  return(sign/MagickEpsilon);
-}
-
 static inline Quantum ClampPixel(const MagickRealType value)
 { 
   if (value < 0.0f)
@@ -219,6 +205,20 @@ static inline MagickRealType GetPixelInfoChannel(
     case IndexPixelChannel: return(pixel_info->index);
     default: return((MagickRealType) 0.0);
   }
+}
+
+static inline double PerceptibleReciprocal(const double x)
+{
+  double
+    sign;
+
+  /*
+    Return 1/x where x is perceptible (not unlimited or infinitesimal).
+  */
+  sign=x < 0.0 ? -1.0 : 1.0;
+  if ((sign*x) >= MagickEpsilon)
+    return(1.0/x);
+  return(sign/MagickEpsilon);
 }
 
 static inline MagickRealType GetPixelInfoLuma(const PixelInfo *restrict pixel)
