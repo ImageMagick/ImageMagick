7c3786c9ad2254585d427aaf8bcf47df5d065701
dirk <dirk@git.imagemagick.org>
2015-06-06 23:31:15 +0000

    Moved extra space to end of string.

--- magick/version.c
+++ magick/version.c
@@ -219,28 +219,28 @@ MagickExport const char *GetMagickFeatures(void)
 {
   return ""
 #if defined(MAGICKCORE_CIPHER_SUPPORT)
-  " Cipher"
+  "Cipher "
 #endif
 #if defined(MAGICKCORE_WINDOWS_SUPPORT) && defined(_DEBUG)
-  " Debug"
+  "Debug "
 #endif
 #if defined(MAGICKCORE_WINDOWS_SUPPORT) || defined(MAGICKCORE_HAVE_SOCKET) && defined(MAGICKCORE_THREAD_SUPPORT)
-  " DPC"
+  "DPC "
 #endif
 #if defined(MAGICKCORE_HDRI_SUPPORT)
-  " HDRI"
+  "HDRI "
 #endif
 #if defined(MAGICKCORE_BUILD_MODULES) || defined(_DLL)
-  " Modules"
+  "Modules "
 #endif
 #if defined(MAGICKCORE_OPENCL_SUPPORT)
-  " OpenCL"
+  "OpenCL "
 #endif
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
-  " OpenMP"
+  "OpenMP "
 #endif
 #if defined(ZERO_CONFIGURATION_SUPPORT)
-  " Zero-configuration"
+  "Zero-configuration "
 #endif
   ;
 }
