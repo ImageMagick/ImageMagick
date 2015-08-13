3d107317003ecc3229d3555e905453330e340410
dirk <dirk@git.imagemagick.org>
2015-08-13 12:57:57 +0200

    Fixed order of items in 'MagicMap'.

--- MagickCore/magic.c
+++ MagickCore/magic.c
@@ -251,7 +251,11 @@ static int CompareMagickInfoSize(const void *a,const void *b)
 
   ma=(MagicInfo *) a;
   mb=(MagicInfo *) b;
-  return((int) ((mb->offset+mb->length)-(ma->offset+ma->length)));
+
+  if (ma->offset != mb->offset)
+    return((int) (ma->offset-mb->offset));
+
+  return((int) (mb->length-ma->length));
 }
 
 static LinkedListInfo *AcquireMagicCache(const char *filename,
