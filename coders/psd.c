5b42284f44f64328d4bf6caff3b3078fb1f30233
dirk <dirk@git.imagemagick.org>
2015-09-18 08:48:24 +0200

    Setting endianness of image earlier.

--- coders/psd.c
+++ coders/psd.c
@@ -1686,6 +1686,7 @@ static Image *ReadPSDImage(const ImageInfo *image_info,ExceptionInfo *exception)
   /*
     Read image header.
   */
+  image->endian=MSBEndian;
   count=ReadBlob(image,4,(unsigned char *) psd_info.signature);
   psd_info.version=ReadBlobMSBShort(image);
   if ((count == 0) || (LocaleNCompare(psd_info.signature,"8BPS",4) != 0) ||
@@ -1842,7 +1843,6 @@ static Image *ReadPSDImage(const ImageInfo *image_info,ExceptionInfo *exception)
           "  read composite only");
       skip_layers=MagickTrue;
     }
-  image->endian=MSBEndian;
   if (length == 0)
     {
       if (image->debug != MagickFalse)
