a1a36e7b8847c3e92518da4f0ac855e325607313
Cristy <urban-warrior@imagemagick.org>
2015-09-27 13:19:14 -0400

    https://github.com/ImageMagick/ImageMagick/pull/42

--- MagickCore/quantum-private.h
+++ MagickCore/quantum-private.h
@@ -229,23 +229,23 @@ static inline const unsigned char *PushCharPixel(const unsigned char *pixels,
 static inline const unsigned char *PushLongPixel(const EndianType endian,
   const unsigned char *pixels,unsigned int *pixel)
 {
-  register unsigned long
+  register unsigned int
     quantum;
 
   if (endian == LSBEndian)
     {
-      quantum=((unsigned long) *pixels++);
-      quantum|=((unsigned long) *pixels++ << 8);
-      quantum|=((unsigned long) *pixels++ << 16);
-      quantum|=((unsigned long) *pixels++ << 24);
-      *pixel=(unsigned int) (quantum & 0xffffffff);
+      quantum=((unsigned int) *pixels++);
+      quantum|=((unsigned int) *pixels++ << 8);
+      quantum|=((unsigned int) *pixels++ << 16);
+      quantum|=((unsigned int) *pixels++ << 24);
+      *pixel=quantum;
       return(pixels);
     }
-  quantum=((unsigned long) *pixels++ << 24);
-  quantum|=((unsigned long) *pixels++ << 16);
-  quantum|=((unsigned long) *pixels++ << 8);
-  quantum|=((unsigned long) *pixels++);
-  *pixel=(unsigned int) (quantum & 0xffffffff);
+  quantum=((unsigned int) *pixels++ << 24);
+  quantum|=((unsigned int) *pixels++ << 16);
+  quantum|=((unsigned int) *pixels++ << 8);
+  quantum|=((unsigned int) *pixels++);
+  *pixel=quantum;
   return(pixels);
 }
 
