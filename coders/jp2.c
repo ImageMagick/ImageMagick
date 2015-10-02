eb02356c4d2e81ae596c5a908e23dbee8c06a920
Cristy <urban-warrior@imagemagick.org>
2015-10-02 17:43:10 -0400

    Read the whole image @ image.jp2[0] or an individual tile @ image.jp2[1], image.jp2[2]

--- coders/jp2.c
+++ coders/jp2.c
@@ -366,16 +366,16 @@ static Image *ReadJP2Image(const ImageInfo *image_info,ExceptionInfo *exception)
           ThrowReaderException(DelegateError,"UnableToDecodeImageFile");
         }
     }
-  if (image_info->number_scenes != 0)
+  if ((image_info->number_scenes != 0) && (image_info->scene != 0))
     jp2_status=opj_get_decoded_tile(jp2_codec,jp2_stream,jp2_image,
-      (unsigned int) image_info->scene);
+      (unsigned int) image_info->scene-1);
   else
-   if (image->ping == MagickFalse)
-     {
-       jp2_status=opj_decode(jp2_codec,jp2_stream,jp2_image);
-       if (jp2_status != 0)
-         jp2_status=opj_end_decompress(jp2_codec,jp2_stream);
-     }
+    if (image->ping == MagickFalse)
+      {
+        jp2_status=opj_decode(jp2_codec,jp2_stream,jp2_image);
+        if (jp2_status != 0)
+          jp2_status=opj_end_decompress(jp2_codec,jp2_stream);
+      }
   if (jp2_status == 0)
     {
       opj_stream_destroy(jp2_stream);
