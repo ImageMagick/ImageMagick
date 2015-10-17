2cba3f6f68b7106b27911e06e1e283b300e2edfb
Cristy <urban-warrior@imagemagick.org>
2015-10-17 09:46:59 -0400

    ...

--- magick/quantize.c
+++ magick/quantize.c
@@ -1113,7 +1113,7 @@ static void ClosestColor(const Image *image,CubeInfo *cube_info,
               distance+=pixel*pixel;
               if (distance <= cube_info->distance)
                 {
-                  pixel=p->opacity-q->opacity;
+                  pixel=GetPixelAlpha(p)-GetPixelAlpha(q);
                   distance+=pixel*pixel;
                   if (distance <= cube_info->distance)
                     {
