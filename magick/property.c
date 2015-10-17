95b6ee1503bdf06ba554cc0bb556e9f6c4d86003
Cristy <urban-warrior@imagemagick.org>
2015-10-17 17:35:28 -0400

    http://www.imagemagick.org/discourse-server/viewtopic.php?f=3&t=28522

--- magick/property.c
+++ magick/property.c
@@ -81,8 +81,8 @@
 #include "magick/version.h"
 #include "magick/xml-tree.h"
 #if defined(MAGICKCORE_LCMS_DELEGATE)
-#if defined(MAGICKCORE_HAVE_LCMS_LCMS2_H)
-#include <lcms/lcms2.h>
+#if defined(MAGICKCORE_HAVE_LCMS2_LCMS2_H)
+#include <lcms2/lcms2.h>
 #elif defined(MAGICKCORE_HAVE_LCMS2_H)
 #include "lcms2.h"
 #elif defined(MAGICKCORE_HAVE_LCMS_LCMS_H)
