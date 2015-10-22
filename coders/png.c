60658b89de660ce8f5a2e795a0a76d3755bcef15
dirk <dirk@git.imagemagick.org>
2015-10-22 13:21:15 +0200

    Also calling png_set_user_limits when the image is written.

--- coders/png.c
+++ coders/png.c
@@ -9339,6 +9339,15 @@ static MagickBooleanType WriteOnePNGImage(MngInfo *mng_info,
   png_set_benign_errors(ping, 1);
 #endif
 
+#ifdef PNG_SET_USER_LIMITS_SUPPORTED
+  /* Reject images with too many rows or columns */
+  png_set_user_limits(ping,
+    (png_uint_32) MagickMin(0x7fffffffL,
+        GetMagickResourceLimit(WidthResource)),
+    (png_uint_32) MagickMin(0x7fffffffL,
+        GetMagickResourceLimit(HeightResource)));
+#endif /* PNG_SET_USER_LIMITS_SUPPORTED */
+
   /*
     Prepare PNG for writing.
   */
