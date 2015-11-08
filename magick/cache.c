78b0ad62ff31a94dbe19931a03b6737e8d822497
dirk <dirk@git.imagemagick.org>
2015-11-08 12:49:39 +0100

    Fixed bug in SetPixelCacheExtent that made images all black.

--- magick/cache.c
+++ magick/cache.c
@@ -3500,22 +3500,27 @@ static MagickBooleanType SetPixelCacheExtent(Image *image,MagickSizeType length)
         cache_info->cache_filename,cache_info->file,format);
       (void) LogMagickEvent(CacheEvent,GetMagickModule(),"%s",message);
     }
-  if (length != (MagickSizeType) ((MagickOffsetType) length))
-    return(MagickFalse);
   offset=(MagickOffsetType) lseek(cache_info->file,0,SEEK_END);
   if (offset < 0)
     return(MagickFalse);
   if ((MagickSizeType) offset >= length)
-    return(MagickTrue);
-  extent=(MagickOffsetType) length-1;
-  count=WritePixelCacheRegion(cache_info,extent,1,(const unsigned char *) "");
+    count=(MagickOffsetType) 1;
+  else
+    {
+      extent=(MagickOffsetType) length-1;
+      count=WritePixelCacheRegion(cache_info,extent,1,
+        (const unsigned char *) "");
 #if defined(MAGICKCORE_HAVE_POSIX_FALLOCATE)
-  if (cache_info->synchronize != MagickFalse)
-    (void) posix_fallocate(cache_info->file,offset+1,extent-offset);
+      if (cache_info->synchronize != MagickFalse)
+        (void) posix_fallocate(cache_info->file,offset+1,extent-offset);
 #endif
 #if defined(SIGBUS)
-  (void) signal(SIGBUS,CacheSignalHandler);
+      (void) signal(SIGBUS,CacheSignalHandler);
 #endif
+    }
+  offset=(MagickOffsetType) lseek(cache_info->file,0,SEEK_SET);
+  if (offset < 0)
+    return(MagickFalse);
   return(count != (MagickOffsetType) 1 ? MagickFalse : MagickTrue);
 }
 
