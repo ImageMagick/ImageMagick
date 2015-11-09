03f50e2377f7736b9fa447e259f28ea3e6fb9853
dirk <dirk@git.imagemagick.org>
2015-11-09 13:01:47 +0100

    Removed file name from path.

--- magick/configure.c
+++ magick/configure.c
@@ -894,8 +894,8 @@ MagickExport LinkedListInfo *GetConfigurePaths(const char *filename,
         /*
           Search $XDG_CONFIG_HOME/ImageMagick.
         */
-        (void) FormatLocaleString(path,MaxTextExtent,"%s%sImageMagick%s%s",
-          home,DirectorySeparator,DirectorySeparator,filename);
+        (void) FormatLocaleString(path,MaxTextExtent,"%s%sImageMagick%s",
+          home,DirectorySeparator,DirectorySeparator);
         (void) AppendValueToLinkedList(paths,ConstantString(path));
         home=DestroyString(home);
       }
