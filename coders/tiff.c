a648c5afc44deeb1b7efdf880c128e015b537932
Cristy <urban-warrior@imagemagick.org>
2015-12-29 14:08:57 -0500

    https://github.com/ImageMagick/ImageMagick/issues/66

--- coders/tiff.c
+++ coders/tiff.c
@@ -3361,15 +3361,9 @@ static MagickBooleanType WriteTIFFImage(const ImageInfo *image_info,
                   MagickFalse ? PHOTOMETRIC_MINISWHITE :
                   PHOTOMETRIC_MINISBLACK);
                 (void) TIFFSetField(tiff,TIFFTAG_SAMPLESPERPIXEL,1);
-                if ((image_info->depth == 0) &&
-                    (image->alpha_trait == UndefinedPixelTrait) &&
-                    (SetImageMonochrome(image,exception) != MagickFalse))
-                  {
-                    status=SetQuantumDepth(image,quantum_info,1);
-                    if (status == MagickFalse)
-                      ThrowWriterException(ResourceLimitError,
-                        "MemoryAllocationFailed");
-                  }
+                if ((image->depth == 1) &&
+                    (image->alpha_trait == UndefinedPixelTrait))
+                  SetImageMonochrome(image,exception);
               }
             else
               if (image->storage_class == PseudoClass)
