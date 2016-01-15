8f8215e76405efa60bb3850c3ff7ae6f5b63656e
dirk <dirk@git.imagemagick.org>
2016-01-15 01:28:01 +0100

    Added missing call to ConstrainColormapIndex.

--- coders/psd.c
+++ coders/psd.c
@@ -819,7 +819,7 @@ static MagickBooleanType ReadPSDChannelPixels(Image *image,const size_t channels
                   SetPixelIndex(indexes+x,(((unsigned char) pixel) &
                     (0x01 << (7-bit))) != 0 ? 0 : 255);
                   SetPixelRGBO(q,image->colormap+(ssize_t)
-                    GetPixelIndex(indexes+x));
+                    ConstrainColormapIndex(GetPixelIndex(indexes+x)));
                   q++;
                   x++;
                 }
