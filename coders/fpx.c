1cf49b01ea68e1b918c3ebcf8e3698d78b91e263
Cristy <urban-warrior@imagemagick.org>
2016-01-14 19:36:51 -0500

    Eliminate compiler warning

--- coders/fpx.c
+++ coders/fpx.c
@@ -60,6 +60,7 @@
 #include "magick/memory_.h"
 #include "magick/monitor.h"
 #include "magick/monitor-private.h"
+#include "magick/option.h"
 #include "magick/pixel.h"
 #include "magick/pixel-accessor.h"
 #include "magick/property.h"
