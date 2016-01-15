a97a5f301305033499e3ed52fc48e123f13136df
dirk <dirk@git.imagemagick.org>
2016-01-16 00:06:59 +0100

    Rewrite reading pixel values.

--- coders/psd.c
+++ coders/psd.c
@@ -741,6 +741,75 @@ static inline void ReversePSDString(Image *image,char *p,size_t length)
   }
 }
 
+static inline void SetPSDPixel(Image *image,const size_t channels,
+  const ssize_t type,const size_t packet_size,const Quantum pixel,
+  PixelPacket *q,IndexPacket *indexes,ssize_t x,ExceptionInfo *exception)
+{
+  if (image->storage_class == PseudoClass)
+    {
+      if (packet_size == 1)
+        SetPixelIndex(indexes+x,ScaleQuantumToChar(pixel));
+      else
+        SetPixelIndex(indexes+x,ScaleQuantumToShort(pixel));
+      SetPixelRGBO(q,image->colormap+(ssize_t)
+        ConstrainColormapIndex(image,GetPixelIndex(indexes+x)));
+      return;
+    }
+  switch (type)
+  {
+    case -1:
+    {
+      SetPixelAlpha(q,pixel);
+      break;
+    }
+    case -2:
+    case 0:
+    {
+      SetPixelRed(q,pixel);
+      if (channels == 1 || type == -2)
+        {
+          SetPixelGreen(q,GetPixelRed(q));
+          SetPixelBlue(q,GetPixelRed(q));
+        }
+      break;
+    }
+    case 1:
+    {
+      if (image->storage_class == PseudoClass)
+        SetPixelAlpha(q,pixel);
+      else
+        SetPixelGreen(q,pixel);
+      break;
+    }
+    case 2:
+    {
+      if (image->storage_class == PseudoClass)
+        SetPixelAlpha(q,pixel);
+      else
+        SetPixelBlue(q,pixel);
+      break;
+    }
+    case 3:
+    {
+      if (image->colorspace == CMYKColorspace)
+        SetPixelIndex(indexes+x,pixel);
+      else
+        if (image->matte != MagickFalse)
+          SetPixelAlpha(q,pixel);
+      break;
+    }
+    case 4:
+    {
+      if ((IssRGBCompatibleColorspace(image->colorspace) != MagickFalse) &&
+          (channels > 3))
+        break;
+      if (image->matte != MagickFalse)
+        SetPixelAlpha(q,pixel);
+      break;
+    }
+  }
+}
+
 static MagickBooleanType ReadPSDChannelPixels(Image *image,const size_t channels,
   const size_t row,const ssize_t type,const unsigned char *pixels,
   ExceptionInfo *exception)
@@ -781,92 +850,29 @@ static MagickBooleanType ReadPSDChannelPixels(Image *image,const size_t channels
         p=PushShortPixel(MSBEndian,p,&nibble);
         pixel=ScaleShortToQuantum(nibble);
       }
-    switch (type)
-    {
-      case -1:
+    if (image->depth > 1)
       {
-        SetPixelAlpha(q,pixel);
-        break;
+        SetPSDPixel(image,channels,type,packet_size,pixel,q++,indexes,x,
+          exception);
       }
-      case -2:
-      case 0:
+    else
       {
-        SetPixelRed(q,pixel);
-        if (channels == 1 || type == -2)
-          {
-            SetPixelGreen(q,GetPixelRed(q));
-            SetPixelBlue(q,GetPixelRed(q));
-          }
-        if (image->storage_class == PseudoClass)
-          {
-            if (packet_size == 1)
-              SetPixelIndex(indexes+x,ScaleQuantumToChar(pixel));
-            else
-              SetPixelIndex(indexes+x,ScaleQuantumToShort(pixel));
-            SetPixelRGBO(q,image->colormap+(ssize_t)
-              ConstrainColormapIndex(image,GetPixelIndex(indexes+x)));
-            if (image->depth == 1)
-              {
-                ssize_t
-                  bit,
-                  number_bits;
+        ssize_t
+          bit,
+          number_bits;
 
-                number_bits=image->columns-x;
-                if (number_bits > 8)
-                  number_bits=8;
-                for (bit=0; bit < number_bits; bit++)
-                {
-                  SetPixelIndex(indexes+x,(((unsigned char) pixel) &
-                    (0x01 << (7-bit))) != 0 ? 0 : 255);
-                  SetPixelRGBO(q,image->colormap+(ssize_t)
-                    ConstrainColormapIndex(image,GetPixelIndex(indexes+x)));
-                  q++;
-                  x++;
-                }
-                x--;
-                continue;
-              }
-          }
-        break;
+        number_bits=image->columns-x;
+        if (number_bits > 8)
+          number_bits=8;
+        for (bit=0; bit < number_bits; bit++)
+        {
+          SetPSDPixel(image,channels,type,packet_size,pixel,q++,indexes,x++,
+            exception);
+        }
+        if (x != image->columns)
+          x--;
+        continue;
       }
-      case 1:
-      {
-        if (image->storage_class == PseudoClass)
-          SetPixelAlpha(q,pixel);
-        else
-          SetPixelGreen(q,pixel);
-        break;
-      }
-      case 2:
-      {
-        if (image->storage_class == PseudoClass)
-          SetPixelAlpha(q,pixel);
-        else
-          SetPixelBlue(q,pixel);
-        break;
-      }
-      case 3:
-      {
-        if (image->colorspace == CMYKColorspace)
-          SetPixelIndex(indexes+x,pixel);
-        else
-          if (image->matte != MagickFalse)
-            SetPixelAlpha(q,pixel);
-        break;
-      }
-      case 4:
-      {
-        if ((IssRGBCompatibleColorspace(image->colorspace) != MagickFalse) &&
-            (channels > 3))
-          break;
-        if (image->matte != MagickFalse)
-          SetPixelAlpha(q,pixel);
-        break;
-      }
-      default:
-        break;
-    }
-    q++;
   }
   return(SyncAuthenticPixels(image,exception));
 }
