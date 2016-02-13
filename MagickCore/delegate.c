f82a2e032b11725550b010cbb2ef02ee432f8b90
Cristy <urban-warrior@imagemagick.org>
2016-02-13 08:35:17 -0500

    http://www.imagemagick.org/discourse-server/viewtopic.php?f=3&t=29141

--- MagickCore/delegate.c
+++ MagickCore/delegate.c
@@ -1118,11 +1118,11 @@ MagickExport MagickBooleanType InvokeDelegate(ImageInfo *image_info,
     status,
     temporary;
 
-  register ssize_t
-    i;
-
   PolicyRights
     rights;
+
+  register ssize_t
+    i;
 
   /*
     Get delegate.
@@ -1331,7 +1331,8 @@ MagickExport MagickBooleanType InvokeDelegate(ImageInfo *image_info,
         if (CopyDelegateFile(image->filename,input_filename,MagickFalse) == MagickFalse)
           (void) RelinquishUniqueFileResource(input_filename);
       }
-    if (CopyDelegateFile(image_info->filename,output_filename,MagickTrue) == MagickFalse)
+    if ((strcmp(input_filename,output_filename) != 0) &&
+        (CopyDelegateFile(image_info->filename,output_filename,MagickTrue) == MagickFalse))
       (void) RelinquishUniqueFileResource(output_filename);
     if (image_info->temporary != MagickFalse)
       (void) RelinquishUniqueFileResource(image_info->filename);
