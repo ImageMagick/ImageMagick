f3b483e8b054c50149912523b4773687e18afe25
Cristy <urban-warrior@imagemagick.org>
2016-02-14 09:24:32 -0500

    https://github.com/ImageMagick/ImageMagick/issues/131

--- coders/mat.c
+++ coders/mat.c
@@ -875,7 +875,7 @@ RestoreMSCWarning
       }
 
   /* ----- Load raster data ----- */
-    BImgBuff = (unsigned char *) AcquireQuantumMemory((size_t) (ldblk),sizeof(unsigned char));    /* Ldblk was set in the check phase */
+    BImgBuff = (unsigned char *) AcquireQuantumMemory((size_t) (ldblk),sizeof(double));    /* Ldblk was set in the check phase */
     if (BImgBuff == NULL)
       ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
 
