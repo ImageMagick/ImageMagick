3c5ae6f8a9d732155be2675b15ca7ea13a7e318b
Cristy <urban-warrior@imagemagick.org>
2016-02-14 13:27:22 -0500

    Update version to 6.93

--- PerlMagick/Magick.pm
+++ PerlMagick/Magick.pm
@@ -43,7 +43,7 @@ require AutoLoader;
       ConfigureError FatalErrorException
     );
 
-$VERSION = '6.91';
+$VERSION = '6.93';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
