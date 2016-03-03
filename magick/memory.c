f4f66741d336c873e21659f84ff7a40f1dd88dc9
Cristy <urban-warrior@imagemagick.org>
2016-03-03 11:39:24 -0500

    Eliminate compiler warning

--- magick/memory.c
+++ magick/memory.c
@@ -628,7 +628,11 @@ MagickExport MemoryInfo *AcquireVirtualMemory(const size_t count,
           file=AcquireUniqueFileResource(memory_info->filename);
           if (file != -1)
             {
-              if ((lseek(file,extent-1,SEEK_SET) == (ssize_t) (extent-1)) &&
+              MagickOffsetType
+                offset;
+
+              offset=(MagickOffsetType) lseek(file,extent-1,SEEK_SET);
+              if ((offset == (MagickOffsetType) (extent-1)) &&
                   (write(file,"",1) == 1))
                 {
                   memory_info->blob=MapBlob(file,IOMode,0,extent);
