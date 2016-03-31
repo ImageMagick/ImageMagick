9a527ef00b2a5f9697e9cd1408f571b55c572900
Cristy <urban-warrior@imagemagick.org>
2016-03-31 12:38:35 -0400

    Eliminate compiler exception

--- MagickCore/geometry.c
+++ MagickCore/geometry.c
@@ -363,7 +363,7 @@ MagickExport char *GetPageGeometry(const char *page_geometry)
   } PageInfo;
 
   static const PageInfo
-    const PageSizes[] =
+    PageSizes[] =
     {
       MagickPageSize("4x6", "288x432"),
       MagickPageSize("5x7", "360x504"),
