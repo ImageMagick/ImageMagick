8c2e6b49ddb95b803f6b4accdbd645340921ba5b
Cristy <urban-warrior@imagemagick.org>
2016-04-02 10:14:03 -0400

    https://www.imagemagick.org/discourse-server/viewtopic.php?f=3&t=29357

--- MagickCore/image.c
+++ MagickCore/image.c
@@ -3470,7 +3470,7 @@ MagickExport MagickBooleanType StripImage(Image *image,ExceptionInfo *exception)
   (void) DeleteImageProperty(image,"date:create");
   (void) DeleteImageProperty(image,"date:modify");
   status=SetImageArtifact(image,"png:exclude-chunk",
-    "cHRM,EXIF,gAMA,iCCP,iTXt,sRGB,tEXt,zCCP,zTXt,date");
+    "bKGD,cHRM,EXIF,gAMA,iCCP,iTXt,sRGB,tEXt,zCCP,zTXt,date");
   return(status);
 }
 
