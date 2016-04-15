5408e216f7ded11b93cae34fb40082f18dbae01d
Cristy <urban-warrior@imagemagick.org>
2016-04-15 19:51:57 -0400

    ...

--- coders/xpm.c
+++ coders/xpm.c
@@ -343,8 +343,7 @@ static Image *ReadXPMImage(const ImageInfo *image_info,ExceptionInfo *exception)
     Remove unquoted characters.
   */
   active=MagickFalse;
-  q=xpm_buffer;
-  while (*p != '\0')
+  while (q=xpm_buffer; *p != '\0'; )
   {
     if (*p++ == '"')
       {
