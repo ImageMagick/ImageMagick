9d8c61a14194917058006cf2cd8dc90e503eae4d
Cristy <urban-warrior@imagemagick.org>
2016-04-30 12:10:08 -0400

    Update release to 7.01

--- PerlMagick/Magick.pm
+++ PerlMagick/Magick.pm
@@ -44,7 +44,7 @@ require AutoLoader;
       ConfigureError FatalErrorException
     );
 
-$VERSION = '7.00';
+$VERSION = '7.01';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
