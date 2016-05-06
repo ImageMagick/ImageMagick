908f14a85f2169b3a46094bbf3b1420cf91a10d2
Cristy <urban-warrior@imagemagick.org>
2016-05-06 19:10:48 -0400

    The number of colors is limited to 65535

--- coders/xpm.c
+++ coders/xpm.c
@@ -340,8 +340,9 @@ static Image *ReadXPMImage(const ImageInfo *image_info,ExceptionInfo *exception)
     if (count == 4)
       break;
   }
-  if ((count != 4) || (width == 0) || (width > 10) || (image->columns == 0) ||
-      (image->rows == 0) || (image->colors == 0))
+  if ((count != 4) || (width == 0) || (width > 2) ||
+      (image->columns == 0) || (image->rows == 0) ||
+      (image->colors == 0) || (image->colors > MaxColormapSize))
     {
       xpm_buffer=DestroyString(xpm_buffer);
       ThrowReaderException(CorruptImageError,"ImproperImageHeader");
