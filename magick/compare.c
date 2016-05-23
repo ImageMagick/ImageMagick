42514ae3eb70d163ce3fddbab9804edfce51a86f
Cristy <urban-warrior@imagemagick.org>
2016-05-22 20:12:19 -0400

    ...

--- magick/compare.c
+++ magick/compare.c
@@ -1057,7 +1057,7 @@ static MagickBooleanType GetNormalizedCrossCorrelationDistortion(
     distortion[i]=0.0;
   rows=MagickMax(image->rows,reconstruct_image->rows);
   columns=MagickMax(image->columns,reconstruct_image->columns);
-  area=1.0/((MagickRealType) columns*rows-1);
+  area=1.0/((MagickRealType) columns*rows);
   image_view=AcquireVirtualCacheView(image,exception);
   reconstruct_view=AcquireVirtualCacheView(reconstruct_image,exception);
   for (y=0; y < (ssize_t) rows; y++)
