267052f07009b70056c8afeadbdf7c87ea25e3b5
Cristy <urban-warrior@imagemagick.org>
2016-05-29 16:14:22 -0400

    ...

--- coders/pcx.c
+++ coders/pcx.c
@@ -56,6 +56,7 @@
 #include "magick/list.h"
 #include "magick/magick.h"
 #include "magick/memory_.h"
+#include "magick/memory-private.h"
 #include "magick/monitor.h"
 #include "magick/monitor-private.h"
 #include "magick/pixel-accessor.h"
