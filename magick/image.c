aa1865e7471a54401c4f549f98c39966b0caa09f
Cristy <urban-warrior@imagemagick.org>
2016-06-24 07:19:35 -0400

    ...

--- magick/image.c
+++ magick/image.c
@@ -2587,7 +2587,7 @@ MagickExport MagickBooleanType SetImageExtent(Image *image,const size_t columns,
   const size_t rows)
 {
   if ((columns == 0) || (rows == 0))
-    return(MagickFalse);
+    ThrowBinaryException(ImageError,"NegativeOrZeroImageSize",image->filename);
   image->columns=columns;
   image->rows=rows;
   if (image->depth > (8*sizeof(MagickSizeType)))
