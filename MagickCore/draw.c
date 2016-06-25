3af104dff7977ef0e313d8c5ddad3cba7f90d1a9
dirk <dirk@git.imagemagick.org>
2016-06-25 22:14:34 +0200

    Fixed checking the pointsize.

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -4965,7 +4965,7 @@ MagickExport void GetDrawInfo(const ImageInfo *image_info,DrawInfo *draw_info)
   if (clone_info->density != (char *) NULL)
     draw_info->density=AcquireString(clone_info->density);
   draw_info->text_antialias=clone_info->antialias;
-  if (fabs(clone_info->pointsize) != DrawEpsilon)
+  if (fabs(clone_info->pointsize) >= DrawEpsilon)
     draw_info->pointsize=clone_info->pointsize;
   draw_info->border_color=clone_info->border_color;
   if (clone_info->server_name != (char *) NULL)
