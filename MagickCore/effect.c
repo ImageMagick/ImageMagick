cb3f2429dd4c12e8cc3532816ccbe68548d45349
Cristy <urban-warrior@imagemagick.org>
2016-07-02 17:33:49 -0400

    ...

--- MagickCore/effect.c
+++ MagickCore/effect.c
@@ -1432,8 +1432,6 @@ MagickExport Image *GaussianBlurImage(const Image *image,const double radius,
 static inline MagickRealType GetMeanLuma(const Image *magick_restrict image,
   const double *magick_restrict pixel)
 {
-  if (image->colorspace == GRAYColorspace)
-    return((MagickRealType) pixel[image->channel_map[GrayPixelChannel].offset]);
   return(0.212656f*pixel[image->channel_map[RedPixelChannel].offset]+
     0.715158f*pixel[image->channel_map[GreenPixelChannel].offset]+
     0.072186f*pixel[image->channel_map[BluePixelChannel].offset]);  /* Rec709 */
