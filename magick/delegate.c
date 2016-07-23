c600fee0e026fb9cfe87bdc23324ef80500f9900
Cristy <urban-warrior@imagemagick.org>
2016-07-23 19:09:40 -0400

    https://www.imagemagick.org/discourse-server/viewtopic.php?f=3&t=30151

--- magick/delegate.c
+++ magick/delegate.c
@@ -1892,9 +1892,9 @@ MagickExport MagickBooleanType InvokeDelegate(ImageInfo *image_info,
               count;
 
             /*
-              Wait for input file to 'disappear', or maximum 10 seconds.
+              Wait for input file to 'disappear', or maximum 2 seconds.
             */
-            count=100;
+            count=20;
             while ((count-- > 0) && (access_utf8(image->filename,F_OK) == 0))
               (void) MagickDelay(100);  /* sleep 0.1 seconds */
           }
