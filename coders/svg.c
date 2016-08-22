f33d5b3092256c0b281a8af3863a1d37e3665606
Cristy <urban-warrior@imagemagick.org>
2016-08-22 07:47:04 -0400

    ...

--- coders/svg.c
+++ coders/svg.c
@@ -376,7 +376,7 @@ static char **GetStyleTokens(void *context,const char *style,
     *text,
     **tokens;
 
-  register ssize_t
+  register size_t
     i;
 
   SVGInfo
@@ -394,7 +394,7 @@ static char **GetStyleTokens(void *context,const char *style,
   text=DestroyString(text);
   for (i=0; tokens[i] != (char *) NULL; i++)
     StripStyleTokens(tokens[i]);
-  *number_tokens=(size_t) i;
+  *number_tokens=i;
   return(tokens);
 }
 
@@ -459,7 +459,7 @@ static char **GetTransformTokens(void *context,const char *text,
   (void) CopyMagickString(tokens[i],p,(size_t) (q-p+1));
   StripString(tokens[i++]);
   tokens[i]=(char *) NULL;
-  *number_tokens=(size_t) i;
+  *number_tokens=i;
   return(tokens);
 }
 
