6040173a6da3e3576b31f049a521a615e45be54f
Cristy <urban-warrior@imagemagick.org>
2016-11-26 15:47:17 -0500

    ...

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -1421,7 +1421,7 @@ MagickExport MagickBooleanType DrawClipPath(Image *image,
   clip_mask=CloneImage(image,image->columns,image->rows,MagickTrue,exception);
   if (clip_mask == (Image *) NULL)
     return(MagickFalse);
-  (void) QueryColorCompliance("#0000",AllCompliance,
+  (void) QueryColorCompliance("#ffffff",AllCompliance,
     &clip_mask->background_color,exception);
   clip_mask->background_color.alpha=(MagickRealType) TransparentAlpha;
   (void) SetImageBackgroundColor(clip_mask,exception);
@@ -1430,12 +1430,11 @@ MagickExport MagickBooleanType DrawClipPath(Image *image,
       draw_info->clip_mask);
   clone_info=CloneDrawInfo((ImageInfo *) NULL,draw_info);
   (void) CloneString(&clone_info->primitive,value);
-  (void) QueryColorCompliance("#ffffff",AllCompliance,&clone_info->fill,
+  (void) QueryColorCompliance("#000000",AllCompliance,&clone_info->fill,
     exception);
   if (clone_info->clip_mask != (char *) NULL)
     clone_info->clip_mask=DestroyString(clone_info->clip_mask);
-  status=NegateImage(clip_mask,MagickFalse,exception);
-  status&=DrawImage(clip_mask,clone_info,exception);
+  status=DrawImage(clip_mask,clone_info,exception);
   (void) SetImageMask(image,ReadPixelMask,clip_mask,exception);
   clip_mask=DestroyImage(clip_mask);
   clone_info=DestroyDrawInfo(clone_info);
