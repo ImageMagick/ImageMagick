e6f904cd3fcd4e6ea26d58ba52fb10a17cfbf566
Cristy <urban-warrior@imagemagick.org>
2016-12-03 09:55:19 -0500

    ...

--- coders/json.c
+++ coders/json.c
@@ -321,7 +321,7 @@ static ChannelStatistics *GetLocationStatistics(const Image *image,
       register ssize_t
         i;
 
-      if (GetPixelReadMask(image,p) == 0)
+      if (GetPixelWriteMask(image,p) == 0)
         {
           p+=GetPixelChannels(image);
           continue;
