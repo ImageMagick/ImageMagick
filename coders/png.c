1aeb2b445f51b5f58e4097ac6c9878116a6b02eb
Cristy <urban-warrior@imagemagick.org>
2016-12-24 13:43:32 -0500

    ...

--- coders/png.c
+++ coders/png.c
@@ -3031,7 +3031,7 @@ static Image *ReadOnePNGImage(MngInfo *mng_info,
           Quantum
             scale;
 
-          scale = (Quantum) ((65535UL)/((1UL << ping_file_depth)-1));
+          scale = (Quantum) (65535/((1UL << ping_file_depth)-1));
 
 #if (MAGICKCORE_QUANTUM_DEPTH > 16)
           scale = ScaleShortToQuantum(scale);
