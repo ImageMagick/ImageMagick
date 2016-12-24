5244fd8b7f337a759cb4ffe0f95dd94c8c9aa43d
Cristy <urban-warrior@imagemagick.org>
2016-12-24 14:12:06 -0500

    ...

--- magick/resource.c
+++ magick/resource.c
@@ -67,6 +67,11 @@
 #include "magick/utility-private.h"
 
 /*
+  Define declarations.
+*/
+#define MagickPathTemplate "XXXXXXXXXXXX"
+
+/*
   Typedef declarations.
 */
 typedef struct _ResourceInfo
@@ -402,7 +407,7 @@ MagickExport MagickBooleanType GetPathTemplate(char *path)
   struct stat
     attributes;
 
-  (void) FormatLocaleString(path,MaxTextExtent,"magick-%.20gXXXXXXXXXXXX",
+  (void) FormatLocaleString(path,MaxTextExtent,"magick-%.20g" MagickPathTemplate,
     (double) getpid());
   exception=AcquireExceptionInfo();
   directory=(char *) GetImageRegistry(StringRegistryType,"temporary-path",
@@ -448,11 +453,11 @@ MagickExport MagickBooleanType GetPathTemplate(char *path)
       return(MagickFalse);
     }
   if (directory[strlen(directory)-1] == *DirectorySeparator)
-    (void) FormatLocaleString(path,MaxTextExtent,"%smagick-%.20gXXXXXXXXXXXX",
-      directory,(double) getpid());
+    (void) FormatLocaleString(path,MaxTextExtent,"%smagick-%.20g"
+      MagickPathTemplate,directory,(double) getpid());
   else
     (void) FormatLocaleString(path,MaxTextExtent,
-      "%s%smagick-%.20gXXXXXXXXXXXX",directory,DirectorySeparator,
+      "%s%smagick-%.20g" MagickPathTemplate,directory,DirectorySeparator,
       (double) getpid());
   directory=DestroyString(directory);
 #if defined(MAGICKCORE_WINDOWS_SUPPORT)
@@ -518,7 +523,7 @@ MagickExport int AcquireUniqueFileResource(char *path)
     */
     (void) GetPathTemplate(path);
     key=GetRandomKey(random_info,6);
-    p=path+strlen(path)-12;
+    p=path+strlen(path)-strlen(MagickPathTemplate);
     datum=GetStringInfoDatum(key);
     for (i=0; i < (ssize_t) GetStringInfoLength(key); i++)
     {
@@ -539,8 +544,8 @@ MagickExport int AcquireUniqueFileResource(char *path)
         break;
       }
 #endif
-    key=GetRandomKey(random_info,12);
-    p=path+strlen(path)-12;
+    key=GetRandomKey(random_info,strlen(MagickPathTemplate));
+    p=path+strlen(path)-strlen(MagickPathTemplate);
     datum=GetStringInfoDatum(key);
     for (i=0; i < (ssize_t) GetStringInfoLength(key); i++)
     {
