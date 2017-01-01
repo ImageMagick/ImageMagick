fde7253f98eb7472508c9f020289ed2fbb5837f8
Cristy <urban-warrior@imagemagick.org>
2017-01-01 14:39:55 -0500

    ...

--- coders/pnm.c
+++ coders/pnm.c
@@ -1713,6 +1713,8 @@ static MagickBooleanType WritePNMImage(const ImageInfo *image_info,Image *image)
           {
             packet_size=1;
             (void) CopyMagickString(type,"GRAYSCALE",MaxTextExtent);
+            if (IsMonochromeImage(image,&image->exception) != MagickFalse)
+              (void) CopyMagickString(type,"BLACKANDWHITE",MaxTextExtent);
             break;
           }
           default:
