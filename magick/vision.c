a97b121a90dbf1309ba665efa1025025a11d9f66
Cristy <urban-warrior@imagemagick.org>
2017-01-14 19:12:20 -0500

    ...

--- magick/vision.c
+++ magick/vision.c
@@ -482,7 +482,7 @@ MagickExport Image *ConnectedComponentsImage(const Image *image,
 
         if (status == MagickFalse)
           continue;
-        if ((double) object[i].area > area_threshold)
+        if ((double) object[i].area >= area_threshold)
           continue;
         for (j=0; j < (ssize_t) component_image->colors; j++)
           object[j].census=0;
