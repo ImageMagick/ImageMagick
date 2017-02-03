eea078d097d540778d97f930edda5ff86e894487
Cristy <urban-warrior@imagemagick.org>
2017-02-03 14:21:08 -0500

    ...

--- MagickCore/channel.c
+++ MagickCore/channel.c
@@ -356,32 +356,6 @@ MagickExport Image *ChannelFxImage(const Image *image,const char *expression,
           (void) SetPixelMetaChannels(destination_image,(size_t) (
             destination_channel-GetPixelChannels(destination_image)+1),
             exception);
-        switch (destination_channel)
-        {
-          case RedPixelChannel:
-          case GreenPixelChannel:
-          case BluePixelChannel:
-          case BlackPixelChannel:
-          case IndexPixelChannel:
-            break;
-          case AlphaPixelChannel:
-          {
-            destination_image->alpha_trait=BlendPixelTrait;
-            break;
-          }
-          case ReadMaskPixelChannel:
-          {
-            destination_image->read_mask=MagickTrue;
-            break;
-          }
-          case WriteMaskPixelChannel:
-          {
-            destination_image->write_mask=MagickTrue;
-            break;
-          }
-          default:
-            break;
-        }
         channel_mask=(ChannelType) (channel_mask | ParseChannelOption(token));
         if (((channels >= 1)  || (destination_channel >= 1)) &&
             (IsGrayColorspace(destination_image->colorspace) != MagickFalse))
