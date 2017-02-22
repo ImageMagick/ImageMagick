38430f67724aaf0b0485296134c5bbfb7fcac647
Cristy <urban-warrior@imagemagick.org>
2017-02-21 20:32:12 -0500

    ...

--- MagickCore/cache.c
+++ MagickCore/cache.c
@@ -3461,7 +3461,7 @@ static MagickBooleanType OpenPixelCache(Image *image,const MapMode mode,
   assert(cache_info->signature == MagickCoreSignature);
   if ((AcquireMagickResource(WidthResource,image->columns) == MagickFalse) ||
       (AcquireMagickResource(HeightResource,image->rows) == MagickFalse))
-    ThrowBinaryException(ImageError,"WidthOrHeightExceedsLimit",
+    ThrowBinaryException(ResourceLimitError,"WidthOrHeightExceedsLimit",
       image->filename);
   source_info=(*cache_info);
   source_info.file=(-1);
