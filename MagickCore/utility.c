21dfe7bed4c75d314366d4a7ee3de9c0b045d49c
Dirk Lemstra <dirk@git.imagemagick.org>
2017-04-01 20:08:44 +0200

    Fixed default page size.

--- MagickCore/utility.c
+++ MagickCore/utility.c
@@ -1119,7 +1119,7 @@ MagickPrivate ssize_t GetMagickPageSize(void)
   page_size=NTGetPageSize();
 #endif
   if (page_size <= 0)
-    page_size=16384;
+    page_size=4096;
   return(page_size);
 }
 
