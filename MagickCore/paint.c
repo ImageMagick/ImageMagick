005b0a876a8c18b7c3272636a309187a363ca1c8
Cristy <urban-warrior@imagemagick.org>
2017-04-23 13:39:49 -0400

    https://www.imagemagick.org/discourse-server/viewtopic.php?f=3&t=31783

--- MagickCore/paint.c
+++ MagickCore/paint.c
@@ -320,10 +320,6 @@ MagickExport MagickBooleanType FloodfillPaintImage(Image *image,
     } while (x <= x2);
   }
   status=MagickTrue;
-#if defined(MAGICKCORE_OPENMP_SUPPORT)
-  #pragma omp parallel for schedule(static,4) shared(status) \
-    magick_threads(floodplane_image,image,floodplane_image->rows,1)
-#endif
   for (y=0; y < (ssize_t) image->rows; y++)
   {
     register const Quantum
