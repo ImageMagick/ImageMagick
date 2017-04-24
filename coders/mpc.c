34c5387a30cbcc03ad2b278195df6e18a4bb659c
Cristy <urban-warrior@imagemagick.org>
2017-04-24 09:05:51 -0400

    ...

--- coders/mpc.c
+++ coders/mpc.c
@@ -1467,8 +1467,14 @@ static MagickBooleanType WriteMPCImage(const ImageInfo *image_info,Image *image,
     /*
       Initialize persistent pixel cache.
     */
-    status=PersistPixelCache(image,cache_filename,MagickFalse,&offset,
-      exception);
+    {
+      Image *clone_image=CloneImage(image,0,0,MagickTrue,exception);
+      if (clone_image == (Image *) NULL)
+        ThrowWriterException(CacheError,"UnableToPersistPixelCache");
+      status=PersistPixelCache(image,cache_filename,MagickFalse,&offset,
+        exception);
+      clone_image=DestroyImage(clone_image);
+    }
     if (status == MagickFalse)
       ThrowWriterException(CacheError,"UnableToPersistPixelCache");
     if (GetNextImageInList(image) == (Image *) NULL)
