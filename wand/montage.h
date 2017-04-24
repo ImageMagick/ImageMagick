02a9c28eedbb85da7bd5e522158028662c4600f7
Cristy <urban-warrior@imagemagick.org>
2017-04-24 11:34:58 -0400

    ...

--- wand/montage.h
+++ wand/montage.h
@@ -5,7 +5,7 @@
   You may not use this file except in compliance with the License.
   obtain a copy of the License at
   
-    http://www.imagemagick.org/script/license.php
+    https://www.imagemagick.org/script/license.php
   
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
