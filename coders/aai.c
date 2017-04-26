c52b177e0cb11c896b8cc9525a3184c5c0f322c3
Cristy <urban-warrior@imagemagick.org>
2017-04-26 16:21:23 -0400

    https://github.com/ImageMagick/ImageMagick/issues/444

--- coders/aai.c
+++ coders/aai.c
@@ -173,7 +173,10 @@ static Image *ReadAAIImage(const ImageInfo *image_info,ExceptionInfo *exception)
     {
       count=ReadBlob(image,length,pixels);
       if ((size_t) count != length)
-        ThrowReaderException(CorruptImageError,"UnableToReadImageData");
+        {
+          pixels=(unsigned char *) RelinquishMagickMemory(pixels);
+          ThrowReaderException(CorruptImageError,"UnableToReadImageData");
+        }
       p=pixels;
       q=QueueAuthenticPixels(image,0,y,image->columns,1,exception);
       if (q == (PixelPacket *) NULL)
