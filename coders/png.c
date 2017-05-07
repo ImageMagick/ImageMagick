7fcca7ff26c0579f3010222b9a44063ba0b45182
Dirk Lemstra <dirk@git.imagemagick.org>
2017-05-07 12:17:31 +0200

    Fixed memory leak reported in #476.

--- coders/png.c
+++ coders/png.c
@@ -5579,7 +5579,10 @@ static Image *ReadOneMNGImage(MngInfo* mng_info, const ImageInfo *image_info,
 
             if ((mng_info->mng_width > 65535L) ||
                 (mng_info->mng_height > 65535L))
-              ThrowReaderException(ImageError,"WidthOrHeightExceedsLimit");
+              {
+                chunk=(unsigned char *) RelinquishMagickMemory(chunk);
+                ThrowReaderException(ImageError,"WidthOrHeightExceedsLimit");
+              }
 
             (void) FormatLocaleString(page_geometry,MagickPathExtent,
               "%.20gx%.20g+0+0",(double) mng_info->mng_width,(double)
