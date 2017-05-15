f5910e91b0778e03ded45b9022be8eb8f77942cd
Dirk Lemstra <dirk@git.imagemagick.org>
2017-05-15 21:10:19 +0200

    Added check to prevent image being 0x0 (reported in #489).

--- coders/dds.c
+++ coders/dds.c
@@ -1673,9 +1673,8 @@ static Image *ReadDDSImage(const ImageInfo *image_info,ExceptionInfo *exception)
   /*
     Initialize image structure.
   */
-  if (ReadDDSInfo(image, &dds_info) != MagickTrue) {
+  if (ReadDDSInfo(image, &dds_info) != MagickTrue)
     ThrowReaderException(CorruptImageError,"ImproperImageHeader");
-  }
 
   if (dds_info.ddscaps2 & DDSCAPS2_CUBEMAP)
     cubemap = MagickTrue;
@@ -1771,6 +1770,9 @@ static Image *ReadDDSImage(const ImageInfo *image_info,ExceptionInfo *exception)
 
   if (volume)
     num_images = dds_info.depth;
+
+  if (num_images < 1)
+    ThrowReaderException(CorruptImageError,"ImproperImageHeader");
 
   for (n = 0; n < num_images; n++)
   {
