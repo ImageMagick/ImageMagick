837085e7725f6eb591eb019e299c1ddcf34b9a79
Cristy <urban-warrior@imagemagick.org>
2017-05-23 19:08:23 -0400

    ...

--- coders/miff.c
+++ coders/miff.c
@@ -1089,6 +1089,7 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
       {
         if (image->previous == (Image *) NULL)
           ThrowReaderException(CorruptImageError,"ImproperImageHeader");
+        DeleteImageFromList(&image);
         (void) ThrowMagickException(&image->exception,GetMagickModule(),
           CorruptImageError,"ImproperImageHeader","`%s'",image->filename);
         break;
