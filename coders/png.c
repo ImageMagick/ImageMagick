46e3aabbf8d59a1bdebdbb65acb9b9e0484577d3
Cristy <urban-warrior@imagemagick.org>
2017-06-25 09:23:22 -0400

    ...

--- coders/png.c
+++ coders/png.c
@@ -4305,6 +4305,8 @@ static Image *ReadOneJNGImage(MngInfo *mng_info,
             c;
 
           c=ReadBlobByte(image);
+          if (c == EOF)
+            break;
           chunk[i]=(unsigned char) c;
         }
 
@@ -5164,6 +5166,8 @@ static Image *ReadOneMNGImage(MngInfo* mng_info, const ImageInfo *image_info,
                 c;
 
               c=ReadBlobByte(image);
+              if (c == EOF)
+                break;
               chunk[i]=(unsigned char) c;
             }
 
