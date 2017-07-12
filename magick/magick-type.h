c943e19e878a36d5fd0bcac47b520cd5650afebe
Cristy <urban-warrior@imagemagick.org>
2017-07-12 13:53:49 -0400

    https://github.com/ImageMagick/ImageMagick/issues/493

--- magick/magick-type.h
+++ magick/magick-type.h
@@ -39,14 +39,35 @@ extern "C" {
 #  define MagickULLConstant(c)  (MagickSizeType) (c ## ULL)
 #endif
 
+#if MAGICKCORE_SIZEOF_FLOAT_T == 0
+typedef float MagickFloat_t;
+#elif (MAGICKCORE_SIZEOF_FLOAT_T == MAGICKCORE_SIZEOF_FLOAT)
+typedef float MagickFloat_t;
+#elif (MAGICKCORE_SIZEOF_FLOAT_T == MAGICKCORE_SIZEOF_DOUBLE)
+typedef double MagickFloat_t;
+#elif (MAGICKCORE_SIZEOF_FLOAT_T == MAGICKCORE_SIZEOF_LONG_DOUBLE)
+typedef long double MagickFloat_t;
+#else
+#error Your MagickFloat_t type is neither a float, nor a double, nor a long double
+#endif
+#if MAGICKCORE_SIZEOF_DOUBLE_T == 0
+typedef double MagickDouble_t;
+#elif (MAGICKCORE_SIZEOF_DOUBLE_T == MAGICKCORE_SIZEOF_DOUBLE)
+typedef double MagickDouble_t;
+#elif (MAGICKCORE_SIZEOF_DOUBLE_T == MAGICKCORE_SIZEOF_LONG_DOUBLE)
+typedef long double MagickDouble_t;
+#else
+#error Your MagickDouble_t type is neither a float, nor a double, nor a long double
+#endif
+
+typedef MagickDouble_t MagickRealType;
+
 #if (MAGICKCORE_QUANTUM_DEPTH == 8)
 #define MaxColormapSize  256UL
 #define MaxMap  255UL
-
-typedef double MagickRealType;
 typedef ssize_t SignedQuantum;
 #if defined(MAGICKCORE_HDRI_SUPPORT)
-typedef float Quantum;
+typedef MagickFloat_t Quantum;
 #define QuantumRange  255.0
 #define QuantumFormat  "%g"
 #else
@@ -57,14 +78,9 @@ typedef unsigned char Quantum;
 #elif (MAGICKCORE_QUANTUM_DEPTH == 16)
 #define MaxColormapSize  65536UL
 #define MaxMap  65535UL
-
-/*
-  Float_t is not an ABI type.
-*/
-typedef double MagickRealType;
 typedef ssize_t SignedQuantum;
 #if defined(MAGICKCORE_HDRI_SUPPORT)
-typedef float Quantum;
+typedef MagickFloat_t Quantum;
 #define QuantumRange  65535.0
 #define QuantumFormat  "%g"
 #else
@@ -75,14 +91,9 @@ typedef unsigned short Quantum;
 #elif (MAGICKCORE_QUANTUM_DEPTH == 32)
 #define MaxColormapSize  65536UL
 #define MaxMap  65535UL
-
-/*
-  Double_t is not an ABI type.
-*/
-typedef double MagickRealType;
-typedef double SignedQuantum;
+typedef MagickDouble_T SignedQuantum;
 #if defined(MAGICKCORE_HDRI_SUPPORT)
-typedef double Quantum;
+typedef MagickDouble_T Quantum;
 #define QuantumRange  4294967295.0
 #define QuantumFormat  "%g"
 #else
@@ -94,10 +105,8 @@ typedef unsigned int Quantum;
 #define MAGICKCORE_HDRI_SUPPORT 1
 #define MaxColormapSize  65536UL
 #define MaxMap  65535UL
-
-typedef long double MagickRealType;
-typedef long double SignedQuantum;
-typedef long double Quantum;
+typedef MagickDouble_T SignedQuantum;
+typedef MagickDouble_T Quantum;
 #define QuantumRange  18446744073709551615.0
 #define QuantumFormat  "%g"
 #else
