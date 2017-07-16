30c4f605be7a4bdd092dd0a42b4c2c9b37687ee5
Glenn Randers-Pehrson <glennrp@gmail.com>
2017-07-15 22:08:46 -0400

    Added caNv, eXIf, and pHYs to the list of PNG chunks to be removed
    by the "-strip" option.

--- magick/image.c
+++ magick/image.c
@@ -3533,7 +3533,7 @@ MagickExport MagickBooleanType StripImage(Image *image)
   (void) DeleteImageProperty(image,"date:create");
   (void) DeleteImageProperty(image,"date:modify");
   status=SetImageArtifact(image,"png:exclude-chunk",
-    "bKGD,cHRM,EXIF,gAMA,iCCP,iTXt,sRGB,tEXt,zCCP,zTXt,date");
+    "bKGD,caNv,cHRM,eXIf,gAMA,iCCP,iTXt,pHYs,sRGB,tEXt,zCCP,zTXt,date");
   return(status);
 }
 
