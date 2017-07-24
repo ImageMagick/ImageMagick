75db34b6a4d642cb6f88c792942de27490c900e0
Cristy <urban-warrior@imagemagick.org>
2017-07-24 09:23:29 -0400

    ...

--- coders/mat.c
+++ coders/mat.c
@@ -1363,8 +1363,8 @@ END_OF_READING:
     clone_info = NULL;
   }
   if (logging) (void)LogMagickEvent(CoderEvent,GetMagickModule(),"return");
-  if(image==NULL)
-    ThrowReaderException(CorruptImageError,"ImproperImageHeader");
+  if (image==NULL)
+    ThrowReaderException(CorruptImageError,"ImproperImageHeader")
   else
     if ((image != image2) && (image2 != (Image *) NULL))
       image2=DestroyImage(image2);
