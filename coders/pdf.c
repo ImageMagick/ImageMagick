7e05cdff6d3459f13a2699fc016f77fdea1d7e65
Dirk Lemstra <dirk@git.imagemagick.org>
2017-08-01 20:59:28 +0200

    Fixed execution of Ghostscript when it is executed on the command line.

--- coders/pdf.c
+++ coders/pdf.c
@@ -158,6 +158,19 @@ static MagickBooleanType InvokePDFDelegate(const MagickBooleanType verbose,
   int
     status;
 
+#define ExecuteGhostscriptCommand(command,status) \
+{ \
+  status=ExternalDelegateCommand(MagickFalse,verbose,command,message, \
+    exception); \
+  if (status == 0) \
+    return(MagickTrue); \
+  if (status < 0) \
+    return(MagickFalse); \
+  (void) ThrowMagickException(exception,GetMagickModule(),DelegateError, \
+    "FailedToExecuteCommand","`%s' (%d)",command,status); \
+  return(MagickFalse); \
+}
+
 #if defined(MAGICKCORE_GS_DELEGATE) || defined(MAGICKCORE_WINDOWS_SUPPORT)
 #define SetArgsStart(command,args_start) \
   if (args_start == (const char *) NULL) \
@@ -171,19 +184,6 @@ static MagickBooleanType InvokePDFDelegate(const MagickBooleanType verbose,
             args_start++; \
         } \
     }
-
-#define ExecuteGhostscriptCommand(command,status) \
-{ \
-  status=ExternalDelegateCommand(MagickFalse,verbose,command,message, \
-    exception); \
-  if (status == 0) \
-    return(MagickTrue); \
-  if (status < 0) \
-    return(MagickFalse); \
-  (void) ThrowMagickException(exception,GetMagickModule(),DelegateError, \
-    "FailedToExecuteCommand","`%s' (%d)",command,status); \
-  return(MagickFalse); \
-}
 
   char
     **argv,
@@ -287,9 +287,7 @@ static MagickBooleanType InvokePDFDelegate(const MagickBooleanType verbose,
     errors=DestroyString(errors);
   return(MagickTrue);
 #else
-  status=ExternalDelegateCommand(MagickFalse,verbose,command,(char *) NULL,
-    exception);
-  return(status == 0 ? MagickTrue : MagickFalse);
+  ExecuteGhostscriptCommand(command,status);
 #endif
 }
 
