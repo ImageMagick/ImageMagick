04178de2247e353fc095846784b9a10fefdbf890
Cristy <urban-warrior@imagemagick.org>
2017-08-09 08:01:38 -0400

    https://github.com/ImageMagick/ImageMagick/issues/662

--- coders/mat.c
+++ coders/mat.c
@@ -1330,8 +1330,6 @@ done_reading:
   if (quantum_info != (QuantumInfo *) NULL)
     quantum_info=DestroyQuantumInfo(quantum_info);
 END_OF_READING:
-  if (clone_info)
-    clone_info=DestroyImageInfo(clone_info);
   CloseBlob(image);
 
 
@@ -1349,6 +1347,8 @@ END_OF_READING:
         Image *tmp=p;
         if ((p->rows == 0) || (p->columns == 0)) {
           p=p->previous;
+          if (tmp == image2)
+            image2=(Image *) NULL;
           DeleteImageFromList(&tmp);
         } else {
           image=p;
