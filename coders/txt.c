d06bdeaaf0513f80e3515e2ea8d413af63d45628
Cristy <urban-warrior@imagemagick.org>
2017-09-01 09:35:10 -0400

    ...

--- coders/txt.c
+++ coders/txt.c
@@ -453,7 +453,7 @@ static Image *ReadTXTImage(const ImageInfo *image_info,ExceptionInfo *exception)
       ThrowReaderException(CorruptImageError,"ImproperImageHeader");
     image->columns=width;
     image->rows=height;
-    if ((max_value == 0) || (max_value > 4294967295))
+    if ((max_value == 0) || (max_value > 4294967295U))
       ThrowReaderException(CorruptImageError,"ImproperImageHeader");
     for (depth=1; (GetQuantumRange(depth)+1) < max_value; depth++) ;
     image->depth=depth;
