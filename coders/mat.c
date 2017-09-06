a43f4155ee916fbed080acd534232a9d2396b5b5
Cristy <urban-warrior@imagemagick.org>
2017-09-06 12:07:01 -0400

    https://github.com/ImageMagick/ImageMagick/issues/734

--- coders/mat.c
+++ coders/mat.c
@@ -885,6 +885,7 @@ static Image *ReadMATImage(const ImageInfo *image_info,ExceptionInfo *exception)
      Open image file.
    */
   image = AcquireImage(image_info,exception);
+  image2 = (Image *) NULL;
 
   status = OpenBlob(image_info, image, ReadBinaryBlobMode, exception);
   if (status == MagickFalse)
@@ -959,6 +960,8 @@ MATLAB_KO:
     filepos += MATLAB_HDR.ObjectSize + 4 + 4;
 
     clone_info=CloneImageInfo(image_info);
+    if ((image != image2) && (image2 != (Image *) NULL))
+      image2=DestroyImage(image2);
     image2 = image;
 #if defined(MAGICKCORE_ZLIB_DELEGATE)
     if(MATLAB_HDR.DataType == miCOMPRESSED)
