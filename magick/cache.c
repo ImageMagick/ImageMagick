5fa264f307473db2b5b2eac41e2c638932c04c97
Cristy <urban-warrior@imagemagick.org>
2017-09-16 08:56:34 -0400

    ...

--- magick/cache.c
+++ magick/cache.c
@@ -517,6 +517,8 @@ MagickExport void CacheComponentTerminus(void)
 {
   if (cache_semaphore == (SemaphoreInfo *) NULL)
     ActivateSemaphoreInfo(&cache_semaphore);
+  /* no op-- nothing to destroy */
+  RelinquishSemaphoreInfo(&cache_semaphore);
 }
 
 /*
