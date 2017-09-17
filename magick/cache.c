c4be25e494fc37a4d36704eed4a5500ccfc7afcc
Dirk Lemstra <dirk@git.imagemagick.org>
2017-09-17 10:38:40 +0200

    Fixed call to undefined function.

--- magick/cache.c
+++ magick/cache.c
@@ -518,7 +518,7 @@ MagickExport void CacheComponentTerminus(void)
   if (cache_semaphore == (SemaphoreInfo *) NULL)
     ActivateSemaphoreInfo(&cache_semaphore);
   /* no op-- nothing to destroy */
-  RelinquishSemaphoreInfo(&cache_semaphore);
+  DestroySemaphoreInfo(&cache_semaphore);
 }
 
 /*
