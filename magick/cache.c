42e4d9d427464c694907367666eb0054f4337448
Cristy <urban-warrior@imagemagick.org>
2017-09-22 07:20:54 -0400

    ...

--- magick/cache.c
+++ magick/cache.c
@@ -3683,21 +3683,6 @@ static MagickBooleanType MaskPixelCacheNexus(Image *image,NexusInfo *nexus_info,
 %
 */
 
-#if defined(__cplusplus) || defined(c_plusplus)
-extern "C" {
-#endif
-
-#if defined(SIGBUS)
-static void CacheSignalHandler(int status)
-{
-  ThrowFatalException(CacheFatalError,"UnableToExtendPixelCache");
-}
-#endif
-
-#if defined(__cplusplus) || defined(c_plusplus)
-}
-#endif
-
 static MagickBooleanType OpenPixelCacheOnDisk(CacheInfo *cache_info,
   const MapMode mode)
 {
@@ -3818,9 +3803,6 @@ static MagickBooleanType SetPixelCacheExtent(Image *image,MagickSizeType length)
 #if defined(MAGICKCORE_HAVE_POSIX_FALLOCATE)
       if (cache_info->synchronize != MagickFalse)
         (void) posix_fallocate(cache_info->file,offset+1,extent-offset);
-#endif
-#if defined(SIGBUS)
-      (void) signal(SIGBUS,CacheSignalHandler);
 #endif
     }
   offset=(MagickOffsetType) lseek(cache_info->file,0,SEEK_SET);
