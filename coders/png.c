f1f80bbaa2ee501667f90ab31c00f3b9c31e7a95
Cristy <urban-warrior@imagemagick.org>
2017-09-22 09:15:49 -0400

    https://github.com/ImageMagick/ImageMagick/issues/773

--- coders/png.c
+++ coders/png.c
@@ -4339,22 +4339,22 @@ DestroyJNG(unsigned char *chunk,Image **color_image,
   ImageInfo **alpha_image_info)
 {
   (void) RelinquishMagickMemory(chunk);
-  if (*color_image_info)
+  if (color_image_info && *color_image_info)
   {
     DestroyImageInfo(*color_image_info);
     *color_image_info = (ImageInfo *)NULL;
   }
-  if (*alpha_image_info)
+  if (alpha_image_info && *alpha_image_info)
   {
     DestroyImageInfo(*alpha_image_info);
     *alpha_image_info = (ImageInfo *)NULL;
   }
-  if (*color_image)
+  if (color_image && *color_image)
   {
     DestroyImage(*color_image);
     *color_image = (Image *)NULL;
   }
-  if (*alpha_image)
+  if (alpha_image && *alpha_image)
   {
     DestroyImage(*alpha_image);
     *alpha_image = (Image *)NULL;
