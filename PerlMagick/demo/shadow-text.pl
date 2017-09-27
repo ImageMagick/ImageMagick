dff67bdba6aaea613bf23311712dd4e29723359d
Cristy <urban-warrior@imagemagick.org>
2017-09-27 18:04:03 -0400

    ...

--- PerlMagick/demo/shadow-text.pl
+++ PerlMagick/demo/shadow-text.pl
@@ -7,9 +7,9 @@ use Image::Magick;
 $image=Image::Magick->new(size=>'500x120');
 $image->Read('xc:white');
 $image->Annotate(fill=>'rgba(100,100,100,0.8)',pointsize=>60,
-   text=>'Works like magick!',geometry=>'+30+90');
+  text=>'Works like magick!',geometry=>'+8+90');
 $image->Blur('0x1');
 $image->Annotate(fill=>'red',stroke=>'blue',pointsize=>60,
-  text=>'Works like magick!',geometry=>'+26+86');
+  text=>'Works like magick!',geometry=>'+4+86');
 $image->Write('shadow.gif');
 $image->Write('win:');
