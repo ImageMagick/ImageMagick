d9fe4da559264a4ace417c2fa9f1860d70cde2df
root <356986351@qq.com>
2017-09-28 17:40:50 +0000

    fix multiple null pointer

--- coders/cmyk.c
+++ coders/cmyk.c
@@ -162,7 +162,7 @@ static Image *ReadCMYKImage(const ImageInfo *image_info,
   */
   canvas_image=CloneImage(image,image->extract_info.width,1,MagickFalse,
     exception);
-  if(canvas_image == (Image *) NULL)
+  if (canvas_image == (Image *) NULL)
     ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
   (void) SetImageVirtualPixelMethod(canvas_image,BlackVirtualPixelMethod,
     exception);
