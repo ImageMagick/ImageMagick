d8e4965bed7b4bdf4db13fe6591706b99159de04
root <356986351@qq.com>
2017-09-28 17:40:50 +0000

    fix multiple null pointer

--- coders/ycbcr.c
+++ coders/ycbcr.c
@@ -169,7 +169,7 @@ static Image *ReadYCBCRImage(const ImageInfo *image_info,
   */
   canvas_image=CloneImage(image,image->extract_info.width,1,MagickFalse,
     exception);
-  if(canvas_image == (Image *) NULL)
+  if (canvas_image == (Image *) NULL)
     ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
   (void) SetImageVirtualPixelMethod(canvas_image,BlackVirtualPixelMethod,
     exception);
