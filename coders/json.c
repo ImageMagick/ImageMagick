cf4bb148f7c39d20e6843c2eba24d406580335f4
Cristy <urban-warrior@imagemagick.org>
2017-10-12 20:01:00 -0400

    https://github.com/ImageMagick/ImageMagick/pull/836

--- coders/json.c
+++ coders/json.c
@@ -1420,7 +1420,7 @@ static MagickBooleanType EncodeImageAttributes(Image *image,FILE *file,
       (double) image->page.width,(double) image->page.height,
       (double) image->page.x,(double) image->page.y);
   if ((image->page.x != 0) || (image->page.y != 0))
-    (void) FormatLocaleFile(file,"    \"originGeometry\": %+.20g%+.20g\n",
+    (void) FormatLocaleFile(file,"    \"originGeometry\": %+.20g%+.20g,\n",
       (double) image->page.x,(double) image->page.y);
   JsonFormatLocaleFile(file,"    \"dispose\": %s,\n",
     CommandOptionToMnemonic(MagickDisposeOptions,(ssize_t) image->dispose));
