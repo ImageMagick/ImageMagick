62f18fcc21a0564314b0e31d253aea7b66307793
Cristy <urban-warrior@imagemagick.org>
2017-10-25 16:16:34 -0400

    ...

--- coders/webp.c
+++ coders/webp.c
@@ -510,7 +510,7 @@ static int WebPEncodeWriter(const unsigned char *stream,size_t length,
     *image;
 
   image=(Image *) picture->custom_ptr;
-  return(length != 0 ? (int) WriteBlob(image,length,stream) : 1);
+  return(length != 0 ? (WriteBlob(image,length,stream) == length) : 1);
 }
 
 static MagickBooleanType WriteWEBPImage(const ImageInfo *image_info,
