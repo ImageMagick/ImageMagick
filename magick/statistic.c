07730205cef944432d5adc04717d5201882ec29a
Cristy <urban-warrior@imagemagick.org>
2017-12-08 18:51:47 -0500

    https://www.imagemagick.org/discourse-server/viewtopic.php?f=3&t=33199

--- magick/statistic.c
+++ magick/statistic.c
@@ -1550,6 +1550,12 @@ MagickExport MagickBooleanType GetImageChannelMean(const Image *image,
   const ChannelType channel,double *mean,double *standard_deviation,
   ExceptionInfo *exception)
 {
+  ChannelStatistics
+    *channel_statistics;
+
+  size_t
+    channels;
+
   assert(image != (Image *) NULL);
   assert(image->signature == MagickCoreSignature);
   if (image->debug != MagickFalse)
@@ -1557,6 +1563,55 @@ MagickExport MagickBooleanType GetImageChannelMean(const Image *image,
   channel_statistics=GetImageChannelStatistics(image,exception);
   if (channel_statistics == (ChannelStatistics *) NULL)
     return(MagickFalse);
+  channels=0;
+  channel_statistics[CompositeChannels].mean=0.0;
+  channel_statistics[CompositeChannels].standard_deviation=0.0;
+  if ((channel & RedChannel) != 0)
+    {
+      channel_statistics[CompositeChannels].mean+=
+        channel_statistics[RedChannel].mean;
+      channel_statistics[CompositeChannels].standard_deviation+=
+        channel_statistics[RedChannel].standard_deviation;
+      channels++;
+    }
+  if ((channel & GreenChannel) != 0)
+    {
+      channel_statistics[CompositeChannels].mean+=
+        channel_statistics[GreenChannel].mean;
+      channel_statistics[CompositeChannels].standard_deviation+=
+        channel_statistics[GreenChannel].standard_deviation;
+      channels++;
+    }
+  if ((channel & BlueChannel) != 0)
+    {
+      channel_statistics[CompositeChannels].mean+=
+        channel_statistics[BlueChannel].mean;
+      channel_statistics[CompositeChannels].standard_deviation+=
+        channel_statistics[BlueChannel].standard_deviation;
+      channels++;
+    }
+  if (((channel & OpacityChannel) != 0) && (image->matte != MagickFalse))
+    {
+      channel_statistics[CompositeChannels].mean+=
+        (QuantumRange-channel_statistics[OpacityChannel].mean);
+      channel_statistics[CompositeChannels].standard_deviation+=
+        channel_statistics[OpacityChannel].standard_deviation;
+      channels++;
+    }
+  if (((channel & IndexChannel) != 0) && (image->colorspace == CMYKColorspace))
+    {
+      channel_statistics[CompositeChannels].mean+=
+        channel_statistics[BlackChannel].mean;
+      channel_statistics[CompositeChannels].standard_deviation+=
+        channel_statistics[CompositeChannels].standard_deviation;
+      channels++;
+    }
+  channel_statistics[CompositeChannels].mean/=channels;
+  channel_statistics[CompositeChannels].standard_deviation/=channels;
+  *mean=channel_statistics[CompositeChannels].mean;
+  *standard_deviation=channel_statistics[CompositeChannels].standard_deviation;
+  channel_statistics=(ChannelStatistics *) RelinquishMagickMemory(
+    channel_statistics);
   return(MagickTrue);
 }
 
@@ -2624,6 +2679,17 @@ MagickExport ChannelStatistics *GetImageChannelStatistics(const Image *image,
       channel_statistics[i].mean)*(standard_deviation*standard_deviation*
       standard_deviation*standard_deviation)-3.0;
   }
+  channel_statistics[CompositeChannels].mean=0.0;
+  channel_statistics[CompositeChannels].standard_deviation=0.0;
+  for (i=0; i < (ssize_t) CompositeChannels; i++)
+  {
+    channel_statistics[CompositeChannels].mean+=
+      channel_statistics[i].mean;
+    channel_statistics[CompositeChannels].standard_deviation+=
+      channel_statistics[i].standard_deviation;
+  }
+  channel_statistics[CompositeChannels].mean/=(double) channels;
+  channel_statistics[CompositeChannels].standard_deviation/=(double) channels;
   histogram=(MagickPixelPacket *) RelinquishMagickMemory(histogram);
   if (y < (ssize_t) image->rows)
     channel_statistics=(ChannelStatistics *) RelinquishMagickMemory(
