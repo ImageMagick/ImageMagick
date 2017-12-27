13354f128fa8d80a53fa34160f27740c11bd9d17
Cristy <urban-warrior@imagemagick.org>
2017-12-26 20:06:37 -0500

    ...

--- MagickCore/colorspace.h
+++ MagickCore/colorspace.h
@@ -27,7 +27,7 @@ typedef enum
   UndefinedColorspace,
   CMYColorspace,           /* negated linear RGB colorspace */
   CMYKColorspace,          /* CMY with Black separation */
-  GRAYColorspace,          /* Single Channel greyscale (linear) image */
+  GRAYColorspace,          /* Single Channel greyscale (non-linear) image */
   HCLColorspace,
   HCLpColorspace,
   HSBColorspace,
@@ -57,7 +57,7 @@ typedef enum
   YIQColorspace,
   YPbPrColorspace,
   YUVColorspace,
-  LinearGRAYColorspace
+  LinearGRAYColorspace     /* Single Channel greyscale (linear) image */
 } ColorspaceType;
 
 extern MagickExport ColorspaceType
