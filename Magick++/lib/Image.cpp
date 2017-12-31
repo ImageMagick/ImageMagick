8341444730923574c149994d205459bc4250d9f5
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-31 16:04:22 +0100

    Removed depth restriction.

--- Magick++/lib/Image.cpp
+++ Magick++/lib/Image.cpp
@@ -691,15 +691,9 @@ Magick::Point Magick::Image::density(void) const
 
 void Magick::Image::depth(const size_t depth_)
 {
-  size_t
-    depth = depth_;
-
-  if (depth > MAGICKCORE_QUANTUM_DEPTH)
-    depth=MAGICKCORE_QUANTUM_DEPTH;
-
   modifyImage();
-  image()->depth=depth;
-  options()->depth(depth);
+  image()->depth=depth_;
+  options()->depth(depth_);
 }
 
 size_t Magick::Image::depth(void) const
