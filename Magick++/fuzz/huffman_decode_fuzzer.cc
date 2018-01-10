fc944e1a4e5cb9f5fd5529f271ca616e9d193a96
Alex Gaynor <alex.gaynor@gmail.com>
2018-01-10 08:29:24 -0500

    [fuzz] Use a static initializer to set the memory limit so it only happens once per process (#936)
    * Use a static initializer to set the memory limit so it only happens once per process
    
    * oops

--- Magick++/fuzz/huffman_decode_fuzzer.cc
+++ Magick++/fuzz/huffman_decode_fuzzer.cc
@@ -3,8 +3,11 @@
 #include <Magick++/Blob.h>
 #include <Magick++/Image.h>
 
+#include "utils.cc"
+
+static FuzzingResourceLimits kFuzzLimits;
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  Magick::ResourceLimits::memory(1500000000);
   const Magick::Blob blob(Data, Size);
   Magick::Image image;
   try {
