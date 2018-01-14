ea8faee2778034963576a7e863ec198c30931df3
Dirk Lemstra <dirk@git.imagemagick.org>
2018-01-14 01:26:57 +0100

    Refactored the code.

--- Magick++/fuzz/main.cc
+++ Magick++/fuzz/main.cc
@@ -15,46 +15,44 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size);
 class FuzzingDebugger
 {
 public:
-  bool load(wstring fileName);
-  void start();
+  bool load(wstring fileName)
+  {
+    ifstream
+      file;
+
+    streampos
+      size;
+
+    file = ifstream(fileName, ios::in | ios::binary | ios::ate);
+    if (!file.is_open())
+      return(false);
+
+    size = file.tellg();
+    _size = size;
+    _data = new char[_size];
+    file.seekg(0, ios::beg);
+    file.read(_data, size);
+    file.close();
+
+    encoderFormat.set(wstring(PathFindExtension(fileName.c_str())));
+
+    return(true);
+  }
+
+  void start()
+  {
+    const uint8_t
+      *data;
+
+    data = reinterpret_cast<const uint8_t *>(_data);
+    LLVMFuzzerTestOneInput(data, _size);
+  }
+
 
 private:
   char * _data;
   size_t _size;
 };
-
-bool FuzzingDebugger::load(wstring fileName)
-{
-  ifstream
-    file;
-
-  streampos
-    size;
-
-  file = ifstream(fileName, ios::in | ios::binary | ios::ate);
-  if (!file.is_open())
-    return(false);
-
-  size = file.tellg();
-  _size = size;
-  _data = new char[_size];
-  file.seekg(0, ios::beg);
-  file.read(_data, size);
-  file.close();
-
-  encoderFormat.set(wstring(PathFindExtension(fileName.c_str())));
-
-  return(true);
-}
-
-void FuzzingDebugger::start()
-{
-  const uint8_t
-    *data;
-
-  data = reinterpret_cast<const uint8_t *>(_data);
-  LLVMFuzzerTestOneInput(data, _size);
-}
 
 int wmain(int argc, wchar_t *argv[])
 {
