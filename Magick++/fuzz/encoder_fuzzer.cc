84a37972e8a69737f09260157332bbf7e0c5de92
Alex Gaynor <alex.gaynor@gmail.com>
2018-01-14 11:07:09 -0500

    Move these calls otuside the try block, they should never fail

--- Magick++/fuzz/encoder_fuzzer.cc
+++ Magick++/fuzz/encoder_fuzzer.cc
@@ -14,9 +14,9 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   const Magick::Blob blob(Data, Size);
   Magick::Image image;
+  image.magick(FUZZ_ENCODER);
+  image.fileName(std::string(FUZZ_ENCODER) + ":");
   try {
-    image.magick(FUZZ_ENCODER);
-    image.fileName(std::string(FUZZ_ENCODER) + ":");
     image.read(blob);
   }
   catch (Magick::Exception &e) {
