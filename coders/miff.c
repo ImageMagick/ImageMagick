4e2ff1cfb7478a5021922e92dba03e05c6651854
Dirk Lemstra <dirk@git.imagemagick.org>
2018-01-18 21:17:20 +0100

    Fixed memory leak. Credit to OSS-Fuzz

--- coders/miff.c
+++ coders/miff.c
@@ -900,8 +900,12 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
                     profile=BlobToStringInfo((const void *) NULL,(size_t)
                       StringToLong(options));
                     if (profile == (StringInfo *) NULL)
-                      ThrowReaderException(ResourceLimitError,
-                        "MemoryAllocationFailed");
+                      {
+                        profiles=DestroyLinkedList(profiles,
+                          RelinquishMagickMemory);
+                        ThrowReaderException(ResourceLimitError,
+                          "MemoryAllocationFailed");
+                      }
                     (void) SetImageProfile(image,keyword+8,profile);
                     profile=DestroyStringInfo(profile);
                     break;
--- coders/mpc.c
+++ coders/mpc.c
@@ -617,8 +617,12 @@ static Image *ReadMPCImage(const ImageInfo *image_info,ExceptionInfo *exception)
                     profile=BlobToStringInfo((const void *) NULL,(size_t)
                       StringToLong(options));
                     if (profile == (StringInfo *) NULL)
-                      ThrowReaderException(ResourceLimitError,
-                        "MemoryAllocationFailed");
+                      {
+                        profiles=DestroyLinkedList(profiles,
+                          RelinquishMagickMemory);
+                        ThrowReaderException(ResourceLimitError,
+                          "MemoryAllocationFailed");
+                      }
                     (void) SetImageProfile(image,keyword+8,profile);
                     profile=DestroyStringInfo(profile);
                     break;
