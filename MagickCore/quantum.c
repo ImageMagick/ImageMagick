901c4bce459d42dd8e945c4b24f94fa885e1556c
Cristy <urban-warrior@imagemagick.org>
2018-01-23 09:54:43 -0500

    Eliminate invalid write Credit OSS Fuzz

--- MagickCore/quantum.c
+++ MagickCore/quantum.c
@@ -689,7 +689,7 @@ MagickExport MagickBooleanType SetQuantumDepth(const Image *image,
   if (quantum_info->pixels != (MemoryInfo **) NULL)
     DestroyQuantumPixels(quantum_info);
   quantum=(quantum_info->pad+MaxPixelChannels)*(quantum_info->depth+7)/8;
-  extent=MagickMax(image->columns,image->rows)*quantum;
+  extent=4*MagickMax(image->columns,image->rows)*quantum;
   if ((MagickMax(image->columns,image->rows) != 0) &&
       (quantum != (extent/MagickMax(image->columns,image->rows))))
     return(MagickFalse);
