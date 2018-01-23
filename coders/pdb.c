487f950075ae21b4634a04cb28c2f47f95d62963
Cristy <urban-warrior@imagemagick.org>
2018-01-23 07:48:56 -0500

    Eliminate uninitialized value Credit OSS Fuzz

--- coders/pdb.c
+++ coders/pdb.c
@@ -330,6 +330,7 @@ static Image *ReadPDBImage(const ImageInfo *image_info,ExceptionInfo *exception)
   /*
     Determine if this a PDB image file.
   */
+  (void) ResetMagickMemory(&pdb_info,0,sizeof(pdb_info));
   count=ReadBlob(image,sizeof(pdb_info.name),(unsigned char *) pdb_info.name);
   if (count != sizeof(pdb_info.name))
     ThrowReaderException(CorruptImageError,"ImproperImageHeader");
