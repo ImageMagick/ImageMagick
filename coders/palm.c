ecd8728428d460b13821cc8e80aef671011329c7
Cristy <urban-warrior@imagemagick.org>
2018-01-27 10:18:54 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=5670

--- coders/palm.c
+++ coders/palm.c
@@ -314,6 +314,7 @@ static Image *ReadPALMImage(const ImageInfo *image_info,
     status=SetImageExtent(image,image->columns,image->rows,exception);
     if (status == MagickFalse)
       return(DestroyImageList(image));
+    (void) SetImageBackgroundColor(image,exception);
     bytes_per_row=ReadBlobMSBShort(image);
     flags=ReadBlobMSBShort(image);
     bits_per_pixel=(size_t) ReadBlobByte(image);
