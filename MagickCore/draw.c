8bb46b3381f8440fbf90bd88e1909dc83e089790
Cristy <urban-warrior@imagemagick.org>
2018-01-26 20:38:11 -0500

    Eliminate use of uninitialized value in GetFillAlpha() Credit OSS Fuzz

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -3941,7 +3941,7 @@ static double GetFillAlpha(PolygonInfo *polygon_info,const double mid,
         continue;
       }
     i=(ssize_t) MagickMax((double) p->highwater,1.0);
-    for ( ; i < (ssize_t) p->number_points; i++)
+    for ( ; i < (ssize_t) (p->number_points-1); i++)
       if ((double) y <= p->points[i].y)
         break;
     q=p->points+i-1;
