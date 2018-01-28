2403f745877ac4aa7f35dbc9822060bf594973d2
Cristy <urban-warrior@imagemagick.org>
2018-01-28 17:37:02 -0500

    "-" filename is a standard stream

--- MagickCore/blob.c
+++ MagickCore/blob.c
@@ -3098,7 +3098,7 @@ MagickExport MagickBooleanType OpenBlob(const ImageInfo *image_info,
       if (strchr(type,'b') != (char *) NULL)
         setmode(fileno(blob_info->file_info.file),_O_BINARY);
 #endif
-      blob_info->type=FileStream;
+      blob_info->type=StandardStream;
       blob_info->exempt=MagickTrue;
       return(SetStreamBuffering(image_info,image));
     }
