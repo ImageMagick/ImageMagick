40878651d360ddedcc3330a22e3b6a7ac1d1134b
Alex Gaynor <alex.gaynor@gmail.com>
2018-01-29 07:58:40 -0500

    Ensure we initialize a string correctly
    Data is not guaranteed to be nul terminated.

--- Magick++/fuzz/encoder_xc_fuzzer.cc
+++ Magick++/fuzz/encoder_xc_fuzzer.cc
@@ -9,7 +9,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   // Allow a bit extra to make sure we do proper bounds checking in Magick++
   if (Size > MagickPathExtent)
     return 0;
-  std::string color(reinterpret_cast<const char*>(Data));
+  std::string color(reinterpret_cast<const char*>(Data), Size);
 
   Magick::Image image;
   try {
