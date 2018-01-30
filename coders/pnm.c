23246df8dfdebe0002bf9abfa0a5490238300ad9
Cristy <urban-warrior@imagemagick.org>
2018-01-29 20:11:11 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=5805

--- coders/pnm.c
+++ coders/pnm.c
@@ -428,6 +428,7 @@ static Image *ReadPNMImage(const ImageInfo *image_info,ExceptionInfo *exception)
     status=SetImageExtent(image,image->columns,image->rows,exception);
     if (status == MagickFalse)
       return(DestroyImageList(image));
+    (void) SetImageBackgroundColor(image,exception);
     /*
       Convert PNM pixels to runextent-encoded MIFF packets.
     */
