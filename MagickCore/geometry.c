c32cba466f3f58a6d6f5ebbbaf0297f6617480ba
Cristy <urban-warrior@imagemagick.org>
2018-02-03 08:29:23 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=5900

--- MagickCore/geometry.c
+++ MagickCore/geometry.c
@@ -1400,7 +1400,7 @@ MagickExport MagickStatusType ParseMetaGeometry(const char *geometry,ssize_t *x,
       */
       (void) ParseGeometry(geometry,&geometry_info);
       geometry_ratio=geometry_info.rho;
-      image_ratio=(double) former_width/former_height;
+      image_ratio=(double) former_width*PerceptibleReciprocal(former_height);
       if (geometry_ratio >= image_ratio)
         {
           *width=former_width;
