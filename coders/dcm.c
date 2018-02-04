75b5f2d2059ec0f3ab9d37c67835f1a521cc7cf8
Cristy <urban-warrior@imagemagick.org>
2018-02-04 15:23:51 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=5448

--- coders/dcm.c
+++ coders/dcm.c
@@ -3795,6 +3795,8 @@ static Image *ReadDCMImage(const ImageInfo *image_info,ExceptionInfo *exception)
       tag=(ReadBlobLSBShort(image) << 16) | ReadBlobLSBShort(image);
       (void) tag;
       length=(size_t) ReadBlobLSBLong(image);
+      if (length > GetBlobSize(image))
+        ThrowDCMException(CorruptImageError,"InsufficientImageDataInFile");
       stream_info->offset_count=length >> 2;
       if (stream_info->offset_count != 0)
         {
