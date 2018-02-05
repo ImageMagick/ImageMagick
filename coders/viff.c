12d1001b1758d54311a8dba474961c6cb904bde8
Cristy <urban-warrior@imagemagick.org>
2018-02-04 20:26:55 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=5819

--- coders/viff.c
+++ coders/viff.c
@@ -412,6 +412,8 @@ static Image *ReadVIFFImage(const ImageInfo *image_info,
           default: bytes_per_pixel=1; break;
         }
         image->colors=viff_info.map_columns;
+        if (image->colors > GetBlobSize(image))
+          ThrowReaderException(CorruptImageError,"InsufficientImageDataInFile");
         if (AcquireImageColormap(image,image->colors,exception) == MagickFalse)
           ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
         if (viff_info.map_rows >
