07e656c7089f3b3ae30a71a042fb903c83e077b6
Cristy <urban-warrior@imagemagick.org>
2018-02-05 19:52:34 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6032

--- coders/hdr.c
+++ coders/hdr.c
@@ -711,7 +711,8 @@ static MagickBooleanType WriteHDRImage(const ImageInfo *image_info,Image *image,
   if ((property != (const char *) NULL) &&
       (strchr(property,'\n') == (char *) NULL))
     {
-      count=FormatLocaleString(header,MagickPathExtent,"#%s\n",property);
+      count=FormatLocaleString(header,MagickPathExtent,"#%.*s\n",
+        MagickPathExtent-3,property);
       (void) WriteBlob(image,(size_t) count,(unsigned char *) header);
     }
   property=GetImageProperty(image,"hdr:exposure",exception);
