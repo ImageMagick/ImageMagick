da0010dfafb8fbc5398e42f984405db81784fcda
Cristy <urban-warrior@imagemagick.org>
2018-02-07 16:05:51 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6068

--- coders/miff.c
+++ coders/miff.c
@@ -903,6 +903,9 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
                     length=StringToLong(options);
                     if ((MagickSizeType) length > GetBlobSize(image))
                       {
+                        if (profiles != (LinkedListInfo *) NULL)
+                          profiles=DestroyLinkedList(profiles,
+                            RelinquishMagickMemory);
                         options=DestroyString(options);
                         ThrowReaderException(CorruptImageError,
                           "InsufficientImageDataInFile");
