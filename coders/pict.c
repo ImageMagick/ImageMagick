820e6361927e3bfa346ce085311d001681863a23
Cristy <urban-warrior@imagemagick.org>
2018-02-09 07:49:07 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6162

--- coders/pict.c
+++ coders/pict.c
@@ -928,6 +928,7 @@ static Image *ReadPICTImage(const ImageInfo *image_info,
   status=SetImageExtent(image,image->columns,image->rows,exception);
   if (status == MagickFalse)
     return(DestroyImageList(image));
+  image->alpha_trait=BlendPixelTrait;
   (void) SetImageBackgroundColor(image,exception);
   /*
     Interpret PICT opcodes.
