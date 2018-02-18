466387e400581cc86248b179b45e73186d6e200f
Dirk Lemstra <dirk@git.imagemagick.org>
2018-02-18 19:16:54 +0100

    Also remove the ReadPixelMask.

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -1425,6 +1425,7 @@ MagickExport MagickBooleanType DrawClipPath(Image *image,
   clip_mask=CloneImage(image,image->columns,image->rows,MagickTrue,exception);
   if (clip_mask == (Image *) NULL)
     return(MagickFalse);
+  (void) SetImageMask(clip_mask,ReadPixelMask,(Image *) NULL,exception);
   (void) SetImageMask(clip_mask,WritePixelMask,(Image *) NULL,exception);
   (void) QueryColorCompliance("#0000",AllCompliance,
     &clip_mask->background_color,exception);
