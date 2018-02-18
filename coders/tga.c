eb4d556c30438df00f96be54dd6c8df9f09455b6
Cristy <urban-warrior@imagemagick.org>
2018-02-18 18:42:17 -0500

    ...

--- coders/tga.c
+++ coders/tga.c
@@ -274,8 +274,6 @@ static Image *ReadTGAImage(const ImageInfo *image_info,
           if (image->colors > GetBlobSize(image))
             ThrowReaderException(CorruptImageError,
               "InsufficientImageDataInFile");
-          if (image->colors > ((~0U)/sizeof(*image->colormap)))
-            ThrowReaderException(CorruptImageError,"ImproperImageHeader");
           if (AcquireImageColormap(image,image->colors,exception) == MagickFalse)
             ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
         }
