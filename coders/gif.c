e71d77f4248daa46b00deb3d238e98b6649f4621
Cristy <urban-warrior@imagemagick.org>
2018-02-19 15:34:15 -0500

    ...

--- coders/gif.c
+++ coders/gif.c
@@ -1363,15 +1363,6 @@ static Image *ReadGIFImage(const ImageInfo *image_info,ExceptionInfo *exception)
     if ((image_info->ping != MagickFalse) && (image_info->number_scenes != 0))
       if (image->scene >= (image_info->scene+image_info->number_scenes-1))
         break;
-    /*
-      Decode image.
-    */
-    if (image_info->ping != MagickFalse)
-      status=PingGIFImage(image,exception);
-    else
-      status=DecodeImage(image,opacity,exception);
-    if ((image_info->ping == MagickFalse) && (status == MagickFalse))
-      ThrowGIFException(CorruptImageError,"CorruptImage");
     status=SetImageExtent(image,image->columns,image->rows,exception);
     if (status == MagickFalse)
       {
@@ -1382,6 +1373,15 @@ static Image *ReadGIFImage(const ImageInfo *image_info,ExceptionInfo *exception)
         meta_image=DestroyImage(meta_image);
         return(DestroyImageList(image));
       }
+    /*
+      Decode image.
+    */
+    if (image_info->ping != MagickFalse)
+      status=PingGIFImage(image,exception);
+    else
+      status=DecodeImage(image,opacity,exception);
+    if ((image_info->ping == MagickFalse) && (status == MagickFalse))
+      ThrowGIFException(CorruptImageError,"CorruptImage");
     duration+=image->delay*image->iterations;
     if (image_info->number_scenes != 0)
       if (image->scene >= (image_info->scene+image_info->number_scenes-1))
