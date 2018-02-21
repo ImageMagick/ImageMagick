8c6ce46d7e22a3801cafe6bc8f9aeccc734e9859
Cristy <urban-warrior@imagemagick.org>
2018-02-21 12:37:14 -0500

    ...

--- MagickCore/xml-tree.c
+++ MagickCore/xml-tree.c
@@ -2184,8 +2184,8 @@ MagickExport XMLTreeInfo *NewXMLTree(const char *xml,ExceptionInfo *exception)
               return(&root->root);
             }
           *p='\0';
-          if (ignore_depth == 0 && ParseCloseTag(root,tag,exception) !=
-              (XMLTreeInfo *) NULL)
+          if ((ignore_depth == 0) &&
+              (ParseCloseTag(root,tag,exception) != (XMLTreeInfo *) NULL))
             {
               utf8=DestroyString(utf8);
               return(&root->root);
