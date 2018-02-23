9e80713e5132a3bd26702ee0a833306f7e801469
Cristy <urban-warrior@imagemagick.org>
2018-02-23 05:15:02 -0500

    https://github.com/ImageMagick/ImageMagick/issues/998

--- coders/webp.c
+++ coders/webp.c
@@ -187,7 +187,7 @@ static MagickBooleanType IsWEBPImageLossless(const unsigned char *stream,
     Read extended header.
   */
   offset=RIFF_HEADER_SIZE+TAG_SIZE+CHUNK_SIZE_BYTES+VP8X_CHUNK_SIZE;
-  while (offset <= (ssize_t) length)
+  while (offset <= (ssize_t) (length-TAG_SIZE))
   {
     uint32_t
       chunk_size,
