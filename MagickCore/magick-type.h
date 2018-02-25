56f1f150f00832bb4b04f001528837819ef7d90a
Cristy <urban-warrior@imagemagick.org>
2018-02-25 18:56:26 -0500

    ...

--- MagickCore/magick-type.h
+++ MagickCore/magick-type.h
@@ -176,7 +176,7 @@ typedef enum
 #  include <float.h>
 #  define IsNaN(a) _isnan(a)
 #else
-#  define IsNaN(a) (a != a)
+#  define IsNaN(a) ((a) != (a))
 #endif
 #if !defined(INFINITY)
 #  define INFINITY (-logf(0f))
