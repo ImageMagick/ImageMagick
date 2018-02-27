d813fea05ec0691415ec4319257b017e4768a024
Cristy <urban-warrior@imagemagick.org>
2018-02-26 20:57:21 -0500

    ...

--- coders/pict.c
+++ coders/pict.c
@@ -802,8 +802,8 @@ static MagickBooleanType ReadRectangle(Image *image,PICTRectangle *rectangle)
   rectangle->bottom=(short) ReadBlobMSBShort(image);
   rectangle->right=(short) ReadBlobMSBShort(image);
   if ((EOFBlob(image) != MagickFalse) || 
-      ((rectangle->bottom-rectangle->top) > 0) ||
-      ((rectangle->right-rectangle->left) > 0))
+      ((rectangle->bottom-rectangle->top) <= 0) ||
+      ((rectangle->right-rectangle->left) <= 0))
     return(MagickFalse);
   return(MagickTrue);
 }
