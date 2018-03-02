164af31c8d4d5afd0a5a6a7e27078b5d579adc56
Cristy <urban-warrior@imagemagick.org>
2018-03-02 06:23:51 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6653

--- coders/scr.c
+++ coders/scr.c
@@ -160,8 +160,11 @@ static Image *ReadSCRImage(const ImageInfo *image_info,ExceptionInfo *exception)
   if (status == MagickFalse)
     return(DestroyImageList(image));
   count=ReadBlob(image,6144,(unsigned char *) zxscr);
-  (void) count;
+  if (count != 6144)
+    ThrowReaderException(CorruptImageError,"ImproperImageHeader");
   count=ReadBlob(image,768,(unsigned char *) zxattr);
+  if (count != 768)
+    ThrowReaderException(CorruptImageError,"ImproperImageHeader");
   for(zonenr=0;zonenr<3;zonenr++)
   {
       for(zoneline=0;zoneline<8;zoneline++)
