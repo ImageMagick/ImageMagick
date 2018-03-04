077a53a1c6e1e4d69cb103c7d6b40f56e319daab
Cristy <urban-warrior@imagemagick.org>
2018-03-04 18:07:05 -0500

    ...

--- coders/pdf.c
+++ coders/pdf.c
@@ -226,8 +226,8 @@ static MagickBooleanType InvokePDFDelegate(const MagickBooleanType verbose,
     gsapi_init_with_args;
   ghost_info_struct.run_string=(int (*)(gs_main_instance *,const char *,int,
     int *)) gsapi_run_string;
-  ghost_info_struct.set_stdio=(int (*)(gs_main_instance *,int(*)(void *,char *,
-    int),int(*)(void *,const char *,int),int(*)(void *, const char *, int)))
+  ghost_info_struct.set_stdio=(int (*)(gs_main_instance *,int (*)(void *,char *,
+    int),int (*)(void *,const char *,int),int (*)(void *, const char *, int)))
     gsapi_set_stdio;
   ghost_info_struct.revision=(int (*)(gsapi_revision_t *,int)) gsapi_revision;
 #endif
@@ -253,7 +253,7 @@ static MagickBooleanType InvokePDFDelegate(const MagickBooleanType verbose,
       (ghost_info->delete_instance)(interpreter);
       return(MagickFalse);
     }
-  (void) (ghost_info->set_stdio)(interpreter,(int(MagickDLLCall *)(void *,
+  (void) (ghost_info->set_stdio)(interpreter,(int (MagickDLLCall *)(void *,
     char *,int)) NULL,PDFDelegateMessage,PDFDelegateMessage);
   status=(ghost_info->init_with_args)(interpreter,argc-1,argv+1);
   if (status == 0)
