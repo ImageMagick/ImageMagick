11c7f47df7cbe2e672c8a975aa1c003d92f4fdc9
Cristy <urban-warrior@imagemagick.org>
2018-03-05 06:50:45 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6747

--- coders/miff.c
+++ coders/miff.c
@@ -1271,6 +1271,7 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
     status=SetImageExtent(image,image->columns,image->rows,exception);
     if (status == MagickFalse)
       return(DestroyImageList(image));
+    (void) SetImageBackgroundColor(image,exception);
     /*
       Allocate image pixels.
     */
