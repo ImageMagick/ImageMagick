1547c6d84dd0312d6f60b36cb49702c4d26260a9
Cristy <urban-warrior@imagemagick.org>
2018-03-07 18:00:59 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6778

--- coders/dcm.c
+++ coders/dcm.c
@@ -2981,6 +2981,8 @@ static Image *ReadDCMImage(const ImageInfo *image_info,ExceptionInfo *exception)
 {
 #define ThrowDCMException(exception,message) \
 { \
+  if (info.scale != (Quantum *) NULL) \
+    info.scale=(Quantum *) RelinquishMagickMemory(info.scale); \
   if (data != (unsigned char *) NULL) \
     data=(unsigned char *) RelinquishMagickMemory(data); \
   if (graymap != (int *) NULL) \
@@ -4037,8 +4039,6 @@ static Image *ReadDCMImage(const ImageInfo *image_info,ExceptionInfo *exception)
             if (stream_info->offsets != (ssize_t *) NULL)
               stream_info->offsets=(ssize_t *)
                 RelinquishMagickMemory(stream_info->offsets);
-            if (info.scale != (Quantum *) NULL)
-              info.scale=(Quantum *) RelinquishMagickMemory(info.scale);
             ThrowDCMException(CorruptImageError,"ImproperImageHeader");
           }
         stream_info->count=0;
