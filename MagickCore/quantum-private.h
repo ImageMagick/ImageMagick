c105b9e477825db271df392b842dcd03bb4efda2
Cristy <urban-warrior@imagemagick.org>
2018-03-09 06:32:24 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6801

--- MagickCore/quantum-private.h
+++ MagickCore/quantum-private.h
@@ -21,6 +21,7 @@
 #include "MagickCore/memory_.h"
 #include "MagickCore/cache.h"
 #include "MagickCore/image-private.h"
+#include "MagickCore/pixel-accessor.h"
 
 #if defined(__cplusplus) || defined(c_plusplus)
 extern "C" {
@@ -310,9 +311,11 @@ static inline Quantum ScaleAnyToQuantum(const QuantumAny quantum,
   if (quantum > range)
     return(QuantumRange);
 #if !defined(MAGICKCORE_HDRI_SUPPORT)
-  return((Quantum) (((double) QuantumRange*quantum)/range+0.5));
+  return((Quantum) (((double) QuantumRange*quantum)*
+    PerceptibleReciprocal(range)+0.5));
 #else
-  return((Quantum) (((double) QuantumRange*quantum)/range));
+  return((Quantum) (((double) QuantumRange*quantum)*
+    PerceptibleReciprocal(range)));
 #endif
 }
 
