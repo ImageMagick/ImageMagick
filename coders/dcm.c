6d4545de769547b57b7b784f302bf9f2d915ed78
Cristy <urban-warrior@imagemagick.org>
2018-03-10 07:43:46 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6818

--- coders/dcm.c
+++ coders/dcm.c
@@ -2993,7 +2993,7 @@ static Image *ReadDCMImage(const ImageInfo *image_info,ExceptionInfo *exception)
     greenmap=(int *) RelinquishMagickMemory(greenmap); \
   if (redmap != (int *) NULL) \
     redmap=(int *) RelinquishMagickMemory(redmap); \
-  if (stream_info->offsets == (ssize_t *) NULL) \
+  if (stream_info->offsets != (ssize_t *) NULL) \
     stream_info->offsets=(ssize_t *) RelinquishMagickMemory( \
       stream_info->offsets); \
   if (stream_info != (DCMStreamInfo *) NULL) \
