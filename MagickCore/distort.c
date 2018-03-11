5c9e6d92ee118c09a2030603893b728bfbf96b0e
Dirk Lemstra <dirk@git.imagemagick.org>
2018-03-11 09:12:53 +0100

    Fixed memory leak (https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6400).

--- MagickCore/distort.c
+++ MagickCore/distort.c
@@ -2274,11 +2274,14 @@ MagickExport Image *DistortImage(const Image *image, DistortMethod method,
   distort_image=CloneImage(image,geometry.width,geometry.height,MagickTrue,
     exception);
   if (distort_image == (Image *) NULL)
-    return((Image *) NULL);
+    {
+      coeff=(double *) RelinquishMagickMemory(coeff);
+      return((Image *) NULL);
+    }
   /* if image is ColorMapped - change it to DirectClass */
   if (SetImageStorageClass(distort_image,DirectClass,exception) == MagickFalse)
     {
-       coeff = (double *) RelinquishMagickMemory(coeff);
+      coeff=(double *) RelinquishMagickMemory(coeff);
       distort_image=DestroyImage(distort_image);
       return((Image *) NULL);
     }
@@ -2758,7 +2761,7 @@ if ( d.x == 0.5 && d.y == 0.5 ) {
     distort_image->page.x = 0;
     distort_image->page.y = 0;
   }
-  coeff = (double *) RelinquishMagickMemory(coeff);
+  coeff=(double *) RelinquishMagickMemory(coeff);
   return(distort_image);
 }
 
