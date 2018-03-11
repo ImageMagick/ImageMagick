9ead0dd75e70bd233867c43d0ae047a2c80629c3
Cristy <urban-warrior@imagemagick.org>
2018-03-10 21:23:12 -0500

    ...

--- coders/bmp.c
+++ coders/bmp.c
@@ -961,7 +961,7 @@ static Image *ReadBMPImage(const ImageInfo *image_info,ExceptionInfo *exception)
       bmp_info.bits_per_pixel<<=1;
     bytes_per_line=4*((image->columns*bmp_info.bits_per_pixel+31)/32);
     length=(size_t) bytes_per_line*image->rows;
-    if ((MagickSizeType) length > GetBlobSize(image))
+    if (((MagickSizeType) length/8) > GetBlobSize(image))
       ThrowReaderException(CorruptImageError,"InsufficientImageDataInFile");
     if ((bmp_info.compression == BI_RGB) ||
         (bmp_info.compression == BI_BITFIELDS))
