105f874a99b002f73812b0b3bbcee9bff44a7ff2
Cristy <urban-warrior@imagemagick.org>
2018-03-12 21:04:32 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6850

--- MagickCore/property.c
+++ MagickCore/property.c
@@ -3669,6 +3669,7 @@ RestoreMSCWarning
             }
           GetPixelInfo(image,&pixel);
           fx_info=AcquireFxInfo(image,pattern+4,exception);
+          value=0.0;
           status=FxEvaluateChannelExpression(fx_info,RedPixelChannel,0,0,
             &value,exception);
           pixel.red=(double) QuantumRange*value;
