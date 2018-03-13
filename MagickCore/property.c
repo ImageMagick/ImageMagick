a73c4ccf4d54d33dbc9897d7b22a2c46d6bb966e
Cristy <urban-warrior@imagemagick.org>
2018-03-12 21:15:59 -0400

    ...

--- MagickCore/property.c
+++ MagickCore/property.c
@@ -3667,9 +3667,10 @@ RestoreMSCWarning
                 OptionWarning,"NoImageForProperty","\"%%[%s]\"",pattern);
               continue; /* else no image to retrieve artifact */
             }
+          if ((image->columns == 0) || (image->rows == 0))
+            break;
           GetPixelInfo(image,&pixel);
           fx_info=AcquireFxInfo(image,pattern+4,exception);
-          value=0.0;
           status=FxEvaluateChannelExpression(fx_info,RedPixelChannel,0,0,
             &value,exception);
           pixel.red=(double) QuantumRange*value;
