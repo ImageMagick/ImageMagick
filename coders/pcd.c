3d9bb554441f7ee134fce0b885e158a9d288bf46
Cristy <urban-warrior@imagemagick.org>
2018-03-15 21:06:02 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6944

--- coders/pcd.c
+++ coders/pcd.c
@@ -703,6 +703,8 @@ static Image *ReadPCDImage(const ImageInfo *image_info,ExceptionInfo *exception)
         image->colorspace=YCCColorspace;
         if (LocaleCompare(image_info->magick,"PCDS") == 0)
           (void) SetImageColorspace(image,sRGBColorspace,exception);
+        if (EOFBlob(image) != MagickFalse)
+          break;
         if (j < (ssize_t) number_images)
           {
             /*
