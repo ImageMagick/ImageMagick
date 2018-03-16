ea8edd54110aa8ebc18ae0108e6dbdfc89012e92
Dirk Lemstra <dirk@git.imagemagick.org>
2018-03-16 11:37:43 +0100

    Fallback to extension when the filename does not start with clusterfuzz-testcase-

--- Magick++/fuzz/encoder_format.h
+++ Magick++/fuzz/encoder_format.h
@@ -1,8 +1,15 @@
 class EncoderFormat {
 public:
   std::string get() { return std::string(_format.begin(), _format.end()); } const
-  void set(const std::wstring fileName)
+  void set(const std::wstring fileName, const std::wstring extension)
   {
+    if (fileName.find(L"clusterfuzz-testcase-") != 0)
+    {
+      if (extension.length() > 1)
+        _format = extension.substr(1, extension.size() - 1);
+      return;
+    }
+
     std::wstring format=fileName;
 
     size_t index = format.find(L"_", 0);
--- Magick++/fuzz/main.cc
+++ Magick++/fuzz/main.cc
@@ -35,7 +35,7 @@ public:
     file.read(_data, size);
     file.close();
 
-    encoderFormat.set(fileName);
+    encoderFormat.set(fileName, wstring(PathFindExtension(fileName.c_str())));
 
     return(true);
   }
