913449fdc4836b9fcff18fc544aa7979a1ee5824
Cristy <urban-warrior@imagemagick.org>
2018-03-16 21:14:56 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6973

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -1682,8 +1682,8 @@ static size_t EllipsePoints(const PrimitiveInfo *primitive_info,
   step=MagickPI/8.0;
   if ((delta >= 0.0) && (delta < (MagickPI/8.0)))
     step=MagickPI/(4*(MagickPI/delta/2+0.5));
-  if (step < 0.0000001)
-    step=0.0000001;
+  if (step < 0.00001)
+    step=0.00001;
   angle.x=DegreesToRadians(degrees.x);
   y=degrees.y;
   while (y < degrees.x)
@@ -5509,8 +5509,8 @@ static void TraceEllipse(PrimitiveInfo *primitive_info,const PointInfo start,
   step=MagickPI/8.0;
   if ((delta >= 0.0) && (delta < (MagickPI/8.0)))
     step=MagickPI/(4*(MagickPI/delta/2+0.5));
-  if (step < 0.0000001)
-    step=0.0000001;
+  if (step < 0.00001)
+    step=0.00001;
   angle.x=DegreesToRadians(degrees.x);
   y=degrees.y;
   while (y < degrees.x)
