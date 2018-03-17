8d6a118a1404412e25443230892deadf59565aa3
Dirk Lemstra <dirk@git.imagemagick.org>
2018-03-17 14:09:26 +0100

    Removed duplicate call to GetMultilineTypeMetrics.

--- coders/label.c
+++ coders/label.c
@@ -145,7 +145,7 @@ static Image *ReadLABELImage(const ImageInfo *image_info,
       image->rows=(size_t) floor(metrics.height+draw_info->stroke_width+0.5);
     }
   else
-    if ((strlen(label) > 0) &&
+    if ((status != MagickFalse) && (strlen(label) > 0) &&
         (((image->columns == 0) || (image->rows == 0)) ||
          (fabs(image_info->pointsize) < MagickEpsilon)))
       {
@@ -180,6 +180,12 @@ static Image *ReadLABELImage(const ImageInfo *image_info,
                 ((image->rows != 0) && (height >= image->rows)))
               break;
         }
+        if (status == MagickFalse)
+          {
+            draw_info=DestroyDrawInfo(draw_info);
+            image=DestroyImageList(image);
+            return((Image *) NULL);
+          }
         high=draw_info->pointsize;
         for (low=1.0; (high-low) > 0.5; )
         {
@@ -207,9 +213,12 @@ static Image *ReadLABELImage(const ImageInfo *image_info,
             else
               high=draw_info->pointsize-0.5;
         }
-        draw_info->pointsize=(low+high)/2.0-0.5;
+        if (status != MagickFalse)
+          {
+            draw_info->pointsize=(low+high)/2.0-0.5;
+            status=GetMultilineTypeMetrics(image,draw_info,&metrics,exception);
+          }
       }
-   status=GetMultilineTypeMetrics(image,draw_info,&metrics,exception);
    if (status == MagickFalse)
      {
        draw_info=DestroyDrawInfo(draw_info);
