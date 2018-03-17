bf10c98fedc8d80f1dec89e7dc0a83c32efc75d5
Cristy <urban-warrior@imagemagick.org>
2018-03-17 18:24:37 -0400

    ...

--- coders/tiff.c
+++ coders/tiff.c
@@ -1760,7 +1760,7 @@ RestoreMSCWarning
     if (((MagickSizeType) TIFFScanlineSize(tiff)) > GetBlobSize(image))
       ThrowTIFFException(CorruptImageError,"InsufficientImageDataInFile");
     tiff_pixels=(unsigned char *) AcquireMagickMemory(MagickMax(
-      TIFFScanlineSize(tiff),(ssize_t) MagickMax(image->columns*
+      TIFFScanlineSize(tiff),MagickMax((ssize_t) image->columns*
       samples_per_pixel*pow(2.0,ceil(log(bits_per_sample)/log(2.0))),
       rows_per_strip)*sizeof(uint32)));
     if (tiff_pixels == (unsigned char *) NULL)
