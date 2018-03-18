73aad72d1d28170b38014168f523d0ee2941c21e
Cristy <urban-warrior@imagemagick.org>
2018-03-18 12:41:46 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=6874

--- coders/rle.c
+++ coders/rle.c
@@ -315,8 +315,11 @@ static Image *ReadRLEImage(const ImageInfo *image_info,ExceptionInfo *exception)
             if (comment == (char *) NULL)
               ThrowRLEException(ResourceLimitError,"MemoryAllocationFailed");
             count=ReadBlob(image,length-1,(unsigned char *) comment);
-            comment[length-1]='\0';
-            (void) SetImageProperty(image,"comment",comment,exception);
+            if (count == (length-1))
+              {
+                comment[length-1]='\0';
+                (void) SetImageProperty(image,"comment",comment,exception);
+              }
             comment=DestroyString(comment);
             if ((length & 0x01) == 0)
               (void) ReadBlobByte(image);
