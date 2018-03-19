fc742abb288c2347300f823ad51f97ed941d9b80
Cristy <urban-warrior@imagemagick.org>
2018-03-19 19:41:58 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7022

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -91,6 +91,7 @@
 */
 #define BezierQuantum  200
 #define DrawEpsilon  (1.0e-10)
+#define EllipseEpsilon  (0.0001)
 #define ThrowPointExpectedException(token,exception) \
 { \
   (void) ThrowMagickException(exception,GetMagickModule(),DrawError, \
@@ -1682,8 +1683,8 @@ static size_t EllipsePoints(const PrimitiveInfo *primitive_info,
   step=MagickPI/8.0;
   if ((delta >= 0.0) && (delta < (MagickPI/8.0)))
     step=MagickPI/(4.0*(MagickPI*PerceptibleReciprocal(delta)/2.0));
-  if (step < 0.00001)
-    step=0.00001;
+  if (step < EllipseEpsilon)
+    step=EllipseEpsilon;
   angle.x=DegreesToRadians(degrees.x);
   y=degrees.y;
   while (y < degrees.x)
@@ -5509,8 +5510,8 @@ static void TraceEllipse(PrimitiveInfo *primitive_info,const PointInfo start,
   step=MagickPI/8.0;
   if ((delta >= 0.0) && (delta < (MagickPI/8.0)))
     step=MagickPI/(4.0*(MagickPI*PerceptibleReciprocal(delta)/2.0));
-  if (step < 0.00001)
-    step=0.00001;
+  if (step < EllipseEpsilon)
+    step=EllipseEpsilon;
   angle.x=DegreesToRadians(degrees.x);
   y=degrees.y;
   while (y < degrees.x)
