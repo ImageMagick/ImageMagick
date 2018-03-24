0daf227874f104a24178d694bba9e4ef48450f6e
Dirk Lemstra <dirk@git.imagemagick.org>
2018-03-24 20:17:24 +0100

    Whitespace.

--- Magick++/fuzz/encoder_utils.cc
+++ Magick++/fuzz/encoder_utils.cc
@@ -1,6 +1,6 @@
 static int fuzzEncoderWithStringFilename(const std::string encoder, const uint8_t *Data, size_t Size)
 {
-    // Allow a bit extra to make sure we do proper bounds checking in Magick++
+  // Allow a bit extra to make sure we do proper bounds checking in Magick++
   if (Size > MagickPathExtent)
     return 0;
   std::string color(reinterpret_cast<const char*>(Data), Size);
