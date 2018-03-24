cc1f75ca5e2e848e906a7417237c14c4c9afe00c
Dirk Lemstra <dirk@git.imagemagick.org>
2018-03-24 21:49:23 +0100

    Renamed variable.

--- Magick++/fuzz/encoder_utils.cc
+++ Magick++/fuzz/encoder_utils.cc
@@ -3,11 +3,12 @@ static int fuzzEncoderWithStringFilename(const std::string encoder, const uint8_
   // Allow a bit extra to make sure we do proper bounds checking in Magick++
   if (Size > MagickPathExtent)
     return 0;
-  std::string color(reinterpret_cast<const char*>(Data), Size);
+
+  std::string fileName(reinterpret_cast<const char*>(Data), Size);
 
   Magick::Image image;
   try {
-    image.read(encoder + ":" + color);
+    image.read(encoder + ":" + fileName);
   }
   catch (Magick::Exception &e) {
   }
