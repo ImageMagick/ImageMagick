fd00146bfc98f9d72cadbbc5f5e4fde5ba0de725
Cristy <urban-warrior@imagemagick.org>
2018-03-25 08:54:30 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7111

--- coders/tiff.c
+++ coders/tiff.c
@@ -1765,7 +1765,7 @@ RestoreMSCWarning
     tiff_pixels=(unsigned char *) AcquireMagickMemory(MagickMax(
       TIFFScanlineSize(tiff),MagickMax((ssize_t) image->columns*
       samples_per_pixel*pow(2.0,ceil(log(bits_per_sample)/log(2.0))),
-      rows_per_strip)*sizeof(uint32)));
+      image->columns*rows_per_strip)*sizeof(uint32)));
     if (tiff_pixels == (unsigned char *) NULL)
       ThrowTIFFException(ResourceLimitError,"MemoryAllocationFailed");
     switch (method)
