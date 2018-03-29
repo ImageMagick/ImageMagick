cafb6660b526e1aad749ff6308218087c127c5a9
Dirk Lemstra <dirk@git.imagemagick.org>
2018-03-29 22:07:26 +0200

    Corrected patch.

--- coders/tiff.c
+++ coders/tiff.c
@@ -1719,18 +1719,19 @@ RestoreMSCWarning
           }
       }
     method=ReadGenericMethod;
+    rows_per_strip=image->columns*image->rows;
     if (TIFFGetField(tiff,TIFFTAG_ROWSPERSTRIP,&rows_per_strip) == 1)
       {
         char
           value[MagickPathExtent];
 
-        if (rows_per_strip > (image->columns*image->rows))
-          ThrowTIFFException(CorruptImageError,"ImproperImageHeader");
         method=ReadStripMethod;
         (void) FormatLocaleString(value,MagickPathExtent,"%u",
           (unsigned int) rows_per_strip);
         (void) SetImageProperty(image,"tiff:rows-per-strip",value,exception);
       }
+    if (rows_per_strip > (image->columns*image->rows))
+      ThrowTIFFException(CorruptImageError,"ImproperImageHeader");
     if ((samples_per_pixel >= 3) && (interlace == PLANARCONFIG_CONTIG))
       if ((image->alpha_trait == UndefinedPixelTrait) ||
           (samples_per_pixel >= 4))
