f37718c9ed1ce8f2c2f35cf9a6e2266d61bd2982
Dirk Lemstra <dirk@git.imagemagick.org>
2018-04-01 15:04:34 +0200

    Fixed boundary check (https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7263)

--- coders/webp.c
+++ coders/webp.c
@@ -189,7 +189,7 @@ static MagickBooleanType IsWEBPImageLossless(const unsigned char *stream,
     Read extended header.
   */
   offset=RIFF_HEADER_SIZE+TAG_SIZE+CHUNK_SIZE_BYTES+VP8X_CHUNK_SIZE;
-  while (offset <= (ssize_t) (length-TAG_SIZE))
+  while (offset+TAG_SIZE <= (ssize_t) (length-TAG_SIZE))
   {
     uint32_t
       chunk_size,
