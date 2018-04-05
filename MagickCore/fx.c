64e258cd746b0eefaa97d01ed034e3a29282a0f5
Cristy <urban-warrior@imagemagick.org>
2018-04-05 08:54:32 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7397

--- MagickCore/fx.c
+++ MagickCore/fx.c
@@ -2755,7 +2755,7 @@ static double FxEvaluateSubexpression(FxInfo *fx_info,
         {
           alpha=FxEvaluateSubexpression(fx_info,channel,x,y,expression+3,
             depth+1,beta,exception);
-          gamma=alpha-floor((alpha/(*beta)))*(*beta);
+          gamma=alpha-floor((alpha*PerceptibleReciprocal(*beta)))*(*beta);
           FxReturn(gamma);
         }
       if (LocaleCompare(expression,"m") == 0)
