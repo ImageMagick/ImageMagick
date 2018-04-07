9e254181b1f2aed000d1de83d00f4adbdde054a3
Cristy <urban-warrior@imagemagick.org>
2018-04-06 20:02:29 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7439

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -1429,6 +1429,7 @@ MagickExport MagickBooleanType DrawClipPath(Image *image,
   clip_mask=CloneImage(image,image->columns,image->rows,MagickTrue,exception);
   if (clip_mask == (Image *) NULL)
     return(MagickFalse);
+  (void) DeleteImageArtifact(image,filename);
   (void) SetImageMask(clip_mask,ReadPixelMask,(Image *) NULL,exception);
   (void) SetImageMask(clip_mask,WritePixelMask,(Image *) NULL,exception);
   (void) QueryColorCompliance("#0000",AllCompliance,
