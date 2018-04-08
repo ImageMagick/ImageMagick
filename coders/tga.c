f5d1a9c8e581e955a9e01abd92d3642d512a278e
Dirk Lemstra <dirk@git.imagemagick.org>
2018-04-08 14:38:43 +0200

    Added check for comment length (https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7248)

--- coders/tga.c
+++ coders/tga.c
@@ -296,10 +296,13 @@ static Image *ReadTGAImage(const ImageInfo *image_info,
           sizeof(*comment));
       if (comment == (char *) NULL)
         ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
-      count=ReadBlob(image,tga_info.id_length,(unsigned char *) comment);
-      comment[tga_info.id_length]='\0';
-      (void) SetImageProperty(image,"comment",comment,exception);
-      comment=DestroyString(comment);
+      count=ReadBlob(image,length,(unsigned char *) comment);
+      if (count == length)
+        {
+          comment[length]='\0';
+          (void) SetImageProperty(image,"comment",comment,exception);
+          comment=DestroyString(comment);
+        }
     }
   if (tga_info.attributes & (1UL << 4))
     {
