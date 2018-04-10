1d69ad385f5bac239b29e71d1c856eabd3ca3fb7
Cristy <urban-warrior@imagemagick.org>
2018-04-10 19:10:10 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7535

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -1531,7 +1531,7 @@ static MagickBooleanType DrawDashPolygon(const DrawInfo *draw_info,
   for (i=0; primitive_info[i].primitive != UndefinedPrimitive; i++) ;
   number_vertices=(size_t) i;
   dash_polygon=(PrimitiveInfo *) AcquireQuantumMemory((size_t)
-    (2UL*(number_vertices+6UL)+6UL),sizeof(*dash_polygon));
+    (2UL*number_vertices+32UL),sizeof(*dash_polygon));
   if (dash_polygon == (PrimitiveInfo *) NULL)
     return(MagickFalse);
   clone_info=CloneDrawInfo((ImageInfo *) NULL,draw_info);
