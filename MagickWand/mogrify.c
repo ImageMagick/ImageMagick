76fdecd663409a50e6a509239f7ced405ecf4a64
Cristy <urban-warrior@imagemagick.org>
2018-04-13 19:36:20 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1093

--- MagickWand/mogrify.c
+++ MagickWand/mogrify.c
@@ -8761,7 +8761,8 @@ WandExport MagickBooleanType MogrifyImageList(ImageInfo *image_info,
             Image
               *p,
               *q,
-              *swap;
+              *u,
+              *v;
 
             ssize_t
               swap_index;
@@ -8793,12 +8794,17 @@ WandExport MagickBooleanType MogrifyImageList(ImageInfo *image_info,
               }
             if (p == q)
               break;
-            swap=CloneImage(p,0,0,MagickTrue,exception);
-            if (swap == (Image *) NULL)
-              ThrowWandFatalException(ResourceLimitFatalError,
-                "MemoryAllocationFailed",(*images)->filename);
-            ReplaceImageInList(&p,CloneImage(q,0,0,MagickTrue,exception));
-            ReplaceImageInList(&q,swap);
+            u=CloneImage(p,0,0,MagickTrue,exception);
+            if (u == (Image *) NULL)
+              break;
+            v=CloneImage(q,0,0,MagickTrue,exception);
+            if (v == (Image *) NULL)
+              {
+                u=DestroyImage(u);
+                break;
+              }
+            ReplaceImageInList(&p,v);
+            ReplaceImageInList(&q,u);
             *images=GetFirstImageInList(q);
             break;
           }
