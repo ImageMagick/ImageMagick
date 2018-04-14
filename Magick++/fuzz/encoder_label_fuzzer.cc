a154a2863ad5fc22ef33640b3b77e9740761062c
Dirk Lemstra <dirk@git.imagemagick.org>
2018-04-14 18:00:26 +0200

    Prevent label that contains '%#' because this will calculate a signature and will probably cause a timeout in the fuzzer.

--- Magick++/fuzz/encoder_label_fuzzer.cc
+++ Magick++/fuzz/encoder_label_fuzzer.cc
@@ -6,8 +6,17 @@
 #include "utils.cc"
 #include "encoder_utils.cc"
 
+static bool validateFileName(const std::string &fileName)
+{
+  // Signature: this will most likely cause a timeout.
+  if (fileName.find("%#") != -1)
+    return false;
+
+  return true;
+}
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  return fuzzEncoderWithStringFilename("label", Data, Size);
+  return fuzzEncoderWithStringFilename("label", Data, Size, validateFileName);
 }
 
 #include "travis.cc"
--- Magick++/fuzz/encoder_utils.cc
+++ Magick++/fuzz/encoder_utils.cc
@@ -1,10 +1,14 @@
-static int fuzzEncoderWithStringFilename(const std::string encoder, const uint8_t *Data, size_t Size)
+static int fuzzEncoderWithStringFilename(const std::string encoder, const uint8_t *Data, size_t Size, bool (*validate)(const std::string &) = NULL)
 {
   // Allow a bit extra to make sure we do proper bounds checking in Magick++
   if (Size > MagickPathExtent)
     return 0;
 
   std::string fileName(reinterpret_cast<const char*>(Data), Size);
+
+  // Can be used to deny specific file names
+  if ((validate != NULL) && (validate(fileName) == false))
+    return 0;
 
   Magick::Image image;
   try {
