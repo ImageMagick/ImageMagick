afbc13c07440c04fa0b6ff6ff59d0758245efb76
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-04-16 09:18:41 +0800

    add lcms to the delegate list for oss-fuzz

--- Magick++/fuzz/build.sh
+++ Magick++/fuzz/build.sh
@@ -35,6 +35,14 @@ make -j$(nproc)
 make install
 popd
 
+# Build liblcms2
+pushd "$SRC/Little-CMS"
+autoreconf -fiv
+./configure --disable-shared --prefix="$WORK"
+make -j$(nproc)
+make install
+popd
+
 # build libraw
 pushd "$SRC/libraw"
 autoreconf -fiv
@@ -84,7 +92,7 @@ MAGICK_COMPILER=$CXX
 MAGICK_COMPILER_FLAGS=$CXXFLAGS
 MAGICK_INCLUDE="$WORK/include/ImageMagick-7"
 MAGICK_SRC="$SRC/imagemagick/Magick++/fuzz"
-MAGICK_LIBS="-lFuzzingEngine $WORK/lib/libMagick++-7.Q16HDRI.a $WORK/lib/libMagickWand-7.Q16HDRI.a $WORK/lib/libMagickCore-7.Q16HDRI.a $WORK/lib/libz.a $WORK/lib/liblzma.a $WORK/lib/libpng.a $WORK/lib/libtiff.a $WORK/lib/libde265.a $WORK/lib/libopenjp2.a $WORK/lib/libwebp.a $WORK/lib/libturbojpeg.a $WORK/lib/libjpeg.a $WORK/lib/libfreetype.a $WORK/lib/libraw.a"
+MAGICK_LIBS="-lFuzzingEngine $WORK/lib/libMagick++-7.Q16HDRI.a $WORK/lib/libMagickWand-7.Q16HDRI.a $WORK/lib/libMagickCore-7.Q16HDRI.a $WORK/lib/libz.a $WORK/lib/liblzma.a $WORK/lib/libpng.a $WORK/lib/libtiff.a $WORK/lib/liblcms2.a $WORK/lib/libde265.a $WORK/lib/libopenjp2.a $WORK/lib/libwebp.a $WORK/lib/libturbojpeg.a $WORK/lib/libjpeg.a $WORK/lib/libfreetype.a $WORK/lib/libraw.a"
 MAGICK_OUTPUT=$OUT
 MAGICK_FAST_BUILD=0
 
