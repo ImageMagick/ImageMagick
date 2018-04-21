ac2484b43e4ca519682e44158c526ccb1115f825
Dirk Lemstra <dirk@git.imagemagick.org>
2018-04-21 10:31:49 +0200

    Added checks for return value of cmsGetProfileInfoASCII (https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=7740).

--- MagickCore/property.c
+++ MagickCore/property.c
@@ -1688,20 +1688,21 @@ static MagickBooleanType GetICCProperty(const Image *image,const char *property,
         char
           info[MagickPathExtent];
 
-        (void) cmsGetProfileInfoASCII(icc_profile,cmsInfoDescription,"en","US",
-          info,MagickPathExtent);
-        (void) SetImageProperty((Image *) image,"icc:description",info,
-          exception);
-        (void) cmsGetProfileInfoASCII(icc_profile,cmsInfoManufacturer,"en","US",
-          info,MagickPathExtent);
-        (void) SetImageProperty((Image *) image,"icc:manufacturer",info,
-          exception);
-        (void) cmsGetProfileInfoASCII(icc_profile,cmsInfoModel,"en","US",info,
-          MagickPathExtent);
-        (void) SetImageProperty((Image *) image,"icc:model",info,exception);
-        (void) cmsGetProfileInfoASCII(icc_profile,cmsInfoCopyright,"en","US",
-          info,MagickPathExtent);
-        (void) SetImageProperty((Image *) image,"icc:copyright",info,exception);
+        if (cmsGetProfileInfoASCII(icc_profile,cmsInfoDescription,"en","US",
+              info,MagickPathExtent) != 0)
+          (void) SetImageProperty((Image *) image,"icc:description",info,
+            exception);
+        if (cmsGetProfileInfoASCII(icc_profile,cmsInfoManufacturer,"en","US",
+              info,MagickPathExtent) != 0)
+          (void) SetImageProperty((Image *) image,"icc:manufacturer",info,
+            exception);
+        if (cmsGetProfileInfoASCII(icc_profile,cmsInfoModel,"en","US",info,
+              MagickPathExtent) != 0)
+          (void) SetImageProperty((Image *) image,"icc:model",info,exception);
+        if (cmsGetProfileInfoASCII(icc_profile,cmsInfoCopyright,"en","US",
+              info,MagickPathExtent) != 0)
+          (void) SetImageProperty((Image *) image,"icc:copyright",info,
+            exception);
 #endif
         (void) cmsCloseProfile(icc_profile);
       }
