d53beb6ec66a83611d8f4445a1158ac559f18f19
Paul Kehrer <paul.l.kehrer@gmail.com>
2018-04-23 07:21:11 -0500

    fix oss-fuzz build with libtiff

--- Magick++/fuzz/build.sh
+++ Magick++/fuzz/build.sh
@@ -23,14 +23,20 @@ popd
 
 # Build libjpeg-turbo
 pushd "$SRC/libjpeg-turbo"
-cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DENABLE_STATIC=on -DENABLE_SHARED=off
+CFLAGS="$CFLAGS -fPIC" cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DENABLE_STATIC=on -DENABLE_SHARED=off
 make -j$(nproc)
 make install
 popd
 
 # Build libtiff
+# We build shared and static here to make IM's libtiff detection via autoconf more reliable
+# The fuzzers themselves link solely against the static build though.
 pushd "$SRC/libtiff"
 cmake . -DCMAKE_INSTALL_PREFIX=$WORK
+make -j$(nproc)
+make install
+git clean -fd
+cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DBUILD_SHARED_LIBS=off
 make -j$(nproc)
 make install
 popd
