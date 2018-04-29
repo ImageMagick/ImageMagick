68219ff1692e9ce6fb47d81335ec6929e420a70c
Cristy <urban-warrior@imagemagick.org>
2018-04-28 21:09:12 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1110

--- MagickCore/layer.c
+++ MagickCore/layer.c
@@ -1759,10 +1759,15 @@ static inline void CompositeCanvas(Image *destination,
   const CompositeOperator compose,Image *source,ssize_t x_offset,
   ssize_t y_offset,ExceptionInfo *exception)
 {
+  const char
+    *value;
+
   x_offset+=source->page.x-destination->page.x;
   y_offset+=source->page.y-destination->page.y;
-  (void) CompositeImage(destination,source,compose,MagickTrue,x_offset,
-    y_offset,exception);
+  value=GetImageArtifact(source,"compose:outside-overlay");
+  (void) CompositeImage(destination,source,compose,
+    (value != (const char *) NULL) && (IsStringTrue(value) != MagickFalse) ?
+    MagickFalse : MagickTrue,x_offset,y_offset,exception);
 }
 
 MagickExport void CompositeLayers(Image *destination,
