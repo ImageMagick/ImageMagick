c945953a9acfd7f0ec935b32daec37e9e2f4e574
Cristy <urban-warrior@imagemagick.org>
2018-05-04 09:13:13 -0400

    ...

--- coders/png.c
+++ coders/png.c
@@ -4502,7 +4502,14 @@ static Image *ReadOneJNGImage(MngInfo *mng_info,
       {
         DestroyJNG(NULL,&color_image,&color_image_info,
           &alpha_image,&alpha_image_info);
-        ThrowReaderException(CorruptImageError,"CorruptImage");
+        ThrowReaderException(CorruptImageError,"ImproperImageHeader");
+      }
+    if (length > GetBlobSize(image))
+      {
+        DestroyJNG(NULL,&color_image,&color_image_info,
+          &alpha_image,&alpha_image_info);
+        ThrowReaderException(CorruptImageError,
+          "InsufficientImageDataInFile");
       }
 
     p=NULL;
@@ -4510,13 +4517,6 @@ static Image *ReadOneJNGImage(MngInfo *mng_info,
 
     if (length != 0)
       {
-        if (length > GetBlobSize(image))
-          {
-            DestroyJNG(NULL,&color_image,&color_image_info,
-              &alpha_image,&alpha_image_info);
-            ThrowReaderException(CorruptImageError,
-              "InsufficientImageDataInFile");
-          }
         chunk=(unsigned char *) AcquireQuantumMemory(length,sizeof(*chunk));
 
         if (chunk == (unsigned char *) NULL)
