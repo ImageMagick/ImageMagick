2eda7238dc6977fa1e0be8eabc9be1e8706a2116
Dirk Lemstra <dirk@git.imagemagick.org>
2018-05-09 22:05:32 +0200

    Also check extent from raw_info to avoid excessive allocations.

--- coders/dng.c
+++ coders/dng.c
@@ -318,6 +318,12 @@ static Image *ReadDNGImage(const ImageInfo *image_info,ExceptionInfo *exception)
         libraw_close(raw_info);
         return(image);
       }
+    status=SetImageExtent(image,image->columns,image->rows,exception);
+    if (status == MagickFalse)
+      {
+        libraw_close(raw_info);
+        return(image);
+      }
     errcode=libraw_unpack(raw_info);
     if (errcode != LIBRAW_SUCCESS)
       {
