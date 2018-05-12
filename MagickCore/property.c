f8766ec663b1faeda81c0c3a8e855f6502276fa9
Cristy <urban-warrior@imagemagick.org>
2018-05-11 20:56:37 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=8234

--- MagickCore/property.c
+++ MagickCore/property.c
@@ -1677,19 +1677,27 @@ static MagickBooleanType GetICCProperty(const Image *image,const char *property,
         char
           info[MagickPathExtent];
 
-        if (cmsGetProfileInfoASCII(icc_profile,cmsInfoDescription,"en","US",
-              info,MagickPathExtent) != 0)
+        int
+          status;
+
+        (void) memset(info,0,sizeof(info));
+        status=cmsGetProfileInfoASCII(icc_profile,cmsInfoDescription,"en","US",
+          info,MagickPathExtent);
+        if (status != 0)
           (void) SetImageProperty((Image *) image,"icc:description",info,
             exception);
-        if (cmsGetProfileInfoASCII(icc_profile,cmsInfoManufacturer,"en","US",
-              info,MagickPathExtent) != 0)
+        status=cmsGetProfileInfoASCII(icc_profile,cmsInfoManufacturer,"en","US",
+          info,MagickPathExtent);
+        if (status != 0)
           (void) SetImageProperty((Image *) image,"icc:manufacturer",info,
             exception);
-        if (cmsGetProfileInfoASCII(icc_profile,cmsInfoModel,"en","US",info,
-              MagickPathExtent) != 0)
+        status=cmsGetProfileInfoASCII(icc_profile,cmsInfoModel,"en","US",
+          info,MagickPathExtent);
+        if (status != 0)
           (void) SetImageProperty((Image *) image,"icc:model",info,exception);
-        if (cmsGetProfileInfoASCII(icc_profile,cmsInfoCopyright,"en","US",
-              info,MagickPathExtent) != 0)
+        status=cmsGetProfileInfoASCII(icc_profile,cmsInfoCopyright,"en","US",
+          info,MagickPathExtent);
+        if (status != 0)
           (void) SetImageProperty((Image *) image,"icc:copyright",info,
             exception);
 #endif
