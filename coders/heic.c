54498123bbe8f6e10fabf3855bd9bc0dcb817f82
Cristy <urban-warrior@imagemagick.org>
2018-05-16 14:27:08 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1131

--- coders/heic.c
+++ coders/heic.c
@@ -375,6 +375,7 @@ ModuleExport size_t RegisterHEICImage(void)
   entry->mime_type=ConstantString("image/x-heic");
   entry->flags|=CoderDecoderSeekableStreamFlag;
   entry->flags^=CoderAdjoinFlag;
+  entry->flags^=CoderDecoderThreadSupportFlag;
   (void) RegisterMagickInfo(entry);
   return(MagickImageCoderSignature);
 }
