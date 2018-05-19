e15d43e7e34043495b2380937b80f008c07e1cb3
Cristy <urban-warrior@imagemagick.org>
2018-05-19 16:36:40 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1132

--- MagickCore/enhance.c
+++ MagickCore/enhance.c
@@ -2364,9 +2364,7 @@ static inline double LevelPixel(const double black_point,
     level_pixel,
     scale;
 
-  if (fabs(white_point-black_point) < MagickEpsilon)
-    return(pixel);
-  scale=1.0/(white_point-black_point);
+  scale=PerceptibleReciprocal(white_point-black_point);
   level_pixel=QuantumRange*gamma_pow(scale*((double) pixel-black_point),
     1.0/gamma);
   return(level_pixel);
