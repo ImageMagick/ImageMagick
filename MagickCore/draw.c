fb140d7d73449da6b8bfe40957c327ebf25debf1
Cristy <urban-warrior@imagemagick.org>
2018-05-21 08:05:09 -0400

    ...

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -6299,11 +6299,13 @@ static size_t TracePath(MVGInfo *mvg_info,const char *path,
         /*
           Move to.
         */
-        if (q != primitive_info)
+        if (mvg_info->offset != subpath_offset)
           {
+            primitive_info=(*mvg_info->primitive_info)+offset;
             primitive_info->coordinates=(size_t) (q-primitive_info);
             number_coordinates+=primitive_info->coordinates;
             primitive_info=q;
+            subpath_offset=mvg_info->offset;
           }
         i=0;
         do
