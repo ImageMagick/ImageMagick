ff0916783fe47380092666287ab0eaa7de1ef09f
Miloslav Číž <tastyfish@seznam.cz>
2018-05-28 11:01:58 +0200

    Start extending magnify function

--- MagickCore/resize.c
+++ MagickCore/resize.c
@@ -2034,6 +2034,16 @@ MagickExport Image *LiquidRescaleImage(const Image *image,
 }
 #endif
 
+
+void Scale2x(const Quantum *neighbourhood, Quantum *result, size_t channels)
+{
+  register ssize_t
+    i;
+
+  for (i=0; i < (ssize_t) channels * 4; i++)
+    result[i] = neighbourhood[4*channels+i];
+}
+
 /*
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                             %
@@ -2079,6 +2089,9 @@ MagickExport Image *MagnifyImage(const Image *image,ExceptionInfo *exception)
   ssize_t
     y;
 
+  unsigned char
+    magnification = 2;
+
   /*
     Initialize magnified image attributes.
   */
@@ -2088,7 +2101,7 @@ MagickExport Image *MagnifyImage(const Image *image,ExceptionInfo *exception)
     (void) LogMagickEvent(TraceEvent,GetMagickModule(),"%s",image->filename);
   assert(exception != (ExceptionInfo *) NULL);
   assert(exception->signature == MagickCoreSignature);
-  magnify_image=CloneImage(image,2*image->columns,2*image->rows,MagickTrue,
+  magnify_image=CloneImage(image,magnification*image->columns,magnification*image->rows,MagickTrue,
     exception);
   if (magnify_image == (Image *) NULL)
     return((Image *) NULL);
@@ -2111,98 +2124,47 @@ MagickExport Image *MagnifyImage(const Image *image,ExceptionInfo *exception)
     register ssize_t
       x;
 
+    Quantum r[128]; // TODO - 128
+
     if (status == MagickFalse)
       continue;
-    q=QueueCacheViewAuthenticPixels(magnify_view,0,2*y,magnify_image->columns,2,
+    q=QueueCacheViewAuthenticPixels(magnify_view,0,magnification*y,magnify_image->columns,magnification,
       exception);
     if (q == (Quantum *) NULL)
       {
         status=MagickFalse;
         continue;
       }
+
     /*
       Magnify this row of pixels.
     */
     for (x=0; x < (ssize_t) image->columns; x++)
     {
-      MagickRealType
-        intensity[9];
-
       register const Quantum
         *magick_restrict p;
 
-      register Quantum
-        *magick_restrict r;
+      ssize_t
+        channels;
 
       register ssize_t
         i;
 
-      size_t
-        channels;
-
       p=GetCacheViewVirtualPixels(image_view,x-1,y-1,3,3,exception);
-      if (p == (const Quantum *) NULL)
-        {
-          status=MagickFalse;
-          continue;
-        }
-      channels=GetPixelChannels(image);
-      for (i=0; i < 9; i++)
-        intensity[i]=GetPixelIntensity(image,p+i*channels);
-      r=q;
-      if ((fabs(intensity[1]-intensity[7]) < MagickEpsilon) ||
-          (fabs(intensity[3]-intensity[5]) < MagickEpsilon))
-        {
-          /*
-            Clone center pixel.
-          */
-          for (i=0; i < (ssize_t) channels; i++)
-            r[i]=p[4*channels+i];
-          r+=GetPixelChannels(magnify_image);
-          for (i=0; i < (ssize_t) channels; i++)
-            r[i]=p[4*channels+i];
-          r+=GetPixelChannels(magnify_image)*(magnify_image->columns-1);
-          for (i=0; i < (ssize_t) channels; i++)
-            r[i]=p[4*channels+i];
-          r+=GetPixelChannels(magnify_image);
-          for (i=0; i < (ssize_t) channels; i++)
-            r[i]=p[4*channels+i];
-        }
-      else
-        {
-          /*
-            Selectively clone pixel.
-          */
-          if (fabs(intensity[1]-intensity[3]) < MagickEpsilon)
-            for (i=0; i < (ssize_t) channels; i++)
-              r[i]=p[3*channels+i];
-          else
-            for (i=0; i < (ssize_t) channels; i++)
-              r[i]=p[4*channels+i];
-          r+=GetPixelChannels(magnify_image);
-          if (fabs(intensity[1]-intensity[5]) < MagickEpsilon)
-            for (i=0; i < (ssize_t) channels; i++)
-              r[i]=p[5*channels+i];
-          else
-            for (i=0; i < (ssize_t) channels; i++)
-              r[i]=p[4*channels+i];
-          r+=GetPixelChannels(magnify_image)*(magnify_image->columns-1);
-          if (fabs(intensity[3]-intensity[7]) < MagickEpsilon)
-            for (i=0; i < (ssize_t) channels; i++)
-              r[i]=p[3*channels+i];
-          else
-            for (i=0; i < (ssize_t) channels; i++)
-              r[i]=p[4*channels+i];
-          r+=GetPixelChannels(magnify_image);
-          if (fabs(intensity[5]-intensity[7]) < MagickEpsilon)
-            for (i=0; i < (ssize_t) channels; i++)
-              r[i]=p[5*channels+i];
-          else
-            for (i=0; i < (ssize_t) channels; i++)
-              r[i]=p[4*channels+i];
-        }
-      q+=2*GetPixelChannels(magnify_image);
+      
+      channels = GetPixelChannels(image);
+
+      Scale2x(p,r,channels);
+
+      for (i=0; i < (ssize_t) channels * 2; i++)
+        q[i]=r[i];
+
+      for (i=0; i < (ssize_t) channels * 2; i++)
+        q[channels * (magnify_image->columns-1) + i]=r[2+i];
+
+      q+=magnification*GetPixelChannels(magnify_image);
     }
+
     if (SyncCacheViewAuthenticPixels(magnify_view,exception) == MagickFalse)
       status=MagickFalse;
     if (image->progress_monitor != (MagickProgressMonitor) NULL)
