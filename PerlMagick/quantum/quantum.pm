8c772a579363554884e022fe7a303c9a5ba02c52
Cristy <urban-warrior@imagemagick.org>
2018-06-11 08:10:55 -0400

    Bump version

--- PerlMagick/quantum/quantum.pm
+++ PerlMagick/quantum/quantum.pm
@@ -43,7 +43,7 @@ require AutoLoader;
       ConfigureError FatalErrorException
     );
 
-$VERSION = '7.07';
+$VERSION = '7.08';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
