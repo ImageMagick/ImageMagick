6dd8d6b071996caddd291031420ebc675e8046f5
Cristy <urban-warrior@imagemagick.org>
2018-07-21 08:09:53 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1218

--- MagickCore/transform.c
+++ MagickCore/transform.c
@@ -1116,6 +1116,9 @@ MagickExport Image *ExtentImage(const Image *image,
   Image
     *extent_image;
 
+  MagickBooleanType
+    status;
+
   /*
     Allocate extent image.
   */
@@ -1126,16 +1129,17 @@ MagickExport Image *ExtentImage(const Image *image,
   assert(geometry != (const RectangleInfo *) NULL);
   assert(exception != (ExceptionInfo *) NULL);
   assert(exception->signature == MagickCoreSignature);
-  if ((image->columns == geometry->width) &&
-      (image->rows == geometry->height) &&
-      (geometry->x == 0) && (geometry->y == 0))
-    return(CloneImage(image,0,0,MagickTrue,exception));
   extent_image=CloneImage(image,geometry->width,geometry->height,MagickTrue,
     exception);
   if (extent_image == (Image *) NULL)
     return((Image *) NULL);
-  (void) SetImageBackgroundColor(extent_image,exception);
-  (void) CompositeImage(extent_image,image,image->compose,MagickTrue,
+  status=SetImageBackgroundColor(extent_image,exception);
+  if (status == MagickFalse)
+    {
+      extent_image=DestroyImage(extent_image);
+      return((Image *) NULL);
+    }
+  status=CompositeImage(extent_image,image,image->compose,MagickTrue,
     -geometry->x,-geometry->y,exception);
   return(extent_image);
 }
