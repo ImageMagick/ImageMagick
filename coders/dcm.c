16916c8979c32765c542e216b31cee2671b7afe7
Cristy <urban-warrior@imagemagick.org>
2018-08-27 19:29:51 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1269

--- coders/dcm.c
+++ coders/dcm.c
@@ -3853,6 +3853,8 @@ static Image *ReadDCMImage(const ImageInfo *image_info,ExceptionInfo *exception)
           tag=((unsigned int) ReadBlobLSBShort(image) << 16) |
             ReadBlobLSBShort(image);
           length=(size_t) ReadBlobLSBLong(image);
+          if (length > (size_t) GetBlobSize(image))
+            ThrowDCMException(CorruptImageError,"InsufficientImageDataInFile");
           if (tag == 0xFFFEE0DD)
             break; /* sequence delimiter tag */
           if (tag != 0xFFFEE000)
