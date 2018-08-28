37747c983793a9e2b4680a38df06497e0e9cb8ce
Cristy <urban-warrior@imagemagick.org>
2018-08-28 07:48:30 -0400

    https://www.imagemagick.org/discourse-server/viewtopic.php?f=3&t=34644

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -3994,12 +3994,6 @@ static MagickBooleanType RenderMVGContent(Image *image,
         beta=bounds.y2-bounds.y1;
         radius=hypot(alpha,beta);
         coordinates=2.0*(ceil(MagickPI*radius))+6.0*BezierQuantum+360.0;
-        if (coordinates > (107*BezierQuantum))
-          {
-            (void) ThrowMagickException(exception,GetMagickModule(),DrawError,
-              "TooManyBezierCoordinates","`%s'",token);
-            status=MagickFalse;
-          }
         break;
       }
       default:
