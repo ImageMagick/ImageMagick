2922c354ce21602ebea5cf9a894d3e28d464624b
Dirk Lemstra <dirk@lemstra.org>
2018-09-20 21:48:04 +0200

    Improved format detection in the heic coder.

--- coders/heic.c
+++ coders/heic.c
@@ -408,7 +408,13 @@ static MagickBooleanType IsHEIC(const unsigned char *magick,const size_t length)
 {
   if (length < 12)
     return(MagickFalse);
+  if (LocaleNCompare((const char *) magick+4,"ftyp",4) != 0)
+  return(MagickFalse);
   if (LocaleNCompare((const char *) magick+8,"heic",4) == 0)
+    return(MagickTrue);
+  if (LocaleNCompare((const char *) magick+8,"heix",4) == 0)
+    return(MagickTrue);
+  if (LocaleNCompare((const char *) magick+8,"mif1",4) == 0)
     return(MagickTrue);
   return(MagickFalse);
 }
