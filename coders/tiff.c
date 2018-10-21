f4c6b0dc1be1801cc993b9c5680f3238767d2d9e
Dirk Lemstra <dirk@lemstra.org>
2018-10-21 13:35:30 +0200

    Also set the min_channels.

--- coders/tiff.c
+++ coders/tiff.c
@@ -292,6 +292,7 @@ static void InitPSDInfo(const Image *image, PSDInfo *info)
   /* Setting the mode to a value that won't change the colorspace */
   info->mode=10;
   info->channels=1U;
+  info->min_channels=1U;
   if (image->storage_class == PseudoClass)
     info->mode=2; /* indexed mode */
   else
