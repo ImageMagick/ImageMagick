58660617ee4f5f6128499bdc2e2ebb2ab6ea8a62
Cristy <urban-warrior@imagemagick.org>
2018-12-08 15:13:37 -0500

    ...

--- MagickCore/image.c
+++ MagickCore/image.c
@@ -2883,9 +2883,8 @@ MagickExport MagickBooleanType SetImageInfo(ImageInfo *image_info,
     {
       (void) CopyMagickString(magic,image_info->magick,MagickPathExtent);
       magick_info=GetMagickInfo(magic,sans_exception);
-      GetPathComponent(image_info->filename,CanonicalPath,component);
       if (frames == 0)
-        GetPathComponent(image_info->filename,CanonicalPath,filename);
+        GetPathComponent(image_info->filename,CanonicalPath,component);
       else
         GetPathComponent(image_info->filename,SubcanonicalPath,component);
       (void) CopyMagickString(image_info->filename,component,MagickPathExtent);
