75db19882ea8b1801e439e6e9d19b3042a82de4c
Cristy <urban-warrior@imagemagick.org>
2018-12-10 20:00:25 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=11799

--- MagickCore/utility.c
+++ MagickCore/utility.c
@@ -1363,7 +1363,8 @@ MagickExport void GetPathComponent(const char *path,PathType type,
     case SubimagePath:
     {
       *component='\0';
-      if (subimage_length != 0)
+      if ((subimage_length != 0) &&
+          (magick_length+subimage_offset) < strlen(path))
         (void) CopyMagickString(component,path+magick_length+subimage_offset,
           subimage_length);
       break;
