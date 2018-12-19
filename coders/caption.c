18df5773b5efbd2c9dc54c9caa021febf486bd22
Cristy <urban-warrior@imagemagick.org>
2018-12-19 08:24:11 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=11930

--- coders/caption.c
+++ coders/caption.c
@@ -134,6 +134,7 @@ static Image *ReadCAPTIONImage(const ImageInfo *image_info,
   assert(exception != (ExceptionInfo *) NULL);
   assert(exception->signature == MagickCoreSignature);
   image=AcquireImage(image_info,exception);
+  (void) ResetImagePixels(image,exception);
   (void) ResetImagePage(image,"0x0+0+0");
   /*
     Format caption.
