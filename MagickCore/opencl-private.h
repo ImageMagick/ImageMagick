57b308b170bb9e0ab184602bb1fed47bafe5a9d4
Cristy <mikayla-grace@urban-warrior.org>
2019-01-19 17:53:40 -0500

    Include missing phrase from copyright

--- MagickCore/opencl-private.h
+++ MagickCore/opencl-private.h
@@ -2,7 +2,7 @@
 Copyright 1999-2019 ImageMagick Studio LLC, a non-profit organization
 dedicated to making software imaging solutions freely available.
 
-You may not use this file except in compliance with the License.
+You may not use this file except in compliance with the License.  You may
 obtain a copy of the License at
 
 https://imagemagick.org/script/license.php
