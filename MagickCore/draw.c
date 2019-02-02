7e4af20d3aee1ff1aa8863315edc01a13ef946c5
Cristy <mikayla-grace@urban-warrior.org>
2019-02-02 11:14:46 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=12809

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -2486,7 +2486,7 @@ static MagickBooleanType RenderMVGContent(Image *image,
     {
       status=SetImageAlphaChannel(image,OpaqueAlphaChannel,exception);
       if (status == MagickFalse)
-        return(status == 0 ? MagickFalse : MagickTrue);
+        return(status);
     }
   primitive=(char *) NULL;
   if (*draw_info->primitive != '@')
