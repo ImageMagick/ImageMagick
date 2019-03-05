41b03b43a5799abd898f871ce5865cb0307f1592
Cristy <mikayla-grace@urban-warrior.org>
2019-03-05 18:12:00 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=13538

--- coders/psd.c
+++ coders/psd.c
@@ -1383,6 +1383,7 @@ static MagickBooleanType ReadPSDChannel(Image *image,
         layer_info->mask.page.height,MagickFalse,exception);
       if (mask != (Image *) NULL)
         {
+          (void) ResetImagePixels(mask,exception);
           (void) SetImageType(mask,GrayscaleType,exception);
           channel_image=mask;
         }
