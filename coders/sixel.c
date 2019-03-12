2eb281174073d12819b52d4873f33cba734e3867
Cristy <mikayla-grace@urban-warrior.org>
2019-03-12 18:54:37 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=13694

--- coders/sixel.c
+++ coders/sixel.c
@@ -203,7 +203,8 @@ static unsigned char *get_params(unsigned char *p, int *param, int *len)
         }
         if (isdigit((int) ((unsigned char) *p))) {
             for (n = 0; isdigit((int) ((unsigned char) *p)); p++) {
-                n = (int) ((ssize_t) n * 10 + (*p - '0'));
+                if (n <= (INT_MAX/10))
+                  n = (int) ((ssize_t) n * 10 + (*p - '0'));
             }
             if (*len < 10) {
                 param[(*len)++] = n;
