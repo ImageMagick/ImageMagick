ee5b9c56b9ca18ed0750f8a15e0d1a6da92a6e99
Cristy <mikayla-grace@urban-warrior.org>
2019-03-30 08:47:09 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1517

--- coders/dpx.c
+++ coders/dpx.c
@@ -2022,8 +2022,10 @@ static MagickBooleanType WriteDPXImage(const ImageInfo *image_info,Image *image,
       break;
     length=ExportQuantumPixels(image,(CacheView *) NULL,quantum_info,
       quantum_type,pixels,exception);
+    if (length == 0)
+      break;
     count=WriteBlob(image,extent,pixels);
-    if (count != (ssize_t) length)
+    if (count != (ssize_t) extent)
       break;
     status=SetImageProgress(image,SaveImageTag,(MagickOffsetType) y,
       image->rows);
