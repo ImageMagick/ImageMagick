e15db5cee1be8008c0179c12e3773697417547c6
Cristy <mikayla-grace@urban-warrior.org>
2019-04-10 19:00:06 -0400

    https://imagemagick.org/discourse-server/viewtopic.php?f=3&t=35386

--- coders/psd.c
+++ coders/psd.c
@@ -1637,8 +1637,10 @@ static inline MagickBooleanType PSDSkipImage(const ImageInfo *image_info,
     return(MagickFalse);
   if (index < image_info->scene)
     return(MagickTrue);
+  /*
   if (index > image_info->scene+image_info->number_scenes-1)
     return(MagickTrue);
+  */
   return(MagickFalse);
 }
 
