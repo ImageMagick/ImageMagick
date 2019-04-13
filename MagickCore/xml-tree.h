0899b5fbcb8bd52106d10a6220e45afd2201aec5
Cristy <mikayla-grace@urban-warrior.org>
2019-04-13 09:05:54 -0400

    ...

--- MagickCore/xml-tree.h
+++ MagickCore/xml-tree.h
@@ -5,7 +5,7 @@
   You may not use this file except in compliance with the License.  You may
   obtain a copy of the License at
 
-    http://imagemagick.org/MagicksToolkit/script/license.php
+    https://imagemagick.org/script/license.php
 
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
