bd46b6c9e4b337eddd165c37b7924d946ac39146
Cristy <mikayla-grace@urban-warrior.org>
2019-06-06 19:08:19 -0400

    ...

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -2508,13 +2508,11 @@ static MagickBooleanType RenderMVGContent(Image *image,
       if (status == MagickFalse)
         return(MagickFalse);
     }
-  primitive=(char *) NULL;
-  if (*draw_info->primitive != '@')
-    primitive=AcquireString(draw_info->primitive);
+  if ((*draw_info->primitive == '@') && (strlen(draw_info->primitive) > 1) &&
+      (*(draw_info->primitive+1) != '-') && (depth == 1))
+    primitive=FileToString(draw_info->primitive+1,~0UL,exception);
   else
-    if ((strlen(draw_info->primitive) > 1) &&
-        (*(draw_info->primitive+1) != '-') && (depth == 1))
-      primitive=FileToString(draw_info->primitive+1,~0UL,exception);
+    primitive=AcquireString(draw_info->primitive);
   if (primitive == (char *) NULL)
     return(MagickFalse);
   primitive_extent=(double) strlen(primitive);
