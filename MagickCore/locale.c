9c42ac0c2c7973befd11ba899cd2bf16744bae6b
Cristy <urban-warrior@imagemagick.org>
2019-06-16 19:14:29 -0400

    https://github.com/ImageMagick/ImageMagick6/issues/42

--- MagickCore/locale.c
+++ MagickCore/locale.c
@@ -66,7 +66,7 @@
 /*
   Define declarations.
 */
-#if defined(MAGICKCORE_HAVE_NEWLOCALE) || defined(MAGICKCORE_WINDOWS_SUPPORT)
+#if (defined(MAGICKCORE_HAVE_NEWLOCALE) || defined(MAGICKCORE_WINDOWS_SUPPORT)) && !defined(__MINGW32__)
 #  define MAGICKCORE_LOCALE_SUPPORT
 #endif
 #define LocaleFilename  "locale.xml"
