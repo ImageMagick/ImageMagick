9c63a7092f055df83b02916679f2a1ffc5e7b4ec
Cristy <urban-warrior@imagemagick.org>
2019-08-07 17:57:19 -0400

    Eliminate compiler warning

--- coders/tim2.c
+++ coders/tim2.c
@@ -180,10 +180,10 @@ static inline Quantum GetChannelValue(unsigned int word,unsigned char channel,
   {
     case RGBA16:
       /* Documentation specifies padding with zeros for converting from 5 to 8 bits. */
-      return ScaleCharToQuantum((word>>channel*5 & ~(~0x0<<5))<<3);
+      return ScaleCharToQuantum((word>>channel*5 & ~(~0x0U<<5))<<3);
     case RGB24:
     case RGBA32:
-      return ScaleCharToQuantum(word>>channel*8 & ~(~0x0<<8));
+      return ScaleCharToQuantum(word>>channel*8 & ~(~0x0U<<8));
     default:
       return -1;
   }
