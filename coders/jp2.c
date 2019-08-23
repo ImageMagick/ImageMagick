752559562758877184e6d35e03f93b9830824809
Dirk Lemstra <dirk@lemstra.org>
2019-08-23 17:46:10 +0200

    Reverted incorrect patch for #1676.

--- coders/jp2.c
+++ coders/jp2.c
@@ -147,9 +147,9 @@ static MagickBooleanType IsJP2(const unsigned char *magick,const size_t length)
     return(MagickFalse);
   if (memcmp(magick,"\x0d\x0a\x87\x0a",4) == 0)
     return(MagickTrue);
-  if (length < 16)
+  if (length < 12)
     return(MagickFalse);
-  if (memcmp(magick+4,"\x00\x00\x00\x0c\x6a\x50\x20\x20\x0d\x0a\x87\x0a",12) == 0)
+  if (memcmp(magick,"\x00\x00\x00\x0c\x6a\x50\x20\x20\x0d\x0a\x87\x0a",12) == 0)
     return(MagickTrue);
   return(MagickFalse);
 }
