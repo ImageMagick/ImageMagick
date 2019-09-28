7e937afd3a10ffc22f4dda1a2553b93ffba54db9
Cristy <urban-warrior@imagemagick.org>
2019-09-28 08:25:13 -0400

    ...

--- MagickCore/annotate.c
+++ MagickCore/annotate.c
@@ -1317,7 +1317,7 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
   FT_UInt
     first_glyph_id,
     last_glyph_id,
-    space_glyph_id;
+    missing_glyph_id;
 
   FT_Vector
     origin;
@@ -1567,7 +1567,7 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
   grapheme=(GraphemeInfo *) NULL;
   length=ComplexTextLayout(image,draw_info,p,strlen(p),face,flags,&grapheme,
     exception);
-  space_glyph_id=FT_Get_Char_Index(face,' ');
+  missing_glyph_id=FT_Get_Char_Index(face,' ');
   code=0;
   for (i=0; i < (ssize_t) length; i++)
   {
@@ -1579,7 +1579,7 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
     */
     glyph.id=(FT_UInt) grapheme[i].index;
     if (glyph.id == 0)
-      glyph.id=space_glyph_id;
+      glyph.id=missing_glyph_id;
     if ((glyph.id != 0) && (last_glyph_id != 0))
       origin.x+=(FT_Pos) (64.0*draw_info->kerning);
     glyph.origin=origin;
