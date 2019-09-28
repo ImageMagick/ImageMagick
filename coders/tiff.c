9627eebf6a034834699dcc05023bb06733618f6b
Cristy <urban-warrior@imagemagick.org>
2019-09-28 16:20:53 -0400

    https://imagemagick.org/discourse-server/viewtopic.php?f=1&t=36792

--- coders/tiff.c
+++ coders/tiff.c
@@ -3633,8 +3633,7 @@ static MagickBooleanType WriteTIFFImage(const ImageInfo *image_info,
     }
   mode=endian_type == LSBEndian ? "wl" : "wb";
 #if defined(TIFF_VERSION_BIG)
-  if ((LocaleCompare(image_info->magick,"TIFF64") == 0) ||
-      (((MagickOffsetType) image->columns*image->rows) > SSIZE_MAX))
+  if (LocaleCompare(image_info->magick,"TIFF64") == 0)
     mode=endian_type == LSBEndian ? "wl8" : "wb8";
 #endif
   tiff=TIFFClientOpen(image->filename,mode,(thandle_t) image,TIFFReadBlob,
