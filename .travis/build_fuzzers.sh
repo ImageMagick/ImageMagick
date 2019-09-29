4d0cb28ff366ef75c414443f106b5df667bc1f32
Alex Gaynor <alex.gaynor@gmail.com>
2019-09-29 10:33:35 -0400

    Don't link the fuzzing engine while compiling encoder_list (#1707)

--- .travis/build_fuzzers.sh
+++ .travis/build_fuzzers.sh
@@ -5,6 +5,7 @@ MAGICK_COMPILER_FLAGS="-DBUILD_TRAVIS=1"
 MAGICK_INCLUDE="appdir/usr/include/ImageMagick-7"
 MAGICK_SRC="Magick++/fuzz"
 MAGICK_LIBS="-Lappdir/usr/lib -lz -lpng -lfontconfig -lxcb -lX11 -lXext -lbz2 -ljpeg -ljbig -lIlmImf -ldjvulibre -ltiff -llqr-1 -lwmf -lwmflite -lraqm -lxml2 -llzma -llcms2 -lpthread -lfreetype -lMagick++-7.Q16 -lMagickWand-7.Q16 -lMagickCore-7.Q16 -lstdc++"
+MAGICK_LIBS_NO_FUZZ="$MAGICK_LIBS"
 MAGICK_OUTPUT="Magick++/fuzz"
 MAGICK_FAST_BUILD=1
 
