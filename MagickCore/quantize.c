d44f8a35558951a21367d306a42e5a097f3a43fe
Cristy <urban-warrior@imagemagick.org>
2019-10-05 09:50:46 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1720

--- MagickCore/quantize.c
+++ MagickCore/quantize.c
@@ -3282,17 +3282,17 @@ extern "C" {
 
 static int IntensityCompare(const void *x,const void *y)
 {
-  double
-    intensity;
-
   PixelInfo
     *color_1,
     *color_2;
 
+  ssize_t
+    intensity;
+
   color_1=(PixelInfo *) x;
   color_2=(PixelInfo *) y;
-  intensity=GetPixelInfoIntensity((const Image *) NULL,color_1)-
-    GetPixelInfoIntensity((const Image *) NULL,color_2);
+  intensity=(ssize_t) (GetPixelInfoIntensity((const Image *) NULL,color_1)-
+    GetPixelInfoIntensity((const Image *) NULL,color_2));
   return((int) intensity);
 }
 
