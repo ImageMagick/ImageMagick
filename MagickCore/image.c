f28e9e56e1b56d4e1f09d2a56d70892ae295d6a4
Cristy <urban-warrior@imagemagick.org>
2019-10-16 15:00:53 -0400

    https://github.com/ImageMagick/ImageMagick/issues/1756

--- MagickCore/image.c
+++ MagickCore/image.c
@@ -2661,8 +2661,18 @@ MagickExport MagickBooleanType SetImageExtent(Image *image,const size_t columns,
     ThrowBinaryException(ImageError,"NegativeOrZeroImageSize",image->filename);
   image->columns=columns;
   image->rows=rows;
-  if ((image->depth == 0) || (image->depth > (8*sizeof(MagickSizeType))))
-    ThrowBinaryException(ImageError,"ImageDepthNotSupported",image->filename);
+  if (image->depth == 0)
+    {
+      image->depth=8;
+      (void) ThrowMagickException(exception,GetMagickModule(),ImageError,
+        "ImageDepthNotSupported","`%s'",image->filename);
+    }
+  if (image->depth > (8*sizeof(MagickSizeType)))
+    {
+      image->depth=8*sizeof(MagickSizeType);
+      (void) ThrowMagickException(exception,GetMagickModule(),ImageError,
+        "ImageDepthNotSupported","`%s'",image->filename);
+    }
   return(SyncImagePixelCache(image,exception));
 }
 
