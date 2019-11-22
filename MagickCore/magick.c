33491ebb8f3906f26c985084ccf88c079892cea3
Michael Witten <mfwitten@gmail.com>
2019-11-22 00:43:31 +0000

    configure: Remove obsolete `AC_TYPE_SIGNAL' test and `RETSIGTYPE' macro
    Consider this documention for GNU Autoconf 2.69:
    
      https://www.gnu.org/software/autoconf/manual/autoconf-2.69/html_node/Obsolete-Macros.html#index-AC_005fTYPE_005fSIGNAL-2213
      info '(autoconf)Obsolete Macros'
    
    It says that `AC_TYPE_SIGNAL' is obsolete; in particular, it says:
    
      -- Macro: AC_TYPE_SIGNAL
          If `signal.h' declares `signal' as  returning a pointer to a
          function returning `void', define `RETSIGTYPE' to be `void';
          otherwise, define it to be `int'. These days, it is portable
          to  assume  C89, and  that  signal  handlers return  `void',
          without needing to use this macro or `RETSIGTYPE'.
    
          When targeting older K&R C,  it is possible to define signal
          handlers as  returning type `RETSIGTYPE', and  omit a return
          statement:
    
               RETSIGTYPE
               hup_handler ()
               {
               ...
               }
    
    I'm not sure the code base is really even compatible with C89, let
    alone K&R C;  there's just no point in continuing  to support this
    possibility.

--- MagickCore/magick.c
+++ MagickCore/magick.c
@@ -103,9 +103,6 @@
 /*
   Define declarations.
 */
-#if !defined(MAGICKCORE_RETSIGTYPE)
-# define MAGICKCORE_RETSIGTYPE  void
-#endif
 #if !defined(SIG_DFL)
 # define SIG_DFL  ((SignalHandler *) 0)
 #endif
@@ -119,8 +116,7 @@
 /*
   Typedef declarations.
 */
-typedef MAGICKCORE_RETSIGTYPE
-  SignalHandler(int);
+typedef void SignalHandler(int);
 
 /*
   Global declarations.
