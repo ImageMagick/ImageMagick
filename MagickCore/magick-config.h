3e50e1453b2b26d5bf8169e03a03b8773c5155ba
Cristy <mikayla-grace@urban-warrior.org>
2019-12-07 13:00:51 -0500

    https://github.com/ImageMagick/ImageMagick/pull/1798

--- MagickCore/magick-config.h
+++ MagickCore/magick-config.h
@@ -197,6 +197,46 @@ extern "C" {
 #endif
 
 #endif
+
+/* for Clang compatibility */
+#ifndef __has_builtin
+#  define __has_builtin(x) 0
+#endif
+
+#if __STDC_VERSION__ >= 201100L
+# define MAGICKCORE_HAVE_STDC_ALIGNED_ALLOC 1
+#endif
+
+#if defined(__GNUC__) && !defined(__clang__)
+# define MAGICKCORE_DIAGNOSTIC_PUSH() \
+   _Pragma("GCC diagnostic push")
+# define MAGICKCORE_DIAGNOSTIC_IGNORE_MAYBE_UNINITIALIZED() \
+   _Pragma("GCC diagnostic ignored \"-Wmaybe-uninitialized\"")
+# define MAGICKCORE_DIAGNOSTIC_POP() \
+   _Pragma("GCC diagnostic pop")
+#else
+# define MAGICKCORE_DIAGNOSTIC_PUSH()
+# define MAGICKCORE_DIAGNOSTIC_IGNORE_MAYBE_UNINITIALIZED()
+# define MAGICKCORE_DIAGNOSTIC_POP()
+#endif
+
+#define MAGICKCORE_BITS_BELOW(power_of_2) \
+  ((power_of_2)-1)
+
+#define MAGICKCORE_MAX_ALIGNMENT_PADDING(power_of_2) \
+  MAGICKCORE_BITS_BELOW(power_of_2)
+
+#define MAGICKCORE_IS_NOT_ALIGNED(n, power_of_2) \
+  ((n) & MAGICKCORE_BITS_BELOW(power_of_2))
+
+#define MAGICKCORE_IS_NOT_POWER_OF_2(n) \
+  MAGICKCORE_IS_NOT_ALIGNED((n), (n))
+
+#define MAGICKCORE_ALIGN_DOWN(n, power_of_2) \
+  ((n) & ~MAGICKCORE_BITS_BELOW(power_of_2))
+
+#define MAGICKCORE_ALIGN_UP(n, power_of_2) \
+  MAGICKCORE_ALIGN_DOWN((n) + MAGICKCORE_MAX_ALIGNMENT_PADDING(power_of_2),power_of_2)
  
 #if defined(__cplusplus) || defined(c_plusplus)
 }
