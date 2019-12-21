f2ffcebad9ff02550c10f2e6424c59e749c9ec0d
Cristy <mikayla-grace@urban-warrior.org>
2019-12-20 21:25:37 -0500

    ...

--- MagickCore/memory_.h
+++ MagickCore/memory_.h
@@ -95,7 +95,7 @@ inline MagickExport MagickBooleanType HeapOverflowSanityCheckGetSize(
       errno=ENOMEM;
       return(MagickTrue);
     }
-  assert(extent != 0);
+  assert(extent != (size_t *const) NULL);
   *extent=length;
   return(MagickFalse);
 }
