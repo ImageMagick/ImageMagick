b0799c4851cbaa95e7e6858c3cb15e8461e576d4
Cristy <mikayla-grace@urban-warrior.org>
2019-12-27 12:57:53 -0500

    https://github.com/ImageMagick/ImageMagick/issues/1817

--- MagickCore/quantize.c
+++ MagickCore/quantize.c
@@ -653,7 +653,8 @@ static MagickBooleanType AssignImageColors(Image *image,CubeInfo *cube_info,
       /*
         Monochrome image.
       */
-      intensity=0.0;
+      intensity=GetPixelInfoLuma(image->colormap+0) < QuantumRange/2.0 ? 0.0 :
+        QuantumRange;
       if ((image->colors > 1) &&
           (GetPixelInfoLuma(image->colormap+0) >
            GetPixelInfoLuma(image->colormap+1)))
