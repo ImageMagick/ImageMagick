2f0e0637d321aa57f610e122cbba407cd98465ea
Cristy <urban-warrior@imagemagick.org>
2019-12-28 13:59:38 -0500

    Not all compilers can grok %zd

--- MagickCore/quantize.c
+++ MagickCore/quantize.c
@@ -2699,8 +2699,9 @@ MagickExport MagickBooleanType KmeansImage(Image *image,
       distortion+=kmeans_pixels[0][i].distortion;
     }
     if (verbose != MagickFalse)
-      (void) (void) FormatLocaleFile(stderr,"distortion[%zd]: %g %g\n",n,
-        distortion,fabs(distortion-previous_distortion));
+      (void) FormatLocaleFile(stderr,"distortion[%.20g]: %*g %*g\n",(double) n,
+        GetMagickPrecision(),distortion,GetMagickPrecision(),
+        fabs(distortion-previous_distortion));
     if (fabs(distortion-previous_distortion) <= max_distortion)
       break;
     previous_distortion=distortion;
