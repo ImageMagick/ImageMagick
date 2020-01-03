1410b61985d074d39714794eec61f9acf1bf0db1
Cristy <mikayla-grace@urban-warrior.org>
2020-01-03 16:37:33 -0500

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=19769

--- MagickCore/fx.c
+++ MagickCore/fx.c
@@ -1248,6 +1248,8 @@ static inline MagickBooleanType IsFxFunction(const char *expression,
     c;
 
   c=name[length];
+  if (c == '\0')
+    return(MagickFalse);
   if ((LocaleNCompare(expression,name,length) == 0) &&
       ((isspace(c) == 0) || (c == '(')))
     return(MagickTrue);
