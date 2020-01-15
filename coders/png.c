84f30814779e5076d96664fd3ed808bd9aeaac8d
Cristy <mikayla-grace@urban-warrior.org>
2020-01-14 19:05:20 -0500

    https://github.com/ImageMagick/ImageMagick/issues/1833

--- coders/png.c
+++ coders/png.c
@@ -3957,7 +3957,8 @@ static Image *ReadOnePNGImage(MngInfo *mng_info,
 
                 (void) FormatLocaleString(key,MagickPathExtent,"%s",
                   text[i].key);
-                if (LocaleCompare(key,"version") == 0)
+                if ((LocaleCompare(key,"version") == 0) ||
+                    (LocaleCompare(key,"width") == 0))
                   (void) FormatLocaleString(key,MagickPathExtent,"png:%s",
                     text[i].key);
                 (void) SetImageProperty(image,key,value,exception);
