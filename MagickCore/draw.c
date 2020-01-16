b57d856e66e56629d9e7a7203b69fa188a573378
Cristy <mikayla-grace@urban-warrior.org>
2020-01-15 19:41:50 -0500

    ...

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -7080,34 +7080,19 @@ static PrimitiveInfo *TraceStrokePolygon(const Image *image,
 {
 #define MaxStrokePad  (6*BezierQuantum+360)
 #define CheckPathExtent(pad) \
-  if ((ssize_t) (p+(pad)) >= (ssize_t) max_strokes) \
+  if (((ssize_t) (p+(pad)) >= (ssize_t) max_strokes) || \
+      ((ssize_t) (q+(pad)) >= (ssize_t) max_strokes)) \
     { \
       if (~max_strokes < (pad)) \
-        path_p=(PointInfo *) RelinquishMagickMemory(path_p); \
+        { \
+          path_p=(PointInfo *) RelinquishMagickMemory(path_p); \
+          path_q=(PointInfo *) RelinquishMagickMemory(path_q); \
+        } \
       else \
         { \
           max_strokes+=(pad); \
           path_p=(PointInfo *) ResizeQuantumMemory(path_p,max_strokes+ \
             MaxStrokePad,sizeof(*path_p)); \
-        } \
-      if ((path_p == (PointInfo *) NULL) || (path_q == (PointInfo *) NULL)) \
-        { \
-          if (path_p != (PointInfo *) NULL) \
-            path_p=(PointInfo *) RelinquishMagickMemory(path_p); \
-          if (path_q != (PointInfo *) NULL) \
-            path_q=(PointInfo *) RelinquishMagickMemory(path_q); \
-          polygon_primitive=(PrimitiveInfo *) \
-            RelinquishMagickMemory(polygon_primitive); \
-          return((PrimitiveInfo *) NULL); \
-        } \
-    } \
-  if ((ssize_t) (q+(pad)) >= (ssize_t) max_strokes) \
-    { \
-      if (~max_strokes < (pad)) \
-        path_q=(PointInfo *) RelinquishMagickMemory(path_q); \
-      else \
-        { \
-          max_strokes+=(pad); \
           path_q=(PointInfo *) ResizeQuantumMemory(path_q,max_strokes+ \
             MaxStrokePad,sizeof(*path_q)); \
         } \
