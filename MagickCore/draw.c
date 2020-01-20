041b0d2fe76bafc21271952160952d84b8d89719
Dirk Lemstra <dirk@lemstra.org>
2020-01-20 07:46:23 +0100

    Moved the call to RelinquishMagickMemory inside the if.

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -992,10 +992,13 @@ static PolygonInfo *DestroyPolygonInfo(PolygonInfo *polygon_info)
     i;
 
   if (polygon_info->edges != (EdgeInfo *) NULL)
-    for (i=0; i < (ssize_t) polygon_info->number_edges; i++)
-      polygon_info->edges[i].points=(PointInfo *)
-        RelinquishMagickMemory(polygon_info->edges[i].points);
-  polygon_info->edges=(EdgeInfo *) RelinquishMagickMemory(polygon_info->edges);
+    {
+      for (i=0; i < (ssize_t) polygon_info->number_edges; i++)
+        polygon_info->edges[i].points=(PointInfo *)
+          RelinquishMagickMemory(polygon_info->edges[i].points);
+      polygon_info->edges=(EdgeInfo *) RelinquishMagickMemory(
+        polygon_info->edges);
+    }
   return((PolygonInfo *) RelinquishMagickMemory(polygon_info));
 }
 
