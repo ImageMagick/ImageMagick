290cb939a8eeffd489f5198d999c57f65518c6c3
Cristy <mikayla-grace@urban-warrior.org>
2020-02-01 09:38:47 -0500

    ...

--- MagickCore/vision.c
+++ MagickCore/vision.c
@@ -516,10 +516,10 @@ MagickExport Image *ConnectedComponentsImage(const Image *image,
             p+=GetPixelChannels(component_image);
           }
         }
-        census=0;
+        census=object[0].census;
         id=0;
-        for (j=0; j < (ssize_t) component_image->colors; j++)
-          if (census > object[j].census)
+        for (j=1; j < (ssize_t) component_image->colors; j++)
+          if (census < object[j].census)
             {
               census=object[j].census;
               id=(size_t) j;
