d6ea9ef1f0fc808b6a506dc640e7bcd2884d4d90
Cristy <mikayla-grace@urban-warrior.org>
2020-02-26 18:18:10 -0500

    Write CCITTFAX4 compression in one strip

--- coders/tiff.c
+++ coders/tiff.c
@@ -3992,7 +3992,7 @@ RestoreMSCWarning
     */
     if (GetTIFFInfo(image_info,tiff,&tiff_info) == MagickFalse)
       ThrowWriterException(ResourceLimitError,"MemoryAllocationFailed");
-    if (image->depth == 1)
+    if (compression == COMPRESSION_CCITTFAX4)
       (void) TIFFSetField(tiff,TIFFTAG_ROWSPERSTRIP,(uint32) image->rows);
     quantum_info->endian=LSBEndian;
     pixels=(unsigned char *) GetQuantumPixels(quantum_info);
