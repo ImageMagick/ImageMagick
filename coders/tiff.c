2f60a4ae1eef0394a72ceac6d7f402f830baacf0
Dirk Lemstra <dirk@lemstra.org>
2020-03-01 17:22:20 +0100

    Corrected conversion from string.

--- coders/tiff.c
+++ coders/tiff.c
@@ -2925,7 +2925,7 @@ static MagickBooleanType GetTIFFInfo(const ImageInfo *image_info,
         rows_per_strip=rows;
       option=GetImageOption(image_info,"tiff:rows-per-strip");
       if (option != (const char *) NULL)
-        rows_per_strip=(size_t) strtol(option,(char **) NULL,10);
+        rows_per_strip=(uint32) strtoul(option,(char **) NULL,10);
       rows_per_strip=TIFFDefaultStripSize(tiff,rows_per_strip);
       (void) TIFFSetField(tiff,TIFFTAG_ROWSPERSTRIP,rows_per_strip);
       return(MagickTrue);
