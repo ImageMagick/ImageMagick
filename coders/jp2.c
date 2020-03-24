b84fd4f61bcc7f107a95614fb085f78c68a9aa83
Yoshihiro Yamazaki <yoya@awm.jp>
2020-03-25 03:27:03 +0900

    Fixed a bug in multi-value of jp:quality BUG: cp_disto_alloc is not set back to 0 by only setting cp_fixed_quality=1 when specifying -define jp2:quality (and -quality)
    ref) https://github.com/ImageMagick/ImageMagick/issues/1873

--- coders/jp2.c
+++ coders/jp2.c
@@ -862,6 +862,7 @@ static MagickBooleanType WriteJP2Image(const ImageInfo *image_info,Image *image,
     {
       parameters->tcp_distoratio[0]=(double) image_info->quality;
       parameters->cp_fixed_quality=OPJ_TRUE;
+      parameters->cp_disto_alloc=0;
     }
   if (image_info->extract != (char *) NULL)
     {
@@ -910,6 +911,7 @@ static MagickBooleanType WriteJP2Image(const ImageInfo *image_info,Image *image,
       }
       parameters->tcp_numlayers=i+1;
       parameters->cp_fixed_quality=OPJ_TRUE;
+      parameters->cp_disto_alloc=0;
     }
   option=GetImageOption(image_info,"jp2:progression-order");
   if (option != (const char *) NULL)
