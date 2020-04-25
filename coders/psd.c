fb1d47b5ba2e42262e06e3cf584ce45ff9058f5f
Dirk Lemstra <dirk@lemstra.org>
2020-04-25 13:38:39 +0200

    Added method that will read the layer names from the additional info because they sometimes get trimmed in the layer info.

--- coders/psd.c
+++ coders/psd.c
@@ -1656,6 +1656,76 @@ static void CheckMergedImageAlpha(const PSDInfo *psd_info,Image *image)
     image->alpha_trait=BlendPixelTrait;
 }
 
+static void ParseAdditionalInfo(LayerInfo *layer_info)
+{
+  char
+    key[5];
+
+  MagickBooleanType
+    found;
+
+  register size_t
+    i;
+
+  size_t
+    remaining_length;
+
+  unsigned char
+    *p;
+
+  unsigned int
+    size;
+
+  p=GetStringInfoDatum(layer_info->info);
+  remaining_length=GetStringInfoLength(layer_info->info);
+  while (remaining_length >= 12)
+  {
+    /* skip over signature */
+    p+=4;
+    key[0]=(char) (*p++);
+    key[1]=(char) (*p++);
+    key[2]=(char) (*p++);
+    key[3]=(char) (*p++);
+    key[4]='\0';
+    size=(unsigned int) (*p++) << 24;
+    size|=(unsigned int) (*p++) << 16;
+    size|=(unsigned int) (*p++) << 8;
+    size|=(unsigned int) (*p++);
+    size=size & 0xffffffff;
+    remaining_length-=12;
+    if ((size_t) size > remaining_length)
+      break;
+    if (LocaleNCompare(key,"luni",sizeof(key)) == 0)
+      {
+        unsigned char
+          *name;
+
+        unsigned int
+          length;
+
+        p+=4;
+        length=(size-4)/2;
+        if (sizeof(layer_info->name) <= length)
+          break;
+        name=layer_info->name;
+        while (length > 0)
+        {
+          /* Only ASCII strings are supported */
+          if (*p++ != '\0')
+            break;
+          *name++=*p++;
+          length--;
+        }
+        if (length == 0)
+          *name='\0';
+        break;
+      }
+    else
+      p+=size;
+    remaining_length-=(size_t) size;
+  }
+}
+
 static MagickBooleanType ReadPSDLayersInternal(Image *image,
   const ImageInfo *image_info,const PSDInfo *psd_info,
   const MagickBooleanType skip_layers,ExceptionInfo *exception)
@@ -1958,6 +2028,7 @@ static MagickBooleanType ReadPSDLayersInternal(Image *image,
             layer_info[i].info=AcquireStringInfo((const size_t) length);
             info=GetStringInfoDatum(layer_info[i].info);
             (void) ReadBlob(image,(const size_t) length,info);
+            ParseAdditionalInfo(&layer_info[i]);
           }
       }
   }
