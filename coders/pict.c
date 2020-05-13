fbfa66173b716b01145ab08728b7c5c563899347
Cristy <mikayla-grace@urban-warrior.org>
2020-05-13 06:35:06 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2218

--- coders/pict.c
+++ coders/pict.c
@@ -1170,6 +1170,7 @@ static Image *ReadPICTImage(const ImageInfo *image_info,
               (size_t) (frame.bottom-frame.top),MagickTrue,exception);
             if (tile_image == (Image *) NULL)
               ThrowPICTException(CorruptImageError,"ImproperImageHeader");
+            status=ResetImagePixels(tile_image,exception);
             if ((code == 0x9a) || (code == 0x9b) ||
                 ((bytes_per_line & 0x8000) != 0))
               {
