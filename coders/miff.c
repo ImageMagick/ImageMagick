01d87f023e5bb1cf09f8fe8aa6ea377f6fda0f83
Cristy <mikayla-grace@urban-warrior.org>
2020-05-31 12:54:57 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=22865

--- coders/miff.c
+++ coders/miff.c
@@ -1175,7 +1175,7 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
         while (name != (const char *) NULL)
         {
           length=ReadBlobMSBLong(image);
-          if ((MagickSizeType) length > GetBlobSize(image))
+          if ((length == 0) || ((MagickSizeType) length > GetBlobSize(image)))
             break;
           profile=AcquireStringInfo(length);
           if (profile == (StringInfo *) NULL)
