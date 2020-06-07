ad86bdfef9e22db10d54e5d414f7d44cf13a6e95
Cristy <mikayla-grace@urban-warrior.org>
2020-06-07 11:43:31 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=23163

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -4095,6 +4095,13 @@ static MagickBooleanType RenderMVGContent(Image *image,
         beta=bounds.y2-bounds.y1;
         radius=hypot(alpha,beta);
         coordinates=2.0*(ceil(MagickPI*radius))+6.0*BezierQuantum+360.0;
+        if (coordinates > (107*BezierQuantum))
+          {
+            (void) ThrowMagickException(exception,GetMagickModule(),DrawError,
+              "TooManyBezierCoordinates","`%s'",token);
+            status=MagickFalse;
+            break;
+          }
         break;
       }
       default:
