c2876582f15396bd1f0651671d72f664655ea095
Cristy <mikayla-grace@urban-warrior.org>
2020-06-08 07:41:02 -0400

    ...

--- MagickCore/property.c
+++ MagickCore/property.c
@@ -3942,8 +3942,7 @@ RestoreMSCWarning
               const char
                 *value;
 
-              (void) QueryColorname(property_image,&pixel,SVGCompliance,name,
-                exception);
+              GetColorTuple(&pixel,MagickFalse,name);
               value=GetImageArtifact(property_image,"pixel:compliance");
               if (value != (char *) NULL)
                 {
