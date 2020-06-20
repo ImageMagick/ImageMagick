53183ce33842f4fc241b56d7f285338acfc3e032
Dirk Lemstra <dirk@lemstra.org>
2020-06-20 14:14:21 +0200

    Whitespace fix.

--- coders/heic.c
+++ coders/heic.c
@@ -567,7 +567,7 @@ static MagickBooleanType IsHEIC(const unsigned char *magick,const size_t length)
   if (length < 12)
     return(MagickFalse);
   if (LocaleNCompare((const char *) magick+4,"ftyp",4) != 0)
-  return(MagickFalse);
+    return(MagickFalse);
   if (LocaleNCompare((const char *) magick+8,"heic",4) == 0)
     return(MagickTrue);
   if (LocaleNCompare((const char *) magick+8,"heix",4) == 0)
