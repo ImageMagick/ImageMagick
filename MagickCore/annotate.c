d91c43f3b92c1bf33e38bf54aee5c6303d54ebb4
Cristy <mikayla-grace@urban-warrior.org>
2020-06-20 11:03:25 -0400

    https://github.com/ImageMagick/ImageMagick/issues/2178

--- MagickCore/annotate.c
+++ MagickCore/annotate.c
@@ -335,8 +335,10 @@ MagickExport MagickBooleanType AnnotateImage(Image *image,
     (void) CloneString(&annotate->text,textlist[i]);
     if ((metrics.width == 0) || (annotate->gravity != NorthWestGravity))
       (void) GetTypeMetrics(image,annotate,&metrics,exception);
-    height=(ssize_t) (metrics.ascent-metrics.descent+
-      draw_info->interline_spacing+0.5);
+    height=(size_t) ceil(metrics.ascent-metrics.descent+
+      draw_info->interline_spacing-0.5);
+    if (height == 0)
+      height=(size_t) draw_info->pointsize;
     switch (annotate->gravity)
     {
       case UndefinedGravity:
