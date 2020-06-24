63dd8a48104f0b3c2807a05c3bd2a7c1990825c9
Remi Collet <remi@remirepo.net>
2020-06-24 13:50:35 +0200

    Issue 85 (#2195)
    * Revert "https://github.com/ImageMagick/ImageMagick6/issues/85"
    
    This reverts commit d01b72cdd51e19fe15e51f4189e97fbd4f0989a0.
    
    * Revert "https://github.com/ImageMagick/ImageMagick/issues/2178"
    
    This reverts commit d91c43f3b92c1bf33e38bf54aee5c6303d54ebb4.
    
    * Fix regression in DrawSetTextInterlineSpacing

--- MagickCore/annotate.c
+++ MagickCore/annotate.c
@@ -335,11 +335,8 @@ MagickExport MagickBooleanType AnnotateImage(Image *image,
     (void) CloneString(&annotate->text,textlist[i]);
     if ((metrics.width == 0) || (annotate->gravity != NorthWestGravity))
       (void) GetTypeMetrics(image,annotate,&metrics,exception);
-    height=(size_t) ceil(metrics.ascent-metrics.descent+
-      draw_info->interline_spacing-0.5);
-    if (height == 0)
-      height=(size_t) ceil(draw_info->pointsize+draw_info->interline_spacing-
-        0.5);
+    height=(ssize_t) (metrics.ascent-metrics.descent+
+      draw_info->interline_spacing+0.5);
     switch (annotate->gravity)
     {
       case UndefinedGravity:
