5cd2cc85b70d03aaad29aea22f61d7d27ee03c38
Cristy <mikayla-grace@urban-warrior.org>
2020-07-02 12:07:07 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=23855

--- coders/ept.c
+++ coders/ept.c
@@ -249,8 +249,7 @@ static Image *ReadEPTImage(const ImageInfo *image_info,ExceptionInfo *exception)
   postscript_data=ReadBlobStream(image,ept_info.postscript_length,
     ept_info.postscript,&count);
   if (count != (ssize_t) (ept_info.postscript_length))
-    (void) ThrowMagickException(exception,GetMagickModule(),CorruptImageWarning,
-      "InsufficientImageDataInFile","`%s'",image->filename);
+    ThrowReaderException(CorruptImageError,"InsufficientImageDataInFile");
   (void) CloseBlob(image);
   image=DestroyImage(image);
   read_info=CloneImageInfo(image_info);
