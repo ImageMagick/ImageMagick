aabb6b7f0565c7cb6154e663eacb904adefec6b4
Dirk Lemstra <dirk@lemstra.org>
2020-07-12 18:44:55 +0200

    Corrected statement.

--- coders/psd.c
+++ coders/psd.c
@@ -1731,7 +1731,7 @@ static void CheckMergedImageAlpha(const PSDInfo *psd_info,Image *image)
     The number of layers cannot be used to determine if the merged image
     contains an alpha channel. So we enable it when we think we should.
   */
-  if (((psd_info->mode == GrayscaleMode) && (psd_info->channels > 2)) ||
+  if (((psd_info->mode == GrayscaleMode) && (psd_info->channels > 1)) ||
       ((psd_info->mode == RGBMode) && (psd_info->channels > 3)) ||
       ((psd_info->mode == CMYKMode) && (psd_info->channels > 4)))
     image->alpha_trait=BlendPixelTrait;
