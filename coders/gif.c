9456300a07627676061d6ba991621ce52361dbaf
Cristy <mikayla-grace@urban-warrior.org>
2020-08-17 17:02:59 -0400

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=22534

--- coders/gif.c
+++ coders/gif.c
@@ -1175,6 +1175,7 @@ static Image *ReadGIFImage(const ImageInfo *image_info,ExceptionInfo *exception)
                 if (info == (unsigned char *) NULL)
                   ThrowGIFException(ResourceLimitError,
                     "MemoryAllocationFailed");
+                (void) memset(info,0,reserved_length*sizeof(*info));
                 for (info_length=0; ; )
                 {
                   block_length=(int) ReadBlobBlock(image,info+info_length);
