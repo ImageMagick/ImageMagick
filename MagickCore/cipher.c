b29cd49dfbe2820a165850625d8e3f4a622d27d6
Cristy <mikayla-grace@urban-warrior.org>
2020-08-22 13:17:26 -0400

    ...

--- MagickCore/cipher.c
+++ MagickCore/cipher.c
@@ -290,7 +290,7 @@ static inline void AddRoundKey(const unsigned int *ciphertext,
     plaintext[i]=key[i] ^ ciphertext[i];
 }
 
-static inline unsigned char ByteMultiply(const unsigned char alpha,
+static inline unsigned int ByteMultiply(const unsigned char alpha,
   const unsigned char beta)
 {
   /*
@@ -298,7 +298,7 @@ static inline unsigned char ByteMultiply(const unsigned char alpha,
   */
   if ((alpha == 0) || (beta == 0))
     return(0);
-  return(InverseLog[(Log[alpha]+Log[beta]) % 0xff]);
+  return((unsigned int) InverseLog[(Log[alpha]+Log[beta]) % 0xff]);
 }
 
 static inline unsigned int ByteSubTransform(unsigned int x,
@@ -310,8 +310,10 @@ static inline unsigned int ByteSubTransform(unsigned int x,
   /*
     Non-linear layer resists differential and linear cryptoanalysis attacks.
   */
-  key=(s_box[x & 0xff]) | (s_box[(x >> 8) & 0xff] << 8) |
-    (s_box[(x >> 16) & 0xff] << 16) | (s_box[(x >> 24) & 0xff] << 24);
+  key=((unsigned int) s_box[x & 0xff]) |
+    ((unsigned int) s_box[(x >> 8) & 0xff] << 8) |
+    ((unsigned int) s_box[(x >> 16) & 0xff] << 16) |
+    ((unsigned int) s_box[(x >> 24) & 0xff] << 24);
   return(key);
 }
 
@@ -362,7 +364,7 @@ static void InitializeRoundKey(const unsigned char *ciphertext,
   {
     value=0;
     for (j=0; j < 4; j++)
-      value|=(*p++ << (8*j));
+      value|=((unsigned int) *p++ << (8*j));
     plaintext[i]=key[i] ^ value;
   }
   /*
