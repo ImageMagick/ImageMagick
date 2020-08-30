984497a5696e51e3bca0f83aebfdf5f6fad59c88
Cristy <mikayla-grace@urban-warrior.org>
2020-08-30 10:46:28 -0400

    read sun-path knot as signed integer

--- MagickCore/property.c
+++ MagickCore/property.c
@@ -1962,18 +1962,8 @@ static char *TracePSClippath(const unsigned char *blob,size_t length)
         */
         for (i=0; i < 3; i++)
         {
-          size_t
-            xx,
-            yy;
-
-          yy=(size_t) ReadPropertyMSBLong(&blob,&length);
-          xx=(size_t) ReadPropertyMSBLong(&blob,&length);
-          x=(ssize_t) xx;
-          if (xx > 2147483647)
-            x=(ssize_t) xx-4294967295U-1;
-          y=(ssize_t) yy;
-          if (yy > 2147483647)
-            y=(ssize_t) yy-4294967295U-1;
+          y=(ssize_t) ReadPropertyMSBLong(&blob,&length);
+          x=(ssize_t) ReadPropertyMSBLong(&blob,&length);
           point[i].x=(double) x/4096/4096;
           point[i].y=1.0-(double) y/4096/4096;
         }
