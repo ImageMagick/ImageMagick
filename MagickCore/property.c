9859944c1be9137029f9ae2c1dfcced614632166
Dirk Lemstra <dirk@lemstra.org>
2020-08-30 15:04:05 +0200

    Removed conversion from signed to unsigned and back to signed.

--- MagickCore/property.c
+++ MagickCore/property.c
@@ -2170,18 +2170,8 @@ static char *TraceSVGClippath(const unsigned char *blob,size_t length,
         */
         for (i=0; i < 3; i++)
         {
-          unsigned int
-            xx,
-            yy;
-
-          yy=(unsigned int) ReadPropertyMSBLong(&blob,&length);
-          xx=(unsigned int) ReadPropertyMSBLong(&blob,&length);
-          x=(ssize_t) xx;
-          if (xx > 2147483647)
-            x=(ssize_t) xx-4294967295U-1;
-          y=(ssize_t) yy;
-          if (yy > 2147483647)
-            y=(ssize_t) yy-4294967295U-1;
+          y=(ssize_t) ReadPropertyMSBLong(&blob,&length);
+          x=(ssize_t) ReadPropertyMSBLong(&blob,&length);
           point[i].x=(double) x*columns/4096/4096;
           point[i].y=(double) y*rows/4096/4096;
         }
