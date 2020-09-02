9f57fa78d1ea2dfc2fb2f29287f4ba8357505129
Cristy <mikayla-grace@urban-warrior.org>
2020-09-02 19:22:51 -0400

    https://github.com/ImageMagick/ImageMagick/issues/24

--- MagickCore/quantize.c
+++ MagickCore/quantize.c
@@ -2940,20 +2940,17 @@ static void PruneChild(CubeInfo *cube_info,const NodeInfo *node_info)
   for (i=0; i < (ssize_t) number_children; i++)
     if (node_info->child[i] != (NodeInfo *) NULL)
       PruneChild(cube_info,node_info->child[i]);
-  if (cube_info->nodes > cube_info->maximum_colors)
-    {
-      /*
-        Merge color statistics into parent.
-      */
-      parent=node_info->parent;
-      parent->number_unique+=node_info->number_unique;
-      parent->total_color.red+=node_info->total_color.red;
-      parent->total_color.green+=node_info->total_color.green;
-      parent->total_color.blue+=node_info->total_color.blue;
-      parent->total_color.alpha+=node_info->total_color.alpha;
-      parent->child[node_info->id]=(NodeInfo *) NULL;
-      cube_info->nodes--;
-    }
+  /*
+    Merge color statistics into parent.
+  */
+  parent=node_info->parent;
+  parent->number_unique+=node_info->number_unique;
+  parent->total_color.red+=node_info->total_color.red;
+  parent->total_color.green+=node_info->total_color.green;
+  parent->total_color.blue+=node_info->total_color.blue;
+  parent->total_color.alpha+=node_info->total_color.alpha;
+  parent->child[node_info->id]=(NodeInfo *) NULL;
+  cube_info->nodes--;
 }
 
 /*
