e1bdc1d46fbf09697205b8abb20a479237f7c8e6
Cristy <mikayla-grace@urban-warrior.org>
2020-09-15 18:53:52 -0400

    https://github.com/ImageMagick/ImageMagick/issues/2581

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -4062,7 +4062,7 @@ static MagickBooleanType RenderMVGContent(Image *image,
       case BezierPrimitive:
       {
         coordinates=(double) (BezierQuantum*primitive_info[j].coordinates);
-        if (primitive_info[j].coordinates > (107*BezierQuantum))
+        if (primitive_info[j].coordinates > (108.0*BezierQuantum))
           {
             (void) ThrowMagickException(exception,GetMagickModule(),DrawError,
               "TooManyBezierCoordinates","`%s'",token);
@@ -4112,7 +4112,7 @@ static MagickBooleanType RenderMVGContent(Image *image,
         beta=bounds.y2-bounds.y1;
         radius=hypot(alpha,beta);
         coordinates=2.0*(ceil(MagickPI*radius))+6.0*BezierQuantum+360.0;
-        if (coordinates > (107.0*BezierQuantum))
+        if (coordinates > (108.0*BezierQuantum))
           {
             (void) ThrowMagickException(exception,GetMagickModule(),DrawError,
               "TooManyBezierCoordinates","`%s'",token);
