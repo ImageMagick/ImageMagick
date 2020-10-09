3b12a5757b5b416e878953abf695ff6d5651e651
Cristy <urban-warrior@imagemagick.org>
2020-10-09 01:19:13 +0000

    ...

--- coders/txt.c
+++ coders/txt.c
@@ -781,15 +781,10 @@ static MagickBooleanType WriteTXTImage(const ImageInfo *image_info,Image *image,
         MagickFalse,value);
     if (LocaleCompare(image_info->magick,"SPARSE-COLOR") != 0)
       {
-        size_t
-          depth;
-
-        depth=compliance == SVGCompliance ? image->depth :
-          MAGICKCORE_QUANTUM_DEPTH;
         (void) FormatLocaleString(buffer,MagickPathExtent,
           "# ImageMagick pixel enumeration: %.20g,%.20g,%.20g,%s\n",(double)
-          image->columns,(double) image->rows,(double) GetQuantumRange(depth),
-          colorspace);
+          image->columns,(double) image->rows,(double)
+          GetQuantumRange(image->depth),colorspace);
         (void) WriteBlobString(image,buffer);
       }
     GetPixelInfo(image,&pixel);
