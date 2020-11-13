94cd4b42bd47eabdf6b4857b1efc9bdcdb1755b7
Cristy <urban-warrior@imagemagick.org>
2020-11-13 19:48:44 +0000

    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=27462

--- MagickCore/stream.c
+++ MagickCore/stream.c
@@ -201,10 +201,13 @@ MagickExport StreamInfo *AcquireStreamInfo(const ImageInfo *image_info,
 static inline void RelinquishStreamPixels(CacheInfo *cache_info)
 {
   assert(cache_info != (CacheInfo *) NULL);
-  if (cache_info->mapped == MagickFalse)
-    (void) RelinquishAlignedMemory(cache_info->pixels);
-  else
-    (void) UnmapBlob(cache_info->pixels,(size_t) cache_info->length);
+  if (cache_info->pixels != (Quantum *) NULL)
+    {
+      if (cache_info->mapped == MagickFalse)
+        (void) RelinquishAlignedMemory(cache_info->pixels);
+      else
+        (void) UnmapBlob(cache_info->pixels,(size_t) cache_info->length);
+    }
   cache_info->pixels=(Quantum *) NULL;
   cache_info->metacontent=(void *) NULL;
   cache_info->length=0;
