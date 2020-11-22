d1b3b2513f8fb48a3958230e3a1de0e3c21913a0
Dirk Lemstra <dirk@lemstra.org>
2020-11-22 18:12:32 +0100

    Moved swapping to the correct position.

--- coders/tiff.c
+++ coders/tiff.c
@@ -697,11 +697,13 @@ static MagickBooleanType TIFFGetProfiles(TIFF *tiff,Image *image,
       const TIFFField
         *field;
 
-      if (TIFFIsByteSwapped(tiff) != 0)
-        TIFFSwabArrayOfLong((uint32 *) profile,(size_t) length);
       field=TIFFFieldWithTag(tiff,TIFFTAG_RICHTIFFIPTC);
       if (TIFFFieldDataType(field) == TIFF_LONG)
-        status=ReadProfile(image,"iptc",profile,4L*length,exception);
+        {
+          if (TIFFIsByteSwapped(tiff) != 0)
+            TIFFSwabArrayOfLong((uint32 *) profile,(size_t) length);
+          status=ReadProfile(image,"iptc",profile,4L*length,exception);
+        }
       else
         status=ReadProfile(image,"iptc",profile,length,exception);
     }
