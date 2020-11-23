7b1e169c04f38c66632433a49ea68b42857ae78c
Cristy <urban-warrior@imagemagick.org>
2020-11-23 15:58:59 +0000

    ...

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -4671,15 +4671,58 @@ static PolygonInfo **AcquirePolygonThreadSet(
   path_info=ConvertPrimitiveToPath(primitive_info,exception);
   if (path_info == (PathInfo *) NULL)
     return(DestroyPolygonThreadSet(polygon_info));
-  for (i=0; i < (ssize_t) number_threads; i++)
+  polygon_info[0]=ConvertPathToPolygon(path_info,exception);
+  if (polygon_info[0] == (PolygonInfo *) NULL)
+    {
+      (void) ThrowMagickException(exception,GetMagickModule(),
+        ResourceLimitError,"MemoryAllocationFailed","`%s'","");
+      return(DestroyPolygonThreadSet(polygon_info));
+    }
+  for (i=1; i < (ssize_t) number_threads; i++)
   {
-    polygon_info[i]=ConvertPathToPolygon(path_info,exception);
+    EdgeInfo
+      *edge_info;
+
+    register ssize_t
+      j;
+
+    polygon_info[i]=(PolygonInfo *) AcquireQuantumMemory(1,
+      sizeof(*polygon_info[i]));
     if (polygon_info[i] == (PolygonInfo *) NULL)
       {
         (void) ThrowMagickException(exception,GetMagickModule(),
           ResourceLimitError,"MemoryAllocationFailed","`%s'","");
         return(DestroyPolygonThreadSet(polygon_info));
       }
+    polygon_info[i]->number_edges=0;
+    edge_info=polygon_info[0]->edges;
+    polygon_info[i]->edges=(EdgeInfo *) AcquireQuantumMemory(
+      polygon_info[0]->number_edges,sizeof(*edge_info));
+    if (polygon_info[i]->edges == (EdgeInfo *) NULL)
+      {
+        (void) ThrowMagickException(exception,GetMagickModule(),
+          ResourceLimitError,"MemoryAllocationFailed","`%s'","");
+        return(DestroyPolygonThreadSet(polygon_info));
+      }
+    (void) memcpy(polygon_info[i]->edges,edge_info,
+      polygon_info[0]->number_edges*sizeof(*edge_info)); 
+    for (j=0; j < (ssize_t) polygon_info[i]->number_edges; j++)
+      polygon_info[i]->edges[j].points=(PointInfo *) NULL;
+    polygon_info[i]->number_edges=polygon_info[0]->number_edges;
+    for (j=0; j < (ssize_t) polygon_info[i]->number_edges; j++)
+    {
+      edge_info=polygon_info[0]->edges+j;
+      polygon_info[i]->edges[j].points=(PointInfo *) AcquireQuantumMemory(
+        edge_info->number_points,sizeof(*edge_info));
+      if (polygon_info[i]->edges[j].points == (PointInfo *) NULL)
+        {
+          (void) ThrowMagickException(exception,GetMagickModule(),
+            ResourceLimitError,"MemoryAllocationFailed","`%s'","");
+          return(DestroyPolygonThreadSet(polygon_info));
+        }
+      (void) memcpy(polygon_info[i]->edges[j].points,edge_info->points,
+        edge_info->number_points*sizeof(*edge_info->points)); 
+    }
   }
   path_info=(PathInfo *) RelinquishMagickMemory(path_info);
   return(polygon_info);
