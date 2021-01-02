89afeb43773d269c8f472e8c3554e8b94d398aa1
Cristy <mikayla-grace@urban-warrior.org>
2021-01-02 16:44:27 -0500

    ...

--- MagickCore/cache.c
+++ MagickCore/cache.c
@@ -5020,7 +5020,7 @@ static inline MagickBooleanType ValidatePixelOffset(const ssize_t x,
 {
   if ((x >= 0) && (a > ((ssize_t) SSIZE_MAX-x)))
     return(MagickFalse);
-  if (a < (((ssize_t) -SSIZE_MAX)-x))
+  if (a < -((ssize_t) SSIZE_MAX-x))
     return(MagickFalse);
   return(MagickTrue);
 }
