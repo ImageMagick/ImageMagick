8132e9aca1e1e5fe996049b80f45b04f2d90184b
Cristy <mikayla-grace@urban-warrior.org>
2021-01-23 19:34:52 -0500

    https://github.com/ImageMagick/ImageMagick/issues/3152

--- coders/jxl.c
+++ coders/jxl.c
@@ -248,15 +248,11 @@ static Image *ReadJXLImage(const ImageInfo *image_info,ExceptionInfo *exception)
   while ((decoder_status != JXL_DEC_SUCCESS) &&
          (decoder_status != JXL_DEC_ERROR))
   {
-    const uint8_t
-      *p;
-
     size_t
       size;
 
     size=input_size;
-    p=input_buffer;
-    decoder_status=JxlDecoderProcessInput(decoder,&p,&input_size);
+    decoder_status=JxlDecoderProcessInput(decoder);
     if ((input_size > 0) && ((size-input_size) > 0))
       (void) memmove(input_buffer,input_buffer+(size-input_size),input_size);
     switch (decoder_status)
@@ -272,6 +268,7 @@ static Image *ReadJXLImage(const ImageInfo *image_info,ExceptionInfo *exception)
               "InsufficientImageDataInFile","`%s'",image->filename);
             break;
           }
+        JxlDecoderSetInput(decoder,input_buffer,input_size);
         break;
       }
       case JXL_DEC_BASIC_INFO:
@@ -347,6 +344,7 @@ static Image *ReadJXLImage(const ImageInfo *image_info,ExceptionInfo *exception)
         decoder_status=JXL_DEC_NEED_IMAGE_OUT_BUFFER;
       }
       case JXL_DEC_FULL_IMAGE:
+      {
         if (output_buffer == (unsigned char *) NULL)
           {
             ThrowMagickException(exception,GetMagickModule(),CorruptImageError,
@@ -360,6 +358,7 @@ static Image *ReadJXLImage(const ImageInfo *image_info,ExceptionInfo *exception)
         if (status == MagickFalse)
           decoder_status=JXL_DEC_ERROR;
         break;
+      }
       case JXL_DEC_SUCCESS:
       case JXL_DEC_ERROR:
         break;
