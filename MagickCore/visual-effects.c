6720594ccb46afeb3365fb54d814271608f754b3
Zhang Xiaohui <ruc_zhangxiaohui@163.com>
2021-02-03 16:03:46 +0800

    uses the PerceptibleReciprocal() to prevent the divide-by-zero from occurring

--- MagickCore/visual-effects.c
+++ MagickCore/visual-effects.c
@@ -1005,11 +1005,11 @@ MagickExport Image *ImplodeImage(const Image *image,const double amount,
   center.y=0.5*canvas_image->rows;
   radius=center.x;
   if (canvas_image->columns > canvas_image->rows)
-    scale.y=(double) canvas_image->columns/(double) canvas_image->rows;
+    scale.y=(double) canvas_image->columns*PerceptibleReciprocal((double) canvas_image->rows);
   else
     if (canvas_image->columns < canvas_image->rows)
       {
-        scale.x=(double) canvas_image->rows/(double) canvas_image->columns;
+        scale.x=(double) canvas_image->rows*PerceptibleReciprocal((double) canvas_image->columns);
         radius=center.y;
       }
   /*
@@ -1085,10 +1085,10 @@ MagickExport Image *ImplodeImage(const Image *image,const double amount,
           */
           factor=1.0;
           if (distance > 0.0)
-            factor=pow(sin(MagickPI*sqrt((double) distance)/radius/2),-amount);
+            factor=pow(sin(MagickPI*sqrt((double) distance)*PerceptibleReciprocal(radius)/2),-amount);
           status=InterpolatePixelChannels(canvas_image,interpolate_view,
-            implode_image,method,(double) (factor*delta.x/scale.x+center.x),
-            (double) (factor*delta.y/scale.y+center.y),q,exception);
+            implode_image,method,(double) (factor*delta.x*PerceptibleReciprocal(scale.x)+center.x),
+            (double) (factor*delta.y*PerceptibleReciprocal(scale.y)+center.y),q,exception);
           if (status == MagickFalse)
             break;
         }
