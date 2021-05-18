c322ab10641c1a89e16924cce259828232e079cb
Cristy <mikayla-grace@urban-warrior.org>
2021-05-18 18:02:45 -0400

    https://github.com/ImageMagick/ImageMagick/issues/3690

--- MagickCore/magick-type.h
+++ MagickCore/magick-type.h
@@ -183,7 +183,7 @@ typedef enum
 
   The macros are thus is only true if the value given is NaN.
 */
-#if defined(MAGICKCORE_HAVE_ISNAN)
+#if defined(MAGICKCORE_HAVE_ISNAN) && !defined(__cplusplus) && !defined(c_plusplus)
 #  define IsNaN(a) isnan(a)
 #elif defined(_MSC_VER) && (_MSC_VER >= 1310)
 #  include <float.h>
