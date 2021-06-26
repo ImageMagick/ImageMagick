43c4c099f349fba0d033b731064a1d021c1c80ad
Cristy <mikayla-grace@urban-warrior.org>
2021-06-26 08:07:00 -0400

    system() is not supported under IOS

--- tests/validate.c
+++ tests/validate.c
@@ -51,6 +51,14 @@
 /*
   Define declarations.
 */
+#if defined(__APPLE__)
+  #include "TargetConditionals.h"
+  #if TARGET_OS_IOS || TARGET_OS_WATCH || TARGET_OS_TV
+    #define system(s) ((s)==NULL ? 0 : -1)
+  #endif // end iOS
+#elif defined(__ANDROID__)
+  #define system(s) ((s)==NULL ? 0 : -1)
+#endif
 #define CIEEpsilon  (216.0/24389.0)
 #define CIEK  (24389.0/27.0)
 #define D65X  0.95047
