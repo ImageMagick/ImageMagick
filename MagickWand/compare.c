e87b9ea62438e2f679548bb2e50a3a64892b001d
Cristy <mikayla-grace@urban-warrior.org>
2021-07-17 17:07:13 -0400

    remove virtual canvas offset from similarity image

--- MagickWand/compare.c
+++ MagickWand/compare.c
@@ -1448,6 +1448,7 @@ WandExport MagickBooleanType CompareImagesCommand(ImageInfo *image_info,
             (void) FormatLocaleFile(stderr,"   Offset: %.20g,%.20g\n",(double)
               difference_image->page.x,(double) difference_image->page.y);
         }
+      (void) ResetImagePage(image,"0x0+0+0");
       status&=WriteImages(image_info,difference_image,argv[argc-1],exception);
       if ((metadata != (char **) NULL) && (format != (char *) NULL))
         {
