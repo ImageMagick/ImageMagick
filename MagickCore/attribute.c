c14022ab5b8593cc5c4c3f6936af93d2f9fb58b7
Cristy <mikayla-grace@urban-warrior.org>
2021-08-12 12:02:06 -0400

    https://github.com/ImageMagick/ImageMagick/issues/4059

--- MagickCore/attribute.c
+++ MagickCore/attribute.c
@@ -2165,6 +2165,11 @@ MagickExport MagickBooleanType SetImageType(Image *image,const ImageType type,
       status=TransformImageColorspace(image,GRAYColorspace,exception);
       (void) NormalizeImage(image,exception);
       status=BilevelImage(image,QuantumRange/2.0,exception);
+      quantize_info=AcquireQuantizeInfo(image_info);
+      quantize_info->number_colors=256;
+      quantize_info->colorspace=GRAYColorspace;
+      status=QuantizeImage(quantize_info,image,exception);
+      quantize_info=DestroyQuantizeInfo(quantize_info);
       image->alpha_trait=UndefinedPixelTrait;
       break;
     }
