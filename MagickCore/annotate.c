bafbac2ace6a4f8b61fd8fe58401582fa8614330
Dirk Lemstra <dirk@lemstra.org>
2021-10-02 12:20:33 +0200

    Correction of the bounds measurements.

--- MagickCore/annotate.c
+++ MagickCore/annotate.c
@@ -1657,17 +1657,13 @@ static MagickBooleanType RenderFreetype(Image *image,const DrawInfo *draw_info,
     ft_status=FT_Outline_Get_BBox(&outline,&bounds);
     if (ft_status != 0)
       continue;
-    if ((p == draw_info->text) || (bounds.xMin < metrics->bounds.x1))
-      if (bounds.xMin != 0)
+    if ((bounds.xMin < metrics->bounds.x1) && (bounds.xMin != 0))
         metrics->bounds.x1=(double) bounds.xMin;
-    if ((p == draw_info->text) || (bounds.yMin < metrics->bounds.y1))
-      if (bounds.yMin != 0)
+    if ((bounds.yMin < metrics->bounds.y1) && (bounds.yMin != 0))
         metrics->bounds.y1=(double) bounds.yMin;
-    if ((p == draw_info->text) || (bounds.xMax > metrics->bounds.x2))
-      if (bounds.xMax != 0)
+    if ((bounds.xMax > metrics->bounds.x2) && (bounds.xMax != 0))
         metrics->bounds.x2=(double) bounds.xMax;
-    if ((p == draw_info->text) || (bounds.yMax > metrics->bounds.y2))
-      if (bounds.yMax != 0)
+    if ((bounds.yMax > metrics->bounds.y2) && (bounds.yMax != 0))
         metrics->bounds.y2=(double) bounds.yMax;
     if (((draw_info->stroke.alpha != TransparentAlpha) ||
          (draw_info->stroke_pattern != (Image *) NULL)) &&
