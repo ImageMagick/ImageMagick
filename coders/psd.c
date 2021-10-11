c191d5d0da3489fd0ab0640bd4450f4cee122471
Dirk Lemstra <dirk@lemstra.org>
2021-10-11 22:14:34 +0200

    Whitespace.

--- coders/psd.c
+++ coders/psd.c
@@ -1726,7 +1726,7 @@ static inline MagickBooleanType PSDSkipImage(const PSDInfo *psd_info,
   const ImageInfo *image_info,const size_t index)
 {
   if (psd_info->has_merged_image == MagickFalse)
-      return(MagickFalse);
+    return(MagickFalse);
   if (image_info->number_scenes == 0)
     return(MagickFalse);
   if (index < image_info->scene)
