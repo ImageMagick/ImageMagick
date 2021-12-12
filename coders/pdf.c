86b8264127054186cd38514d8e986bf848f2e9cd
Cristy <mikayla-grace@urban-warrior.org>
2021-12-11 19:31:06 -0500

    https://github.com/ImageMagick/ImageMagick/discussions/4580

--- coders/pdf.c
+++ coders/pdf.c
@@ -1465,7 +1465,7 @@ static MagickBooleanType WritePDFImage(const ImageInfo *image_info,Image *image,
     version=1.6;
   for (next=image; next != (Image *) NULL; next=GetNextImageInList(next))
   {
-    if (IdentifyImageCoderGray(next,exception) != MagickFalse)
+    if (SetImageGray(next,exception) != MagickFalse)
       SetImageColorspace(next,GRAYColorspace,exception);
     icc_profile=GetCompatibleColorProfile(next);
     if (icc_profile != (StringInfo *) NULL)
@@ -1995,7 +1995,7 @@ static MagickBooleanType WritePDFImage(const ImageInfo *image_info,Image *image,
     if ((4*number_pixels) != (MagickSizeType) ((size_t) (4*number_pixels)))
       ThrowPDFException(ResourceLimitError,"MemoryAllocationFailed");
     if ((compression == FaxCompression) || (compression == Group4Compression) ||
-        (IsImageGray(image) != MagickFalse))
+        (SetImageGray(image,exception) != MagickFalse))
       {
         switch (compression)
         {
@@ -2358,7 +2358,7 @@ static MagickBooleanType WritePDFImage(const ImageInfo *image_info,Image *image,
     else
       if ((compression == FaxCompression) ||
           (compression == Group4Compression) ||
-          (IsImageGray(image) != MagickFalse))
+          (SetImageGray(image,exception) != MagickFalse))
         {
           device="DeviceGray";
           channels=1;
@@ -2525,7 +2525,7 @@ static MagickBooleanType WritePDFImage(const ImageInfo *image_info,Image *image,
     number_pixels=(MagickSizeType) tile_image->columns*tile_image->rows;
     if ((compression == FaxCompression) ||
         (compression == Group4Compression) ||
-        (IdentifyImageCoderGray(tile_image,exception) != MagickFalse))
+        (SetImageGray(tile_image,exception) != MagickFalse))
       {
         switch (compression)
         {
