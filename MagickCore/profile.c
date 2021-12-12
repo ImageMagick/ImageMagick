dd263149f709f1a8fe4e17e23fe6eb17f15dac04
Dirk Lemstra <dirk@lemstra.org>
2021-12-12 12:09:33 +0100

    Swapped the Sync8BimProfile and the SyncExifProfile methods.

--- MagickCore/profile.c
+++ MagickCore/profile.c
@@ -2160,68 +2160,6 @@ static void WriteProfileShort(const EndianType endian,
   (void) memcpy(p,buffer,2);
 }
 
-static MagickBooleanType Sync8BimProfile(Image *image,StringInfo *profile)
-{
-  size_t
-    length;
-
-  ssize_t
-    count;
-
-  unsigned char
-    *p;
-
-  unsigned short
-    id;
-
-  length=GetStringInfoLength(profile);
-  p=GetStringInfoDatum(profile);
-  while (length != 0)
-  {
-    if (ReadProfileByte(&p,&length) != 0x38)
-      continue;
-    if (ReadProfileByte(&p,&length) != 0x42)
-      continue;
-    if (ReadProfileByte(&p,&length) != 0x49)
-      continue;
-    if (ReadProfileByte(&p,&length) != 0x4D)
-      continue;
-    if (length < 7)
-      return(MagickFalse);
-    id=ReadProfileMSBShort(&p,&length);
-    count=(ssize_t) ReadProfileByte(&p,&length);
-    if ((count >= (ssize_t) length) || (count < 0))
-      return(MagickFalse);
-    p+=count;
-    length-=count;
-    if ((*p & 0x01) == 0)
-      (void) ReadProfileByte(&p,&length);
-    count=(ssize_t) ReadProfileMSBLong(&p,&length);
-    if ((count > (ssize_t) length) || (count < 0))
-      return(MagickFalse);
-    if ((id == 0x3ED) && (count == 16))
-      {
-        if (image->units == PixelsPerCentimeterResolution)
-          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
-            image->resolution.x*2.54*65536.0),p);
-        else
-          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
-            image->resolution.x*65536.0),p);
-        WriteProfileShort(MSBEndian,(unsigned short) image->units,p+4);
-        if (image->units == PixelsPerCentimeterResolution)
-          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
-            image->resolution.y*2.54*65536.0),p+8);
-        else
-          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
-            image->resolution.y*65536.0),p+8);
-        WriteProfileShort(MSBEndian,(unsigned short) image->units,p+12);
-      }
-    p+=count;
-    length-=count;
-  }
-  return(MagickTrue);
-}
-
 MagickBooleanType SyncExifProfile(Image *image,StringInfo *profile)
 {
 #define MaxDirectoryStack  16
@@ -2445,6 +2383,68 @@ MagickBooleanType SyncExifProfile(Image *image,StringInfo *profile)
     }
   } while (level > 0);
   exif_resources=DestroySplayTree(exif_resources);
+  return(MagickTrue);
+}
+
+static MagickBooleanType Sync8BimProfile(Image *image,StringInfo *profile)
+{
+  size_t
+    length;
+
+  ssize_t
+    count;
+
+  unsigned char
+    *p;
+
+  unsigned short
+    id;
+
+  length=GetStringInfoLength(profile);
+  p=GetStringInfoDatum(profile);
+  while (length != 0)
+  {
+    if (ReadProfileByte(&p,&length) != 0x38)
+      continue;
+    if (ReadProfileByte(&p,&length) != 0x42)
+      continue;
+    if (ReadProfileByte(&p,&length) != 0x49)
+      continue;
+    if (ReadProfileByte(&p,&length) != 0x4D)
+      continue;
+    if (length < 7)
+      return(MagickFalse);
+    id=ReadProfileMSBShort(&p,&length);
+    count=(ssize_t) ReadProfileByte(&p,&length);
+    if ((count >= (ssize_t) length) || (count < 0))
+      return(MagickFalse);
+    p+=count;
+    length-=count;
+    if ((*p & 0x01) == 0)
+      (void) ReadProfileByte(&p,&length);
+    count=(ssize_t) ReadProfileMSBLong(&p,&length);
+    if ((count > (ssize_t) length) || (count < 0))
+      return(MagickFalse);
+    if ((id == 0x3ED) && (count == 16))
+      {
+        if (image->units == PixelsPerCentimeterResolution)
+          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
+            image->resolution.x*2.54*65536.0),p);
+        else
+          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
+            image->resolution.x*65536.0),p);
+        WriteProfileShort(MSBEndian,(unsigned short) image->units,p+4);
+        if (image->units == PixelsPerCentimeterResolution)
+          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
+            image->resolution.y*2.54*65536.0),p+8);
+        else
+          WriteProfileLong(MSBEndian,(unsigned int) CastDoubleToLong(
+            image->resolution.y*65536.0),p+8);
+        WriteProfileShort(MSBEndian,(unsigned short) image->units,p+12);
+      }
+    p+=count;
+    length-=count;
+  }
   return(MagickTrue);
 }
 
