53fb13e21b41877a2f1b5a9039e06c989fbc7995
Cristy <mikayla-grace@urban-warrior.org>
2021-12-17 14:05:39 -0500

    ...

--- coders/tiff.c
+++ coders/tiff.c
@@ -1266,7 +1266,7 @@ static Image *ReadTIFFImage(const ImageInfo *image_info,
     *pixels;
 
   void
-    *sans[4] = { NULL, NULL, NULL, NULL };
+    *sans[5] = { NULL, NULL, NULL, NULL, NULL };
 
   /*
     Open image.
