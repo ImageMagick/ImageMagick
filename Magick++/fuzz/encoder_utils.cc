78e4a56a833cd64f1676bf54847409cae1debaf4
Dirk Lemstra <dirk@lemstra.org>
2022-02-05 18:20:35 +0100

    Added copyright headers.

--- Magick++/fuzz/encoder_utils.cc
+++ Magick++/fuzz/encoder_utils.cc
@@ -1,3 +1,19 @@
+/*
+  Copyright @ 2018 ImageMagick Studio LLC, a non-profit organization
+  dedicated to making software imaging solutions freely available.
+  
+  You may not use this file except in compliance with the License.  You may
+  obtain a copy of the License at
+  
+    https://imagemagick.org/script/license.php
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 static int fuzzEncoderWithStringFilename(const std::string encoder, const uint8_t *Data, size_t Size, bool (*validate)(const std::string &) = NULL)
 {
   // Allow a bit extra to make sure we do proper bounds checking in Magick++
