7d5e159ed8872c1ce51f1b89b4027689fb80e3a0
Cristy <urban-warrior@imagemagick.org>
2022-02-14 06:29:17 -0500

    revert

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -4794,7 +4794,7 @@ static double GetFillAlpha(PolygonInfo *polygon_info,const double mid,
             }
           else
             {
-              alpha=1.0/alpha;
+              alpha=PerceptibleReciprocal(alpha);
               beta=delta.x*(y-q->y)-delta.y*(x-q->x);
               distance=alpha*beta*beta;
             }
