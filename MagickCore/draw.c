56f1643600448447dbdb6c03716ec9474153c43b
Cristy <urban-warrior@imagemagick.org>
2022-02-14 21:36:43 -0500

    off-by-one fix

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -4863,11 +4863,11 @@ static double GetFillAlpha(PolygonInfo *polygon_info,const double mid,
       continue;
     if ((double) x > p->bounds.x2)
       {
-        winding_number+=p->direction ? 1 : -1;
+        winding_number+=p->direction != 0 ? 1 : -1;
         continue;
       }
     i=(ssize_t) MagickMax((double) p->highwater,1.0);
-    for ( ; i < (ssize_t) (p->number_points-1); i++)
+    for ( ; i < (ssize_t) p->number_points; i++)
       if ((double) y <= p->points[i].y)
         break;
     q=p->points+i-1;
