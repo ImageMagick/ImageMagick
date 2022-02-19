57be6784c1bea811aa35199f56e5cec9777946f0
Cristy <urban-warrior@imagemagick.org>
2022-02-19 15:14:11 -0500

    no need for conditional compile

--- MagickCore/draw.c
+++ MagickCore/draw.c
@@ -5016,7 +5016,6 @@ static MagickBooleanType DrawPolygonPrimitive(Image *image,
   poly_extent.y1=CastDoubleToLong(ceil(bounds.y1-0.5));
   poly_extent.x2=CastDoubleToLong(floor(bounds.x2+0.5));
   poly_extent.y2=CastDoubleToLong(floor(bounds.y2+0.5));
-#if defined(MAGICKCORE_OPENMP_SUPPORT)
   number_threads=GetMagickNumberThreads(image,image,poly_extent.y2-
     poly_extent.y1+1,1);
   status=ClonePolygonEdges(polygon_info,number_threads,exception);
@@ -5025,8 +5024,6 @@ static MagickBooleanType DrawPolygonPrimitive(Image *image,
       polygon_info=DestroyPolygonThreadSet(polygon_info);
       return(status);
     }
-#endif
-  status=MagickTrue;
   image_view=AcquireAuthenticCacheView(image,exception);
   if ((primitive_info->coordinates == 1) ||
       (polygon_info[0]->number_edges == 0))
