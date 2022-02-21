da3971cb2ecde8856358e2221aec5b924467ef85
Cristy <urban-warrior@imagemagick.org>
2022-02-21 08:00:51 -0500

    https://github.com/ImageMagick/ImageMagick/issues/4867

--- MagickWand/magick-image.c
+++ MagickWand/magick-image.c
@@ -8249,17 +8249,11 @@ WandExport MagickBooleanType MagickPingImageBlob(MagickWand *wand,
   Image
     *images;
 
-  ImageInfo
-    *read_info;
-
   assert(wand != (MagickWand *) NULL);
   assert(wand->signature == MagickWandSignature);
   if (wand->debug != MagickFalse)
     (void) LogMagickEvent(WandEvent,GetMagickModule(),"%s",wand->name);
-  read_info=CloneImageInfo(wand->image_info);
-  SetImageInfoBlob(read_info,blob,length);
-  images=PingImage(read_info,wand->exception);
-  read_info=DestroyImageInfo(read_info);
+  images=PingBlob(wand->image_info,blob,length,wand->exception);
   if (images == (Image *) NULL)
     return(MagickFalse);
   return(InsertImageInWand(wand,images));
