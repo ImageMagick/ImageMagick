9c9a84cec4ab28ee0b57c2b9266d6fbe68183512
xiaoxiaoafeifei <lliangliang2007@163.com>
2022-03-19 18:50:38 +0800

    fix issue: outside the range of representable values of type 'unsigned int' at coders/psd.c:1025 (#4963)
    * fix Division by zero in XMenuWidget() of MagickCore/widget.c
    
    * Fix memory leak in AnimateImageCommand() of MagickWand/animate.c and DisplayImageCommand() of MagickWand/display.c
    
    * fix Division by zero in ReadEnhMetaFile() of coders/emf.c
    
    * Resolve conflicts
    
    * fix issue: outside the range of representable values of type 'unsigned char' at coders/psd.c:1025
    
    Co-authored-by: zhailiangliang <zhailiangliang@loongson.cn>

--- coders/psd.c
+++ coders/psd.c
@@ -1022,7 +1022,7 @@ static MagickBooleanType ReadPSDChannelPixels(Image *image,const ssize_t row,
           number_bits=8;
         for (bit = 0; bit < (ssize_t) number_bits; bit++)
         {
-          SetPSDPixel(image,channel,packet_size,(((unsigned char) pixel)
+          SetPSDPixel(image,channel,packet_size,(((unsigned char)((ssize_t)pixel))
             & (0x01 << (7-bit))) != 0 ? 0 : QuantumRange,q,exception);
           q+=GetPixelChannels(image);
           x++;
