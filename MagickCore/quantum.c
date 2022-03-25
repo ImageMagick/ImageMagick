219d19f96e6a87b17ab26dfa6eadc7ad3169092a
Cristy <urban-warrior@imagemagick.org>
2022-03-25 10:57:36 -0400

    over allocate quantum pixel buffer

--- MagickCore/quantum.c
+++ MagickCore/quantum.c
@@ -689,13 +689,18 @@ MagickExport MagickBooleanType SetQuantumDepth(const Image *image,
           else
             quantum_info->depth=16;
     }
-  if (quantum_info->pixels != (MemoryInfo **) NULL)
-    DestroyQuantumPixels(quantum_info);
-  quantum=(quantum_info->pad+MaxPixelChannels)*(quantum_info->depth+7)/8;
+  quantum=(GetPixelChannels(image)+quantum_info->pad+8)*
+    ((quantum_info->depth+7)/8);
   extent=MagickMax(image->columns,image->rows)*quantum;
   if ((MagickMax(image->columns,image->rows) != 0) &&
       (quantum != (extent/MagickMax(image->columns,image->rows))))
     return(MagickFalse);
+  if (quantum_info->pixels != (MemoryInfo **) NULL)
+    {
+      if (extent <= quantum_info->extent)
+        return(MagickTrue);
+      DestroyQuantumPixels(quantum_info);
+    }
   return(AcquireQuantumPixels(quantum_info,extent));
 }
 
@@ -967,8 +972,7 @@ MagickExport void SetQuantumMinIsWhite(QuantumInfo *quantum_info,
 %
 %  The format of the SetQuantumQuantum method is:
 %
-%      void SetQuantumQuantum(QuantumInfo *quantum_info,
-%        const size_t quantum)
+%      void SetQuantumQuantum(QuantumInfo *quantum_info,const size_t quantum)
 %
 %  A description of each parameter follows:
 %
