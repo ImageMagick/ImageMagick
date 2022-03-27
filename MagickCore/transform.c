347b82801da2b0223a4de34d6c30123041061c2b
Cristy <urban-warrior@imagemagick.org>
2022-03-27 09:30:43 -0400

    https://github.com/ImageMagick/ImageMagick/discussions/4861

--- MagickCore/transform.c
+++ MagickCore/transform.c
@@ -41,6 +41,7 @@
 */
 #include "MagickCore/studio.h"
 #include "MagickCore/attribute.h"
+#include "MagickCore/artifact.h"
 #include "MagickCore/cache.h"
 #include "MagickCore/cache-view.h"
 #include "MagickCore/color.h"
@@ -2398,6 +2399,9 @@ MagickExport Image *TransverseImage(const Image *image,ExceptionInfo *exception)
 */
 MagickExport Image *TrimImage(const Image *image,ExceptionInfo *exception)
 {
+  const char
+    *artifact;
+
   Image
     *trim_image;
 
@@ -2424,6 +2428,36 @@ MagickExport Image *TrimImage(const Image *image,ExceptionInfo *exception)
       crop_image->page.x=(-1);
       crop_image->page.y=(-1);
       return(crop_image);
+    }
+  artifact=GetImageArtifact(image,"trim:minSize");
+  if (artifact != (const char *) NULL)
+    {
+      RectangleInfo
+        page;
+
+      (void) ParseAbsoluteGeometry(artifact,&page);
+      if ((geometry.width < page.width) && (geometry.height < page.height))
+        {
+          switch (image->gravity)
+          {
+            case NorthWestGravity:
+            {
+              geometry.x-=(ssize_t) (page.width-geometry.width);
+              geometry.y-=(ssize_t) (page.height-geometry.height);
+              break;
+            }
+            case CenterGravity:
+            {
+              geometry.x-=(ssize_t) (page.width-geometry.width)/2;
+              geometry.y-=(ssize_t) (page.height-geometry.height)/2;
+              break;
+            }
+            default:
+              break;
+          }
+          geometry.width=page.width;
+          geometry.height=page.height;
+        }
     }
   geometry.x+=image->page.x;
   geometry.y+=image->page.y;
