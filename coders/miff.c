0b92950123c6dea27a9abb088ae69e329ec31a0f
Cristy <urban-warrior@imagemagick.org>
2022-06-02 20:52:56 -0400

    fix lint issues

--- coders/miff.c
+++ coders/miff.c
@@ -1132,7 +1132,7 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
         (image->compression == UndefinedCompression) ||
         (image->colorspace == UndefinedColorspace) ||
         (image->columns == 0) || (image->rows == 0) ||
-        (image->number_meta_channels > (MaxPixelChannels-MetaPixelChannels)) ||
+        (image->number_meta_channels > (size_t) (MaxPixelChannels-MetaPixelChannels)) ||
         ((image->number_channels+image->number_meta_channels) >= MaxPixelChannels) ||
         (image->depth == 0) || (image->depth > 64))
       {
