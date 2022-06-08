a637245a2fb2c5f3937ffb0540de593b4e9e5910
Cristy <urban-warrior@imagemagick.org>
2022-06-08 19:54:23 -0400

    change per lint advisement

--- MagickCore/resource.c
+++ MagickCore/resource.c
@@ -126,8 +126,8 @@ static ResourceInfo
     MagickULLConstant(0),              /* initial thread */
     MagickULLConstant(0),              /* initial throttle */
     MagickULLConstant(0),              /* initial time */
-    MAGICK_SSIZE_MAX/sizeof(Quantum)/5,/* width limit */
-    MAGICK_SSIZE_MAX/sizeof(Quantum)/5,/* height limit */
+    (MagickSizeType) MAGICK_SSIZE_MAX/sizeof(Quantum)/5,/* width limit */
+    (MagickSizeType) MAGICK_SSIZE_MAX/sizeof(Quantum)/5,/* height limit */
     MagickResourceInfinity,            /* list length limit */
     MagickULLConstant(3072)*1024*1024, /* area limit */
     MagickULLConstant(1536)*1024*1024, /* memory limit */
