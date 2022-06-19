7922af1e56acb0f5fca6fef9aeec1c7b7061cf99
Dirk Lemstra <dirk@lemstra.org>
2022-06-19 10:54:21 +0200

    Also remove date:timestamp when stripping the image.

--- MagickCore/image.c
+++ MagickCore/image.c
@@ -3860,6 +3860,7 @@ MagickExport MagickBooleanType StripImage(Image *image,ExceptionInfo *exception)
   (void) DeleteImageProperty(image,"comment");
   (void) DeleteImageProperty(image,"date:create");
   (void) DeleteImageProperty(image,"date:modify");
+  (void) DeleteImageProperty(image,"date:timestamp");
   status=SetImageArtifact(image,"png:exclude-chunk",
     "bKGD,caNv,cHRM,eXIf,gAMA,iCCP,iTXt,pHYs,sRGB,tEXt,zCCP,zTXt,date");
   return(status);
