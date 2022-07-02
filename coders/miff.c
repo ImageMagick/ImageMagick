5e87813a9935e9b5ed634e601bf5b329cfc765bf
Cristy <urban-warrior@imagemagick.org>
2022-07-02 18:39:23 -0400

    revert

--- coders/miff.c
+++ coders/miff.c
@@ -740,9 +740,6 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
                   }
                 if (LocaleCompare(keyword,"columns") == 0)
                   {
-                    if (image->columns != 0)
-                      ThrowMIFFException(CorruptImageError,
-                        "ImproperImageHeader");
                     image->columns=StringToUnsignedLong(options);
                     break;
                   }
@@ -1021,9 +1018,6 @@ static Image *ReadMIFFImage(const ImageInfo *image_info,
                   }
                 if (LocaleCompare(keyword,"rows") == 0)
                   {
-                    if (image->rows != 0)
-                      ThrowMIFFException(CorruptImageError,
-                        "ImproperImageHeader");
                     image->rows=StringToUnsignedLong(options);
                     break;
                   }
