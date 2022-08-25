afee576934f3e4865d131f4691d77b743c7cb5e9
Dirk Lemstra <dirk@lemstra.org>
2022-08-25 18:57:27 +0200

    Added missing LDFLAGS for the oss-fuzz build.

--- Magick++/fuzz/build_imagemagick.sh
+++ Magick++/fuzz/build_imagemagick.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
 
-./configure --prefix="$WORK" --disable-shared --disable-docs LDFLAGS="${LDFLAGS:-} -L$WORK/lib" CFLAGS="$CFLAGS -I$WORK/include" PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
+autoreconf -fiv
+./configure --prefix="$WORK" --disable-shared --disable-docs LDFLAGS="${LDFLAGS:-} -L$WORK/lib -stdlib=libc++" CFLAGS="$CFLAGS -I$WORK/include" PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
 make "-j$(nproc)"
 make install
