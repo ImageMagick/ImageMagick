05673e63c919e61ffa1107804d1138c46547a475
Cristy <urban-warrior@imagemagick.org>
2022-10-22 13:28:46 -0400

    possible DoS @ stdin (OCE-2022-70); possible arbitrary file leak (OCE-2022-72)

--- coders/png.c
+++ coders/png.c
@@ -3980,6 +3980,7 @@ static Image *ReadOnePNGImage(MngInfo *mng_info,
                 (void) FormatLocaleString(key,MagickPathExtent,"%s",
                   text[i].key);
                 if ((LocaleCompare(key,"version") == 0) ||
+                    (LocaleCompare(key,"profile") == 0) ||
                     (LocaleCompare(key,"width") == 0))
                   (void) FormatLocaleString(key,MagickPathExtent,"png:%s",
                     text[i].key);
