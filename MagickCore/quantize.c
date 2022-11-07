6ea94f252d4a03e24eb3ef31cdada95667de3aca
Cristy <urban-warrior@imagemagick.org>
2022-11-07 14:58:40 -0500

    set color reduction colorspace to that of the remap image @ https://github.com/ImageMagick/ImageMagick/issues/5731

--- MagickCore/quantize.c
+++ MagickCore/quantize.c
@@ -3626,6 +3626,7 @@ MagickExport MagickBooleanType RemapImage(const QuantizeInfo *quantize_info,
   if (cube_info == (CubeInfo *) NULL)
     ThrowBinaryException(ResourceLimitError,"MemoryAllocationFailed",
       image->filename);
+  cube_info->quantize_info->colorspace=remap_image->colorspace;
   status=ClassifyImageColors(cube_info,remap_image,exception);
   if (status != MagickFalse)
     {
