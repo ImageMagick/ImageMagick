4e72ced6ab8da36b2e56fc74e128c917a18606d8
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-20 23:34:33 -0500

    spelling: private
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- MagickCore/profile-private.h
+++ MagickCore/profile-private.h
@@ -13,7 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 
-  MagickCore pribate image profile methods.
+  MagickCore private image profile methods.
 */
 #ifndef MAGICKCORE_PROFILE_PRIVATE_H
 #define MAGICKCORE_PROFILE_PRIVATE_H
