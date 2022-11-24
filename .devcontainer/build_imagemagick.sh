fd8486ef4ea1e54655a24448f404964acd1527a9
Dirk Lemstra <dirk@lemstra.org>
2022-11-24 16:47:55 +0100

    Use different policy.

---
+++ .devcontainer/build_imagemagick.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -eu
+
+../Magick++/fuzz/build_imagemagick.sh
+
+cat <<EOT >> /ImageMagick/etc/ImageMagick-7/policy.xml
+<policymap>
+  <policy domain="resource" name="memory" value="256MiB"/>
+  <policy domain="resource" name="list-length" value="32"/>
+  <policy domain="resource" name="width" value="8KP"/>
+  <policy domain="resource" name="height" value="8KP"/>
+  <policy domain="resource" name="map" value="512MiB"/>
+  <policy domain="resource" name="area" value="16KP"/>
+  <policy domain="resource" name="disk" value="1GiB"/>
+  <policy domain="resource" name="file" value="768"/>
+  <policy domain="resource" name="thread" value="2"/>
+  <policy domain="resource" name="time" value="120"/>
+</policymap>
+EOT
