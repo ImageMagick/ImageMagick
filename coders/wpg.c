f3a3f5f2fd56b3ee637d8fe4df3cfc863de20112
Cristy <urban-warrior@imagemagick.org>
2023-01-08 21:59:07 -0500

    read blob byte rather than short

--- coders/wpg.c
+++ coders/wpg.c
@@ -960,8 +960,8 @@ static Image *ReadWPGImage(const ImageInfo *image_info,
   {
     size_t FileId;
     MagickOffsetType DataOffset;
-    unsigned int ProductType;
-    unsigned int FileType;
+    unsigned char ProductType;
+    unsigned char FileType;
     unsigned char MajorVersion;
     unsigned char MinorVersion;
     unsigned int EncryptKey;
@@ -1100,14 +1100,14 @@ static Image *ReadWPGImage(const ImageInfo *image_info,
   */
   Header.FileId=ReadBlobLSBLong(image);
   Header.DataOffset=(MagickOffsetType) ReadBlobLSBLong(image);
-  Header.ProductType=ReadBlobLSBShort(image);
-  Header.FileType=ReadBlobLSBShort(image);
+  Header.ProductType=ReadBlobByte(image);
+  Header.FileType=ReadBlobByte(image);
   Header.MajorVersion=ReadBlobByte(image);
   Header.MinorVersion=ReadBlobByte(image);
   Header.EncryptKey=ReadBlobLSBShort(image);
   Header.Reserved=ReadBlobLSBShort(image);
 
-  if (Header.FileId!=0x435057FF || (Header.ProductType>>8)!=0x16)
+  if ((Header.FileId != 0x435057FF) || ((Header.ProductType >> 8) != 0x16))
     ThrowReaderException(CorruptImageError,"ImproperImageHeader");
   if (Header.EncryptKey!=0)
     ThrowReaderException(CoderError,"EncryptedWPGImageFileNotSupported");
@@ -1121,7 +1121,7 @@ static Image *ReadWPGImage(const ImageInfo *image_info,
   BitmapHeader2.RotAngle=0;
   Rec2.RecordLength=0;
 
-  switch(Header.FileType)
+  switch(Header.MajorVersion)
     {
     case 1:     /* WPG level 1 */
       while(!EOFBlob(image)) /* object parser loop */
