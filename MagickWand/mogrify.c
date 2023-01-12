fba0b2d8d63c2ca250fd09b975bad947490aa8ec
Cristy <urban-warrior@imagemagick.org>
2023-01-12 13:01:07 -0500

    set SetImageOption() for -family option

--- MagickWand/mogrify.c
+++ MagickWand/mogrify.c
@@ -1402,6 +1402,7 @@ WandExport MagickBooleanType MogrifyImage(ImageInfo *image_info,const int argc,
                   draw_info->family=DestroyString(draw_info->family);
                 break;
               }
+            (void) SetImageOption(image_info,option+1,argv[i+1]);
             (void) CloneString(&draw_info->family,argv[i+1]);
             break;
           }
