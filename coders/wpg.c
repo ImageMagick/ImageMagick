30d823eb749bee9a341ba9393438ee42b8b96500
Dirk Lemstra <dirk@lemstra.org>
2023-01-23 19:52:07 +0100

    Replace tabs with spaces.

--- coders/wpg.c
+++ coders/wpg.c
@@ -1730,13 +1730,13 @@ ModuleExport void UnregisterWPGImage(void)
 
 typedef struct
 {
-	size_t
+  size_t
     count;
 
-	ssize_t
+  ssize_t
     offset;
 
-	unsigned char
+  unsigned char
     pixels[256];
 } WPGRLEInfo;
 
