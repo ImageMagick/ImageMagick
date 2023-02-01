e23ce3bc66b26b2e7c63be6a77bc700810eac993
Cristy <urban-warrior@imagemagick.org>
2023-01-31 22:04:10 -0500

    fix copyright

--- MagickCore/geometry.c
+++ MagickCore/geometry.c
@@ -14,10 +14,7 @@
 %                                                                             %
 %                             Software Design                                 %
 %                                  Cristy                                     %
-%  Copyright @ 2003 ImageMagick Studio LLC, a non-profit organization         %
-%                                                                             %
-%                                                                             %
-%  Copyright 1999-2021 ImageMagick Studio LLC, a non-profit organization      %
+%  Copyright 1999 ImageMagick Studio LLC, a non-profit organization           %
 %  dedicated to making software imaging solutions freely available.           %
 %                                                                             %
 %  You may not use this file except in compliance with the License.  You may  %
