9c4efd24fe851f4ae1f894cb91c7110ae11f53ba
Remi Collet <remi@remirepo.net>
2023-03-09 08:50:42 +0100

    restore library symbol versioning to fix ABI break

--- MagickCore/pixel.h
+++ MagickCore/pixel.h
@@ -49,8 +49,8 @@ typedef enum
   IndexChannel = 0x0020,             /* Color Index Table? */
   ReadMaskChannel = 0x0040,          /* Pixel is Not Readable? */
   WriteMaskChannel = 0x0080,         /* Pixel is Write Protected? */
-  CompositeMaskChannel = 0x0100,     /* SVG mask */
-  MetaChannel = 0x0200,              /* not used */
+  MetaChannel = 0x0100,              /* not used */
+  CompositeMaskChannel = 0x0200,     /* SVG mask */
   CompositeChannels = 0x001F,
   AllChannels = 0x7ffffff,
   /*
@@ -89,8 +89,8 @@ typedef enum
   IndexPixelChannel = 5,
   ReadMaskPixelChannel = 6,
   WriteMaskPixelChannel = 7,
-  CompositeMaskPixelChannel = 8,
-  MetaPixelChannel = 9,
+  MetaPixelChannel = 8,
+  CompositeMaskPixelChannel = 9,
   MetaPixelChannels = 10,
   IntensityPixelChannel = MaxPixelChannels,  /* ???? */
   CompositePixelChannel = MaxPixelChannels,  /* ???? */
