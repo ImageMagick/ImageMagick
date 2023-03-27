984294d86affe3af5d856c0ba3a5b399c5b4afd8
Cristy <urban-warrior@imagemagick.org>
2023-03-26 20:01:57 -0400

    set setting to True

--- MagickWand/operation.c
+++ MagickWand/operation.c
@@ -2873,7 +2873,7 @@ static MagickBooleanType CLISimpleOperatorImage(MagickCLI *cli_wand,
               (void) DeleteImageArtifact(_image,"identify:moments");
               break;
             }
-          (void) SetImageArtifact(_image,"identify:moments",arg1);
+          (void) SetImageArtifact(_image,"identify:moments","true");
           (void) SetImageArtifact(_image,"verbose","true");
           break;
         }
