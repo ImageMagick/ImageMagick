d0489724c071b4bc5883878e70a97ee9aa5a942f
Dirk Lemstra <dirk@lemstra.org>
2023-04-08 12:08:47 +0200

    More cleanup of header files.

--- MagickCore/studio.h
+++ MagickCore/studio.h
@@ -128,14 +128,13 @@ extern "C" {
 # include <pthread.h>
 #endif
 #if defined(MAGICKCORE_WINDOWS_SUPPORT)
-#if !defined(__CYGWIN__)
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#ifdef _MSC_VER
-#pragma comment (lib, "ws2_32.lib")
-#endif
-#endif
-#include <windows.h>
+#  if !defined(__CYGWIN__)
+#    include <winsock2.h>
+#    include <ws2tcpip.h>
+#    ifdef _MSC_VER
+#      pragma comment (lib, "ws2_32.lib")
+#    endif
+#  endif
 #endif
 #if defined(MAGICKCORE_HAVE_SYS_SYSLIMITS_H)
 # include <sys/syslimits.h>
