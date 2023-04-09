1fe2162f91e19da9cef89f14d2c3ab445aa6f09b
Cristy <urban-warrior@imagemagick.org>
2023-04-09 07:52:18 -0400

    cosmetic

--- MagickCore/profile.c
+++ MagickCore/profile.c
@@ -2175,7 +2175,7 @@ static MagickBooleanType SyncExifProfile(const Image *image,unsigned char *exif,
   } DirectoryInfo;
 
   DirectoryInfo
-    directory_stack[MaxDirectoryStack] = {{ 0, 0 }};
+    directory_stack[MaxDirectoryStack] = { { 0, 0 } };
 
   EndianType
     endian;
