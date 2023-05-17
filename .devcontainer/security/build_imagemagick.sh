dfb0b6e2c957269ef858e1858c806372c46945ae
Dirk Lemstra <dirk@lemstra.org>
2023-05-17 20:57:45 +0200

    Tweaks to devcontainer to also make it possible to run it locally

--- .devcontainer/security/build_imagemagick.sh
+++ .devcontainer/security/build_imagemagick.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-/workspaces/ImageMagick/Magick++/fuzz/build_imagemagick.sh
+sudo ./Magick++/fuzz/build_imagemagick.sh
 
 cat <<EOT >> /ImageMagick/etc/ImageMagick-7/policy.xml
 <policymap>
