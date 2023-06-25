9db502e6a70ca2534dfc78c09f156aa55f88dcfb
Dirk Lemstra <dirk@lemstra.org>
2023-06-25 16:21:46 +0200

    Patches for upcoming breaking changes in the jxl library.

--- coders/jxl.c
+++ coders/jxl.c
@@ -448,8 +448,13 @@ static Image *ReadJXLImage(const ImageInfo *image_info,ExceptionInfo *exception)
 
         memset(&color_encoding,0,sizeof(color_encoding));
         JXLSetFormat(image,&pixel_format,exception);
+#if JPEGXL_NUMERIC_VERSION >= JPEGXL_COMPUTE_NUMERIC_VERSION(0,9,0)
+        jxl_status=JxlDecoderGetColorAsEncodedProfile(jxl_info,
+          JXL_COLOR_PROFILE_TARGET_DATA,&color_encoding);
+#else
         jxl_status=JxlDecoderGetColorAsEncodedProfile(jxl_info,&pixel_format,
           JXL_COLOR_PROFILE_TARGET_DATA,&color_encoding);
+#endif
         if (jxl_status == JXL_DEC_SUCCESS)
           {
             if (color_encoding.transfer_function == JXL_TRANSFER_FUNCTION_LINEAR)
@@ -522,14 +527,25 @@ static Image *ReadJXLImage(const ImageInfo *image_info,ExceptionInfo *exception)
         else
           if (jxl_status != JXL_DEC_ERROR)
             break;
+#if JPEGXL_NUMERIC_VERSION >= JPEGXL_COMPUTE_NUMERIC_VERSION(0,9,0)
+        jxl_status=JxlDecoderGetICCProfileSize(jxl_info,
+          JXL_COLOR_PROFILE_TARGET_ORIGINAL,&profile_size);
+#else
         jxl_status=JxlDecoderGetICCProfileSize(jxl_info,&pixel_format,
           JXL_COLOR_PROFILE_TARGET_ORIGINAL,&profile_size);
+#endif
         if (jxl_status != JXL_DEC_SUCCESS)
           break;
         profile=AcquireStringInfo(profile_size);
+#if JPEGXL_NUMERIC_VERSION >= JPEGXL_COMPUTE_NUMERIC_VERSION(0,9,0)
+        jxl_status=JxlDecoderGetColorAsICCProfile(jxl_info,
+          JXL_COLOR_PROFILE_TARGET_ORIGINAL,GetStringInfoDatum(profile),
+          profile_size);
+#else
         jxl_status=JxlDecoderGetColorAsICCProfile(jxl_info,&pixel_format,
           JXL_COLOR_PROFILE_TARGET_ORIGINAL,GetStringInfoDatum(profile),
           profile_size);
+#endif
         if (jxl_status == JXL_DEC_SUCCESS)
           (void) SetImageProfile(image,"icm",profile,exception);
         profile=DestroyStringInfo(profile);
