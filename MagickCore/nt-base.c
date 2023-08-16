a844266dbfd274c75bcc8c66d7a06e51afb48184
Dirk Lemstra <dirk@lemstra.org>
2023-08-16 07:28:19 +0200

    Use the correct abs method instead.

--- MagickCore/nt-base.c
+++ MagickCore/nt-base.c
@@ -629,10 +629,8 @@ MagickPrivate double NTErf(double x)
   a4=-1.453152027;
   a5=1.061405429;
   p=0.3275911;
-  sign=1;
-  if (x < 0)
-    sign=-1;
-  x=abs(x);
+  sign=x < 0 ? -1 : 1;
+  x=fabs(x);
   t=1.0/(1.0+p*x);
   y=1.0-(((((a5*t+a4)*t)+a3)*t+a2)*t+a1)*t*exp(-x*x);
   return(sign*y);
