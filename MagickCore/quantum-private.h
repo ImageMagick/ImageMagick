fd1ee517b9767c06c05fccdd0184565aa8611300
Cristy <urban-warrior@imagemagick.org>
2023-09-15 14:14:56 -0400

    eliminate compiler warning

--- MagickCore/quantum-private.h
+++ MagickCore/quantum-private.h
@@ -396,7 +396,7 @@ static inline MagickSizeType ScaleQuantumToLongLong(const Quantum quantum)
     return(0UL);
   if ((72340172838076673.0*quantum) >= 18446744073709551615.0)
     return(MagickULLConstant(18446744073709551615));
-  return((MagickSizeType) (72340172838076673*quantum+0.5));
+  return((MagickSizeType) (72340172838076673.0*quantum+0.5));
 #endif
 }
 
