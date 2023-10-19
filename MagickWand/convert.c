3c559c82c910242c08697f8f515b20ab6fcd1e99
Cristy <urban-warrior@imagemagick.org>
2023-10-18 20:17:59 -0400

    convert -help should return a success status (https://github.com/ImageMagick/ImageMagick/issues/6797)

--- MagickWand/convert.c
+++ MagickWand/convert.c
@@ -550,6 +550,9 @@ WandExport MagickBooleanType ConvertImageCommand(ImageInfo *image_info,
   if (argc == 2)
     {
       option=argv[1];
+      if ((LocaleCompare("help",option+1) == 0) ||
+          (LocaleCompare("-help",option+1) == 0))
+        return(ConvertUsage());
       if ((LocaleCompare("version",option+1) == 0) ||
           (LocaleCompare("-version",option+1) == 0))
         {
