8eb046dbf6bad8d2752a7465d17637ce68245712
Cristy <urban-warrior@imagemagick.org>
2023-10-20 17:28:57 -0400

    off by one exception

--- coders/bmp.c
+++ coders/bmp.c
@@ -1112,7 +1112,7 @@ static Image *ReadBMPImage(const ImageInfo *image_info,ExceptionInfo *exception)
         (bmp_info.compression == BI_ALPHABITFIELDS))
       {
         pixel_info=AcquireVirtualMemory(image->rows,
-          MagickMax(bytes_per_line,image->columns+256UL)*sizeof(*pixels));
+          MagickMax(bytes_per_line,image->columns+512UL)*sizeof(*pixels));
         if (pixel_info == (MemoryInfo *) NULL)
           ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
         pixels=(unsigned char *) GetVirtualMemoryBlob(pixel_info);
