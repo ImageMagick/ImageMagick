4d18167db34ea2fd199acd99420d61192d708f06
Dirk Lemstra <dirk@lemstra.org>
2023-10-25 07:51:55 +0200

    Added missing null check (#6811).

--- MagickCore/cache.c
+++ MagickCore/cache.c
@@ -2574,9 +2574,14 @@ MagickExport const void *GetVirtualMetacontent(const Image *image)
   assert(image->cache != (Cache) NULL);
   cache_info=(CacheInfo *) image->cache;
   assert(cache_info->signature == MagickCoreSignature);
-  metacontent=cache_info->methods.get_virtual_metacontent_from_handler(image);
-  if (metacontent != (void *) NULL)
-    return(metacontent);
+  if (cache_info->methods.get_virtual_metacontent_from_handler !=
+       (GetVirtualMetacontentFromHandler) NULL)
+    {
+      metacontent=cache_info->methods.get_virtual_metacontent_from_handler(
+        image);
+      if (metacontent != (void *) NULL)
+        return(metacontent);
+    }
   assert(id < (int) cache_info->number_threads);
   metacontent=GetVirtualMetacontentFromNexus(cache_info,
     cache_info->nexus_info[id]);
