08a5dff205905159f1fad597d7884ac47c3b8ee5
Cristy <urban-warrior@imagemagick.org>
2023-11-11 15:19:39 -0500

    revert

--- MagickCore/statistic.c
+++ MagickCore/statistic.c
@@ -1513,6 +1513,8 @@ MagickExport ChannelMoments *GetImageMoments(const Image *image,
         PixelTrait traits = GetPixelChannelTraits(image,channel);
         if (traits == UndefinedPixelTrait)
           continue;
+        if ((traits & UpdatePixelTrait) == 0)
+          continue;
         M00[channel]+=QuantumScale*(double) p[i];
         M00[MaxPixelChannels]+=QuantumScale*(double) p[i];
         M10[channel]+=x*QuantumScale*(double) p[i];
@@ -1555,6 +1557,8 @@ MagickExport ChannelMoments *GetImageMoments(const Image *image,
         PixelChannel channel = GetPixelChannelChannel(image,i);
         PixelTrait traits = GetPixelChannelTraits(image,channel);
         if (traits == UndefinedPixelTrait)
+          continue;
+        if ((traits & UpdatePixelTrait) == 0)
           continue;
         M11[channel]+=(x-centroid[channel].x)*(y-centroid[channel].y)*
           QuantumScale*(double) p[i];
@@ -1890,7 +1894,9 @@ MagickExport MagickBooleanType GetImageRange(const Image *image,double *minima,
         PixelTrait traits = GetPixelChannelTraits(image,channel);
         if (traits == UndefinedPixelTrait)
           continue;
-	if (row_initialize != MagickFalse)
+        if ((traits & UpdatePixelTrait) == 0)
+          continue;
+				if (row_initialize != MagickFalse)
           {
             row_minima=(double) p[i];
             row_maxima=(double) p[i];
@@ -2100,6 +2106,8 @@ MagickExport ChannelStatistics *GetImageStatistics(const Image *image,
         PixelTrait traits = GetPixelChannelTraits(image,channel);
         if (traits == UndefinedPixelTrait)
           continue;
+        if ((traits & UpdatePixelTrait) == 0)
+          continue;
         if (channel_statistics[channel].depth != MAGICKCORE_QUANTUM_DEPTH)
           {
             depth=channel_statistics[channel].depth;
@@ -2233,6 +2241,8 @@ MagickExport ChannelStatistics *GetImageStatistics(const Image *image,
         PixelChannel channel = GetPixelChannelChannel(image,i);
         PixelTrait traits = GetPixelChannelTraits(image,channel);
         if (traits == UndefinedPixelTrait)
+          continue;
+        if ((traits & UpdatePixelTrait) == 0)
           continue;
         for (y=0; y < (ssize_t) image->rows; y++)
         {
