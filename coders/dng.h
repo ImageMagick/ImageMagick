1fe2ea058b8f33a50ce965cb56f3872a8ec504c6
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-12-24 10:47:20 +0100

    Add Canon Raw v2 (CR2) as mime type
    And represent the magic signature in hexadecimal.

--- coders/dng.h
+++ coders/dng.h
@@ -17,8 +17,8 @@
 #include "coders/coders-private.h"
 
 #define MagickDNGHeaders \
-  MagickCoderHeader("CR2", 0, "\115\115\000\052\000\020\000\000\122\103\002") \
-  MagickCoderHeader("CR2", 0, "\111\111\052\000\020\000\000\000\103\122\002") \
+  MagickCoderHeader("CR2", 0, "MM\x00\x2a\x00\x10\x00\x00RC\x02") \
+  MagickCoderHeader("CR2", 0, "II\x2a\x00\x10\x00\x00\x00CR\x02") \
   MagickCoderHeader("CR3", 4, "ftypcrx ") \
   MagickCoderHeader("CRW", 0, "II\x1a\x00\x00\x00HEAPCCDR") \
   MagickCoderHeader("ORF", 0, "IIRO\x08\x00\x00\x00") \
