a29b58cb7b9870cb1912ba10921871d11b14903b
Dirk Lemstra <dirk@lemstra.org>
2023-12-26 11:59:06 +0100

    Also include -ldeflate in the LIBS for the oss-fuzz build.

--- Magick++/fuzz/build_imagemagick.sh
+++ Magick++/fuzz/build_imagemagick.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
 autoreconf -fiv
-./configure --prefix="$WORK" --disable-shared --disable-docs --with-jxl CFLAGS="$CFLAGS -I$WORK/include" LIBS="-L$WORK/lib -lbrotlidec -lbrotlienc -lbrotlicommon -lde265" PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
+./configure --prefix="$WORK" --disable-shared --disable-docs --with-jxl CFLAGS="$CFLAGS -I$WORK/include" LIBS="-L$WORK/lib -lbrotlidec -lbrotlienc -lbrotlicommon -lde265 -ldeflate" PKG_CONFIG_PATH="$WORK/lib/pkgconfig"
 make "-j$(nproc)"
 make install
