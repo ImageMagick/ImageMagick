188888ef080dd5d2cd50d1d9cd40b2688bfee9b4
Dirk Lemstra <dirk@lemstra.org>
2024-05-17 14:34:21 +0200

    Still set the ExplicitFormatType flag when build without libraw.

--- coders/dng.c
+++ coders/dng.c
@@ -664,6 +664,9 @@ static inline void RegisterDNGMagickInfo(const char *name,
   entry->decoder=(DecodeImageHandler *) ReadDNGImage;
   entry->flags|=CoderDecoderSeekableStreamFlag;
   entry->flags^=CoderBlobSupportFlag;
+#if !defined(MAGICKCORE_RAW_R_DELEGATE)
+  entry->format_type=ExplicitFormatType;
+#endif
   if (*version != '\0')
     entry->version=ConstantString(version);
   (void) RegisterMagickInfo(entry);
