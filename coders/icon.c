bc58e20dfcb204da0bd540415c32ee9818146e5d
Dirk Lemstra <dirk@lemstra.org>
2024-05-24 11:38:53 +0200

    Removed reserved and resource_type from IconFile.

--- coders/icon.c
+++ coders/icon.c
@@ -95,8 +95,6 @@ typedef struct _IconEntry
 typedef struct _IconFile
 {
   short
-    reserved,
-    resource_type,
     count;
 
   IconEntry
@@ -235,6 +233,10 @@ static Image *ReadICONImage(const ImageInfo *image_info,
   Quantum
     *q;
 
+  short
+    reserved,
+    resource_type;
+
   size_t
     bit,
     byte,
@@ -268,14 +270,12 @@ static Image *ReadICONImage(const ImageInfo *image_info,
       return((Image *) NULL);
     }
   (void) memset(&icon_file,0,sizeof(icon_file));
-  icon_file.reserved=(short) ReadBlobLSBShort(image);
-  if ((icon_file.reserved == 0x0001) || (icon_file.reserved == 0x0101) ||
-      (icon_file.reserved == 0x0201))
+  reserved=(short) ReadBlobLSBShort(image);
+  if ((reserved == 0x0001) || (reserved == 0x0101) || (reserved == 0x0201))
     return(Read1XImage(image,exception));
-  icon_file.resource_type=(short) ReadBlobLSBShort(image);
+  resource_type=(short) ReadBlobLSBShort(image);
   icon_file.count=(short) ReadBlobLSBShort(image);
-  if ((icon_file.reserved != 0) ||
-      ((icon_file.resource_type != 1) && (icon_file.resource_type != 2)) ||
+  if ((reserved != 0) || ((resource_type != 1) && (resource_type != 2)) ||
       (icon_file.count > MaxIcons))
     ThrowReaderException(CorruptImageError,"ImproperImageHeader");
   extent=0;
