de9705fc1ab017ec214dec3c25ef455a825af795
wdongw <39701382+wdongw@users.noreply.github.com>
2024-09-19 12:42:09 +0800

    Add files via upload

---
+++ homebrew-imagemagick.rb
@@ -0,0 +1,104 @@
+class Imagemagick < Formula
+  desc "Home Brewed version of ImageMagick with display delegates"
+  homepage "https://imagemagick.org/index.php"
+  url "https://github.com/wdongw/homebrew-ImageMagick/releases/download/untagged-f844c72b6fda76d89950/imagemagick_7.11-38.zip"
+  sha256 "9e18354d5f1baf94e94d70a14fe8ef702b4ce505916c82acaff2a478f20d3790"
+  license "ImageMagick"
+  revision 1
+  head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
+
+
+  bottle do
+    sha256 arm64_sonoma:  "9e18354d5f1baf94e94d70a14fe8ef702b4ce505916c82acaff2a478f20d3790"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "ghostscript"
+  depends_on "jpeg-turbo"
+  depends_on "jpeg-xl"
+  depends_on "libheif"
+  depends_on "liblqr"
+  depends_on "libpng"
+  depends_on "libraw"
+  depends_on "libtiff"
+  depends_on "libtool"
+  depends_on "little-cms2"
+  depends_on "openexr"
+  depends_on "openjpeg"
+  depends_on "webp"
+  depends_on "xz"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "gettext"
+    depends_on "glib"
+    depends_on "imath"
+    depends_on "libomp"
+  end
+
+  on_linux do
+    depends_on "libx11"
+  end
+
+  skip_clean :la
+
+  def install
+    # Avoid references to shim
+    inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
+    # versioned stuff in main tree is pointless for us
+    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_BASE_VERSION}", "${PACKAGE_NAME}"
+
+    args = [
+      "--enable-delegate-build", 
+      "--enable-shared", 
+      "--disable-static", 
+      "--with-freetype=yes",
+      "--with-modules", 
+      "--with-quantum-depth=16", 
+      "--enable-hdri"
+      "--with-openjp2",
+      "--with-openexr",
+      "--with-webp=yes",
+      "--with-heic=yes",
+      "--with-raw=yes",
+      "--with-gslib", -
+      "--with-rsvg", -
+      "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts",
+      "--with-x",
+    ]
+    if OS.mac?
+      args += [
+        # "--without-x",
+        # Work around "checking for clang option to support OpenMP... unsupported"
+        "ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp",
+        "ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp",
+        "LDFLAGS=-lomp -lz",
+      ]
+    end
+
+    system "./configure", *std_configure_args, *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+
+    # Check support for recommended features and delegates.
+    features = shell_output("#{bin}/magick -version")
+    %w[Modules freetype heic jpeg png raw tiff].each do |feature|
+      assert_match feature, features
+    end
+
+    # Check support for a few specific image formats, mostly to ensure LibRaw linked correctly.
+    formats = shell_output("#{bin}/magick -list format")
+    ["AVIF  HEIC      rw+", "ARW  DNG       r--", "DNG  DNG       r--"].each do |format|
+      assert_match format, formats
+    end
+    assert_match "Helvetica", shell_output("#{bin}/magick -list font")
+  end
+end
