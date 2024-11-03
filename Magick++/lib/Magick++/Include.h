a62103338859b600f4b53c7415a019b22b004659
Dirk Lemstra <dirk@lemstra.org>
2024-11-03 09:36:10 +0100

    Removed statement to fix macos build error.

--- Magick++/lib/Magick++/Include.h
+++ Magick++/lib/Magick++/Include.h
@@ -32,8 +32,6 @@
 #include <errno.h>
 #include <stdint.h>
 
-void* operator new(size_t, void*) throw();
-
 //
 // Include ImageMagick headers into namespace "MagickCore". If
 // MAGICKCORE_IMPLEMENTATION is defined, include ImageMagick development
