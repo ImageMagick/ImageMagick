b20a90cc3362b853c903e6448500e3b7650fd17e
Dirk Lemstra <dirk@lemstra.org>
2024-11-11 19:27:39 +0100

    Fixed calculation of the padding in the tiff decoder.

--- coders/tiff.c
+++ coders/tiff.c
@@ -1793,54 +1793,57 @@ static Image *ReadTIFFImage(const ImageInfo *image_info,
       quantum_type=IndexQuantum;
     if (interlace != PLANARCONFIG_SEPARATE)
       {
-        size_t
+        ssize_t
           pad;
 
-        pad=(size_t) MagickMax((ssize_t) samples_per_pixel-1,0);
-        if (image->alpha_trait != UndefinedPixelTrait)
-          {
-            if (image->storage_class == PseudoClass)
-              quantum_type=IndexAlphaQuantum;
-            else
-              quantum_type=samples_per_pixel == 1 ? AlphaQuantum :
-                GrayAlphaQuantum;
-          }
+        pad=(ssize_t) samples_per_pixel-extra_samples;
         if (samples_per_pixel > 2)
           {
             if (image->colorspace == CMYKColorspace)
               {
+                pad-=4;
                 quantum_type=CMYKQuantum;
-                pad=(size_t) MagickMax((ssize_t) samples_per_pixel+
-                  extra_samples-4,0);
                 if (image->alpha_trait != UndefinedPixelTrait)
-                  {
-                    quantum_type=CMYKAQuantum;
-                    pad=(size_t) MagickMax((ssize_t) samples_per_pixel+
-                      extra_samples-5,0);
-                  }
-              }
-            else if (image->alpha_trait != UndefinedPixelTrait)
-              {
-                quantum_type=RGBAQuantum;
-                pad=(size_t) MagickMax((ssize_t) samples_per_pixel+
-                  extra_samples-4,0);
+                  quantum_type=CMYKAQuantum;
               }
             else
               {
+                pad-=3;
                 quantum_type=RGBQuantum;
-                pad=(size_t) MagickMax((ssize_t) samples_per_pixel+
-                  extra_samples-3,0);
+                if (image->alpha_trait != UndefinedPixelTrait)
+                  quantum_type=RGBAQuantum;
               }
-            status=SetQuantumPad(image,quantum_info,pad*
-              (size_t) ((bits_per_sample+7) >> 3));
+          }
+        else
+          {
+            pad--;
+            if (image->alpha_trait != UndefinedPixelTrait)
+              {
+                if (image->storage_class == PseudoClass)
+                  quantum_type=IndexAlphaQuantum;
+                else if (samples_per_pixel == 1)
+                  quantum_type=AlphaQuantum;
+                else
+                  {
+                    quantum_type=GrayAlphaQuantum;
+                    pad--;
+                  }
+              }
+          }
+        if (pad < 0)
+          ThrowTIFFException(CorruptImageError,"CorruptImage");
+        if (pad > 0)
+          {
+            status=SetQuantumPad(image,quantum_info,(size_t) pad*
+              ((bits_per_sample+7) >> 3));
             if (status == MagickFalse)
               ThrowTIFFException(ResourceLimitError,"MemoryAllocationFailed");
           }
-        if (image->number_meta_channels != 0)
-          {
-            quantum_type=MultispectralQuantum;
-            (void) SetQuantumPad(image,quantum_info,0);
-          }
+      }
+    if (image->number_meta_channels != 0)
+      {
+        quantum_type=MultispectralQuantum;
+        (void) SetQuantumPad(image,quantum_info,0);
       }
     if (exception->severity < ErrorException)
       switch (method)
