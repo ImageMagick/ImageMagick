659c3a9c0b7bf7cdce0ff2156900e55498cbd6b8
Dirk Lemstra <dirk@lemstra.org>
2024-12-26 10:27:04 +0100

    Fixed perl unit tests for the Q32 and Q64 builds.

--- PerlMagick/t/subroutines.pl
+++ PerlMagick/t/subroutines.pl
@@ -962,15 +962,20 @@ sub testGetAttribute {
 #       [, expected REF_16] );
 #
 sub testMontage {
-  my( $imageOptions, $montageOptions, $ref_8, $ref_16, $ref_16_hdri ) = @_;
+  my( $imageOptions, $montageOptions, $ref_8, $ref_16, $ref_16_hdri, $ref_32, $ref_64 ) = @_;
 
   my($image,$ref_expected,$ref_signature);
 
   $ref_expected = $ref_8;
-  if ( defined( $ref_16 ) || defined( $ref_16_hdri ) )
+  
+  if ( defined( $ref_16 ) || defined( $ref_16_hdri ) || defined( $ref_32 ) || defined( $ref_64 ) )
     {
       my $version = $montage->GetAttribute('version');
-      if ( $version =~ /Q16\-HDRI/ ) {
+      if ( $version =~ /Q64/ ) {
+        $ref_expected = $ref_64;
+      } elsif ( $version =~ /Q32/ ) {
+        $ref_expected = $ref_32;
+      } elsif ( $version =~ /Q16\-HDRI/ ) {
         $ref_expected = $ref_16_hdri;
       } elsif ( $version =~ /Q16/ ) {
         $ref_expected = $ref_16;
