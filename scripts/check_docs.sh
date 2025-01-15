46bc58bd6f593d88086be6f31d6fed7489455b6b
Anne-Marie <Anne-Marie.Kourieh@student.uclouvain.be>
2025-01-15 23:27:08 +0100

    uncomment doxygen local run

--- scripts/check_docs.sh
+++ scripts/check_docs.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-MAGICKCWWWPATH="www/Magick++"
-APIPATH="www/api/Magick++"
 
 cd ..
-# cd config && doxygen Magick++.dox &> /dev/null
+cd config && doxygen Magick++.dox &> /dev/null
 
-# these are all files with function signatures that we can compare with the doxygen generated files currently
+# documentation files with function signatures that we can compare with their corresponding doxygen file (currently)
 DOC_HTML=("Blob.html" "CoderInfo.html" "Geometry.html" "Image++.html" "Montage.html" "Pixels.html")
-DOXY_HTML=("Blob.html" "CoderInfo.html" "Geometry.html" "Image.html" "Montage.html" "Pixels.html")
+DOXY_HTML=("Blob.html" "CoderInfo.html" "Geometry.html" "Image.html" "Montage.html" "Pixels.html")  # Image.html is the only one that differs from the doxygen file
 
 
 for i in "${!DOC_HTML[@]}"; do
-  python3 scripts/compare_signatures.py "${MAGICKCWWWPATH}"/"${DOC_HTML[$i]}" "${APIPATH}"/classMagick_1_1"${DOXY_HTML[$i]}"
+  python3 scripts/compare_signatures.py www/Magick++/"${DOC_HTML[$i]}" www/api/Magick++/classMagick_1_1"${DOXY_HTML[$i]}"
   echo ""
 done
