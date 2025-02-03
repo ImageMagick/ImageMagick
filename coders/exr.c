19a0631da8259b744e8df9521e92d402b8f49b09
Dirk Lemstra <dirk@lemstra.org>
2025-02-03 17:39:05 +0100

    Don't call exr_decoding_destroy inside InitializeEXRChannels.

--- coders/exr.c
+++ coders/exr.c
@@ -206,11 +206,8 @@ static MagickBooleanType InitializeEXRChannels(Image *image,exr_context_t ctxt,
 
   *data=(uint8_t) NULL;
   if (decoder.channel_count >= (MaxPixelChannels-MetaPixelChannels))
-    {
-      exr_decoding_destroy(ctxt,&decoder);
       ThrowBinaryException(CorruptImageError,"MaximumChannelsExceeded",
         image->filename);
-    }
   channel=decoder.channels;
   prefix_length=0;
   prefix=strrchr(decoder.channels[0].channel_name,'.');
@@ -282,10 +279,7 @@ static MagickBooleanType InitializeEXRChannels(Image *image,exr_context_t ctxt,
   if ((status != MagickFalse) && (number_meta_channels > 0))
     status=SetPixelMetaChannels(image,number_meta_channels,exception);
   if (status == MagickFalse)
-    {
-      exr_decoding_destroy(ctxt,&decoder);
-      return(status);
-    }
+    return(status);
   *data=(uint8_t *) AcquireQuantumMemory(*pixel_size,pixel_count);
   if (*data == (uint8_t*)NULL)
     {
