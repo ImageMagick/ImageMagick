ddf1f5576b2e3e5028bcc3aad5a600a541aa0c14
Cristy <urban-warrior@imagemagick.org>
2025-03-25 20:11:14 -0400

    correct PSNR distortion

--- MagickCore/compare.c
+++ MagickCore/compare.c
@@ -1211,7 +1211,7 @@ static MagickBooleanType GetPeakSignalToNoiseRatio(const Image *image,
     i;
 
   status=GetMeanSquaredDistortion(image,reconstruct_image,distortion,exception);
-  for (i=0; i <= MaxPixelChannels; i++)
+  for (i=0; i < MaxPixelChannels; i++)
     if ((fabs(distortion[i]) < MagickEpsilon) || (fabs(distortion[i]) >= 1.0))
       distortion[i]=fabs(distortion[i]) < MagickEpsilon ? 0.0 : 1.0;
     else
