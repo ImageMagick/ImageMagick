554c38f8913062a2dd6796453480c98ee7929a70
Cristy <urban-warrior@imagemagick.org>
2025-07-07 21:50:55 -0400

    adjust workload factor

--- MagickCore/compare.c
+++ MagickCore/compare.c
@@ -4578,7 +4578,7 @@ MagickExport Image *SimilarityImage(const Image *image,const Image *reconstruct,
   similarity_view=AcquireAuthenticCacheView(similarity_image,exception);
 #if defined(MAGICKCORE_OPENMP_SUPPORT)
   #pragma omp parallel for schedule(static) shared(similarity_info,status) \
-    magick_number_threads(image,reconstruct,similarity_image->rows << 2,1)
+    magick_number_threads(image,reconstruct,similarity_image->rows,1)
 #endif
   for (y=0; y < (ssize_t) similarity_image->rows; y++)
   {
