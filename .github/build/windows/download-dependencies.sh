a4a62596bc93d6f168b7a5cd3d0e752ccfe1f475
Dirk Lemstra <dirk@lemstra.org>
2025-10-07 08:21:05 +0200

    Updated the Windows dependencies.

--- .github/build/windows/download-dependencies.sh
+++ .github/build/windows/download-dependencies.sh
@@ -52,4 +52,4 @@ if [[ -z "$dependenciesArtifact" ]]; then
   exit 1
 fi
 
-download_dependencies "2025.10.02.1742" "$dependenciesArtifact"
+download_dependencies "2025.10.06.2123" "$dependenciesArtifact"
