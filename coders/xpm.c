21525d8f27b86e8063fe359616086fd6b71eb05b
Dirk Lemstra <dirk@lemstra.org>
2026-02-08 14:15:46 +0100

    Fixed out of bound read with negative pixel index (GHSA-vpxv-r9pg-7gpr)

--- coders/xpm.c
+++ coders/xpm.c
@@ -1134,10 +1134,14 @@ static MagickBooleanType WriteXPMImage(const ImageInfo *image_info,Image *image,
     for (x=0; x < (ssize_t) image->columns; x++)
     {
       k=((ssize_t) GetPixelIndex(image,p) % MaxCixels);
+      if (k < 0)
+        k=0;
       symbol[0]=Cixel[k];
       for (j=1; j < (ssize_t) characters_per_pixel; j++)
       {
         k=(((int) GetPixelIndex(image,p)-k)/MaxCixels) % MaxCixels;
+        if (k < 0)
+          k=0;
         symbol[j]=Cixel[k];
       }
       symbol[j]='\0';
