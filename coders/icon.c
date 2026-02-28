b95fae68f2bf47b13ac5031bda6f94ca2a836a1c
STPR <43796508+STPR@users.noreply.github.com>
2026-02-28 17:38:43 +0100

    Minor corrections
    Minor corrections

--- coders/icon.c
+++ coders/icon.c
@@ -1385,21 +1385,13 @@ static MagickBooleanType WriteICONImage(const ImageInfo *image_info,
           Write 40-byte version 3+ bitmap header.
         */
         if (width < 256)
-          {
-            directory->icons[scene]->width=(unsigned char) width;
-          }
+          directory->icons[scene]->width=(unsigned char) width;
         else
-          {
-            directory->icons[scene]->width=(unsigned char) 0;
-          }
+          directory->icons[scene]->width=(unsigned char) 0;
         if (height < 256)
-          {
-            directory->icons[scene]->height=(unsigned char) height;
-          }
+          directory->icons[scene]->height=(unsigned char) height;
         else
-          {
-            directory->icons[scene]->height=(unsigned char) 0;
-          }
+          directory->icons[scene]->height=(unsigned char) 0;
         directory->icons[scene]->colors=(unsigned char) number_colors;
         directory->icons[scene]->reserved=0;
         directory->icons[scene]->planes=planes;
