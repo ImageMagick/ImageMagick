3ed104956fed43da2ed472d330d542de2f73d450
Dirk Lemstra <dirk@lemstra.org>
2026-03-15 15:18:13 +0100

    Apply correct patch for GHSA-cqw9-w2m7-r2m2.

--- MagickCore/effect.c
+++ MagickCore/effect.c
@@ -1039,7 +1039,7 @@ MagickExport Image *BilateralBlurImage(const Image *image,const size_t width,
           double
             intensity;
 
-          r=p+(ssize_t) (GetPixelChannels(image)*MagickMax(width,1)*
+          r=p-(ssize_t) (GetPixelChannels(image)*MagickMax(width,1)*
             (size_t) (mid.y-v)+GetPixelChannels(image)*(size_t) (mid.x-u));
           intensity=ScaleQuantumToChar((const Quantum) GetPixelIntensity(image,r))-
             (double) ScaleQuantumToChar((const Quantum) GetPixelIntensity(image,p));
@@ -1084,8 +1084,8 @@ MagickExport Image *BilateralBlurImage(const Image *image,const size_t width,
             {
               for (u=0; u < (ssize_t) MagickMax(width,1); u++)
               {
-                r=p+GetPixelChannels(image)*MagickMax(width,1)*(size_t)
-                  (mid.y-v)+GetPixelChannels(image)*(size_t) (mid.x-u);
+                 r=p-(ssize_t) (GetPixelChannels(image)*MagickMax(width,1)*(size_t)
+                  (mid.y-v)+GetPixelChannels(image)*(size_t) (mid.x-u));
                 pixel+=weights[id][n]*(double) r[i];
                 gamma+=weights[id][n];
                 n++;
@@ -1106,8 +1106,8 @@ MagickExport Image *BilateralBlurImage(const Image *image,const size_t width,
               alpha,
               beta;
 
-            r=p+GetPixelChannels(image)*MagickMax(width,1)*(size_t) (mid.y-v)+
-              GetPixelChannels(image)*(size_t) (mid.x-u);
+            r=p-(ssize_t) (GetPixelChannels(image)*MagickMax(width,1)*(size_t) (mid.y-v)+
+              GetPixelChannels(image)*(size_t) (mid.x-u));
             alpha=(double) (QuantumScale*(double) GetPixelAlpha(image,p));
             beta=(double) (QuantumScale*(double) GetPixelAlpha(image,r));
             pixel+=weights[id][n]*(double) r[i];
