cddcd139b81515184c5bd8e9bb6c0be89bbad454
Dirk Lemstra <dirk@lemstra.org>
2026-04-11 21:14:38 +0200

    Code style changes.

--- coders/pcx.c
+++ coders/pcx.c
@@ -941,11 +941,9 @@ static MagickBooleanType WritePCXImage(const ImageInfo *image_info,Image *image,
     pcx_info.version=5;
     pcx_info.encoding=image_info->compression == NoCompression ? 0 : 1;
     pcx_info.bits_per_pixel=8;
-    if (image->storage_class == PseudoClass)
-      {
-        if (SetImageMonochrome(image,exception) != MagickFalse || image->colors <= 16)
+    if ((image->storage_class == PseudoClass) &&
+        (SetImageMonochrome(image,exception) != MagickFalse || image->colors <= 16))
           pcx_info.bits_per_pixel=1;
-      }
     else
       if (IssRGBCompatibleColorspace(image->colorspace) == MagickFalse)
         (void) TransformImageColorspace(image,sRGBColorspace,exception);
@@ -972,8 +970,7 @@ static MagickBooleanType WritePCXImage(const ImageInfo *image_info,Image *image,
     }
     pcx_info.reserved=0;
     pcx_info.planes=1;
-    if ((image->storage_class == PseudoClass) &&
-        (image->colors <= 16) &&
+    if ((image->storage_class == PseudoClass) && (image->colors <= 16) &&
         (image->type != BilevelType))
       pcx_info.planes=4;
     else if ((image->storage_class == DirectClass) || (image->colors > 256))
