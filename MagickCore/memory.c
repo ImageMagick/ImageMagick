0f870154177109229cc4de1a484b335f38e68f95
Cristy <urban-warrior@imagemagick.org>
2026-04-19 11:12:29 -0400

    do not call ThrowFatalException()

--- MagickCore/memory.c
+++ MagickCore/memory.c
@@ -603,6 +603,9 @@ MagickExport void *AcquireMagickMemory(const size_t size)
 */
 MagickExport void *AcquireCriticalMemory(const size_t size)
 {
+  static const char fatal_message[] =
+    "ImageMagick: fatal error: unable to acquire critical memory\n";
+
   void
     *memory;
 
@@ -610,9 +613,11 @@ MagickExport void *AcquireCriticalMemory(const size_t size)
     Fail if memory request cannot be fulfilled.
   */
   memory=AcquireMagickMemory(size);
-  if (memory == (void *) NULL)
-    ThrowFatalException(ResourceLimitFatalError,"MemoryAllocationFailed");
-  return(memory);
+  if (memory != (void *) NULL)
+    return(memory);
+  (void) write(STDERR_FILENO,fatal_message,sizeof(fatal_message)-1);
+  MagickCoreTerminus();
+  _exit(EXIT_FAILURE);
 }
 
 /*
