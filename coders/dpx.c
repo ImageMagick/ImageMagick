e341fd91d5ff244d1fa326f316465837992e5624
Cristy <urban-warrior@imagemagick.org>
2026-05-23 17:12:37 -0400

    remove extraneous heap check

--- coders/dpx.c
+++ coders/dpx.c
@@ -1141,8 +1141,6 @@ static Image *ReadDPXImage(const ImageInfo *image_info,ExceptionInfo *exception)
       (void) CloseBlob(image);
       return(GetFirstImageInList(image));
     }
-  if (HeapOverflowSanityCheck(image->columns,4*image->depth/8) != MagickFalse)
-    ThrowReaderException(ResourceLimitError,"MemoryAllocationFailed");
   status=SetImageExtent(image,image->columns,image->rows,exception);
   if (status == MagickFalse)
     return(DestroyImageList(image));
