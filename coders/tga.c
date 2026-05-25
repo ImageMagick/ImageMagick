35cb17198b15ca64dc9f2f9e5ba92e120c01a745
dxbjavid <dxbjavid@gmail.com>
2026-05-25 21:28:12 +0530

    reject tga files with zero columns or rows (#8756)

--- coders/tga.c
+++ coders/tga.c
@@ -344,6 +344,8 @@ static Image *ReadTGAImage(const ImageInfo *image_info,ExceptionInfo *exception)
     image->orientation=TopLeftOrientation;
   if (image_info->ping != MagickFalse)
     {
+      if ((image->columns == 0) || (image->rows == 0))
+        ThrowReaderException(CorruptImageError,"ImproperImageHeader");
       (void) CloseBlob(image);
       return(image);
     }
