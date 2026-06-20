a202d286d319b93d9193ef70c4adbe78fabe7da8
Cristy <urban-warrior@imagemagick.org>
2026-06-20 07:41:10 -0400

    compute the composite pHash channel

--- MagickCore/statistic.c
+++ MagickCore/statistic.c
@@ -1801,7 +1801,7 @@ MagickExport ChannelPerceptualHash *GetImagePerceptualHash(const Image *image,
         perceptual_hash[channel].phash[i][j]=(-MagickSafeLog10(fabs(
           moments[channel].invariant[j])));
         perceptual_hash[CompositePixelChannel].phash[i][j]+=(
-          -MagickSafeLog10(fabs(moments[channel].invariant[j])))/
+          -MagickSafeLog10(fabs(moments[CompositePixelChannel].invariant[j])))/
           GetImageChannels(hash_image);;
       }
     hash_image=DestroyImage(hash_image);
