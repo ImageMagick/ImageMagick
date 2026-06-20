b7da5e25769f2786b8461ecaddf2a87b0acffc39
Javid Khan <dxbjavid@gmail.com>
2026-06-20 22:39:05 +0530

    bound 8bim resolution resource read in GetProfilesFromResourceBlock (#8798)

--- MagickCore/profile.c
+++ MagickCore/profile.c
@@ -1844,7 +1844,7 @@ static void GetProfilesFromResourceBlock(Image *image,
         /*
           Resolution.
         */
-        if (count < 10)
+        if (count < 16)
           break;
         p=ReadResourceLong(p,&resolution);
         image->resolution.x=((double) resolution)/65536.0;
