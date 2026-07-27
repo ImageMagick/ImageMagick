f35294ec0d07d8b3ad6b92df3fd7d298fe5391fa
Cristy <urban-warrior@imagemagick.org>
2026-07-27 07:55:39 -0400

    add WEBP compress case when writing

--- coders/tiff.c
+++ coders/tiff.c
@@ -3712,6 +3712,13 @@ static MagickBooleanType WriteTIFFImage(const ImageInfo *image_info,
         compress_tag=COMPRESSION_PACKBITS;
         break;
       }
+#if defined(COMPRESSION_WEBP)
+      case WebPCompression:
+      {
+        compress_tag=COMPRESSION_WEBP;
+        break;
+      }
+#endif
       case ZipCompression:
       {
         compress_tag=COMPRESSION_ADOBE_DEFLATE;
