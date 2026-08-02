0908f10abe40f76f64ed443bce184587e0826092
Cristy <urban-warrior@imagemagick.org>
2026-08-02 09:29:41 -0400

    revert

--- coders/svg.c
+++ coders/svg.c
@@ -3479,7 +3479,6 @@ ModuleExport size_t RegisterSVGImage(void)
 #endif
   entry->encoder=(EncodeImageHandler *) WriteSVGImage;
   entry->magick=(IsImageFormatHandler *) IsSVG;
-  entry->format_type=ImplicitFormatType;
   (void) RegisterMagickInfo(entry);
   return(MagickImageCoderSignature);
 }
