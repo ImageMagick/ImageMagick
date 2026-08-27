dcdbbf4278fab20a32a4ad80e64cb95c368ca8dd
Dirk Lemstra <dirk@lemstra.org>
2026-08-27 20:32:18 +0200

    More fixes for GHSA-vcjj-32hg-qpx5.

--- MagickCore/constitute.c
+++ MagickCore/constitute.c
@@ -785,7 +785,7 @@ MagickExport Image *ReadImage(const ImageInfo *image_info,
       */
       if (GetMagickDecoderThreadSupport(magick_info) == MagickFalse)
         LockSemaphoreInfo(magick_info->semaphore);
-      status=IsCoderAuthorized(magick_info->magick_module,read_info->magick,
+      status=IsCoderAuthorized(magick_info->magick_module,magick_info->name,
         ReadPolicyRights,exception);
       image=(Image *) NULL;
       if (status != MagickFalse)
@@ -1367,7 +1367,7 @@ MagickExport MagickBooleanType WriteImage(const ImageInfo *image_info,
       */
       if (GetMagickEncoderThreadSupport(magick_info) == MagickFalse)
         LockSemaphoreInfo(magick_info->semaphore);
-      status=IsCoderAuthorized(magick_info->magick_module,write_info->magick,
+      status=IsCoderAuthorized(magick_info->magick_module,magick_info->name,
         WritePolicyRights,exception);
       if (status != MagickFalse)
         status=encoder(write_info,image,exception);
