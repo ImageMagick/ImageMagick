d779ac52f92c3045ced59362b483bee25a3fc784
Cristy <urban-warrior@imagemagick.org>
2026-08-28 11:44:50 -0400

    https://github.com/ImageMagick/ImageMagick/security/advisories/GHSA-89wq-f8f6-2j2v

--- coders/pcd.c
+++ coders/pcd.c
@@ -872,8 +872,12 @@ static Image *ReadPCDImage(const ImageInfo *image_info,ExceptionInfo *exception)
   if (LocaleCompare(image_info->magick,"PCDS") == 0)
     (void) SetImageColorspace(image,sRGBColorspace,exception);
   if (image_info->scene != 0)
-    for (i=0; i < (ssize_t) image_info->scene; i++)
-      AppendImageToList(&image,CloneImage(image,0,0,MagickTrue,exception));
+    {
+      if (AcquireMagickResource(ListLengthResource,image_info->scene) == MagickFalse)
+        ThrowPCDException(ResourceLimitError,"ListLengthExceedsLimit");
+      for (i=0; i < (ssize_t) image_info->scene; i++)
+        AppendImageToList(&image,CloneImage(image,0,0,MagickTrue,exception));
+    }
   if (status == MagickFalse)
     return(image=DestroyImageList(image));
   return(GetFirstImageInList(image));
